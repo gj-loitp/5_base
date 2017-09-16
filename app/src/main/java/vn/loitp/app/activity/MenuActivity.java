50e0f231b0e3d81caafbdb9c5b92a15f236b9fd3
Loitp <loitp@pateco.vn>
2017-09-16 16:16:46 +0700

    update

---
+++ app/src/main/java/vn/loitp/app/activity/MenuActivity.java
@@ -0,0 +1,55 @@
+package vn.loitp.app.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import vn.loitp.app.activity.placeholderview.PlaceHolderViewMenuActivity;
+import vn.loitp.app.base.BaseActivity;
+import vn.loitp.app.utilities.LUIUtil;
+import vn.loitp.livestar.R;
+
+public class MenuActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        findViewById(R.id.bt_api).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, TestAPIActivity.class);
+                startActivity(intent);
+                LUIUtil.transActivityFadeIn(activity);
+            }
+        });
+        findViewById(R.id.bt_place_holder_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, PlaceHolderViewMenuActivity.class);
+                startActivity(intent);
+                LUIUtil.transActivityFadeIn(activity);
+            }
+        });
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_menu;
+    }
+}
