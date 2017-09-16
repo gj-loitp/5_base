126f55dfce37b207c597e8669988a76c5beabbcc
Loitp <loitp@pateco.vn>
2017-09-16 16:41:16 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/placeholderview/PlaceHolderViewMenuActivity.java
+++ app/src/main/java/vn/loitp/app/activity/placeholderview/PlaceHolderViewMenuActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import vn.loitp.app.activity.placeholderview.androidbeginnerimagegallery.AndroidBeginnerImageGalleryActivity;
 import vn.loitp.app.activity.placeholderview.androidnavigationdrawer.AndroidNavigationDrawerActivity;
 import vn.loitp.app.base.BaseActivity;
 import vn.loitp.app.utilities.LUIUtil;
@@ -19,6 +20,14 @@ public class PlaceHolderViewMenuActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(activity, AndroidNavigationDrawerActivity.class);
+                startActivity(intent);
+                LUIUtil.transActivityFadeIn(activity);
+            }
+        });
+        findViewById(R.id.bt_android_beginner_image_gallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, AndroidBeginnerImageGalleryActivity.class);
                 startActivity(intent);
                 LUIUtil.transActivityFadeIn(activity);
             }
---
+++ app/src/main/java/vn/loitp/app/activity/placeholderview/androidbeginnerimagegallery/GalleryItem.java
@@ -0,0 +1,37 @@
+package vn.loitp.app.activity.placeholderview.androidbeginnerimagegallery;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import com.mindorks.placeholderview.Animation;
+import com.mindorks.placeholderview.annotations.Animate;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+import vn.loitp.livestar.R;
+
+/**
+ * Created by www.muathu@gmail.com on 9/16/2017.
+ */
+
+@Animate(Animation.CARD_LEFT_IN_DESC)
+@NonReusable
+@Layout(R.layout.gallery_item)
+public class GalleryItem {
+
+    @View(R.id.imageView)
+    private ImageView imageView;
+
+    private Drawable mDrawable;
+
+    public GalleryItem(Drawable drawable) {
+        mDrawable = drawable;
+    }
+
+    @Resolve
+    private void onResolved() {
+        imageView.setImageDrawable(mDrawable);
+    }
+}
