02fdbe2952805c801f007dd15d9e68f068602b0e
Loitp <loitp@pateco.vn>
2017-10-08 16:56:24 +0700

    update

---
+++ app/src/main/java/vn/loitp/app/activity/demo/gallery/GalleryDemoSlideActivity.java
@@ -0,0 +1,35 @@
+package vn.loitp.app.activity.demo.gallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import vn.loitp.app.base.BaseActivity;
+import vn.loitp.livestar.R;
+
+public class GalleryDemoSlideActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_gallery_demo_slide;
+    }
+}
