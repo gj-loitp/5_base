bb3e19302c37d41be0279777d30c30a7ae17b3a5
Loitp <loitp@pateco.vn>
2017-10-20 15:18:09 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/bottomnavigationbar/bottombar/BottomBarActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/bottomnavigationbar/bottombar/BottomBarActivity.java
@@ -2,8 +2,10 @@ package vn.loitp.app.activity.customviews.bottomnavigationbar.bottombar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import vn.loitp.app.base.BaseActivity;
+import vn.loitp.app.utilities.LStoreUtil;
 import vn.loitp.livestar.R;
 
 public class BottomBarActivity extends BaseActivity {
@@ -11,6 +13,8 @@ public class BottomBarActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(LStoreUtil.readTxtFromRawFolder(activity, R.raw.loitp));
     }
 
     @Override
