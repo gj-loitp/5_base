0c550b8a13ace40243f6badb005f2dd3d59ec91f
Loitp <loitp@pateco.vn>
2017-10-21 15:09:58 +0700

    update

--- app/src/main/java/vn/loitp/app/common/Constants.java
+++ app/src/main/java/vn/loitp/app/common/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
     public final static boolean IS_DEBUG = true;
 
     public final static String URL_IMG = "https://c1.staticflickr.com/9/8438/28818520263_c7ea1b3e3f_b.jpg";
-    public final static String URL_IMG_LONG = "https://c1.staticflickr.com/9/8037/28864529852_3d5a8f86a3_o.jpg";
+    public final static String URL_IMG_LONG = "https://c2.staticflickr.com/6/5476/29412311793_8067369e64_b.jpg";
 }
