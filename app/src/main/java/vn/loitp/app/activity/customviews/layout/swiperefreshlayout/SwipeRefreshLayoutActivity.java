c6547e5bb6c5f873e425e3350b1e56909f765848
Loitp <loitp@pateco.vn>
2017-12-08 11:25:16 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/swiperefreshlayout/SwipeRefreshLayoutActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/swiperefreshlayout/SwipeRefreshLayoutActivity.java
@@ -2,16 +2,43 @@ package vn.loitp.app.activity.customviews.layout.swiperefreshlayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.TextView;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LStoreUtil;
+import vn.loitp.core.utilities.LUIUtil;
+import vn.loitp.utils.util.ToastUtils;
 
 public class SwipeRefreshLayoutActivity extends BaseActivity {
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                doTask();
+            }
+        });
+        LUIUtil.setColorForSwipeRefreshLayout(swipeRefreshLayout);
 
+        TextView tv = (TextView) findViewById(R.id.tv);
+        String poem = LStoreUtil.readTxtFromRawFolder(activity, R.raw.loitp);
+        tv.setText(poem);
+    }
+
+    private void doTask() {
+        LUIUtil.setDelay(5000, new LUIUtil.DelayCallback() {
+            @Override
+            public void doAfter(int mls) {
+                swipeRefreshLayout.setRefreshing(false);
+                ToastUtils.showShort("Finish");
+            }
+        });
     }
 
     @Override
