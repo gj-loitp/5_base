63eba27ca962ede168828098ca425b9ad17fb93c
Loitp <loitp@pateco.vn>
2017-12-08 15:21:18 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/swiperefreshlayout/withplaceholderview/SwipeRefreshLayoutPlaceHolderViewActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/swiperefreshlayout/withplaceholderview/SwipeRefreshLayoutPlaceHolderViewActivity.java
@@ -2,7 +2,10 @@ package vn.loitp.app.activity.customviews.layout.swiperefreshlayout.withplacehol
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -21,6 +24,7 @@ import vn.loitp.views.placeholderview.lib.placeholderview.PlaceHolderView;
 public class SwipeRefreshLayoutPlaceHolderViewActivity extends BaseActivity {
     private SwipeRefreshLayout swipeRefreshLayout;
     private PlaceHolderView mGalleryView;
+    private PlaceHolderView.OnScrollListener mOnScrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,7 @@ public class SwipeRefreshLayoutPlaceHolderViewActivity extends BaseActivity {
         LUIUtil.setColorForSwipeRefreshLayout(swipeRefreshLayout);
 
         mGalleryView = (PlaceHolderView) findViewById(R.id.galleryView);
+        setLoadMoreListener();
         //LUIUtil.setPullLikeIOSVertical(mGalleryView);
 
         setupGallery();
@@ -76,6 +81,7 @@ public class SwipeRefreshLayoutPlaceHolderViewActivity extends BaseActivity {
 
     private void refresh() {
         mGalleryView.removeAllViews();
+        mGalleryView.refresh();
         LUIUtil.setDelay(2000, new LUIUtil.DelayCallback() {
             @Override
             public void doAfter(int mls) {
@@ -86,13 +92,43 @@ public class SwipeRefreshLayoutPlaceHolderViewActivity extends BaseActivity {
     }
 
     private void loadMore() {
-        LLog.d(TAG, "loadMore");
+        LLog.d(TAG, ">>>>loadMore");
         LUIUtil.setDelay(2000, new LUIUtil.DelayCallback() {
             @Override
             public void doAfter(int mls) {
-                List<Image> imageList = Utils.loadImages(activity);
                 mGalleryView.addView(new ImageTypeSmallList(activity, imageList));
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
+    }
+
+    private void setLoadMoreListener() {
+        mOnScrollListener =
+                new PlaceHolderView.OnScrollListener() {
+                    @Override
+                    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                        super.onScrolled(recyclerView, dx, dy);
+                        PlaceHolderView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                        if (layoutManager instanceof LinearLayoutManager) {
+                            LinearLayoutManager linearLayoutManager = (LinearLayoutManager) layoutManager;
+                            int totalItemCount = linearLayoutManager.getItemCount();
+                            int lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition();
+                            if (totalItemCount > 0 && totalItemCount == lastVisibleItem + 1) {
+                                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        LLog.d(TAG, "setLoadMoreListener");
+                                        if (swipeRefreshLayout.isRefreshing()) {
+                                            return;
+                                        }
+                                        swipeRefreshLayout.setRefreshing(true);
+                                        loadMore();
+                                    }
+                                });
+                            }
+                        }
+                    }
+                };
+        mGalleryView.addOnScrollListener(mOnScrollListener);
     }
 }
