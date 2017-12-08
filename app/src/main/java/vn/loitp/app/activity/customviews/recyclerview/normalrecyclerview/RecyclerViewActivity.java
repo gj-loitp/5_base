71692f99619fda96b9aa1c56322f68e81d30e33f
Loitp <loitp@pateco.vn>
2017-12-08 13:30:40 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/normalrecyclerview/RecyclerViewActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/normalrecyclerview/RecyclerViewActivity.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LLog;
 import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.utils.util.ToastUtils;
 
