0fb7d632b6c9428ff3423fc2949a34fe6f28c005
Loitp <loitp@pateco.vn>
2017-12-08 13:51:27 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/swiperefreshlayout/withrecyclerview/SwipeRefreshLayoutRecyclerViewActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/swiperefreshlayout/withrecyclerview/SwipeRefreshLayoutRecyclerViewActivity.java
@@ -3,16 +3,25 @@ package vn.loitp.app.activity.customviews.layout.swiperefreshlayout.withrecycler
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import loitp.basemaster.R;
-import vn.loitp.app.activity.customviews.recyclerview.parallaxrecyclerview.ParallaxAdapter;
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie;
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.MoviesAdapter;
 import vn.loitp.core.base.BaseActivity;
 import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.utils.util.ToastUtils;
 
 public class SwipeRefreshLayoutRecyclerViewActivity extends BaseActivity {
     private SwipeRefreshLayout swipeRefreshLayout;
+    private List<Movie> movieList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private MoviesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +30,40 @@ public class SwipeRefreshLayoutRecyclerViewActivity extends BaseActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                doTask();
+                refresh();
             }
         });
         LUIUtil.setColorForSwipeRefreshLayout(swipeRefreshLayout);
 
-        RecyclerView recyclerView = findViewById(R.id.rv);
-        recyclerView.setAdapter(new ParallaxAdapter(this));
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
 
-        LUIUtil.setPullLikeIOSVertical(recyclerView);
+        mAdapter = new MoviesAdapter(movieList, new MoviesAdapter.Callback() {
+            @Override
+            public void onClick(Movie movie) {
+                ToastUtils.showShort("Click " + movie.getTitle());
+            }
+        });
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        //LUIUtil.setPullLikeIOSVertical(recyclerView);
+
+        prepareMovieData();
     }
 
-    private void doTask() {
-        LUIUtil.setDelay(5000, new LUIUtil.DelayCallback() {
+    private void refresh() {
+        LUIUtil.setDelay(3000, new LUIUtil.DelayCallback() {
             @Override
             public void doAfter(int mls) {
+                int index = movieList.size();
+                for (int i = index; i < index + 10; i++) {
+                    Movie movie = new Movie("Add new " + i, "Add new " + i, "Add new: " + i);
+                    movieList.add(movie);
+                }
+                mAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(index);
                 swipeRefreshLayout.setRefreshing(false);
                 ToastUtils.showShort("Finish");
             }
@@ -60,5 +88,13 @@ public class SwipeRefreshLayoutRecyclerViewActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_swipe_refresh_recycler_view_layout;
+    }
+
+    private void prepareMovieData() {
+        for (int i = 0; i < 50; i++) {
+            Movie movie = new Movie("Loitp " + i, "Action & Adventure " + i, "Year: " + i);
+            movieList.add(movie);
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
