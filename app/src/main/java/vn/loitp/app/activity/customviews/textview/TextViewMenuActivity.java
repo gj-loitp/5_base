f1f05e97cfe955da77dec2f9d3cf7bb1d7fff771
Loitp <loitp@pateco.vn>
2017-12-20 13:22:18 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/textview/TextViewMenuActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/textview/TextViewMenuActivity.java
@@ -9,6 +9,7 @@ import vn.loitp.app.activity.customviews.textview.colortextview.ColorTextViewAct
 import vn.loitp.app.activity.customviews.textview.countdown.CountDownActivity;
 import vn.loitp.app.activity.customviews.textview.scoretext.ScoreTextViewActivity;
 import vn.loitp.app.activity.customviews.textview.scrollnumber.ScrollNumberActivity;
+import vn.loitp.app.activity.customviews.textview.textdecorator.TextDecoratorActivity;
 import vn.loitp.app.activity.customviews.textview.translucentview.TranslucentViewActivity;
 import vn.loitp.app.activity.customviews.textview.typewritertextview.TypeWriterTextViewActivity;
 import vn.loitp.app.activity.customviews.textview.verticalmarqueetextview.VerticalMarqueeTextViewActivity;
@@ -82,6 +83,14 @@ public class TextViewMenuActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(activity, TypeWriterTextViewActivity.class);
+                startActivity(intent);
+                LUIUtil.transActivityFadeIn(activity);
+            }
+        });
+        findViewById(R.id.bt_text_decorator).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, TextDecoratorActivity.class);
                 startActivity(intent);
                 LUIUtil.transActivityFadeIn(activity);
             }
---
+++ app/src/main/java/vn/loitp/app/activity/customviews/textview/textdecorator/TextDecoratorActivity.java
@@ -0,0 +1,80 @@
+package vn.loitp.app.activity.customviews.textview.textdecorator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import loitp.basemaster.R;
+import vn.loitp.app.activity.customviews.textview.textdecorator.lib.OnTextClickListener;
+import vn.loitp.app.activity.customviews.textview.textdecorator.lib.TextDecorator;
+import vn.loitp.core.base.BaseActivity;
+import vn.loitp.views.textview.scrollnumber.lib.MultiScrollNumber;
+
+//https://github.com/nntuyen/text-decorator?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=4982
+public class TextDecoratorActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final TextView tvContent = (TextView) findViewById(R.id.tvContent);
+        final String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et porta ipsum. Praesent vitae libero a mi sodales accumsan. Donec tempor nulla turpis, vitae pellentesque ligula consectetur sed. Quisque commodo lorem eget ipsum pulvinar consequat. Nam erat risus, rhoncus quis ligula sed, tempor venenatis nulla. Duis quis placerat quam.";
+
+        TextDecorator
+                .decorate(tvContent, text)
+                .setTextColor(R.color.colorAccent, "Lorem", "amet")
+                .setBackgroundColor(R.color.colorPrimary, "dolor", "elit")
+                .strikethrough("Duis", "Praesent")
+                .underline("sodales", "quam")
+                .setSubscript("vitae")
+                .makeTextClickable(new OnTextClickListener() {
+                    @Override
+                    public void onClick(View view, String text) {
+                        Toast.makeText(activity, text, Toast.LENGTH_SHORT).show();
+                    }
+                }, false, "porta", "commodo", "tempor venenatis nulla")
+                .setTextColor(android.R.color.holo_green_light, "porta", "commodo", "tempor venenatis nulla")
+                .build();
+
+        //TextDecorator
+        //    .decorate(tvContent, text)
+        //    .setTextColor(R.color.colorAccent, 0, 5)
+        //    .setBackgroundColor(R.color.colorPrimary, 6, 11)
+        //    .strikethrough(12, 26)
+        //    .setTextStyle(Typeface.BOLD | Typeface.ITALIC, 27, 40)
+        //    .setTypeface("serif", 70, 77)
+        //    .setSuperscript(78, 86)
+        //    .setSubscript(87, 92)
+        //    .underline(120, 200)
+        //    .blur(3, BlurMaskFilter.Blur.NORMAL, 0, 2)
+        //    .makeTextClickable(new OnTextClickListener() {
+        //      @Override public void onClick(View view, String text) {
+        //        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+        //      }
+        //    }, 250, 270, false)
+        //    .setTextColor(android.R.color.holo_green_light, 250, 270)
+        //    .build();
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_text_decorator;
+    }
+
+}
