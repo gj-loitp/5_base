7192525ba0abfcae97bc7c595f671873faf9724b
Loitp <loitp@pateco.vn>
2017-12-29 13:47:16 +0700

    update

--- xwallpaper/src/main/java/vn/loitp/app/activity/customviews/placeholderview/_lib/placeholderview/listeners/ItemRemovedListener.java
+++
@@ -1,9 +0,0 @@
-package vn.loitp.app.activity.customviews.placeholderview._lib.placeholderview.listeners;
-
-/**
- * Created by janisharali on 18/01/17.
- */
-
-public interface ItemRemovedListener {
-    void onItemRemoved(int count);
-}
