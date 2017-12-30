c475ce3810bdf7a0d9a45b586aa07180a64f817e
Loitp <loitp@pateco.vn>
2017-12-30 14:10:40 +0700

    update

--- core/src/main/java/vn/loitp/core/utilities/LImageUtil.java
+++ core/src/main/java/vn/loitp/core/utilities/LImageUtil.java
@@ -7,6 +7,9 @@ import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.facebook.common.executors.UiThreadImmediateExecutorService;
 import com.facebook.common.references.CloseableReference;
 import com.facebook.common.util.UriUtil;
@@ -23,6 +26,7 @@ import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
 import loitp.core.R;
+import vn.loitp.views.progressloadingview.avloadingindicatorview.lib.avi.AVLoadingIndicatorView;
 
 /**
  * Created by www.muathu@gmail.com on 10/7/2017.
@@ -32,6 +36,28 @@ public class LImageUtil {
     //for flide
     public static void load(Activity activity, String url, ImageView imageView) {
         Glide.with(activity).load(url).into(imageView);
+    }
+
+    public static void load(Activity activity, String url, ImageView imageView, final AVLoadingIndicatorView avLoadingIndicatorView) {
+        Glide.with(activity).load(url)
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        if (avLoadingIndicatorView != null) {
+                            avLoadingIndicatorView.smoothToHide();
+                        }
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        if (avLoadingIndicatorView != null) {
+                            avLoadingIndicatorView.smoothToHide();
+                        }
+                        return false;
+                    }
+                })
+                .into(imageView);
     }
 
     public static void load(Activity activity, String url, ImageView imageView, int sizeW, int sizeH) {
--- zbestwallpapers/src/main/java/vn/loitp/app/activity/slide/GallerySlideActivity.java
+++ zbestwallpapers/src/main/java/vn/loitp/app/activity/slide/GallerySlideActivity.java
@@ -18,6 +18,7 @@ import vn.loitp.core.utilities.LImageUtil;
 import vn.loitp.core.utilities.LLog;
 import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.restapi.flickr.model.photosetgetphotos.Photo;
+import vn.loitp.views.progressloadingview.avloadingindicatorview.lib.avi.AVLoadingIndicatorView;
 import vn.loitp.views.viewpager.parrallaxviewpager.lib.parrallaxviewpager.Mode;
 import vn.loitp.views.viewpager.parrallaxviewpager.lib.parrallaxviewpager.ParallaxViewPager;
 
@@ -69,8 +70,10 @@ public class GallerySlideActivity extends BaseActivity {
             RelativeLayout rootView = (RelativeLayout) layout.findViewById(R.id.root_view);
             rootView.setBackgroundColor(AppUtil.getColor(activity));
 
+            AVLoadingIndicatorView avLoadingIndicatorView = (AVLoadingIndicatorView) layout.findViewById(R.id.avi);
+
             ImageView imageView = (ImageView) layout.findViewById(R.id.imageView);
-            LImageUtil.load(activity, photo.getUrlO(), imageView);
+            LImageUtil.load(activity, photo.getUrlO(), imageView, avLoadingIndicatorView);
 
             //TextView tv = (TextView) layout.findViewById(R.id.tv);
             //LUIUtil.printBeautyJson(photo, tv);
