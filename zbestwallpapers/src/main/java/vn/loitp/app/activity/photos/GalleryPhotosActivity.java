1af7833cee6274c98f89c14f6a073cea43cf8e16
Loitp <loitp@pateco.vn>
2017-12-31 13:47:46 +0700

    update

--- zbestwallpapers/src/main/java/vn/loitp/app/activity/photos/GalleryPhotosActivity.java
+++ zbestwallpapers/src/main/java/vn/loitp/app/activity/photos/GalleryPhotosActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
-import java.text.ParseException;
 import java.util.List;
 
 import loitp.basemaster.R;
