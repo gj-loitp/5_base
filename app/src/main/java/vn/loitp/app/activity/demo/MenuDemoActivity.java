19be13f2e11cfe9d8a6135f2961fc8bb69d468a2
Loitp <loitp@pateco.vn>
2018-01-02 10:28:21 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/demo/MenuDemoActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/MenuDemoActivity.java
@@ -7,40 +7,21 @@ import android.view.View;
 
 import vn.loitp.app.activity.demo.alarmdemoapp.activity.AlarmMeActivity;
 import vn.loitp.app.activity.demo.gallery.GalleryDemoSplashActivity;
+import vn.loitp.app.activity.demo.sound.SoundActivity;
 import vn.loitp.app.activity.demo.video.VideoActivity;
 import vn.loitp.core.base.BaseActivity;
 import loitp.basemaster.R;
 import vn.loitp.core.utilities.LUIUtil;
 
-public class MenuDemoActivity extends BaseActivity {
+public class MenuDemoActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.bt_alarm).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, AlarmMeActivity.class);
-                startActivity(intent);
-                LUIUtil.transActivityFadeIn(activity);
-            }
-        });
-        findViewById(R.id.bt_gallery).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, GalleryDemoSplashActivity.class);
-                startActivity(intent);
-                LUIUtil.transActivityFadeIn(activity);
-            }
-        });
-        findViewById(R.id.bt_video).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, VideoActivity.class);
-                startActivity(intent);
-                LUIUtil.transActivityFadeIn(activity);
-            }
-        });
+        findViewById(R.id.bt_alarm).setOnClickListener(this);
+        findViewById(R.id.bt_gallery).setOnClickListener(this);
+        findViewById(R.id.bt_video).setOnClickListener(this);
+        findViewById(R.id.bt_sound).setOnClickListener(this);
 
     }
 
@@ -62,5 +43,28 @@ public class MenuDemoActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_menu_demo;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.bt_alarm:
+                intent = new Intent(activity, AlarmMeActivity.class);
+                break;
+            case R.id.bt_gallery:
+                intent = new Intent(activity, GalleryDemoSplashActivity.class);
+                break;
+            case R.id.bt_video:
+                intent = new Intent(activity, VideoActivity.class);
+                break;
+            case R.id.bt_sound:
+                intent = new Intent(activity, SoundActivity.class);
+                break;
+        }
+        if (intent != null) {
+            startActivity(intent);
+            LUIUtil.transActivityFadeIn(activity);
+        }
     }
 }
---
+++ app/src/main/java/vn/loitp/app/activity/demo/sound/SoundActivity.java
@@ -0,0 +1,77 @@
+package vn.loitp.app.activity.demo.sound;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Surface;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.decoder.DecoderCounters;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
+
+import loitp.basemaster.R;
+import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LLog;
+import vn.loitp.core.utilities.LSoundUtil;
+
+public class SoundActivity extends BaseActivity implements OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        findViewById(R.id.bt_play).setOnClickListener(this);
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_sound;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_play:
+                LSoundUtil.startMusicFromAsset(activity, "ting.ogg");
+                break;
+        }
+    }
+}
