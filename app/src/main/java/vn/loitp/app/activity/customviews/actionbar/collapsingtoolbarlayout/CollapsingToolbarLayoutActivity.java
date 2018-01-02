69fd6017899f8f5e410e301adf009a4c5162c443
Loitp <loitp@pateco.vn>
2018-01-02 17:09:13 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/actionbar/collapsingtoolbarlayout/CollapsingToolbarLayoutActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/actionbar/collapsingtoolbarlayout/CollapsingToolbarLayoutActivity.java
@@ -1,20 +1,21 @@
 package vn.loitp.app.activity.customviews.actionbar.collapsingtoolbarlayout;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LPopupMenu;
 import vn.loitp.views.LToast;
 
-public class CollapsingToolbarLayoutActivity extends BaseActivity {
+public class CollapsingToolbarLayoutActivity extends BaseActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,45 +24,28 @@ public class CollapsingToolbarLayoutActivity extends BaseActivity {
         setSupportActionBar(toolbar);
 
         toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
 
-        toolbar.inflateMenu(R.menu.menu_popup);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                LToast.show(activity, item.getTitle().toString());
-                return true;
-            }
-        });
-
-        /*toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        /*CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         collapsingToolbar.setTitle("Profile");
         collapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
         collapsingToolbar.setExpandedTitleColor(Color.WHITE);*/
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        findViewById(R.id.bt_menu).setOnClickListener(this);
     }
 
     @Override
@@ -82,5 +66,19 @@ public class CollapsingToolbarLayoutActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_scrolling;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_menu:
+                LPopupMenu.show(activity, v, R.menu.menu_popup, new LPopupMenu.CallBack() {
+                    @Override
+                    public void clickOnItem(MenuItem menuItem) {
+                        LToast.show(activity, menuItem.getTitle().toString());
+                    }
+                });
+                break;
+        }
     }
 }
