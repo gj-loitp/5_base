6b5ceaa5ba49893dbdf488543891b8fd263c817e
Loitp <loitp@pateco.vn>
2018-01-02 16:31:03 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/actionbar/collapsingtoolbarlayout/CollapsingToolbarLayoutActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/actionbar/collapsingtoolbarlayout/CollapsingToolbarLayoutActivity.java
@@ -1,14 +1,18 @@
 package vn.loitp.app.activity.customviews.actionbar.collapsingtoolbarlayout;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.views.LToast;
 
 public class CollapsingToolbarLayoutActivity extends BaseActivity {
 
@@ -17,6 +21,38 @@ public class CollapsingToolbarLayoutActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu_popup);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                LToast.show(activity, item.getTitle().toString());
+                return true;
+            }
+        });
+
+        /*toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbar.setTitle("Profile");
+        collapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
+        collapsingToolbar.setExpandedTitleColor(Color.WHITE);*/
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
