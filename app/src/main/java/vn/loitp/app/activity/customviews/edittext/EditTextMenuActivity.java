81c1dadded36af0ece38140a878658f54eb3aacd
Loitp <loitp@pateco.vn>
2018-01-04 14:18:25 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/edittext/EditTextMenuActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/edittext/EditTextMenuActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import vn.loitp.app.activity.customviews.edittext.autoresizeedittext.AutoResizeEditTextActivity;
 import vn.loitp.app.activity.customviews.edittext.biuedittext.BiuEditTextActivity;
 import vn.loitp.app.activity.customviews.edittext.materialtextfield.MaterialTextFieldActivity;
 import vn.loitp.core.base.BaseActivity;
@@ -12,27 +13,14 @@ import loitp.basemaster.R;
 import vn.loitp.core.utilities.LActivityUtil;
 import vn.loitp.core.utilities.LUIUtil;
 
-public class EditTextMenuActivity extends BaseActivity {
+public class EditTextMenuActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.bt_material_text_field).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, MaterialTextFieldActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_biu_edit_text).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, BiuEditTextActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
+        findViewById(R.id.bt_auto_resize_edit_text).setOnClickListener(this);
+        findViewById(R.id.bt_material_text_field).setOnClickListener(this);
+        findViewById(R.id.bt_biu_edit_text).setOnClickListener(this);
     }
 
     @Override
@@ -53,5 +41,25 @@ public class EditTextMenuActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_menu_edittext;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.bt_auto_resize_edit_text:
+                intent = new Intent(activity, AutoResizeEditTextActivity.class);
+                break;
+            case R.id.bt_material_text_field:
+                intent = new Intent(activity, MaterialTextFieldActivity.class);
+                break;
+            case R.id.bt_biu_edit_text:
+                intent = new Intent(activity, BiuEditTextActivity.class);
+                break;
+        }
+        if (intent != null) {
+            startActivity(intent);
+            LActivityUtil.tranIn(activity);
+        }
     }
 }
---
+++ app/src/main/java/vn/loitp/app/activity/customviews/edittext/autoresizeedittext/AutoResizeEditTextActivity.java
@@ -0,0 +1,37 @@
+package vn.loitp.app.activity.customviews.edittext.autoresizeedittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import loitp.basemaster.R;
+import vn.loitp.core.base.BaseActivity;
+
+//guide https://github.com/xujinyang/BiuEditText
+public class AutoResizeEditTextActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_autoresize_editext;
+    }
+
+}
