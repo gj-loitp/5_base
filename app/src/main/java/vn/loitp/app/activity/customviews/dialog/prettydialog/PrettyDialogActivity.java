3b397759e7016fcc10d84888906c310db7569b74
Loitp <loitp@pateco.vn>
2018-01-04 13:22:33 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/dialog/prettydialog/PrettyDialogActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/dialog/prettydialog/PrettyDialogActivity.java
@@ -1,6 +1,7 @@
 package vn.loitp.app.activity.customviews.dialog.prettydialog;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,6 +22,7 @@ public class PrettyDialogActivity extends BaseActivity implements OnClickListene
         findViewById(R.id.bt_show_1).setOnClickListener(this);
         findViewById(R.id.bt_show_2).setOnClickListener(this);
         findViewById(R.id.bt_show_3).setOnClickListener(this);
+        findViewById(R.id.bt_show_4).setOnClickListener(this);
     }
 
     @Override
@@ -55,6 +57,9 @@ public class PrettyDialogActivity extends BaseActivity implements OnClickListene
             case R.id.bt_show_3:
                 show3();
                 break;
+            case R.id.bt_show_4:
+                show4();
+                break;
         }
     }
 
@@ -85,6 +90,65 @@ public class PrettyDialogActivity extends BaseActivity implements OnClickListene
         prettyDialog = new PrettyDialog(activity);
         prettyDialog.setTitle("PrettyDialog Title")
                 .setMessage("PrettyDialog Message")
+                .setIcon(R.drawable.pdlg_icon_info)
+                .setIconTint(R.color.pdlg_color_green)
+                .setIconCallback(new PrettyDialogCallback() {
+                    @Override
+                    public void onClick() {
+                        LToast.show(activity, "onClick setIconCallback");
+                    }
+                })
+                // OK button
+                .addButton(
+                        "OK",                    // button text
+                        R.color.pdlg_color_white,        // button text color
+                        R.color.pdlg_color_green,        // button background color
+                        new PrettyDialogCallback() {        // button OnClick listener
+                            @Override
+                            public void onClick() {
+                                LToast.show(activity, "onClick OK");
+                                prettyDialog.cancel();
+                            }
+                        }
+                )
+                //Cancel button
+                .addButton(
+                        "Cancel",
+                        R.color.pdlg_color_white,
+                        R.color.pdlg_color_red,
+                        new PrettyDialogCallback() {
+                            @Override
+                            public void onClick() {
+                                LToast.show(activity, "onClick Cancel");
+                                prettyDialog.cancel();
+                            }
+                        }
+                )
+                // 3rd button
+                .addButton(
+                        "Option 3",
+                        R.color.pdlg_color_black,
+                        R.color.pdlg_color_gray,
+                        new PrettyDialogCallback() {
+                            @Override
+                            public void onClick() {
+                                LToast.show(activity, "onClick Option 3");
+                                prettyDialog.cancel();
+                            }
+                        }
+                )
+                .show();
+    }
+
+    private void show4() {
+        prettyDialog = new PrettyDialog(activity);
+        prettyDialog
+                .setTitle("PrettyDialog Title")
+                .setTitleColor(R.color.pdlg_color_blue)
+                .setMessage("By agreeing to our terms and conditions, you agree to our terms and conditions.")
+                .setMessageColor(R.color.pdlg_color_gray)
+                .setTypeface(Typeface.createFromAsset(getResources().getAssets(), "fonts/OpenSans-Bold.ttf"))
+                .setAnimationEnabled(true)
                 .setIcon(R.drawable.pdlg_icon_info)
                 .setIconTint(R.color.pdlg_color_green)
                 .setIconCallback(new PrettyDialogCallback() {
