c1be20fbedd74caec3a3588506d8da5b6c1161f8
Loitp <loitp@pateco.vn>
2018-01-16 16:50:01 +0700

    update

--- zbestcomic/src/main/java/vn/loitp/app/activity/home/allmanga/FrmAllManga.java
+++ zbestcomic/src/main/java/vn/loitp/app/activity/home/allmanga/FrmAllManga.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.List;
 
@@ -17,9 +16,10 @@ import vn.loitp.app.helper.comiclist.GetComicTask;
 import vn.loitp.app.model.comic.Comic;
 import vn.loitp.app.model.comictype.ComicType;
 import vn.loitp.core.base.BaseFragment;
+import vn.loitp.core.utilities.LDialogUtil;
 import vn.loitp.core.utilities.LLog;
-import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.utils.util.ToastUtils;
+import vn.loitp.views.placeholderview.lib.placeholderview.PlaceHolderView;
 import vn.loitp.views.progressloadingview.avloadingindicatorview.lib.avi.AVLoadingIndicatorView;
 
 /**
@@ -28,12 +28,11 @@ import vn.loitp.views.progressloadingview.avloadingindicatorview.lib.avi.AVLoadi
 
 public class FrmAllManga extends BaseFragment {
     private final String TAG = getClass().getSimpleName();
-    private TextView tvTitle;
-    //private TextView tv;
     private AVLoadingIndicatorView avi;
     private Button btSelect;
 
     private List<ComicType> comicTypeList;
+    private PlaceHolderView placeHolderView;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -49,8 +48,8 @@ public class FrmAllManga extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frm_all_manga, container, false);
         btSelect = (Button) view.findViewById(R.id.bt_select);
-        tvTitle = (TextView) view.findViewById(R.id.tv_title);
-        //tv = (TextView) view.findViewById(R.id.tv);
+        placeHolderView = (PlaceHolderView) view.findViewById(R.id.place_hoder_view);
+
         avi = (AVLoadingIndicatorView) view.findViewById(R.id.avi);
         avi.hide();
 
@@ -66,36 +65,40 @@ public class FrmAllManga extends BaseFragment {
     }
 
     private void showDialogSelect() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Chọn thể loại:");
-
         String[] items = new String[comicTypeList.size()];
         for (int i = 0; i < comicTypeList.size(); i++) {
             //LLog.d(TAG, i + " : " + photosetList.get(i).getTitle().getContent());
             items[i] = comicTypeList.get(i).getType();
         }
-
-        builder.setItems(items, new DialogInterface.OnClickListener() {
+        LDialogUtil.showDialogList(getActivity(), getString(R.string.choose_type), items, new LDialogUtil.CallbackList() {
             @Override
-            public void onClick(DialogInterface dialog, int position) {
+            public void onClick(int position) {
                 LLog.d(TAG, "onClick " + position);
-                //tv.setText("");
-                tvTitle.setText("");
-                new GetComicTask(getActivity(), comicTypeList.get(position).getUrl(), avi, new GetComicTask.Callback() {
+                getComicTask = new GetComicTask(getActivity(), comicTypeList.get(position).getUrl(), avi, new GetComicTask.Callback() {
                     @Override
                     public void onSuccess(List<Comic> comicList) {
-                        //LUIUtil.printBeautyJson(comicList, tv);
-                        tvTitle.setText("Danh sách truyện: " + comicList.size());
+
                     }
 
                     @Override
                     public void onError() {
                         ToastUtils.showShort("Error");
                     }
-                }).execute();
+                });
+                if (getComicTask != null) {
+                    getComicTask.execute();
+                }
             }
         });
-        AlertDialog dialog = builder.create();
-        dialog.show();
+    }
+
+    private GetComicTask getComicTask;
+
+    @Override
+    public void onDestroyView() {
+        if (getComicTask != null) {
+            getComicTask.cancel(true);
+        }
+        super.onDestroyView();
     }
 }
