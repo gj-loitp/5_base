7c19fb9ba2d23556f5ad85d623c3a56b05334aab
Loitp <loitp@pateco.vn>
2018-01-23 11:31:12 +0700

    update

--- zbestcomic/src/main/java/vn/loitp/app/activity/home/allwallpaper/FrmPhotoCategory.java
+++ zbestcomic/src/main/java/vn/loitp/app/activity/home/allwallpaper/FrmPhotoCategory.java
@@ -2,7 +2,6 @@ package vn.loitp.app.activity.home.allwallpaper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
