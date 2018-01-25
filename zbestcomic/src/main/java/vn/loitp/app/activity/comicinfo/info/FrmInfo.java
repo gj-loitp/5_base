7d26b4e1c8f4604043f0e0543a1155372891db12
Loitp <loitp@pateco.vn>
2018-01-25 17:19:42 +0700

    update

--- zbestcomic/src/main/java/vn/loitp/app/activity/comicinfo/info/FrmInfo.java
+++ zbestcomic/src/main/java/vn/loitp/app/activity/comicinfo/info/FrmInfo.java
@@ -5,14 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdView;
+
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseFragment;
+import vn.loitp.core.utilities.LUIUtil;
 
 /**
  * Created by www.muathu@gmail.com on 7/26/2017.
  */
 
 public class FrmInfo extends BaseFragment {
+    private AdView adView;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -27,7 +31,26 @@ public class FrmInfo extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frm_comic_info, container, false);
-
+        adView = (AdView) view.findViewById(R.id.adView);
+        LUIUtil.createAdBanner(adView);
         return view;
+    }
+
+    @Override
+    public void onPause() {
+        adView.pause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        adView.resume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        adView.destroy();
+        super.onDestroy();
     }
 }
