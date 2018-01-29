e08922b93c4b16ae91e7516bf8e43132c165cfdb
Loitp <loitp@pateco.vn>
2018-01-29 16:45:59 +0700

    update

--- zbestcomic/src/main/java/vn/loitp/app/activity/comicinfo/info/FrmInfo.java
+++ zbestcomic/src/main/java/vn/loitp/app/activity/comicinfo/info/FrmInfo.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdView;
 
 import loitp.basemaster.R;
+import vn.loitp.app.activity.comicinfo.chap.ComicInfoData;
+import vn.loitp.app.model.chap.Info;
 import vn.loitp.core.base.BaseFragment;
 import vn.loitp.core.utilities.LUIUtil;
+import vn.loitp.views.textview.textdecorator.lib.OnTextClickListener;
+import vn.loitp.views.textview.textdecorator.lib.TextDecorator;
 
 /**
  * Created by www.muathu@gmail.com on 7/26/2017.
@@ -17,6 +23,7 @@ import vn.loitp.core.utilities.LUIUtil;
 
 public class FrmInfo extends BaseFragment {
     private AdView adView;
+    private TextView tvInfo;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -33,6 +40,38 @@ public class FrmInfo extends BaseFragment {
         View view = inflater.inflate(R.layout.frm_comic_info, container, false);
         adView = (AdView) view.findViewById(R.id.adView);
         LUIUtil.createAdBanner(adView);
+
+        tvInfo = (TextView) view.findViewById(R.id.tv_info);
+        String author = "Tác giả: ";
+        String otherName = "Tên khác: ";
+        String summary = "Nội dung: ";
+        String type = "Thể loại: ";
+        String newChap = "Chương mới nhất: ";
+        Info info = ComicInfoData.getInstance().getTttChap().getInfo();
+        if (info != null) {
+            final String text = author + info.getAuthor() + "\n\n" +
+                    otherName + info.getOtherName() + "\n\n"
+                    + summary + info.getSummary() + "\n\n"
+                    + type + info.getType() + "\n\n"
+                    + newChap + info.getNewChap();
+
+            TextDecorator
+                    .decorate(tvInfo, text)
+                    .setTextColor(R.color.Black, author, otherName, summary, type, newChap)
+                    //.setBackgroundColor(R.color.colorPrimary, "dolor", "elit")
+                    //.strikethrough("Duis", "Praesent")
+                    //.underline("sodales", "quam")
+                    //.setSubscript("vitae")
+                    /*.makeTextClickable(new OnTextClickListener() {
+                        @Override
+                        public void onClick(View view, String text) {
+                            Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+                        }
+                    }, false, "porta", "commodo", "tempor venenatis nulla")*/
+                    //.setTextColor(android.R.color.holo_green_light, "porta", "commodo", "tempor venenatis nulla")
+                    .build();
+
+        }
         return view;
     }
 
