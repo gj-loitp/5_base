2af1c443aa231c08c11088058ce0a0d1706c93f3
Loitp <loitp@pateco.vn>
2018-01-29 13:56:31 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/imageview/ImageViewMenuActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/imageview/ImageViewMenuActivity.java
@@ -4,90 +4,35 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 
+import loitp.basemaster.R;
 import vn.loitp.app.activity.customviews.imageview.blurimageview.BlurImageViewActivity;
 import vn.loitp.app.activity.customviews.imageview.circleimageview.CircleImageViewActivity;
 import vn.loitp.app.activity.customviews.imageview.circularroundrectimageview.CircularRoundRectImageViewActivity;
+import vn.loitp.app.activity.customviews.imageview.continuousscrollableimageview.ContinuousScrollableImageViewActivity;
 import vn.loitp.app.activity.customviews.imageview.fidgetspinnerimageview.FidgetSpinnerImageViewActivity;
 import vn.loitp.app.activity.customviews.imageview.img2ascii.Img2AsciiActivity;
 import vn.loitp.app.activity.customviews.imageview.strectchyimageview.StrectchyImageViewActivity;
 import vn.loitp.app.activity.customviews.imageview.touchimageview.TouchImageViewActivity;
 import vn.loitp.app.activity.customviews.imageview.zoomimageview.ZoomImageViewActivity;
 import vn.loitp.core.base.BaseActivity;
-import loitp.basemaster.R;
 import vn.loitp.core.utilities.LActivityUtil;
-import vn.loitp.core.utilities.LUIUtil;
 
-public class ImageViewMenuActivity extends BaseActivity {
+public class ImageViewMenuActivity extends BaseActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.bt_blur_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, BlurImageViewActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_cirle_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, CircleImageViewActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_stretchy_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, StrectchyImageViewActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_touch_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, TouchImageViewActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_zoom_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, ZoomImageViewActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_fidgetspinner).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, FidgetSpinnerImageViewActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_cirlularroundrect_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, CircularRoundRectImageViewActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_img2ascii).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, Img2AsciiActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-
+        findViewById(R.id.bt_blur_imageview).setOnClickListener(this);
+        findViewById(R.id.bt_cirle_imageview).setOnClickListener(this);
+        findViewById(R.id.bt_stretchy_imageview).setOnClickListener(this);
+        findViewById(R.id.bt_touch_imageview).setOnClickListener(this);
+        findViewById(R.id.bt_zoom_imageview).setOnClickListener(this);
+        findViewById(R.id.bt_fidgetspinner).setOnClickListener(this);
+        findViewById(R.id.bt_cirlularroundrect_imageview).setOnClickListener(this);
+        findViewById(R.id.bt_img2ascii).setOnClickListener(this);
+        findViewById(R.id.bt_continuous_scrollable_imageview).setOnClickListener(this);
     }
 
     @Override
@@ -108,5 +53,43 @@ public class ImageViewMenuActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_menu_imageview;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.bt_blur_imageview:
+                intent = new Intent(activity, BlurImageViewActivity.class);
+                break;
+            case R.id.bt_cirle_imageview:
+                intent = new Intent(activity, CircleImageViewActivity.class);
+                break;
+            case R.id.bt_stretchy_imageview:
+                intent = new Intent(activity, StrectchyImageViewActivity.class);
+                break;
+            case R.id.bt_touch_imageview:
+                intent = new Intent(activity, TouchImageViewActivity.class);
+                break;
+            case R.id.bt_zoom_imageview:
+                intent = new Intent(activity, ZoomImageViewActivity.class);
+                break;
+            case R.id.bt_fidgetspinner:
+                intent = new Intent(activity, FidgetSpinnerImageViewActivity.class);
+                break;
+            case R.id.bt_cirlularroundrect_imageview:
+                intent = new Intent(activity, CircularRoundRectImageViewActivity.class);
+                break;
+            case R.id.bt_img2ascii:
+                intent = new Intent(activity, Img2AsciiActivity.class);
+                break;
+            case R.id.bt_continuous_scrollable_imageview:
+                intent = new Intent(activity, ContinuousScrollableImageViewActivity.class);
+                break;
+        }
+        if (intent != null) {
+            startActivity(intent);
+            LActivityUtil.tranIn(activity);
+        }
     }
 }
---
+++ app/src/main/java/vn/loitp/app/activity/customviews/imageview/continuousscrollableimageview/ContinuousScrollableImageViewActivity.java
@@ -0,0 +1,57 @@
+package vn.loitp.app.activity.customviews.imageview.continuousscrollableimageview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import loitp.basemaster.R;
+import vn.loitp.core.base.BaseActivity;
+import vn.loitp.views.imageview.fidgetspinnerimageview.FidgetSpinner;
+
+//https://github.com/Cutta/ContinuousScrollableImageView?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=6094
+public class ContinuousScrollableImageViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //XML way
+
+        //Classic Way
+        /*image = new ContinuousScrollableImageView(this);
+        image.setResourceId(R.drawable.bg_sample);
+        image.setDirection(ContinuousScrollableImageView.DOWN);
+        image.setScaleType(ContinuousScrollableImageView.FIT_XY);
+        image.setDuration(3000);
+        rootLayout.addView(image);*/
+
+        //Builder Way
+        /*image = new ContinuousScrollableImageView.Builder(MainActivity.this)
+                .setResourceId(R.drawable.bg_sample)
+                .setDirection(ContinuousScrollableImageView.UP)
+                .setDuration(3000)
+                .setScaleType(ContinuousScrollableImageView.FIT_XY)
+                .build();
+        rootLayout.addView(image);*/
+
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_continuousscrollable_imageview;
+    }
+}
