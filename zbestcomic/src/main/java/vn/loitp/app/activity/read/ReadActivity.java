e14acac81ed5fbc83e10e97ec1b8a0fa3fcec648
Loitp <loitp@pateco.vn>
2018-01-31 10:39:33 +0700

    update

--- zbestcomic/src/main/java/vn/loitp/app/activity/read/ReadActivity.java
+++ zbestcomic/src/main/java/vn/loitp/app/activity/read/ReadActivity.java
@@ -210,7 +210,7 @@ public class ReadActivity extends BaseActivity implements View.OnClickListener {
                 LTouchImageView imageView = (LTouchImageView) layout.findViewById(R.id.imageView);
                 LLog.d(TAG, ">instantiateItem: " + imagesListOfOneChap.get(position));
 
-                LImageUtil.load(activity, imagesListOfOneChap.get(position), imageView, avLoadingIndicatorView, R.color.transparent, R.drawable.err);
+                LImageUtil.load(activity, imagesListOfOneChap.get(position), imageView, avLoadingIndicatorView, R.color.transparent, R.drawable.ic_error_svgrepo_com);
             } else {
                 LLog.e(TAG, "SlidePagerAdapter instantiateItem with incorrect position " + position);
             }
