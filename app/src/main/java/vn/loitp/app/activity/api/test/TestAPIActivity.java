e3b5b875c539789d38b9fb740288d6b08f5d3e2c
Loitp <loitp@pateco.vn>
2018-02-08 16:11:42 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/api/test/TestAPIActivity.java
+++ app/src/main/java/vn/loitp/app/activity/api/test/TestAPIActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import loitp.basemaster.R;
 import vn.loitp.app.app.LSApplication;
 import vn.loitp.core.base.BaseActivity;
-import loitp.basemaster.R;
 import vn.loitp.core.utilities.LLog;
 import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.restapi.livestar.corev3.api.model.v3.categoryget.CategoryGet;
 import vn.loitp.restapi.livestar.corev3.api.model.v3.getposter.GetPoster;
-import vn.loitp.restapi.restclient.RestClient;
 import vn.loitp.restapi.livestar.corev3.api.service.LSService;
+import vn.loitp.restapi.restclient.RestClient;
+import vn.loitp.restapi.wtt.WTTService;
 import vn.loitp.rxandroid.ApiSubscriber;
 
 public class TestAPIActivity extends BaseActivity {
@@ -26,13 +27,22 @@ public class TestAPIActivity extends BaseActivity {
         findViewById(R.id.bt_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RestClient.init(getString(R.string.webService_URL), "");//TODO truyen token
                 testGetPoster();
             }
         });
         findViewById(R.id.bt_3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RestClient.init(getString(R.string.webService_URL), "");//TODO truyen token
                 testCategoryGet();
+            }
+        });
+        findViewById(R.id.bt_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RestClient.init(getString(R.string.wtt_URL), "BIH80NYmucZwCoqPvrdI3ZU9ATB909Gi-1512972145301");
+                test();
             }
         });
     }
@@ -99,6 +109,23 @@ public class TestAPIActivity extends BaseActivity {
             @Override
             public void onFinally(boolean success) {
                 LLog.d(TAG, "onFinally success " + success);
+            }
+        });
+    }
+
+    private void test() {
+        LLog.d(TAG, "testCategoryGet");
+        WTTService service = RestClient.createService(WTTService.class);
+        subscribe(service.getMetadatList(100), new ApiSubscriber<Object>() {
+            @Override
+            public void onSuccess(Object result) {
+                LLog.d(TAG, "onSuccess " + LSApplication.getInstance().getGson().toJson(result));
+                LUIUtil.printBeautyJson(result, tv);
+            }
+
+            @Override
+            public void onFail(Throwable e) {
+                handleException(e);
             }
         });
     }
