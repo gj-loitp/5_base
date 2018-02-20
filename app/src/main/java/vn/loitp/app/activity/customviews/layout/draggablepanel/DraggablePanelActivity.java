7056125948322b15d397acbdac1b6cd9615202dd
Loitp <loitp@pateco.vn>
2018-02-20 14:47:52 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/draggablepanel/DraggablePanelActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/draggablepanel/DraggablePanelActivity.java
@@ -5,13 +5,38 @@ import android.os.Bundle;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LLog;
+import vn.loitp.views.draggablepanel.DraggableListener;
+import vn.loitp.views.draggablepanel.DraggableView;
 
 public class DraggablePanelActivity extends BaseActivity {
+    private DraggableView draggableView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        draggableView = (DraggableView) findViewById(R.id.draggable_view);
+        draggableView.setDraggableListener(new DraggableListener() {
+            @Override
+            public void onMaximized() {
+                LLog.d(TAG, "onMaximized");
+            }
 
+            @Override
+            public void onMinimized() {
+                LLog.d(TAG, "onMinimized");
+            }
+
+            @Override
+            public void onClosedToLeft() {
+                LLog.d(TAG, "onClosedToLeft");
+            }
+
+            @Override
+            public void onClosedToRight() {
+                LLog.d(TAG, "onClosedToRight");
+            }
+        });
     }
 
     @Override
