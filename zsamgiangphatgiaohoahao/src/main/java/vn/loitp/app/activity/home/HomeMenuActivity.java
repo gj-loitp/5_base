6b720fd24ce807ba4c31600d05e8d985f9899988
Loitp <loitp@pateco.vn>
2018-02-21 13:52:37 +0700

    update

--- zsamgiangphatgiaohoahao/src/main/java/vn/loitp/app/activity/home/HomeMenuActivity.java
+++ zsamgiangphatgiaohoahao/src/main/java/vn/loitp/app/activity/home/HomeMenuActivity.java
@@ -33,7 +33,6 @@ import com.google.android.exoplayer2.extractor.mp3.Mp3Extractor;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
@@ -44,7 +43,6 @@ import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
@@ -84,19 +82,19 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
         return dataManager;
     }
 
-    private final String STATE_RESUME_WINDOW = "resumeWindow";
-    private final String STATE_RESUME_POSITION = "resumePosition";
-    private final String STATE_PLAYER_FULLSCREEN = "playerFullscreen";
+    //private final String STATE_RESUME_WINDOW = "resumeWindow";
+    //private final String STATE_RESUME_POSITION = "resumePosition";
+    //private final String STATE_PLAYER_FULLSCREEN = "playerFullscreen";
 
     private SimpleExoPlayerView mExoPlayerView;
     private MediaSource mVideoSource;
-    private boolean mExoPlayerFullscreen = false;
-    private FrameLayout mFullScreenButton;
-    private ImageView mFullScreenIcon;
-    private Dialog mFullScreenDialog;
+    //private boolean mExoPlayerFullscreen = false;
+    //private FrameLayout mFullScreenButton;
+    //private ImageView mFullScreenIcon;
+    //private Dialog mFullScreenDialog;
 
-    private int mResumeWindow;
-    private long mResumePosition;
+    //private int mResumeWindow;
+    //private long mResumePosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -119,6 +117,8 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
         AppUtil.loadBackground(activity, toolbarImage);
 
         setCustomStatusBar(Color.TRANSPARENT, ContextCompat.getColor(activity, R.color.colorPrimary));
+
+        mExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.exoplayer);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -170,11 +170,11 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
 
         findViewById(R.id.bt_menu).setOnClickListener(this);
 
-        if (savedInstanceState != null) {
+        /*if (savedInstanceState != null) {
             mResumeWindow = savedInstanceState.getInt(STATE_RESUME_WINDOW);
             mResumePosition = savedInstanceState.getLong(STATE_RESUME_POSITION);
             mExoPlayerFullscreen = savedInstanceState.getBoolean(STATE_PLAYER_FULLSCREEN);
-        }
+        }*/
 
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
@@ -192,7 +192,15 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
 
             @Override
             public void onPageSelected(int position) {
+                LLog.d(TAG, "onPageSelected " + position);
                 LPref.setIndex(activity, position);
+                releaseVideo();
+                String linkMp3 = chapList.get(position).getLinkMp3();
+                if (linkMp3 == null) {
+                    return;
+                }
+                playMp3(linkMp3);
+                initExoPlayer();
             }
 
             @Override
@@ -320,15 +328,16 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
                 break;
         }
     }
-    @Override
+
+    /*@Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putInt(STATE_RESUME_WINDOW, mResumeWindow);
         outState.putLong(STATE_RESUME_POSITION, mResumePosition);
         outState.putBoolean(STATE_PLAYER_FULLSCREEN, mExoPlayerFullscreen);
         super.onSaveInstanceState(outState);
-    }
+    }*/
 
-    private void initFullscreenDialog() {
+    /*private void initFullscreenDialog() {
         mFullScreenDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen) {
             public void onBackPressed() {
                 if (mExoPlayerFullscreen) {
@@ -337,27 +346,26 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
                 super.onBackPressed();
             }
         };
-    }
+    }*/
 
-    private void openFullscreenDialog() {
+    /*private void openFullscreenDialog() {
         ((ViewGroup) mExoPlayerView.getParent()).removeView(mExoPlayerView);
         mFullScreenDialog.addContentView(mExoPlayerView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_fullscreen_skrink));
         mExoPlayerFullscreen = true;
         mFullScreenDialog.show();
-    }
+    }*/
 
 
-    private void closeFullscreenDialog() {
+    /*private void closeFullscreenDialog() {
         ((ViewGroup) mExoPlayerView.getParent()).removeView(mExoPlayerView);
         ((FrameLayout) findViewById(R.id.main_media_frame)).addView(mExoPlayerView);
         mExoPlayerFullscreen = false;
         mFullScreenDialog.dismiss();
         mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_fullscreen_expand));
-    }
+    }*/
 
-
-    private void initFullscreenButton() {
+    /*private void initFullscreenButton() {
         PlaybackControlView controlView = mExoPlayerView.findViewById(R.id.exo_controller);
         mFullScreenIcon = controlView.findViewById(R.id.exo_fullscreen_icon);
         mFullScreenButton = controlView.findViewById(R.id.exo_fullscreen_button);
@@ -370,7 +378,7 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
                     closeFullscreenDialog();
             }
         });
-    }
+    }*/
 
     private void initExoPlayer() {
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
@@ -380,11 +388,11 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
         SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), trackSelector, loadControl);
         mExoPlayerView.setPlayer(player);
 
-        boolean haveResumePosition = mResumeWindow != C.INDEX_UNSET;
+        //boolean haveResumePosition = mResumeWindow != C.INDEX_UNSET;
 
-        if (haveResumePosition) {
-            mExoPlayerView.getPlayer().seekTo(mResumeWindow, mResumePosition);
-        }
+        //if (haveResumePosition) {
+        //    mExoPlayerView.getPlayer().seekTo(mResumeWindow, mResumePosition);
+        //}
 
         mExoPlayerView.getPlayer().prepare(mVideoSource);
         mExoPlayerView.getPlayer().setPlayWhenReady(true);
@@ -447,18 +455,16 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
     }
 
 
-    @Override
+    /*@Override
     protected void onResume() {
-
         super.onResume();
-
         if (mExoPlayerView == null) {
             mExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.exoplayer);
 
             initFullscreenDialog();
             initFullscreenButton();
 
-            //playM3u8();
+            playM3u8();
             playMp3();
         }
 
@@ -470,18 +476,18 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
             mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_fullscreen_skrink));
             mFullScreenDialog.show();
         }
-    }
+    }*/
 
-    private void playM3u8() {
+    /*private void playM3u8() {
         String streamUrl = "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8";
         String userAgent = Util.getUserAgent(activity, getApplicationContext().getApplicationInfo().packageName);
         DefaultHttpDataSourceFactory httpDataSourceFactory = new DefaultHttpDataSourceFactory(userAgent, null, DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS, DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS, true);
         DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(activity, null, httpDataSourceFactory);
         Uri daUri = Uri.parse(streamUrl);
         mVideoSource = new HlsMediaSource(daUri, dataSourceFactory, 1, null, null);
-    }
+    }*/
 
-    private void playMp3() {
+    /*private void playMp3() {
         Handler mHandler = new Handler();
         String url = "http://www.hoahaomedia.org/hoahao/_media/PGHH/Audio/Sam_Giang/Hue_Duyen/001_Sam%20Giang%20Khuyen%20Nguoi%20Doi%20Tu%20Niem%20Hue%20Duyen%20-%20Tri%20Tung.mp3";
         Uri uri = Uri.parse(url);
@@ -492,19 +498,42 @@ public class HomeMenuActivity extends BaseActivity implements View.OnClickListen
                 DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,
                 true);
         mVideoSource = new ExtractorMediaSource(uri, dataSourceFactory, Mp3Extractor.FACTORY, mHandler, null);
+    }*/
+
+    private void playMp3(String url) {
+        Handler mHandler = new Handler();
+        Uri uri = Uri.parse(url);
+        String userAgent = Util.getUserAgent(activity, getApplicationContext().getApplicationInfo().packageName);
+        DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory(
+                userAgent, null,
+                DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS,
+                DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,
+                true);
+        mVideoSource = new ExtractorMediaSource(uri, dataSourceFactory, Mp3Extractor.FACTORY, mHandler, null);
     }
 
-    @Override
+    /*@Override
     protected void onPause() {
         super.onPause();
         mResumeWindow = mExoPlayerView.getPlayer().getCurrentWindowIndex();
         mResumePosition = Math.max(0, mExoPlayerView.getPlayer().getContentPosition());
 
+        releaseVideo();
+
+        if (mFullScreenDialog != null) {
+            mFullScreenDialog.dismiss();
+        }
+    }*/
+
+    private void releaseVideo() {
         if (mExoPlayerView != null && mExoPlayerView.getPlayer() != null) {
             mExoPlayerView.getPlayer().release();
         }
+    }
 
-        if (mFullScreenDialog != null)
-            mFullScreenDialog.dismiss();
+    @Override
+    protected void onDestroy() {
+        releaseVideo();
+        super.onDestroy();
     }
 }
