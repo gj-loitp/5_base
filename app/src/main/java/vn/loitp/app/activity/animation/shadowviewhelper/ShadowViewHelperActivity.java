5b2f9b65977a06f66202b1a3927349dda69797aa
Loitp <loitp@pateco.vn>
2018-02-24 16:40:31 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/animation/shadowviewhelper/ShadowViewHelperActivity.java
+++ app/src/main/java/vn/loitp/app/activity/animation/shadowviewhelper/ShadowViewHelperActivity.java
@@ -3,10 +3,9 @@ package vn.loitp.app.activity.animation.shadowviewhelper;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
@@ -23,6 +22,7 @@ public class ShadowViewHelperActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setAllSide();
         setCustomSide();
+        setCustomSide2();
     }
 
     private void setAllSide() {
@@ -49,6 +49,19 @@ public class ShadowViewHelperActivity extends BaseActivity {
         LinearLayout ll1 = (LinearLayout) findViewById(R.id.ll_1);
         ViewCompat.setBackground(ll1, sd);
         ViewCompat.setLayerType(ll1, ViewCompat.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    private void setCustomSide2() {
+        // only all sides except top shadow
+        ShadowProperty sp = new ShadowProperty()
+                .setShadowColor(0x7700ff00)
+                .setShadowDy(dip2px(this, 0.5f))
+                .setShadowRadius(dip2px(this, 3))
+                .setShadowSide(ShadowProperty.RIGHT | ShadowProperty.BOTTOM);
+        ShadowViewDrawable sd = new ShadowViewDrawable(sp, Color.TRANSPARENT, 0, 0);
+        ImageView iv = (ImageView) findViewById(R.id.iv);
+        ViewCompat.setBackground(iv, sd);
+        ViewCompat.setLayerType(iv, ViewCompat.LAYER_TYPE_SOFTWARE, null);
     }
 
     @Override
