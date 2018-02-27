183c0f504bffb7f759cfb0c2074582e4edd9a97d
Loitp <loitp@pateco.vn>
2018-02-27 13:33:32 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/videoview/exoplayer2withdragpanel3/FrmContainer.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/videoview/exoplayer2withdragpanel3/FrmContainer.java
@@ -6,17 +6,27 @@ package vn.loitp.app.activity.customviews.videoview.exoplayer2withdragpanel3;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import loitp.basemaster.R;
 import vn.loitp.app.activity.customviews.layout.draggablepanel.FrmTest;
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie;
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.MoviesAdapter;
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerviewwithsingletondata.DummyData;
 import vn.loitp.core.base.BaseFragment;
 import vn.loitp.core.utilities.LLog;
+import vn.loitp.core.utilities.LPopupMenu;
+import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.views.LToast;
-import vn.loitp.views.draggablepanel.DraggableListener;
 import vn.loitp.views.draggablepanel.DraggablePanel;
 
 /**
@@ -26,6 +36,10 @@ import vn.loitp.views.draggablepanel.DraggablePanel;
 public class FrmContainer extends BaseFragment {
     private final String TAG = getClass().getSimpleName();
     private DraggablePanel draggablePanel;
+
+    private RecyclerView recyclerView;
+    private MoviesAdapter mAdapter;
+    private TextView tvType;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -65,18 +79,7 @@ public class FrmContainer extends BaseFragment {
             }
         });*/
 
-        view.findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (draggablePanel.isClosedAtLeft() || draggablePanel.isClosedAtRight()) {
-                    LLog.d(TAG, "isClosedAtLeft || isClosedAtRight");
-                    draggablePanel.minimize();
-                    draggablePanel.setVisibility(View.VISIBLE);
-                } else {
-                    LLog.d(TAG, "do nothing");
-                }
-            }
-        });
+        setupUI(view);
         return view;
     }
 
@@ -90,12 +93,112 @@ public class FrmContainer extends BaseFragment {
 
         //draggablePanel.setXScaleFactor(xScaleFactor);
         //draggablePanel.setYScaleFactor(yScaleFactor);
-        draggablePanel.setTopViewHeight(600);
+        draggablePanel.setTopViewHeight(600);//px
         //draggablePanel.setTopFragmentMarginRight(topViewMarginRight);
         //draggablePanel.setTopFragmentMarginBottom(topViewMargnBottom);
         draggablePanel.setClickToMaximizeEnabled(false);
         draggablePanel.setClickToMinimizeEnabled(false);
 
         draggablePanel.initializeView();
+    }
+
+    private void setupUI(View view) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv);
+        tvType = (TextView) view.findViewById(R.id.tv_type);
+
+        mAdapter = new MoviesAdapter(DummyData.getInstance().getMovieList(), new MoviesAdapter.Callback() {
+            @Override
+            public void onClick(Movie movie, int position) {
+                if (draggablePanel.isClosedAtLeft() || draggablePanel.isClosedAtRight()) {
+                    LLog.d(TAG, "isClosedAtLeft || isClosedAtRight");
+                    draggablePanel.minimize();
+                    draggablePanel.setVisibility(View.VISIBLE);
+                } else {
+                    LLog.d(TAG, "do nothing");
+                }
+            }
+
+            @Override
+            public void onLongClick(Movie movie, int position) {
+                boolean isRemoved = DummyData.getInstance().getMovieList().remove(movie);
+                if (isRemoved) {
+                    mAdapter.notifyItemRemoved(position);
+                    mAdapter.notifyItemRangeChanged(position, DummyData.getInstance().getMovieList().size());
+                }
+            }
+
+            @Override
+            public void onLoadMore() {
+                loadMore();
+            }
+        });
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        LUIUtil.setPullLikeIOSVertical(recyclerView);
+
+        prepareMovieData();
+
+        view.findViewById(R.id.bt_setting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LPopupMenu.show(getActivity(), v, R.menu.menu_recycler_view, new LPopupMenu.CallBack() {
+                    @Override
+                    public void clickOnItem(MenuItem menuItem) {
+                        tvType.setText(menuItem.getTitle().toString());
+                        switch (menuItem.getItemId()) {
+                            case R.id.menu_linear_vertical:
+                                RecyclerView.LayoutManager lmVertical = new LinearLayoutManager(getActivity());
+                                recyclerView.setLayoutManager(lmVertical);
+                                break;
+                            case R.id.menu_linear_horizontal:
+                                RecyclerView.LayoutManager lmHorizontal = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+                                recyclerView.setLayoutManager(lmHorizontal);
+                                break;
+                            case R.id.menu_gridlayoutmanager_2:
+                                recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+                                break;
+                            case R.id.menu_gridlayoutmanager_3:
+                                recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+                                break;
+                            case R.id.menu_staggeredgridlayoutmanager_2:
+                                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+                                break;
+                            case R.id.menu_staggeredgridlayoutmanager_4:
+                                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.HORIZONTAL));
+                                break;
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void loadMore() {
+        LLog.d(TAG, "loadMore");
+        LUIUtil.setDelay(2000, new LUIUtil.DelayCallback() {
+            @Override
+            public void doAfter(int mls) {
+                int newSize = 5;
+                for (int i = 0; i < newSize; i++) {
+                    Movie movie = new Movie("Add new " + i, "Add new " + i, "Add new: " + i);
+                    DummyData.getInstance().getMovieList().add(movie);
+                }
+                mAdapter.notifyDataSetChanged();
+                LToast.show(getActivity(), "Finish loadMore");
+            }
+        });
+    }
+
+    private void prepareMovieData() {
+        if (DummyData.getInstance().getMovieList().isEmpty()) {
+            for (int i = 0; i < 10; i++) {
+                Movie movie = new Movie("Loitp " + i, "Action & Adventure " + i, "Year: " + i);
+                DummyData.getInstance().getMovieList().add(movie);
+            }
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
