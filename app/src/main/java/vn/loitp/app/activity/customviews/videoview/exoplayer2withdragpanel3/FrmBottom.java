b5db832753771cad0dfe2fc5f584eec7d7cecc40
Loitp <loitp@pateco.vn>
2018-02-27 13:55:59 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/videoview/exoplayer2withdragpanel3/FrmBottom.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/videoview/exoplayer2withdragpanel3/FrmBottom.java
@@ -11,13 +11,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 
 import loitp.basemaster.R;
 import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie;
 import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.MoviesAdapter;
 import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerviewwithsingletondata.DummyData;
 import vn.loitp.core.base.BaseFragment;
-import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.views.LToast;
 
 /**
@@ -64,20 +66,37 @@ public class FrmBottom extends BaseFragment {
                 //do nothing
             }
         });
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
-        LUIUtil.setPullLikeIOSVertical(recyclerView);
+        //LUIUtil.setPullLikeIOSHorizontal(recyclerView);
+
+        //NestedScrollView nestedScrollView = (NestedScrollView) view.findViewById(R.id.scroll_view);
+        //LUIUtil.setPullLikeIOSVertical(recyclerView);
 
         prepareMovieData();
+
+        LinearLayout llHorizontal = (LinearLayout) view.findViewById(R.id.ll_horizontal);
+        for (int i = 0; i < 20; i++) {
+            Button button = new Button(getActivity());
+            button.setText("Button " + i);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LToast.show(getActivity(), "Click " + button.getText().toString());
+                }
+            });
+            llHorizontal.addView(button);
+        }
+
         return view;
     }
 
     private void prepareMovieData() {
         if (DummyData.getInstance().getMovieList().isEmpty()) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 20; i++) {
                 Movie movie = new Movie("Loitp " + i, "Action & Adventure " + i, "Year: " + i);
                 DummyData.getInstance().getMovieList().add(movie);
             }
