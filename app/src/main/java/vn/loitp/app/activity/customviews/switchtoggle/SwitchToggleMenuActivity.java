48b69fad47452e86a0f7cfda8196ff57e4a52175
Loitp <loitp@pateco.vn>
2018-03-01 16:13:49 +0700

    add toggle

--- app/src/main/java/vn/loitp/app/activity/customviews/switchtoggle/SwitchToggleMenuActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/switchtoggle/SwitchToggleMenuActivity.java
@@ -7,33 +7,21 @@ import android.view.View;
 
 import vn.loitp.app.activity.customviews.switchtoggle.appcompatswitch.AppcompatSwitchActivity;
 import vn.loitp.app.activity.customviews.switchtoggle.customtogglebutton.CustomToggleButtonActivity;
+import vn.loitp.app.activity.customviews.switchtoggle.toggle.ToggleActivity;
 import vn.loitp.core.base.BaseActivity;
 
 import loitp.basemaster.R;
 import vn.loitp.core.utilities.LActivityUtil;
 import vn.loitp.core.utilities.LUIUtil;
 
-public class SwitchToggleMenuActivity extends BaseActivity {
+public class SwitchToggleMenuActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.bt_appcompat_switch).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, AppcompatSwitchActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
-        findViewById(R.id.bt_custom_toggle_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, CustomToggleButtonActivity.class);
-                startActivity(intent);
-                LActivityUtil.tranIn(activity);
-            }
-        });
+        findViewById(R.id.bt_appcompat_switch).setOnClickListener(this);
+        findViewById(R.id.bt_custom_toggle_button).setOnClickListener(this);
+        findViewById(R.id.bt_toggle).setOnClickListener(this);
     }
 
     @Override
@@ -54,5 +42,25 @@ public class SwitchToggleMenuActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_menu_switch_toggle;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.bt_appcompat_switch:
+                intent = new Intent(activity, AppcompatSwitchActivity.class);
+                break;
+            case R.id.bt_custom_toggle_button:
+                intent = new Intent(activity, CustomToggleButtonActivity.class);
+                break;
+            case R.id.bt_toggle:
+                intent = new Intent(activity, ToggleActivity.class);
+                break;
+        }
+        if (intent != null) {
+            startActivity(intent);
+            LActivityUtil.tranIn(activity);
+        }
     }
 }
---
+++ app/src/main/java/vn/loitp/app/activity/customviews/switchtoggle/toggle/ToggleActivity.java
@@ -0,0 +1,48 @@
+package vn.loitp.app.activity.customviews.switchtoggle.toggle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import loitp.basemaster.R;
+import vn.loitp.core.base.BaseActivity;
+import vn.loitp.utils.util.ToastUtils;
+import vn.loitp.views.LToast;
+import vn.loitp.views.switchtoggle.customtogglebutton.lib.CustomToggle;
+import vn.loitp.views.switchtoggle.toggle.LabeledSwitch;
+import vn.loitp.views.switchtoggle.toggle.interfaces.OnToggledListener;
+
+//https://github.com/Angads25/android-toggle?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=6778
+public class ToggleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LabeledSwitch labeledSwitch = findViewById(R.id.ls);
+        labeledSwitch.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(LabeledSwitch labeledSwitch, boolean isOn) {
+                LToast.show(activity, "isOn " + isOn);
+            }
+        });
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_toggle;
+    }
+}
