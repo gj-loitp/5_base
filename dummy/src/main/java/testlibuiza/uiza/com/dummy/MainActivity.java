0bc25e81d48952ffb400bbe3ebb968eed617adeb
Loitp <loitp@pateco.vn>
2018-03-28 14:09:12 +0700

    update

--- dummy/src/main/java/testlibuiza/uiza/com/dummy/MainActivity.java
+++ dummy/src/main/java/testlibuiza/uiza/com/dummy/MainActivity.java
@@ -5,7 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import testlibuiza.uiza.com.dummy.app.LSApplication;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LLog;
+import vn.loitp.core.utilities.LUIUtil;
+import vn.loitp.restapi.restclient.RestClient;
+import vn.loitp.restapi.uiza.UizaService;
+import vn.loitp.rxandroid.ApiSubscriber;
 
 public class MainActivity extends BaseActivity {
 
@@ -15,7 +21,7 @@ public class MainActivity extends BaseActivity {
         findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                testAPI();
             }
         });
     }
@@ -38,5 +44,21 @@ public class MainActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_main;
+    }
+
+    private void testAPI() {
+        RestClient.init("https://uqc-api.uiza.io/", "zHiQCup9CzTr1eP5ZQsbPK5sYNYa8kRL-1517457089350");
+        UizaService service = RestClient.createService(UizaService.class);
+        subscribe(service.getMetadatList(), new ApiSubscriber<Object>() {
+            @Override
+            public void onSuccess(Object result) {
+                LLog.d(TAG, "testUizaStag onSuccess " + LSApplication.getInstance().getGson().toJson(result));
+            }
+
+            @Override
+            public void onFail(Throwable e) {
+                handleException(e);
+            }
+        });
     }
 }
