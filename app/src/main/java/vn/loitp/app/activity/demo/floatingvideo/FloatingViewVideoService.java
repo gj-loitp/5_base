b1ce289dd1f0fe48966cd068864dce74a76ff97e
Loitp <loitp@pateco.vn>
2018-03-28 11:14:24 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/demo/floatingvideo/FloatingViewVideoService.java
+++ app/src/main/java/vn/loitp/app/activity/demo/floatingvideo/FloatingViewVideoService.java
@@ -3,24 +3,56 @@ package vn.loitp.app.activity.demo.floatingvideo;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.decoder.DecoderCounters;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
+
 import loitp.basemaster.R;
+import vn.loitp.core.utilities.LLog;
 
 /**
  * Created by LENOVO on 3/27/2018.
  */
 
 
-public class FloatingViewVideoService extends Service {
+public class FloatingViewVideoService extends Service implements VideoRendererEventListener {
+    private final String TAG = getClass().getSimpleName();
     private WindowManager mWindowManager;
     private View mFloatingView;
+    private SimpleExoPlayerView simpleExoPlayerView;
+    private SimpleExoPlayer player;
 
     public FloatingViewVideoService() {
     }
@@ -63,8 +95,11 @@ public class FloatingViewVideoService extends Service {
             }
         });
 
+        simpleExoPlayerView = (SimpleExoPlayerView) mFloatingView.findViewById(R.id.player_view);
+        playVideo();
+
         //Drag and move floating view using user's touch action.
-        mFloatingView.findViewById(R.id.root_container).setOnTouchListener(new View.OnTouchListener() {
+        mFloatingView.findViewById(R.id.move_view).setOnTouchListener(new View.OnTouchListener() {
             private int initialX;
             private int initialY;
             private float initialTouchX;
@@ -101,19 +136,13 @@ public class FloatingViewVideoService extends Service {
         });
     }
 
-    /**
-     * Detect if the floating view is collapsed or expanded.
-     *
-     * @return true if the floating view is collapsed.
-     */
-    private boolean isViewCollapsed() {
-        return mFloatingView == null || mFloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
-    }
-
     @Override
     public void onDestroy() {
         super.onDestroy();
         if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+        if (player != null) {
+            player.release();
+        }
     }
 
     //Set the view while floating view is expanded.
@@ -127,5 +156,156 @@ public class FloatingViewVideoService extends Service {
 
         //close the service and remove view from the view hierarchy
         stopSelf();
+    }
+
+    private void playVideo() {
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // 2. Create a default LoadControl
+        LoadControl loadControl = new DefaultLoadControl();
+
+        // 3. Create the player
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+
+        //Set media controller
+        simpleExoPlayerView.setUseController(false);
+        simpleExoPlayerView.requestFocus();
+
+        // Bind the player to the view.
+        simpleExoPlayerView.setPlayer(player);
+
+        // I. ADJUST HERE:
+        //CHOOSE CONTENT: LiveStream / SdCard
+
+        //LIVE STREAM SOURCE: * Livestream links may be out of date so find any m3u8 files online and replace:
+
+        //Uri mp4VideoUri =Uri.parse("http://81.7.13.162/hls/ss1/index.m3u8"); //random 720p source
+        Uri mp4VideoUri = Uri.parse("http://54.255.155.24:1935//Live/_definst_/amlst:sweetbcha1novD235L240P/playlist.m3u8"); //Radnom 540p indian channel
+        //Uri mp4VideoUri =Uri.parse("FIND A WORKING LINK ABD PLUg INTO HERE");
+
+
+        //VIDEO FROM SD CARD: (2 steps. set up file and path, then change videoSource to get the file)
+        //String urimp4 = "path/FileName.mp4"; //upload file to device and add path/name.mp4
+        //Uri mp4VideoUri = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+urimp4);
+
+
+        //Measures bandwidth during playback. Can be null if not required.
+        DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
+        //Produces DataSource instances through which media data is loaded.
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoplayer2example"), bandwidthMeterA);
+        //Produces Extractor instances for parsing the media data.
+        //ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        // II. ADJUST HERE:
+
+        //This is the MediaSource representing the media to be played:
+        //FOR SD CARD SOURCE:
+        //MediaSource videoSource = new ExtractorMediaSource(mp4VideoUri, dataSourceFactory, extractorsFactory, null, null);
+
+        //FOR LIVESTREAM LINK:
+        MediaSource videoSource = new HlsMediaSource(mp4VideoUri, dataSourceFactory, 1, null, null);
+        final LoopingMediaSource loopingSource = new LoopingMediaSource(videoSource);
+
+        player.prepare(loopingSource);
+
+        player.addListener(new ExoPlayer.EventListener() {
+            @Override
+            public void onTimelineChanged(Timeline timeline, Object manifest) {
+                LLog.d(TAG, "Listener-onTimelineChanged...");
+            }
+
+            @Override
+            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+                LLog.d(TAG, "Listener-onTracksChanged...");
+            }
+
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+                LLog.d(TAG, "Listener-onLoadingChanged...isLoading:" + isLoading);
+            }
+
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                LLog.d(TAG, "Listener-onPlayerStateChanged..." + playbackState);
+            }
+
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+                LLog.d(TAG, "Listener-onRepeatModeChanged...");
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+                LLog.d(TAG, "Listener-onPlayerError...");
+                player.stop();
+                player.prepare(loopingSource);
+                player.setPlayWhenReady(true);
+            }
+
+            @Override
+            public void onPositionDiscontinuity() {
+                LLog.d(TAG, "Listener-onPositionDiscontinuity...");
+            }
+
+            @Override
+            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+                LLog.d(TAG, "Listener-onPlaybackParametersChanged...");
+            }
+        });
+
+        player.setPlayWhenReady(true);
+        player.setVideoDebugListener(this); //for listening to resolution change and  outputing the resolution
+
+        /*
+
+        //MPD
+        //DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "ExoPlayer"));
+        Uri uri = Uri.parse("http://yt-dash-mse-test.commondatastorage.googleapis.com/media/feelings_vp9-20130806-manifest.mpd");
+        DashMediaSource dashMediaSource = new DashMediaSource(uri, dataSourceFactory, new DefaultDashChunkSource.Factory(dataSourceFactory), null, null);
+        //BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        //TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+        simpleExoPlayerView.setPlayer(player);
+        player.prepare(dashMediaSource);
+
+        */
+    }
+
+    @Override
+    public void onVideoEnabled(DecoderCounters counters) {
+        LLog.d(TAG, "onVideoEnabled");
+    }
+
+    @Override
+    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+        LLog.d(TAG, "onVideoDecoderInitialized");
+    }
+
+    @Override
+    public void onVideoInputFormatChanged(Format format) {
+        LLog.d(TAG, "onVideoInputFormatChanged");
+    }
+
+    @Override
+    public void onDroppedFrames(int count, long elapsedMs) {
+        LLog.d(TAG, "onDroppedFrames");
+    }
+
+    @Override
+    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+        LLog.d(TAG, "onVideoSizeChanged [" + " width: " + width + " height: " + height + "]");
+    }
+
+    @Override
+    public void onRenderedFirstFrame(Surface surface) {
+        LLog.d(TAG, "onRenderedFirstFrame");
+    }
+
+    @Override
+    public void onVideoDisabled(DecoderCounters counters) {
+        LLog.d(TAG, "onVideoDisabled");
     }
 }
