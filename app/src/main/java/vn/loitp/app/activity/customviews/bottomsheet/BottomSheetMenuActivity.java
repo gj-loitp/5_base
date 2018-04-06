1ff4e47ee7762d9b51a875eb0689be8bd635eb36
Loitp <loitp@pateco.vn>
2018-04-06 17:50:47 +0700

    update

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/bottomsheet/BottomSheetMenuActivity.java
@@ -0,0 +1,100 @@
+package vn.loitp.app.activity.customviews.bottomsheet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import loitp.basemaster.R;
+import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LLog;
+import vn.loitp.views.LToast;
+
+public class BottomSheetMenuActivity extends BaseActivity {
+    BottomSheetBehavior sheetBehavior;
+    LinearLayout layoutBottomSheet;
+    Button bt0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bt0 = (Button) findViewById(R.id.bt_0);
+        layoutBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+        layoutBottomSheet.findViewById(R.id.bt_payment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LToast.show(activity, "Click layoutBottomSheet R.id.bt_payment");
+            }
+        });
+
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        LLog.d(TAG, "STATE_HIDDEN");
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        LLog.d(TAG, "STATE_HIDDEN");
+                        bt0.setText("Close Sheet");
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        LLog.d(TAG, "STATE_COLLAPSED");
+                        bt0.setText("Expand Sheet");
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        LLog.d(TAG, "STATE_DRAGGING");
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        LLog.d(TAG, "STATE_SETTLING");
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                LLog.d(TAG, "onSlide " + slideOffset);
+            }
+        });
+
+        bt0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    bt0.setText("Close sheet");
+                } else {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    bt0.setText("Expand sheet");
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected Activity setActivity() {
+        return this;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_bottomsheet_menu;
+    }
+}
