a8464aebf4f10252da6311c22bc2960e50a1100d
Loitp <loitp@pateco.vn>
2018-04-22 11:00:26 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/ConstraintlayoutActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/ConstraintlayoutActivity.java
@@ -1,43 +1,15 @@
 package vn.loitp.app.activity.customviews.layout.constraintlayout;
 
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.view.MotionEvent;
-import android.view.View;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
 
 public class ConstraintlayoutActivity extends BaseActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraintlayout);
-
-        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.ll);
-        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        dX = constraintLayout.getX() - event.getRawX();
-                        dY = constraintLayout.getY() - event.getRawY();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        constraintLayout.animate()
-                                .x(event.getRawX() + dX)
-                                .y(event.getRawY() + dY)
-                                .setDuration(0)
-                                .scaleX(1.5f)
-                                .start();
-                        break;
-                    default:
-                        return false;
-                }
-                return true;
-            }
-        });
     }
 
     @Override
