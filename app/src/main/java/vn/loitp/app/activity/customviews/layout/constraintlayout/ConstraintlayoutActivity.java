2e543140aa996c10a95da7ab456f18f5ab7bb3d6
Loitp <loitp@pateco.vn>
2018-05-18 16:55:17 +0700

    update 1.20

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/ConstraintlayoutActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/ConstraintlayoutActivity.java
@@ -1,15 +1,44 @@
 package vn.loitp.app.activity.customviews.layout.constraintlayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseActivity;
 
 public class ConstraintlayoutActivity extends BaseActivity {
+    private Button button;
+    private Button bt0;
+    private Button bt1;
+    private Button bt2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraintlayout);
+        button = (Button) findViewById(R.id.button);
+        bt0 = (Button) findViewById(R.id.bt_0);
+        bt1 = (Button) findViewById(R.id.bt_1);
+        bt2 = (Button) findViewById(R.id.bt_2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setVisibility(View.GONE);
+            }
+        });
+        bt0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bt2.setVisibility(View.GONE);
+            }
+        });
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bt2.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     @Override
@@ -26,6 +55,4 @@ public class ConstraintlayoutActivity extends BaseActivity {
     protected int setLayoutResourceId() {
         return R.layout.activity_constraintlayout;
     }
-
-    float dX, dY;
 }
