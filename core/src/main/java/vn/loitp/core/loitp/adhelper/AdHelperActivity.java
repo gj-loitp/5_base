68d3cbe5e950527667690b80af83b5301133bfb9
Loitp <loitp@pateco.vn>
2018-05-31 18:36:20 +0700

    update

--- core/src/main/java/vn/loitp/core/loitp/adhelper/AdHelperActivity.java
+++ core/src/main/java/vn/loitp/core/loitp/adhelper/AdHelperActivity.java
@@ -1,26 +1,16 @@
 package vn.loitp.core.loitp.adhelper;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.widget.ImageView;
 
 import loitp.core.R;
 import vn.loitp.core.base.BaseActivity;
-import vn.loitp.core.utilities.LStoreUtil;
-import vn.loitp.views.ariana.ArianaBackgroundListener;
 
 /**
  * Created by LENOVO on 5/31/2018.
  */
 
 public class AdHelperActivity extends BaseActivity {
-    public ViewPager viewPager;
-    private ImageView imageView;
-    private int NUM_PAGES = 7;
-    private ScreenSlidePagerAdapter mPagerAdapter;
+
 
     @Override
     protected boolean setFullScreen() {
@@ -41,39 +31,6 @@ public class AdHelperActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         isShowAdWhenExist = false;
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        imageView = (ImageView) findViewById(R.id.imageView);
 
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(mPagerAdapter);
-        viewPager.addOnPageChangeListener(new ArianaBackgroundListener(LStoreUtil.getColors(), imageView, viewPager));
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (viewPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
-        }
-    }
-
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return ScreenSlidePageFragment.newInstance(LStoreUtil.getTexts()[position]);
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
     }
 }
