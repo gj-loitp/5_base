0a41dcc5ff7c952afef08fa3b70cf7d49c59dae2
Loitp <loitp@pateco.vn>
2018-06-05 17:43:42 +0700

    update firebase

--- app/src/main/java/vn/loitp/app/activity/demo/firebase/databasesimple/DatabaseSimpleFirebaseActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/firebase/databasesimple/DatabaseSimpleFirebaseActivity.java
@@ -2,7 +2,10 @@ package vn.loitp.app.activity.demo.firebase.databasesimple;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,8 +20,15 @@ import java.util.List;
 
 import loitp.basemaster.R;
 import vn.loitp.app.activity.BaseFontActivity;
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie;
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.MoviesAdapter;
 import vn.loitp.app.app.LSApplication;
+import vn.loitp.app.common.Constants;
 import vn.loitp.core.utilities.LLog;
+import vn.loitp.core.utilities.LUIUtil;
+import vn.loitp.views.LToast;
+import vn.loitp.views.recyclerview.animator.adapters.ScaleInAnimationAdapter;
+import vn.loitp.views.recyclerview.animator.animators.SlideInRightAnimator;
 
 //http://www.zoftino.com/firebase-realtime-database-android-example
 public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements View.OnClickListener {
@@ -26,6 +36,8 @@ public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements 
     private FirebaseDatabase mFirebaseInstance;
     private DatabaseReference mFirebaseDatabase;
     private List<User> userList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private UserAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +46,8 @@ public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements 
         mFirebaseDatabase = mFirebaseInstance.getReference();
 
         findViewById(R.id.bt_add).setOnClickListener(this);
+
+        setupUI();
 
         mFirebaseDatabase.child(ROOT_NODE).addValueEventListener(new ValueEventListener() {
             @Override
@@ -51,6 +65,9 @@ public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements 
                     }
                 }
                 LLog.d(TAG, "userList.size: " + userList.size());
+                if (mAdapter != null) {
+                    mAdapter.notifyDataSetChanged();
+                }
             }
 
             @Override
@@ -86,10 +103,10 @@ public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements 
 
     private void addData() {
         User user = new User();
-        user.setAvt("avt");
-        user.setName("loitp");
-        user.setMsg("dummy msg");
         user.setTimestamp(System.currentTimeMillis());
+        user.setAvt(Constants.URL_IMG);
+        user.setName("loitp");
+        user.setMsg("dummy msg " + user.getTimestamp());
         mFirebaseDatabase.child(ROOT_NODE).child(user.getTimestamp() + "").setValue(user)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
@@ -101,5 +118,39 @@ public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements 
                         }*/
                     }
                 });
+    }
+
+    private void setupUI() {
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+
+        SlideInRightAnimator animator = new SlideInRightAnimator(new OvershootInterpolator(1f));
+        animator.setAddDuration(300);
+        recyclerView.setItemAnimator(animator);
+        //recyclerView.getItemAnimator().setAddDuration(1000);
+
+        mAdapter = new UserAdapter(activity, userList, new UserAdapter.Callback() {
+            @Override
+            public void onClick(User user, int position) {
+                LToast.show(activity, "Click " + user.getMsg());
+            }
+        });
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        //recyclerView.setAdapter(mAdapter);
+
+        //AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(mAdapter);
+        //alphaAdapter.setDuration(1000);
+        //alphaAdapter.setInterpolator(new OvershootInterpolator());
+        //alphaAdapter.setFirstOnly(true);
+        //recyclerView.setAdapter(alphaAdapter);
+
+        ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(mAdapter);
+        scaleAdapter.setDuration(1000);
+        scaleAdapter.setInterpolator(new OvershootInterpolator());
+        scaleAdapter.setFirstOnly(true);
+        recyclerView.setAdapter(scaleAdapter);
+
+        LUIUtil.setPullLikeIOSVertical(recyclerView);
     }
 }
---
+++ app/src/main/java/vn/loitp/app/activity/demo/firebase/databasesimple/UserAdapter.java
@@ -0,0 +1,84 @@
+package vn.loitp.app.activity.demo.firebase.databasesimple;
+
+/**
+ * Created by www.muathu@gmail.com on 12/8/2017.
+ */
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import loitp.basemaster.R;
+import vn.loitp.core.utilities.LImageUtil;
+import vn.loitp.views.imageview.circleimageview.lib.LCircleImageView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    public interface Callback {
+        public void onClick(User user, int position);
+    }
+
+    private Context context;
+    private Callback callback;
+    private List<User> userList;
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvName, tvMsg, tvTime;
+        public LCircleImageView avt;
+        public LinearLayout rootView;
+
+        public UserViewHolder(View view) {
+            super(view);
+            tvName = (TextView) view.findViewById(R.id.tv_name);
+            tvTime = (TextView) view.findViewById(R.id.tv_time);
+            tvMsg = (TextView) view.findViewById(R.id.tv_msg);
+            avt = (LCircleImageView) view.findViewById(R.id.avt);
+            rootView = (LinearLayout) view.findViewById(R.id.root_view);
+        }
+    }
+
+    public UserAdapter(Context context, List<User> userList, Callback callback) {
+        this.context = context;
+        this.userList = userList;
+        this.callback = callback;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new UserViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        User user = userList.get(position);
+        holder.tvName.setText(user.getName());
+        holder.tvMsg.setText(user.getMsg());
+        holder.tvTime.setText(user.getTimestamp() + "");
+
+        LImageUtil.load(context, user.getAvt(), holder.avt);
+
+        holder.rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (callback != null) {
+                    callback.onClick(user, position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if (userList == null) {
+            return 0;
+        }
+        return userList.size();
+    }
+}
