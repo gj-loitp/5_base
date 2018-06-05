29a7f995d81ea05232252a6191f09663bb705941
Loitp <loitp@pateco.vn>
2018-06-05 16:33:29 +0700

    update firebase

--- app/src/main/java/vn/loitp/app/activity/demo/firebase/databasesimple/DatabaseSimpleFirebaseActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/firebase/databasesimple/DatabaseSimpleFirebaseActivity.java
@@ -1,18 +1,11 @@
 package vn.loitp.app.activity.demo.firebase.databasesimple;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,34 +14,38 @@ import com.google.firebase.database.ValueEventListener;
 
 import loitp.basemaster.R;
 import vn.loitp.app.activity.BaseFontActivity;
-import vn.loitp.app.activity.demo.firebase.database.models.User;
 import vn.loitp.core.utilities.LLog;
 
 //http://www.zoftino.com/firebase-realtime-database-android-example
-public class DatabaseSimpleFirebaseActivity extends BaseFontActivity {
-    DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements View.OnClickListener {
+    private final String ROOT_NODE = "loitp";
+    private FirebaseDatabase mFirebaseInstance;
+    private DatabaseReference mFirebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference();
 
-        findViewById(R.id.bt_add).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.bt_add).setOnClickListener(this);
+
+        mFirebaseDatabase.child(ROOT_NODE).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                dbRef.child("loitp").child(System.currentTimeMillis() + "").setValue("dummy msg")
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    LLog.d(TAG, "onClick isSuccessful");
-                                } else {
-                                    LLog.d(TAG, "onClick !isSuccessful");
-                                }
-                            }
-                        });
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot == null || dataSnapshot.getValue() == null) {
+                    LLog.d(TAG, "onDataChange null => return");
+                    return;
+                }
+                String appTitle = dataSnapshot.getValue().toString();
+                LLog.d(TAG, "onDataChange " + appTitle);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                LLog.e(TAG, "Failed to read app title value " + error.toException());
             }
         });
-
     }
 
     @Override
@@ -66,4 +63,26 @@ public class DatabaseSimpleFirebaseActivity extends BaseFontActivity {
         return R.layout.activity_menu_firebase_simple;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_add:
+                addData();
+                break;
+        }
+    }
+
+    private void addData() {
+        mFirebaseDatabase.child(ROOT_NODE).child(System.currentTimeMillis() + "").setValue("dummy msg")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        /*if (task.isSuccessful()) {
+                            LLog.d(TAG, "onClick isSuccessful");
+                        } else {
+                            LLog.d(TAG, "onClick !isSuccessful");
+                        }*/
+                    }
+                });
+    }
 }
