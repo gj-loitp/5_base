58d7e589d40b20dc638aab651b1c0670640dbfd5
Loitp <loitp@pateco.vn>
2018-06-07 13:50:56 +0700

    update runtime permission

--- app/src/main/java/vn/loitp/app/activity/demo/firebase/fcm/FirebaseMsgService.java
+++ app/src/main/java/vn/loitp/app/activity/demo/firebase/fcm/FirebaseMsgService.java
@@ -4,23 +4,22 @@ package vn.loitp.app.activity.demo.firebase.fcm;
  * Created by LENOVO on 6/7/2018.
  */
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
+import android.graphics.Color;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import loitp.basemaster.R;
 import vn.loitp.app.activity.SplashActivity;
+import vn.loitp.app.activity.function.notification.MenuNotificationActivity;
 import vn.loitp.core.utilities.LLog;
-import vn.loitp.core.utilities.LNotificationUtil;
+import vn.loitp.function.notification.Notti;
+import vn.loitp.function.notification.NottiFactory;
+import vn.loitp.function.notification.actions.ContentAction;
+import vn.loitp.function.notification.config.LightSettings;
+import vn.loitp.function.notification.config.NottiConf;
+import vn.loitp.function.notification.config.VibrationSettings;
 import vn.loitp.utils.util.AppUtils;
 
 public class FirebaseMsgService extends FirebaseMessagingService {
@@ -37,15 +36,18 @@ public class FirebaseMsgService extends FirebaseMessagingService {
 
             String appName = AppUtils.getAppName();
             String title = appName + " miss you!";
-            String bigTitle = appName + " miss you!";
             String messageBody = remoteMessage.getNotification().getBody();
-            int resLargeIcon = R.mipmap.ic_launcher;
-            int resSmallIcon = R.mipmap.ic_launcher;
-            LNotificationUtil.createNotification(this, SplashActivity.class, title, bigTitle, messageBody, resLargeIcon, resSmallIcon);
+
+            Notti notti = new Notti(this,
+                    new NottiConf(R.mipmap.ic_launcher,
+                            new VibrationSettings(VibrationSettings.STD_VIBRATION),
+                            new LightSettings(Color.RED)));
+            notti.show(NottiFactory
+                    .get(NottiFactory.TYPE.STANDARD, title, messageBody)
+                    .setContentAction(new ContentAction(new Intent(this, SplashActivity.class), this)));
         } catch (Exception e) {
             LLog.e(TAG, "onMessageReceived Exception " + e.toString());
         }
     }
-
 
 }
