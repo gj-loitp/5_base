6749bd202077ae5fe38c3ad67287a751c7f334f2
Loitp <loitp@pateco.vn>
2018-06-14 15:03:00 +0700

    update 1.31

--- app/src/main/java/vn/loitp/app/activity/customviews/imageview/bigimageview/BigImageViewActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/imageview/bigimageview/BigImageViewActivity.java
@@ -1,33 +1,16 @@
 package vn.loitp.app.activity.customviews.imageview.bigimageview;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 
-import com.github.piasy.biv.BigImageViewer;
 import com.github.piasy.biv.loader.ImageLoader;
-import com.github.piasy.biv.loader.glide.GlideImageLoader;
 import com.github.piasy.biv.view.BigImageView;
 
 import java.io.File;
 
 import loitp.basemaster.R;
-import vn.loitp.app.activity.customviews.imageview.blurimageview.BlurImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.circleimageview.CircleImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.circularroundrectimageview.CircularRoundRectImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.continuousscrollableimageview.ContinuousScrollableImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.fidgetspinnerimageview.FidgetSpinnerImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.img2ascii.Img2AsciiActivity;
-import vn.loitp.app.activity.customviews.imageview.panoramaimageview.PanoramaImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.scrollparallaximageview.ScrollParallaxImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.strectchyimageview.StrectchyImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.touchimageview.TouchImageViewActivity;
-import vn.loitp.app.activity.customviews.imageview.zoomimageview.ZoomImageViewActivity;
 import vn.loitp.app.common.Constants;
 import vn.loitp.core.base.BaseFontActivity;
-import vn.loitp.core.utilities.LActivityUtil;
 import vn.loitp.core.utilities.LLog;
 
 //https://github.com/Piasy/BigImageViewer
