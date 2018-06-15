dce59beca7ab3f99b70d88315377e9a82f3596ae
Loitp <loitp@pateco.vn>
2018-06-15 14:58:51 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/watchwhilelayout/fragments/WWLPlaylistFragment.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/watchwhilelayout/fragments/WWLPlaylistFragment.java
@@ -74,7 +74,7 @@ public class WWLPlaylistFragment extends BaseFragment {
 
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.wwl_playlist_item, parent, false);
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.wwl_music_playlist_item, parent, false);
             return new CustomAdapter.ViewHolder(v);
         }
 
