e3b954588787525ba81e1f23bf862b51c10c62bd
Loitp <loitp@pateco.vn>
2018-06-22 14:04:02 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/function/sensor/SensorActivity.java
+++ app/src/main/java/vn/loitp/app/activity/function/sensor/SensorActivity.java
@@ -1,25 +1,36 @@
 package vn.loitp.app.activity.function.sensor;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import loitp.basemaster.R;
+import vn.loitp.app.common.Constants;
 import vn.loitp.core.base.BaseFontActivity;
+import vn.loitp.core.utilities.LImageUtil;
 import vn.loitp.core.utilities.LLog;
 import vn.loitp.core.utilities.LScreenUtil;
 
 public class SensorActivity extends BaseFontActivity {
     private RelativeLayout rl;
     private TextView tv;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LLog.d(TAG, "onCreate");
         rl = (RelativeLayout) findViewById(R.id.rl);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+
+        LImageUtil.load(activity, Constants.URL_IMG, iv);
+
         rl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,16 +48,53 @@ public class SensorActivity extends BaseFontActivity {
     private void toggleFullScreen() {
         if (isFullScreen) {
             //landscape -> portrait
-            rl.animate().rotation(0).start();
-            int w = LScreenUtil.getScreenWidth();
-            int h = w * 9 / 16;
-            setSizeRelativeLayout(w, h);
+            //rl.animate().rotation(0).start();
+
+            // get screen size from DisplayMetrics if you need to rotate before the screen is shown
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+            int height = displayMetrics.heightPixels;
+            int width = displayMetrics.widthPixels;
+
+            // if you are rotating after the view was shown
+            // acquire width and height from the layout's parent's LayoutParams
+
+            // calculate offset to move the view into correct position
+            int offset = (width - height) / 2;
+
+            // rotate the layout
+            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(height, width);
+            getRootView().setLayoutParams(lp);
+            // 90° clockwise
+            getRootView().setRotation(0);
+            getRootView().setTranslationX(offset);
+            getRootView().setTranslationY(-offset);
+
         } else {
             //portrait -> landscape
-            rl.animate().rotation(90).start();
-            int w = LScreenUtil.getScreenWidth();
-            int h = LScreenUtil.getScreenHeightIncludeNavigationBar(activity);
-            setSizeRelativeLayout(w, h);
+            //rl.animate().rotation(90).start();
+
+            // get screen size from DisplayMetrics if you need to rotate before the screen is shown
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+            int height = displayMetrics.heightPixels;
+            int width = displayMetrics.widthPixels;
+
+            // if you are rotating after the view was shown
+            // acquire width and height from the layout's parent's LayoutParams
+
+            // calculate offset to move the view into correct position
+            int offset = (width - height) / 2;
+
+            // rotate the layout
+            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(height, width);
+            getRootView().setLayoutParams(lp);
+            // 90° clockwise
+            getRootView().setRotation(90.0f);
+            getRootView().setTranslationX(offset);
+            getRootView().setTranslationY(-offset);
         }
         isFullScreen = !isFullScreen;
     }
