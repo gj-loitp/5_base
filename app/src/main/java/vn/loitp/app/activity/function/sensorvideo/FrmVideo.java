cbba87e252500ab86b9c38ee387a145c921f0eaf
Loitp <loitp@pateco.vn>
2018-06-22 16:55:00 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/function/sensorvideo/FrmVideo.java
+++ app/src/main/java/vn/loitp/app/activity/function/sensorvideo/FrmVideo.java
@@ -1,5 +1,12 @@
 package vn.loitp.app.activity.function.sensorvideo;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.google.android.exoplayer2.ui.PlayerView;
+
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseFragment;
 
@@ -9,9 +16,36 @@ import vn.loitp.core.base.BaseFragment;
 
 public class FrmVideo extends BaseFragment {
     private final String TAG = getClass().getSimpleName();
+    private PlayerView playerView;
+    private PlayerManager player;
 
     @Override
     protected int setLayoutResourceId() {
         return R.layout.frm_sensor_video;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        playerView = frmRootView.findViewById(R.id.player_view);
+        player = new PlayerManager(getActivity());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        player.init(getActivity(), playerView);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        player.reset();
+    }
+
+    @Override
+    public void onDestroy() {
+        player.release();
+        super.onDestroy();
     }
 }
