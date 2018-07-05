aa5dcdc0f7af425dca0251db0fc03fdb8d3c3e01
Loitp <loitp@pateco.vn>
2018-07-05 15:54:13 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/button/autosizebutton/AutoSizeButtonActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/button/autosizebutton/AutoSizeButtonActivity.java
@@ -1,6 +1,5 @@
 package vn.loitp.app.activity.customviews.button.autosizebutton;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,23 +8,38 @@ import loitp.basemaster.R;
 import vn.loitp.core.base.BaseFontActivity;
 import vn.loitp.core.utilities.LActivityUtil;
 import vn.loitp.core.utilities.LLog;
+import vn.loitp.core.utilities.LScreenUtil;
 import vn.loitp.views.LToast;
 import vn.loitp.views.autosize.imagebuttonwithsize.ImageButtonWithSize;
 
 public class AutoSizeButtonActivity extends BaseFontActivity implements OnClickListener {
-    private ImageButtonWithSize bt0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         LLog.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         findViewById(R.id.bt_rotate).setOnClickListener(this);
-        bt0 = (ImageButtonWithSize) findViewById(R.id.bt_0);
+
+        ImageButtonWithSize bt0 = (ImageButtonWithSize) findViewById(R.id.bt_0);
         bt0.setPortraitSizeWInDp(50);
         bt0.setPortraitSizeHInDp(50);
-        bt0.setLandscapeSizeWInDp(150);
-        bt0.setLandscapeSizeHInDp(150);
+        bt0.setLandscapeSizeWInDp(250);
+        bt0.setLandscapeSizeHInDp(250);
         bt0.setOnClickListener(this);
+
+        ImageButtonWithSize bt1 = (ImageButtonWithSize) findViewById(R.id.bt_1);
+        bt1.setPortraitSizeWInDp(150);
+        bt1.setPortraitSizeHInDp(150);
+        bt1.setLandscapeSizeWInDp(100);
+        bt1.setLandscapeSizeHInDp(100);
+        bt1.setOnClickListener(this);
+
+        ImageButtonWithSize bt2 = (ImageButtonWithSize) findViewById(R.id.bt_2);
+        bt2.setPortraitSizeWInPx(LScreenUtil.getScreenWidth());
+        bt2.setPortraitSizeHInPx(LScreenUtil.getScreenWidth() / 10);
+        bt2.setLandscapeSizeWInPx(LScreenUtil.getScreenWidth() / 2);
+        bt2.setLandscapeSizeHInPx(LScreenUtil.getScreenWidth() / 2);
+        bt2.setOnClickListener(this);
     }
 
     @Override
