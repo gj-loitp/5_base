84955dcc167b82223d0fe8f1ecc8f95ced79687d
Loitp <loitp@pateco.vn>
2018-07-06 12:06:40 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/ldebugview/LDebugViewActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/ldebugview/LDebugViewActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-import com.google.firebase.auth.UserInfo;
-
 import loitp.basemaster.R;
 import vn.loitp.app.common.Constants;
 import vn.loitp.core.base.BaseFontActivity;
