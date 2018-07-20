1a7f536b9c87a86877aa58dc09979bfe141bae52
Loitp <loitp@pateco.vn>
2018-07-20 11:22:24 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/demo/film/FilmDemoActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/film/FilmDemoActivity.java
@@ -1,6 +1,7 @@
 package vn.loitp.app.activity.demo.film;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import loitp.basemaster.R;
@@ -8,14 +9,14 @@ import vn.loitp.app.activity.demo.film.group0.FrmGroup0;
 import vn.loitp.core.base.BaseFontActivity;
 import vn.loitp.utils.util.FragmentUtils;
 
-public class FilmDemoActivity extends BaseFontActivity {
+public class FilmDemoActivity extends BaseFontActivity implements View.OnClickListener {
     private LinearLayout llBaseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         llBaseView = (LinearLayout) findViewById(R.id.ll_base_view);
-        FragmentUtils.addFragment(getSupportFragmentManager(), new FrmGroup0(), llBaseView.getId(), false);
+        findViewById(R.id.bt_add).setOnClickListener(this);
     }
 
     @Override
@@ -31,5 +32,18 @@ public class FilmDemoActivity extends BaseFontActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.activity_film_demo;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_add:
+                add();
+                break;
+        }
+    }
+
+    private void add() {
+        FragmentUtils.addFragment(getSupportFragmentManager(), new FrmGroup0(), llBaseView.getId(), false);
     }
 }
