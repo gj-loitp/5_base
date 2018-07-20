fa73c986131bf21d728371f9bc3fd76e8823c65c
Loitp <loitp@pateco.vn>
2018-07-20 11:38:59 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/demo/film/FilmDemoActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/film/FilmDemoActivity.java
@@ -1,21 +1,36 @@
 package vn.loitp.app.activity.demo.film;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import loitp.basemaster.R;
 import vn.loitp.app.activity.demo.film.group0.FrmGroup0;
 import vn.loitp.core.base.BaseFontActivity;
+import vn.loitp.core.utilities.LUIUtil;
 import vn.loitp.utils.util.FragmentUtils;
+import vn.loitp.views.LToast;
 
 public class FilmDemoActivity extends BaseFontActivity implements View.OnClickListener {
+    private SwipeRefreshLayout swipeRefreshLayout;
     private LinearLayout llBaseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         llBaseView = (LinearLayout) findViewById(R.id.ll_base_view);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
+        LUIUtil.setColorForSwipeRefreshLayout(swipeRefreshLayout);
+
+        findViewById(R.id.bt_clear_all).setOnClickListener(this);
         findViewById(R.id.bt_add_group_0).setOnClickListener(this);
         findViewById(R.id.bt_remove_group_0).setOnClickListener(this);
     }
@@ -38,6 +53,9 @@ public class FilmDemoActivity extends BaseFontActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.bt_clear_all:
+                clearAllFrm();
+                break;
             case R.id.bt_add_group_0:
                 add();
                 break;
@@ -46,7 +64,26 @@ public class FilmDemoActivity extends BaseFontActivity implements View.OnClickLi
         }
     }
 
+    private void refresh() {
+        LUIUtil.setDelay(3000, new LUIUtil.DelayCallback() {
+            @Override
+            public void doAfter(int mls) {
+                swipeRefreshLayout.setRefreshing(false);
+                LToast.show(activity, "Finish refresh -> clear all views");
+                clearAllFrm();
+            }
+        });
+    }
+
+    private void clearAllFrm() {
+        FragmentUtils.removeAllFragments(getSupportFragmentManager());
+    }
+
     private void add() {
         FragmentUtils.addFragment(getSupportFragmentManager(), new FrmGroup0(), llBaseView.getId(), false);
+    }
+
+    private void remove() {
+        FragmentUtils.removeFragment();
     }
 }
