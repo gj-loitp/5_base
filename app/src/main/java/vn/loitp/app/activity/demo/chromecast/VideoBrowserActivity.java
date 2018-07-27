ffba00cb226be47798af5244c7b0fa8c41f528c7
Loitp <loitp@pateco.vn>
2018-07-27 11:39:03 +0700

    update chromecast

--- app/src/main/java/vn/loitp/app/activity/demo/chromecast/VideoBrowserActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/chromecast/VideoBrowserActivity.java
@@ -1,26 +1,9 @@
-/*
- * Copyright (C) 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package vn.loitp.app.activity.demo.chromecast;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -37,16 +20,11 @@ import com.google.android.gms.cast.framework.SessionManagerListener;
 import loitp.basemaster.R;
 import vn.loitp.app.activity.demo.chromecast.queue.ui.QueueListViewActivity;
 import vn.loitp.app.activity.demo.chromecast.settings.CastPreference;
+import vn.loitp.core.base.BaseFontActivity;
 
-/**
- * The main activity that displays the list of videos.
- */
-public class VideoBrowserActivity extends AppCompatActivity {
-
-    private static final String TAG = "VideoBrowserActivity";
+public class VideoBrowserActivity extends BaseFontActivity {
     private CastContext mCastContext;
-    private final SessionManagerListener<CastSession> mSessionManagerListener =
-            new MySessionManagerListener();
+    private final SessionManagerListener<CastSession> mSessionManagerListener = new MySessionManagerListener();
     private CastSession mCastSession;
     private MenuItem mediaRouteMenuItem;
     private MenuItem mQueueMenuItem;
@@ -55,7 +33,6 @@ public class VideoBrowserActivity extends AppCompatActivity {
     private CastStateListener mCastStateListener;
 
     private class MySessionManagerListener implements SessionManagerListener<CastSession> {
-
         @Override
         public void onSessionEnded(CastSession session, int error) {
             if (session == mCastSession) {
@@ -101,17 +78,25 @@ public class VideoBrowserActivity extends AppCompatActivity {
         }
     }
 
-    /*
-     * (non-Javadoc)
-     * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
-     */
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_gg_chrome_cast_video_browser;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.video_browser);
         setupActionBar();
-
         mCastStateListener = new CastStateListener() {
             @Override
             public void onCastStateChanged(int newState) {
@@ -131,9 +116,8 @@ public class VideoBrowserActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.browse, menu);
-        mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
-                R.id.media_route_menu_item);
+        getMenuInflater().inflate(R.menu.menu_gg_chromecast_video_browser, menu);
+        mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu, R.id.media_route_menu_item);
         mQueueMenuItem = menu.findItem(R.id.action_show_queue);
         showIntroductoryOverlay();
         return true;
@@ -141,8 +125,7 @@ public class VideoBrowserActivity extends AppCompatActivity {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.findItem(R.id.action_show_queue).setVisible(
-                (mCastSession != null) && mCastSession.isConnected());
+        menu.findItem(R.id.action_show_queue).setVisible((mCastSession != null) && mCastSession.isConnected());
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -150,10 +133,10 @@ public class VideoBrowserActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
         if (item.getItemId() == R.id.action_settings) {
-            intent = new Intent(VideoBrowserActivity.this, CastPreference.class);
+            intent = new Intent(activity, CastPreference.class);
             startActivity(intent);
         } else if (item.getItemId() == R.id.action_show_queue) {
-            intent = new Intent(VideoBrowserActivity.this, QueueListViewActivity.class);
+            intent = new Intent(activity, QueueListViewActivity.class);
             startActivity(intent);
         }
         return true;
@@ -161,22 +144,18 @@ public class VideoBrowserActivity extends AppCompatActivity {
 
     @Override
     public boolean dispatchKeyEvent(@NonNull KeyEvent event) {
-        return mCastContext.onDispatchVolumeKeyEventBeforeJellyBean(event)
-                || super.dispatchKeyEvent(event);
+        return mCastContext.onDispatchVolumeKeyEventBeforeJellyBean(event) || super.dispatchKeyEvent(event);
     }
 
     @Override
     protected void onResume() {
         mCastContext.addCastStateListener(mCastStateListener);
-        mCastContext.getSessionManager().addSessionManagerListener(
-                mSessionManagerListener, CastSession.class);
+        mCastContext.getSessionManager().addSessionManagerListener(mSessionManagerListener, CastSession.class);
         if (mCastSession == null) {
-            mCastSession = CastContext.getSharedInstance(this).getSessionManager()
-                    .getCurrentCastSession();
+            mCastSession = CastContext.getSharedInstance(this).getSessionManager().getCurrentCastSession();
         }
         if (mQueueMenuItem != null) {
-            mQueueMenuItem.setVisible(
-                    (mCastSession != null) && mCastSession.isConnected());
+            mQueueMenuItem.setVisible((mCastSession != null) && mCastSession.isConnected());
         }
         super.onResume();
     }
@@ -184,8 +163,7 @@ public class VideoBrowserActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         mCastContext.removeCastStateListener(mCastStateListener);
-        mCastContext.getSessionManager().removeSessionManagerListener(
-                mSessionManagerListener, CastSession.class);
+        mCastContext.getSessionManager().removeSessionManagerListener(mSessionManagerListener, CastSession.class);
         super.onPause();
     }
 
@@ -198,7 +176,7 @@ public class VideoBrowserActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     mIntroductoryOverlay = new IntroductoryOverlay.Builder(
-                            VideoBrowserActivity.this, mediaRouteMenuItem)
+                            activity, mediaRouteMenuItem)
                             .setTitleText(getString(R.string.introducing_cast))
                             .setOverlayColor(R.color.primary)
                             .setSingleTime()
