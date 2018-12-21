7789a9a2bd32012032b7e59de53e4f058b9910f6
Loitp <loitp@pateco.vn>
2018-12-21 14:10:27 +0700

    update uiza

--- core/src/main/java/vn/loitp/core/loitp/facebookcomment/FrmFBComment.java
+++ core/src/main/java/vn/loitp/core/loitp/facebookcomment/FrmFBComment.java
@@ -38,12 +38,15 @@ public class FrmFBComment extends BaseFragment {
     private String postUrl;
     // the default number of comments should be visible
     // on page load.
-    private static final int NUMBER_OF_COMMENTS = 50;
+    private static final int NUMBER_OF_COMMENTS = 80;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mWebViewComments = (WebView) view.findViewById(R.id.commentsView);
+        //mWebViewComments.setBackgroundColor(Color.TRANSPARENT);
+        //mWebViewComments.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.WhiteTrans99));
+        //mWebViewComments.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
         mContainer = (RelativeLayout) view.findViewById(R.id.webview_frame);
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         LUIUtil.setColorProgressBar(progressBar, ContextCompat.getColor(getActivity(), R.color.colorPrimary));
