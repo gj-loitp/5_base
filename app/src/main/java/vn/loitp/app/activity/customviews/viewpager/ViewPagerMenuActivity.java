bb85a3ae2c2c16ab1996486d30aa13bc12cdfcdc
Loitp <loitp@pateco.vn>
2019-02-01 11:57:43 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/viewpager/ViewPagerMenuActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/viewpager/ViewPagerMenuActivity.java
@@ -8,7 +8,6 @@ import loitp.basemaster.R;
 import vn.loitp.app.activity.customviews.viewpager.autoviewpager.AutoViewPagerActivity;
 import vn.loitp.app.activity.customviews.viewpager.detectviewpagerswipeout.ex.DetectViewPagerSwipeOutActivity;
 import vn.loitp.app.activity.customviews.viewpager.detectviewpagerswipeout2.ViewPagerSwipeOut2Activity;
-import vn.loitp.app.activity.customviews.viewpager.doubleviewpager.DoubleViewPagerSplashActivity;
 import vn.loitp.app.activity.customviews.viewpager.parrallaxviewpager.ParallaxViewPagerActivity;
 import vn.loitp.app.activity.customviews.viewpager.viewpagerwithtablayout.ViewPagerWithTabLayoutActivity;
 import vn.loitp.core.base.BaseFontActivity;
@@ -22,7 +21,6 @@ public class ViewPagerMenuActivity extends BaseFontActivity implements View.OnCl
         findViewById(R.id.bt_auto_viewpager).setOnClickListener(this);
         findViewById(R.id.bt_parallax_viewpager).setOnClickListener(this);
         findViewById(R.id.bt_detect_viewpager_swipe_out).setOnClickListener(this);
-        findViewById(R.id.bt_double_viewpager).setOnClickListener(this);
         findViewById(R.id.bt_view_pager_tablayout).setOnClickListener(this);
         findViewById(R.id.bt_detect_viewpager_swipe_out_2).setOnClickListener(this);
     }
@@ -54,9 +52,6 @@ public class ViewPagerMenuActivity extends BaseFontActivity implements View.OnCl
                 break;
             case R.id.bt_detect_viewpager_swipe_out:
                 intent = new Intent(activity, DetectViewPagerSwipeOutActivity.class);
-                break;
-            case R.id.bt_double_viewpager:
-                intent = new Intent(activity, DoubleViewPagerSplashActivity.class);
                 break;
             case R.id.bt_view_pager_tablayout:
                 intent = new Intent(activity, ViewPagerWithTabLayoutActivity.class);
