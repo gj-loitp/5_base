1f256747dbfdf22b8e2d4146ca797d324f5b9023
Loitp <loitp@pateco.vn>
2019-02-23 00:01:21 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/function/viewdraghelpersimple/ViewDragHelperSimpleActivity1.java
+++ app/src/main/java/vn/loitp/app/activity/function/viewdraghelpersimple/ViewDragHelperSimpleActivity1.java
@@ -140,7 +140,7 @@ public class ViewDragHelperSimpleActivity1 extends BaseFontActivity implements V
 
     @Override
     public void onOverScroll(VDHView.State state, VDHView.Part part) {
-        
+
     }
 
     @Override
