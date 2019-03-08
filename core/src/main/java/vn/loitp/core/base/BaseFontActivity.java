b99810037b9bcb580bc008426d462ec5f8b6d45c
Loitp <loitp@pateco.vn>
2019-03-08 15:54:04 +0700

    update

--- core/src/main/java/vn/loitp/core/base/BaseFontActivity.java
+++ core/src/main/java/vn/loitp/core/base/BaseFontActivity.java
@@ -9,20 +9,6 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
  */
 
 public abstract class BaseFontActivity extends BaseActivity {
-    /*@Override
-    protected boolean setFullScreen() {
-        return false;
-    }
-
-    @Override
-    protected String setTag() {
-        return null;
-    }
-
-    @Override
-    protected int setLayoutResourceId() {
-        return 0;
-    }*/
 
     @Override
     protected void attachBaseContext(Context newBase) {
