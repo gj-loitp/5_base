90d33db8ec82e013826fbac1d3ad1b7aaf370084
Loitp <loitp@pateco.vn>
2019-03-19 16:13:24 +0700

    update

---
+++ app/src/main/java/vn/loitp/app/activity/demo/butterknife/ButterKnifeActivity.java
@@ -0,0 +1,44 @@
+package vn.loitp.app.activity.demo.butterknife;
+
+import android.os.Bundle;
+
+import loitp.basemaster.R;
+import vn.loitp.core.base.BaseFontActivity;
+
+public class ButterKnifeActivity extends BaseFontActivity {
+    /*@BindView(R.id.bt)
+    Button bt;
+    @BindView(R.id.tv)
+    TextView tv;
+
+    @BindString(R.string.large_text)
+    String largeText;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*ButterKnife.bind(this);
+        tv.setText(largeText);
+        bt.setText("Button");*/
+    }
+
+    @Override
+    protected boolean setFullScreen() {
+        return false;
+    }
+
+    @Override
+    protected String setTag() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.activity_butter_knife;
+    }
+
+    /*@OnClick(R.id.bt_click)
+    void click() {
+        LToast.showShort(activity, "Click", R.drawable.bkg_horizontal);
+    }*/
+}
