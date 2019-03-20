04699ca6780b4b72882747ba5054e86e124d786a
Loitp <loitp@pateco.vn>
2019-03-20 14:19:47 +0700

    add butter knife

--- app/src/main/java/vn/loitp/app/activity/demo/butterknife/ButterKnifeActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/butterknife/ButterKnifeActivity.java
@@ -1,25 +1,32 @@
 package vn.loitp.app.activity.demo.butterknife;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseFontActivity;
+import vn.loitp.views.LToast;
 
 public class ButterKnifeActivity extends BaseFontActivity {
-    /*@BindView(R.id.bt)
+    @BindView(R.id.bt)
     Button bt;
     @BindView(R.id.tv)
     TextView tv;
 
     @BindString(R.string.large_text)
-    String largeText;*/
+    String largeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*ButterKnife.bind(this);
+        ButterKnife.bind(this);
         tv.setText(largeText);
-        bt.setText("Button");*/
+        bt.setText("Button");
     }
 
     @Override
@@ -37,8 +44,8 @@ public class ButterKnifeActivity extends BaseFontActivity {
         return R.layout.activity_butter_knife;
     }
 
-    /*@OnClick(R.id.bt_click)
+    @OnClick(R.id.bt_click)
     void click() {
         LToast.showShort(activity, "Click", R.drawable.bkg_horizontal);
-    }*/
+    }
 }
