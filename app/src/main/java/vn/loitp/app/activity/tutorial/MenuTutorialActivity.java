fea24286da10c2e88a1fd0b6df9db93fd96f1bdf
tplloi <www.muathu@gmail.com>
2019-05-13 16:17:57 +0700

    update retrofit2

--- app/src/main/java/vn/loitp/app/activity/tutorial/MenuTutorialActivity.java
+++ app/src/main/java/vn/loitp/app/activity/tutorial/MenuTutorialActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import loitp.basemaster.R;
+import vn.loitp.app.activity.tutorial.retrofit2.Retrofit2Activity;
 import vn.loitp.app.activity.tutorial.rxjava2.MenuRxJava2Activity;
 import vn.loitp.core.base.BaseFontActivity;
 import vn.loitp.core.utilities.LActivityUtil;
@@ -15,6 +16,7 @@ public class MenuTutorialActivity extends BaseFontActivity implements View.OnCli
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         findViewById(R.id.bt_rx_java_2).setOnClickListener(this);
+        findViewById(R.id.bt_retrofit_2).setOnClickListener(this);
     }
 
     @Override
@@ -38,6 +40,9 @@ public class MenuTutorialActivity extends BaseFontActivity implements View.OnCli
         switch (v.getId()) {
             case R.id.bt_rx_java_2:
                 intent = new Intent(activity, MenuRxJava2Activity.class);
+                break;
+            case R.id.bt_retrofit_2:
+                intent = new Intent(activity, Retrofit2Activity.class);
                 break;
         }
         if (intent != null) {
---
+++ app/src/main/java/vn/loitp/app/activity/tutorial/retrofit2/Retrofit2Activity.kt
@@ -0,0 +1,78 @@
+package vn.loitp.app.activity.tutorial.retrofit2
+
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_retrofit_2.*
+import loitp.basemaster.R
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import vn.loitp.core.base.BaseFontActivity
+import vn.loitp.views.LToast
+
+//https://code.tutsplus.com/tutorials/connect-to-an-api-with-retrofit-rxjava-2-and-kotlin--cms-32133
+class Retrofit2Activity : BaseFontActivity(), Retrofit2Adapter.Listener {
+    private var retrofit2Adapter: Retrofit2Adapter? = null
+    private var compositeDisposable: CompositeDisposable? = null
+    private var retroCryptoArrayList: ArrayList<RetroCrypto>? = null
+    private val BASE_URL = "https://api.nomics.com/v1/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        compositeDisposable = CompositeDisposable()
+        initRecyclerView()
+        loadData()
+    }
+
+    private fun initRecyclerView() {
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        cryptocurrency_list.layoutManager = layoutManager
+    }
+
+    private fun loadData() {
+        val requestInterface = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build().create(Retrofit2GetData::class.java)
+
+        compositeDisposable?.add(requestInterface.getData()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleResponse))
+    }
+
+
+    private fun handleResponse(cryptoList: List<RetroCrypto>) {
+        retroCryptoArrayList = ArrayList(cryptoList)
+        retrofit2Adapter = Retrofit2Adapter(retroCryptoArrayList!!, this)
+        cryptocurrency_list.adapter = retrofit2Adapter
+    }
+
+
+    override fun onItemClick(retroCrypto: RetroCrypto) {
+        LToast.show(activity, "You clicked: ${retroCrypto.currency}")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_retrofit_2
+    }
+
+}
