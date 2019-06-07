b4c5d105dbddb52ddfe51ff864e5256985651f90
tplloi <tloi@yahoo-corp.jp>
2019-06-07 14:47:27 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/ads/MenuAdsActivity.java
+++ app/src/main/java/vn/loitp/app/activity/ads/MenuAdsActivity.java
@@ -2,7 +2,6 @@ package vn.loitp.app.activity.ads;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import loitp.basemaster.R;
 import vn.loitp.app.activity.ads.admobbanner.AdmobBannerActivity;
@@ -17,31 +16,22 @@ public class MenuAdsActivity extends BaseFontActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         isShowAdWhenExit = false;
-        findViewById(R.id.bt_1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, AdmobBannerActivity.class);
-                startActivity(intent);
-                LActivityUtil.INSTANCE.tranIn(activity);
-            }
+        findViewById(R.id.bt_1).setOnClickListener(v -> {
+            final Intent intent = new Intent(activity, AdmobBannerActivity.class);
+            startActivity(intent);
+            LActivityUtil.tranIn(activity);
         });
-        findViewById(R.id.bt_2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, AdmobInterstitialActivity.class);
-                startActivity(intent);
-                LActivityUtil.INSTANCE.tranIn(activity);
-            }
+        findViewById(R.id.bt_2).setOnClickListener(v -> {
+            final Intent intent = new Intent(activity, AdmobInterstitialActivity.class);
+            startActivity(intent);
+            LActivityUtil.tranIn(activity);
         });
-        findViewById(R.id.bt_3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, AdmobRewardedVideoActivity.class);
-                intent.putExtra(AdmobRewardedVideoActivity.APP_ID, getString(R.string.str_app_id));
-                intent.putExtra(AdmobRewardedVideoActivity.ID_REWARD, getString(R.string.str_reward));
-                startActivity(intent);
-                LActivityUtil.INSTANCE.tranIn(activity);
-            }
+        findViewById(R.id.bt_3).setOnClickListener(v -> {
+            final Intent intent = new Intent(activity, AdmobRewardedVideoActivity.class);
+            intent.putExtra(AdmobRewardedVideoActivity.APP_ID, getString(R.string.str_app_id));
+            intent.putExtra(AdmobRewardedVideoActivity.ID_REWARD, getString(R.string.str_reward));
+            startActivity(intent);
+            LActivityUtil.tranIn(activity);
         });
     }
 
