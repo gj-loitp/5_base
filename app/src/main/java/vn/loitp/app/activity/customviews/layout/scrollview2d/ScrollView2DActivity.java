ff2229d7e2bfb385a0ea5c6b4916c0bc34ccce52
Loitp <loitp@pateco.vn>
2019-06-12 23:13:02 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/scrollview2d/ScrollView2DActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/scrollview2d/ScrollView2DActivity.java
@@ -1,19 +1,22 @@
 package vn.loitp.app.activity.customviews.layout.scrollview2d;
 
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Window;
 
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseFontActivity;
 import vn.loitp.core.utilities.LLog;
-import vn.loitp.views.layout.swipablelayout.SwipeableLayout;
+import vn.loitp.core.utilities.LUIUtil;
 
 public class ScrollView2DActivity extends BaseFontActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final TwoDScrollView twoDScrollView = findViewById(R.id.sv);
+        twoDScrollView.setScrollChangeListner((view, x, y, oldx, oldy) -> {
+            LLog.d(TAG, "setScrollChangeListner " + x + " - " + y);
+        });
+        LUIUtil.setDelay(2000, mls -> twoDScrollView.smoothScrollTo(300, 300));
     }
 
     @Override
