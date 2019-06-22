60b0a02da39a5762b9eaadd52e4079a5f1c13075
Loitp <loitp@pateco.vn>
2019-06-22 18:40:19 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/picker/crop/CropActivity.java
+++ app/src/main/java/vn/loitp/app/activity/picker/crop/CropActivity.java
@@ -1,9 +1,17 @@
 package vn.loitp.app.activity.picker.crop;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.yalantis.ucrop.util.FileUtils;
 
 import java.io.File;
@@ -12,15 +20,19 @@ import androidx.annotation.Nullable;
 import loitp.basemaster.R;
 import vn.loitp.core.base.BaseFontActivity;
 import vn.loitp.core.utilities.LActivityUtil;
+import vn.loitp.core.utilities.LImageUtil;
 import vn.loitp.core.utilities.LLog;
 import vn.loitp.picker.crop.CropImage;
 import vn.loitp.picker.crop.LGalleryActivity;
+import vn.loitp.views.LToast;
 
 public class CropActivity extends BaseFontActivity {
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        iv = findViewById(R.id.iv);
         findViewById(R.id.bt_crop).setOnClickListener(view -> {
             crop();
         });
@@ -42,10 +54,25 @@ public class CropActivity extends BaseFontActivity {
     }
 
     private void crop() {
-        //TODO need to check permission storage before
-        LLog.d(TAG, "crop");
-        startActivityForResult(new Intent(this, LGalleryActivity.class), LGalleryActivity.PICK_FROM_ALBUM);
-        LActivityUtil.tranIn(activity);
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        startActivityForResult(new Intent(activity, LGalleryActivity.class), LGalleryActivity.PICK_FROM_ALBUM);
+                        LActivityUtil.tranIn(activity);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        LToast.showShort(activity, "Error onPermissionDenied WRITE_EXTERNAL_STORAGE", R.drawable.bkg_horizontal);
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        LToast.showShort(activity, "Error onPermissionDenied WRITE_EXTERNAL_STORAGE", R.drawable.bkg_horizontal);
+                    }
+                }).check();
     }
 
     @Override
@@ -63,6 +90,7 @@ public class CropActivity extends BaseFontActivity {
                     }
                     final File file = new File(realPath);
                     LLog.d(TAG, "onActivityResult file " + file.getPath());
+                    LImageUtil.load(activity, file, iv);
                 }
                 break;
         }
