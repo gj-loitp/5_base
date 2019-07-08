6192ed4b5f8a91596e5faf5c1448a9ec55511740
v.loitp <v.Loitp@cloundhms.net>
2019-07-08 10:55:17 +0700

    update actionbar

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/actionbar/lactionbar/LActionbarActivity.kt
@@ -0,0 +1,49 @@
+package vn.loitp.app.activity.customviews.actionbar.lactionbar
+
+import android.os.Bundle
+import android.widget.TextView
+
+import loitp.basemaster.R
+import vn.loitp.core.base.BaseFontActivity
+import vn.loitp.core.utilities.LStoreUtil
+import vn.loitp.views.LToast
+import vn.loitp.views.actionbar.LActionBar
+
+class LActionbarActivity : BaseFontActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupActionBar()
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_action_bar
+    }
+
+    private fun setupActionBar() {
+        val lActionBar = findViewById<LActionBar>(R.id.l_action_bar)
+        val tv = findViewById<TextView>(R.id.tv)
+        tv.text = LStoreUtil.readTxtFromRawFolder(activity, R.raw.lactionbar)
+
+        lActionBar.setOnClickBack(object : LActionBar.Callback {
+            override fun onClickBack() {
+                onBackPressed()
+            }
+
+            override fun onClickMenu() {
+                LToast.show(activity, "onClickMenu")
+            }
+        })
+        lActionBar.showMenuIcon()
+        lActionBar.setImageMenuIcon(R.mipmap.ic_launcher)
+        lActionBar.setTvTitle("Demo LActionbarActivity")
+    }
+}
