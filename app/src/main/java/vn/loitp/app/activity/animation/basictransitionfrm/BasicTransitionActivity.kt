04f38ea90d4d5d6f3ace3fdda333f0cbf512c3fc
v.loitp <v.Loitp@cloundhms.net>
2019-07-11 17:55:46 +0700

    update kotlin

---
+++ app/src/main/java/vn/loitp/app/activity/animation/basictransitionfrm/BasicTransitionActivity.kt
@@ -0,0 +1,31 @@
+package vn.loitp.app.activity.animation.basictransitionfrm
+
+import android.os.Bundle
+import com.core.base.BaseFontActivity
+import loitp.basemaster.R
+
+//https://github.com/googlesamples/android-BasicTransition/#readme
+class BasicTransitionActivity : BaseFontActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            val fragment = BasicTransitionFragment()
+            transaction.replace(R.id.sample_content_fragment, fragment)
+            transaction.commit()
+        }
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_basic_transition
+    }
+}
