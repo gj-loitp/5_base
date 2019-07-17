ef07ef232ac3c242a7185b7550574b784707714c
v.loitp <v.Loitp@cloundhms.net>
2019-07-17 14:18:00 +0700

    update kotlin

--- app/src/main/java/vn/loitp/app/activity/customviews/button/roundedbutton/RoundedButtonActivity.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/button/roundedbutton/RoundedButtonActivity.java
@@ -1,7 +1,6 @@
 package vn.loitp.app.activity.customviews.button.roundedbutton;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.core.base.BaseFontActivity;
 import com.views.LToast;
@@ -13,30 +12,10 @@ public class RoundedButtonActivity extends BaseFontActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.bt_0).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LToast.INSTANCE.showShort(getActivity(), "Click");
-            }
-        });
-        findViewById(R.id.bt_1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LToast.INSTANCE.showShort(getActivity(), "Click");
-            }
-        });
-        findViewById(R.id.bt_2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LToast.INSTANCE.showShort(getActivity(), "Click");
-            }
-        });
-        findViewById(R.id.bt_3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LToast.INSTANCE.showShort(getActivity(), "Click");
-            }
-        });
+        findViewById(R.id.bt_0).setOnClickListener(view -> LToast.showShort(getActivity(), "Click"));
+        findViewById(R.id.bt_1).setOnClickListener(view -> LToast.showShort(getActivity(), "Click"));
+        findViewById(R.id.bt_2).setOnClickListener(view -> LToast.showShort(getActivity(), "Click"));
+        findViewById(R.id.bt_3).setOnClickListener(view -> LToast.showShort(getActivity(), "Click"));
     }
 
     @Override
