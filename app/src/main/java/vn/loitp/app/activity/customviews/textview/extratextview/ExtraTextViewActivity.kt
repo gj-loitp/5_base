75f3bc70629a402e0da8acf9f31abad8a3dfee4d
v.loitp <v.Loitp@cloundhms.net>
2019-07-18 16:08:23 +0700

    update extra textview

--- app/src/main/java/vn/loitp/app/activity/customviews/textview/extratextview/ExtraTextViewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/textview/extratextview/ExtraTextViewActivity.kt
@@ -1,9 +1,9 @@
 package vn.loitp.app.activity.customviews.textview.extratextview
 
 import android.os.Bundle
-
 import com.core.base.BaseFontActivity
-
+import com.views.textview.extratextview.ToggleExtraTextView
+import kotlinx.android.synthetic.main.activity_extra_textview.*
 import loitp.basemaster.R
 
 //https://github.com/chuross/extra-textview
@@ -11,6 +11,24 @@ class ExtraTextViewActivity : BaseFontActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val toggleText = findViewById<ToggleExtraTextView>(R.id.toggle_txt)
+        toggleText.setOnClickListener { toggleText.toggle() }
+
+        waitingBtn.setOnClickListener {
+            if (toggleText.state == ToggleExtraTextView.State.WAITING) {
+                toggleText.state = ToggleExtraTextView.State.IDLE
+            } else {
+                toggleText.state = ToggleExtraTextView.State.WAITING
+            }
+        }
+
+        activeBtn.setOnClickListener {
+            if (toggleText.state == ToggleExtraTextView.State.ACTIVE) {
+                toggleText.state = ToggleExtraTextView.State.IDLE
+            } else {
+                toggleText.state = ToggleExtraTextView.State.ACTIVE
+            }
+        }
     }
 
     override fun setFullScreen(): Boolean {
