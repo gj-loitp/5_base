c453f6973b910be028f14c72a25521343a3573ef
v.loitp <v.Loitp@cloundhms.net>
2019-07-19 16:31:03 +0700

    update pdf

--- app/src/main/java/vn/loitp/app/activity/demo/pdf/PdfDemoActivity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/pdf/PdfDemoActivity.java
@@ -103,7 +103,6 @@ public class PdfDemoActivity extends BaseFontActivity {
 //        pdfView.fromFile(file)
 //                .defaultPage(0)
 //                .enableAnnotationRendering(true)
-//                .scrollHandle(new DefaultScrollHandle(this))
 //                .load();
 
         //Option 2 vertical scroll
@@ -141,39 +140,39 @@ public class PdfDemoActivity extends BaseFontActivity {
 //                .load();
 
 
-            //Option 3 horizontal scroll
-            pdfView.fromFile(file)
-                    //.pages(0) // all pages are displayed by default
-                    .enableSwipe(true) // allows to block changing pages using swipe
-                    .swipeHorizontal(true)
-                    .enableDoubletap(true)
-                    .defaultPage(0)
-                    .onLoad(nbPages -> LLog.d(TAG, "loadComplete " + nbPages)) // called after document is loaded and starts to be rendered
-                    .onPageChange((page, pageCount) -> {
-                        LLog.d(TAG, "onPageChange " + page + "/" + pageCount);
-                    })
-                    .onPageScroll((page, positionOffset) -> LLog.d(TAG, "onPageScrolled " + page + " - " + positionOffset))
-                    .onError(t -> LLog.e(TAG, "onError " + t.toString()))
-                    .onPageError((page, t) -> LLog.e(TAG, "onPageError " + page + " -> " + t.toString()))
-                    .onRender(nbPages -> LLog.d(TAG, "onInitiallyRendered nbPages " + nbPages)) // called after document is rendered for the first time
-                    // called on single tap, return true if handled, false to toggle scroll handle visibility
-                    .onTap(e -> {
-                        LLog.d(TAG, "onTap");
-                        return false;
-                    })
-                    .onLongPress(e -> LLog.d(TAG, "OnLongPressListener"))
-                    .enableAnnotationRendering(true) // render annotations (such as comments, colors or forms)
-                    .password(null)
-                    .scrollHandle(new DefaultScrollHandle(this))
-                    .enableAntialiasing(true) // improve rendering a little bit on low-res screens
-                    // spacing between pages in dp. To define spacing color, set view background
-                    .spacing(0)
-                    .autoSpacing(true) // add dynamic spacing to fit each page on its own on the screen
-                    .pageFitPolicy(FitPolicy.WIDTH)
-                    .pageSnap(true) // snap pages to screen boundaries
-                    .pageFling(true) // make a fling change only a single page like ViewPager
-                    .nightMode(false) // toggle night mode
-                    .load();
+        //Option 3 horizontal scroll
+        pdfView.fromFile(file)
+                //.pages(0) // all pages are displayed by default
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .onLoad(nbPages -> LLog.d(TAG, "loadComplete " + nbPages)) // called after document is loaded and starts to be rendered
+                .onPageChange((page, pageCount) -> {
+                    LLog.d(TAG, "onPageChange " + page + "/" + pageCount);
+                })
+                .onPageScroll((page, positionOffset) -> LLog.d(TAG, "onPageScrolled " + page + " - " + positionOffset))
+                .onError(t -> LLog.e(TAG, "onError " + t.toString()))
+                .onPageError((page, t) -> LLog.e(TAG, "onPageError " + page + " -> " + t.toString()))
+                .onRender(nbPages -> LLog.d(TAG, "onInitiallyRendered nbPages " + nbPages)) // called after document is rendered for the first time
+                // called on single tap, return true if handled, false to toggle scroll handle visibility
+                .onTap(e -> {
+                    LLog.d(TAG, "onTap");
+                    return false;
+                })
+                .onLongPress(e -> LLog.d(TAG, "OnLongPressListener"))
+                .enableAnnotationRendering(true) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .autoSpacing(true) // add dynamic spacing to fit each page on its own on the screen
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .pageSnap(true) // snap pages to screen boundaries
+                .pageFling(true) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night mode
+                .load();
     }
 
     @Override
