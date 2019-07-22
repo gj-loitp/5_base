52ff3901bd073c4f5c8ed6ff87d3aea5c639275d
v.loitp <v.Loitp@cloundhms.net>
2019-07-22 17:32:43 +0700

    update lbutton

---
+++ core/src/main/java/com/views/button/lbutton/LButton.kt
@@ -0,0 +1,42 @@
+package com.views.button.lbutton
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+import androidx.appcompat.widget.AppCompatImageButton
+
+import loitp.core.R
+
+/**
+ * Created by Loitp on 7/5/2018.
+ */
+
+class LButton : AppCompatImageButton {
+
+    private var pressedDrawable = R.drawable.circle_effect
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    //constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {}
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val maskedAction = event.actionMasked
+        if (maskedAction == MotionEvent.ACTION_DOWN) {
+            this.setBackgroundResource(pressedDrawable)
+        } else if (maskedAction == MotionEvent.ACTION_UP) {
+            this.setBackgroundResource(0)
+        }
+        return super.onTouchEvent(event)
+    }
+
+    fun setPressedDrawable(pressedDrawable: Int) {
+        this.pressedDrawable = pressedDrawable
+    }
+}
