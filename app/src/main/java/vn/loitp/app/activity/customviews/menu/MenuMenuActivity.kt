07b96f78d9bbd6f8f8517e498948c54f01daf4b1
v.loitp <v.Loitp@cloundhms.net>
2019-07-23 09:53:14 +0700

    update kotlin

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/menu/MenuMenuActivity.kt
@@ -0,0 +1,39 @@
+package vn.loitp.app.activity.customviews.menu
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import com.core.base.BaseFontActivity
+import com.core.utilities.LActivityUtil
+import loitp.basemaster.R
+import vn.loitp.app.activity.customviews.menu.residemenu.ResideMenuActivity
+
+class MenuMenuActivity : BaseFontActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        findViewById<View>(R.id.bt_reside_menu).setOnClickListener(this)
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_menu_menu
+    }
+
+    override fun onClick(v: View) {
+        var intent: Intent? = null
+        when (v.id) {
+            R.id.bt_reside_menu -> intent = Intent(activity, ResideMenuActivity::class.java)
+        }
+        intent?.let {
+            startActivity(intent)
+            LActivityUtil.tranIn(activity)
+        }
+    }
+}
