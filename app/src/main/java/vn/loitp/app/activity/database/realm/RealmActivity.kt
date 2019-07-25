56107bea940892fbb9eead8581208fa46f75568e
v.loitp <v.Loitp@cloundhms.net>
2019-07-25 17:31:07 +0700

    update realm

---
+++ app/src/main/java/vn/loitp/app/activity/database/realm/RealmActivity.kt
@@ -0,0 +1,136 @@
+package vn.loitp.app.activity.database.realm
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.core.base.BaseFontActivity
+import com.core.utilities.LActivityUtil
+import com.core.utilities.LLog
+import com.views.OnSingleClickListener
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_realm.*
+import loitp.basemaster.R
+import vn.loitp.app.activity.demo.ebookwithrealm.EbookWithRealmActivity
+import vn.loitp.app.app.LApplication
+
+class RealmActivity : BaseFontActivity() {
+    private lateinit var mRealm: Realm
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mRealm = RealmController.with(application).realm
+
+        // refresh the realm instance
+        RealmController.with(application).refresh()
+
+        getAll()
+
+        btRealm.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View) {
+                val intent = Intent(activity, EbookWithRealmActivity::class.java)
+                startActivity(intent)
+                LActivityUtil.tranIn(activity)
+            }
+        })
+        btAdd.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View) {
+                addMyBook()
+            }
+        })
+        btClearAll.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View) {
+                clearUI()
+                RealmController.getInstance().clearAllMyBook()
+                getAll()
+            }
+        })
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_realm
+    }
+
+    private fun addMyBook() {
+        LLog.d(TAG, "add " + RealmController.getInstance().books.size)
+        mRealm.beginTransaction()
+
+        val myBook = MyBook()
+        myBook.id = RealmController.getInstance().books.size + System.currentTimeMillis()
+        myBook.title = "Title ${System.currentTimeMillis()}"
+        myBook.author = "Author ${System.currentTimeMillis()}"
+
+        mRealm.copyToRealm(myBook)
+        mRealm.commitTransaction()
+
+        addViewMyBook(myBook)
+    }
+
+    private fun logMyBook(myBook: MyBook) {
+        LLog.d(TAG, "> " + LApplication.gson.toJson(myBook))
+    }
+
+    private fun addViewMyBook(myBook: MyBook) {
+        val button = Button(activity)
+        button.text = "${myBook.title} - ${myBook.author}"
+        button.textSize = 10f
+        button.isAllCaps = false
+        button.setOnClickListener { updateMyBook(myBook, button) }
+        button.setOnLongClickListener {
+            removeMyBook(myBook, button)
+            true
+        }
+        ll.addView(button)
+    }
+
+    private fun clearUI() {
+        val viewList = ll.touchables
+        for (view in viewList) {
+            if (view is Button) {
+                ll.removeView(view)
+            }
+        }
+    }
+
+    private fun printUI(myBookList: List<MyBook>) {
+        for (mb in myBookList) {
+            addViewMyBook(mb)
+        }
+    }
+
+    private fun getAll() {
+        val myBookList = RealmController.getInstance().myBookListSortByID
+        printUI(myBookList)
+    }
+
+    private fun removeMyBook(myBook: MyBook, button: Button) {
+        LLog.d(TAG, "removeMyBook " + LApplication.gson.toJson(myBook))
+        mRealm.beginTransaction()
+        val mb = RealmController.getInstance().getBooks(myBook)
+        if (!mb.isEmpty()) {
+            for (i in mb.indices.reversed()) {
+                mb[i].removeFromRealm()
+            }
+        }
+        mRealm.commitTransaction()
+        ll.removeView(button)
+    }
+
+    private fun updateMyBook(myBook: MyBook, button: Button) {
+        LLog.d(TAG, "updateMyBook " + LApplication.gson.toJson(myBook))
+        mRealm.beginTransaction()
+        val mb = RealmController.getInstance().getMyBook(myBook.id)
+        mb.title = "Title ${System.currentTimeMillis()}"
+        mb.author = "Author ${System.currentTimeMillis()}"
+        mRealm.commitTransaction()
+        button.text = "${mb.title} - ${mb.author}"
+    }
+}
