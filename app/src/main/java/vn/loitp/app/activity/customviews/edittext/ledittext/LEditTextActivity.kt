0bc5b7de570b3bb5e690ed188ba1951db551ece8
v.loitp <v.Loitp@cloundhms.net>
2019-07-31 16:02:56 +0700

    update ledittext

--- app/src/main/java/vn/loitp/app/activity/customviews/edittext/ledittext/LEditTextActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/edittext/ledittext/LEditTextActivity.kt
@@ -8,10 +8,10 @@ import android.view.inputmethod.EditorInfo
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import com.core.base.BaseFontActivity
+import com.core.utilities.LKeyBoardUtil
 import com.core.utilities.LLog
 import com.views.edittext.leditext.LEditText
 import kotlinx.android.synthetic.main.activity_l_edit_text.*
-import loitp.basemaster.R
 
 class LEditTextActivity : BaseFontActivity() {
 
@@ -24,10 +24,11 @@ class LEditTextActivity : BaseFontActivity() {
             setCardBackgroundColor(Color.WHITE)
             setCardRadius(45f)
             setPaddingDp(5f)
-            colorFocus = ContextCompat.getColor(activity, R.color.colorPrimary)
-            colorUnfocus = ContextCompat.getColor(activity, R.color.Black)
-            ivLeft.setImageResource(R.mipmap.ic_launcher)
-            ivRight.setImageResource(R.drawable.remove)
+            colorFocus = ContextCompat.getColor(activity, loitp.basemaster.R.color.Black)
+            colorUnfocus = ContextCompat.getColor(activity, loitp.basemaster.R.color.Gray)
+            ivLeft.setImageResource(loitp.basemaster.R.mipmap.ic_launcher)
+            ivRight.setImageResource(loitp.basemaster.R.drawable.remove)
+            editText.hint = "Account"
             setMaxLines(1)
             setInputType(InputType.TYPE_CLASS_TEXT)
             setImeiActionEditText(EditorInfo.IME_ACTION_NEXT, Runnable {
@@ -55,17 +56,62 @@ class LEditTextActivity : BaseFontActivity() {
             }
         }
 
+        lEditTextPw.apply {
+            setStrokeWidth(5)
+            setCardElevation(15f)
+            setCardBackgroundColor(Color.WHITE)
+            setCardRadius(45f)
+            setPaddingDp(5f)
+            colorFocus = ContextCompat.getColor(activity, loitp.basemaster.R.color.Black)
+            colorUnfocus = ContextCompat.getColor(activity, loitp.basemaster.R.color.Gray)
+            ivLeft.setImageResource(loitp.basemaster.R.mipmap.ic_launcher)
+            ivRight.setImageResource(loitp.basemaster.R.drawable.search_color)
+            editText.hint = "Password"
+            setMaxLines(1)
+            setInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+            setImeiActionEditText(EditorInfo.IME_ACTION_DONE, Runnable {
+                LKeyBoardUtil.hide(activity)
+            })
+            callback = object : LEditText.Callback {
+                override fun onClickIvRight(imageView: ImageView) {
+
+                }
+
+                override fun onTextChanged(s: String) {
+                    LLog.d(TAG, "onTextChanged s: $s")
+                    if (s.isEmpty()) {
+                        ivRight.visibility = View.GONE
+                    } else {
+                        ivRight.visibility = View.VISIBLE
+                    }
+                    hideMessage()
+                }
+
+                override fun setOnFocusChangeListener(isFocus: Boolean) {
+                    LLog.d(TAG, "setOnFocusChangeListener isFocus: $isFocus")
+                }
+
+            }
+        }
+
         btLogin.setOnClickListener {
             val id = lEditTextId.editText.text.toString()
             val pw = lEditTextPw.editText.text.toString()
+            var isCorrectId = false;
+            var isCorrectPw = false;
+
             if (id == "loitp") {
-                if (pw == "123456789") {
-                    showShort("Correct!!!")
-                } else {
-                    lEditTextPw.showMessage("Wrong pw!!!")
-                }
+                isCorrectId = true;
             } else {
                 lEditTextId.showMessage("Wrong id!!!")
+            }
+            if (pw == "123456789") {
+                isCorrectPw = true;
+            } else {
+                lEditTextPw.showMessage("Wrong pw!!!")
+            }
+            if (isCorrectId && isCorrectPw) {
+                showShort("Correct!!!")
             }
         }
     }
@@ -79,6 +125,6 @@ class LEditTextActivity : BaseFontActivity() {
     }
 
     override fun setLayoutResourceId(): Int {
-        return R.layout.activity_l_edit_text
+        return loitp.basemaster.R.layout.activity_l_edit_text
     }
 }
