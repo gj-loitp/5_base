455271f5b8e3a22ac2ac7005832882b6af92a831
v.loitp <v.Loitp@cloudhms.net>
2019-08-24 09:24:14 +0700

    add progress indicator

---
+++ core/src/main/java/com/views/progressloadingview/circularprogressindicator/PatternProgressTextAdapter.java
@@ -0,0 +1,22 @@
+package com.views.progressloadingview.circularprogressindicator;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Created by Anton on 06.06.2018.
+ */
+
+public final class PatternProgressTextAdapter implements CircularProgressIndicator.ProgressTextAdapter {
+
+    private String pattern;
+
+    public PatternProgressTextAdapter(String pattern) {
+        this.pattern = pattern;
+    }
+
+    @NonNull
+    @Override
+    public String formatText(double currentProgress) {
+        return String.format(pattern, currentProgress);
+    }
+}
