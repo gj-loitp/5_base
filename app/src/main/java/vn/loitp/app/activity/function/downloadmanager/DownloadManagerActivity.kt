582e336eb01596eb6f83fcef49a6c84adf6f17ef
v.loitp <v.Loitp@cloudhms.net>
2019-09-26 15:11:31 +0700

    update downloader

--- app/src/main/java/vn/loitp/app/activity/function/downloadmanager/DownloadManagerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/function/downloadmanager/DownloadManagerActivity.kt
@@ -53,11 +53,11 @@ class DownloadManagerActivity : BaseFontActivity() {
         val path = LStoreUtil.getFolderPath(activity, "ZZZTestDownloader")
         LLog.d(TAG, "path $path")
         val map = HashMap<String, String>()
-        map.put("token", "abc")
-        downloader = Downloader.Builder(
-                this,
-                "https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall.jpg"
-        )
+        map.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIwOGQ3MWQ2Zi0xZTc0LTYwYjQtOWJmMC1mM2E0YzVkMTkwZGUiLCJyb2xlIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAyIiwianRpIjoiMzUxMmZhM2YtYWMzNi00YmM2LWI5ZTEtOTEyMzc5Y2NlZjQ1IiwiRGF0YVR5cGVzIjoiMiIsIm5iZiI6MTU2OTQ3OTc1OSwiZXhwIjoxNTY5NTY5NzU5LCJpYXQiOjE1Njk0Nzk3NTksImlzcyI6Imh0dHBzOi8vZGV2LXBvcnRhbC52aW5ob21lcy52biIsImF1ZCI6Imh0dHBzOi8vZGV2LXBvcnRhbC52aW5ob21lcy52biJ9.6IfkQkMhI0g-XAbKdHNSH5HiP8fsRAJxnpojjyqwFBI")
+
+        //val url = "https://vh-api.dev.cloudhms.io/app-api/files/08d730fa-0cbc-6e39-3c35-3bfdce8e43f8"
+        val url = "https://images.unsplash.com/photo-1499909762747-883b3641e787?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1336&q=80"
+        downloader = Downloader.Builder(this, url)
                 .downloadDirectory(path)
                 .fileName("abc", "jpg")
                 .header(map)
