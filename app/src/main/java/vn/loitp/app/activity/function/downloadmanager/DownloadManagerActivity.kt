888361b6d961193a70db350695049bdeef173ddf
v.loitp <v.Loitp@cloudhms.net>
2019-09-26 13:58:49 +0700

    add download manager

--- app/src/main/java/vn/loitp/app/activity/function/downloadmanager/DownloadManagerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/function/downloadmanager/DownloadManagerActivity.kt
@@ -4,12 +4,11 @@ import android.app.DownloadManager
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.view.View
 import com.core.base.BaseFontActivity
+import com.utils.util.AppUtils
 import com.views.OnSingleClickListener
 import kotlinx.android.synthetic.main.activity_download_manager.*
-import java.io.File
 
 class DownloadManagerActivity : BaseFontActivity() {
 
@@ -37,24 +36,20 @@ class DownloadManagerActivity : BaseFontActivity() {
     }
 
     private fun download() {
-//        val url = ""
-//        val token = ""
-//        val downloadManager = activity.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-//        val request = DownloadManager.Request(Uri.parse(url))
-//        request.addRequestHeader("Authorization", "Token $token")
-//        request.addRequestHeader("Accept-Language", "en-US,en;q=0.5")
-//        request.addRequestHeader("Content-Type", "application/json; charset=UTF-8")
-//        request.addRequestHeader("Accept-Encoding", "identity")
-//        request.addRequestHeader("User-Agent", "USER_AGENT")
-//
-//        //val direct = File(Environment.getExternalStorageDirectory() + "/Abeti")
-//        //if (!direct.exists()) {
-//        //    direct.mkdirs()
-//        //}
-//
-//        request.setDestinationInExternalPublicDir("/Abeti", filenamex)
-//        request.setTitle("titleDownloadManager" + System.currentTimeMillis())
-//
-//        downloadManager.enqueue(request)
+        val url = "https://images.unsplash.com/photo-1474690870753-1b92efa1f2d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        val token = ""
+        val downloadManager = activity.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val request = DownloadManager.Request(Uri.parse(url))
+        request.addRequestHeader("Authorization", "Token $token")
+        //request.addRequestHeader("Accept-Language", "en-US,en;q=0.5")
+        //request.addRequestHeader("Content-Type", "application/json; charset=UTF-8")
+        //request.addRequestHeader("Accept-Encoding", "identity")
+        //request.addRequestHeader("User-Agent", "USER_AGENT")
+
+        val fileName = "fileName.jpg"
+        request.setDestinationInExternalPublicDir("/." + AppUtils.getAppName(), fileName)
+        request.setTitle("titleDownloadManager" + System.currentTimeMillis())
+
+        downloadManager.enqueue(request)
     }
 }
