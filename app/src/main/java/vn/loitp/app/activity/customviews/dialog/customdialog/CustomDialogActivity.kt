d3b37bd9819105e3fcb38ab2054fcc53ba80e436
v.loitp <v.Loitp@cloudhms.net>
2019-10-10 12:25:04 +0700

    update custom dialog

--- app/src/main/java/vn/loitp/app/activity/customviews/dialog/customdialog/CustomDialogActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/dialog/customdialog/CustomDialogActivity.kt
@@ -2,7 +2,6 @@ package vn.loitp.app.activity.customviews.dialog.customdialog
 
 import android.os.Bundle
 import com.core.base.BaseFontActivity
-import com.core.utilities.LLog
 import com.core.utilities.LScreenUtil
 import kotlinx.android.synthetic.main.activity_custom_dialog.*
 import loitp.basemaster.R
@@ -33,8 +32,12 @@ class CustomDialogActivity : BaseFontActivity() {
         btShowDialog4.setOnClickListener { v ->
             val posX = v.right
             val posY = v.top
-            LLog.d(TAG, "posX $posX, posY $posY")
             PositionDialog().showImmersivePos(activity = activity, posX = posX, posY = posY, sizeWidthPx = null, sizeHeightPx = null, isAlignLeft = true, isAlignTop = true)
+        }
+        btShowDialog5.setOnClickListener { v ->
+            val posX = v.left
+            val posY = v.top
+            PositionDialog().showImmersivePos(activity = activity, posX = posX, posY = posY, sizeWidthPx = null, sizeHeightPx = null, isAlignLeft = false, isAlignTop = true)
         }
     }
 
