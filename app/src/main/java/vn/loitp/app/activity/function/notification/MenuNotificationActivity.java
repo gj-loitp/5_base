f717943bc8141e9ce339e1462755ec92ce4a76ee
v.loitp <v.Loitp@cloudhms.net>
2019-10-17 15:56:58 +0700

    update noti

--- app/src/main/java/vn/loitp/app/activity/function/notification/MenuNotificationActivity.java
+++ app/src/main/java/vn/loitp/app/activity/function/notification/MenuNotificationActivity.java
@@ -1,11 +1,19 @@
 package vn.loitp.app.activity.function.notification;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.core.app.NotificationCompat;
 
 import com.core.base.BaseFontActivity;
 import com.function.notification.Notti;
@@ -20,6 +28,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import loitp.basemaster.R;
+import vn.loitp.app.activity.SplashActivity;
 
 public class MenuNotificationActivity extends BaseFontActivity implements View.OnClickListener {
     private Notti notti;
@@ -36,6 +45,7 @@ public class MenuNotificationActivity extends BaseFontActivity implements View.O
         findViewById(R.id.bigTextNotification).setOnClickListener(this);
         findViewById(R.id.inboxNotification).setOnClickListener(this);
         findViewById(R.id.bigPictureNotification).setOnClickListener(this);
+        findViewById(R.id.btNotificationHeadsup).setOnClickListener(this);
     }
 
     @Override
@@ -91,6 +101,73 @@ public class MenuNotificationActivity extends BaseFontActivity implements View.O
                         .get(NottiFactory.TYPE.BIG_PICTURE, "some text", "some " + "content")
                         .setBigPicture(iconBig).setLargeIcon(icon));
                 break;
+            case R.id.btNotificationHeadsup:
+                createNotification("Loitpppppppppppppppppppppppppppp");
+                break;
         }
+    }
+
+    private NotificationManager notifManager;
+
+    public void createNotification(String aMessage) {
+        final int NOTIFY_ID = 1002;
+
+        // There are hardcoding only for show it's just strings
+        String name = "my_package_channel";
+        String id = "my_package_channel_1"; // The user-visible name of the channel.
+        String description = "my_package_first_channel"; // The user-visible description of the channel.
+
+        Intent intent;
+        PendingIntent pendingIntent;
+        NotificationCompat.Builder builder;
+
+        if (notifManager == null) {
+            notifManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = notifManager.getNotificationChannel(id);
+            if (mChannel == null) {
+                mChannel = new NotificationChannel(id, name, importance);
+                mChannel.setDescription(description);
+                mChannel.enableVibration(true);
+                mChannel.setLightColor(Color.GREEN);
+                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                notifManager.createNotificationChannel(mChannel);
+            }
+            builder = new NotificationCompat.Builder(this, id);
+
+            intent = new Intent(this, SplashActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            builder.setContentTitle(aMessage)  // required
+                    .setSmallIcon(android.R.drawable.ic_popup_reminder) // required
+                    .setContentText(this.getString(R.string.app_name))  // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker(aMessage)
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        } else {
+            builder = new NotificationCompat.Builder(this);
+            intent = new Intent(this, SplashActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            builder.setContentTitle(aMessage)                           // required
+                    .setSmallIcon(android.R.drawable.ic_popup_reminder) // required
+                    .setContentText(this.getString(R.string.app_name))  // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker(aMessage)
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
+                    .setPriority(Notification.PRIORITY_HIGH);
+        } // else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+        Notification notification = builder.build();
+        notifManager.notify(NOTIFY_ID, notification);
     }
 }
