77dbbda488a8ad477a986618bbc47a4e547dc697
v.loitp <v.Loitp@cloudhms.net>
2019-10-22 11:40:40 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/recyclerbanner/WebBannerAdapter.java
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/recyclerbanner/WebBannerAdapter.java
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.views.recyclerview.banner.BannerLayout;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 import loitp.basemaster.R;
@@ -23,35 +25,33 @@ public class WebBannerAdapter extends RecyclerView.Adapter<WebBannerAdapter.MzVi
     private List<String> urlList;
     private BannerLayout.OnBannerItemClickListener onBannerItemClickListener;
 
-    public WebBannerAdapter(Context context, List<String> urlList) {
+    WebBannerAdapter(Context context, List<String> urlList) {
         this.context = context;
         this.urlList = urlList;
     }
 
-    public void setOnBannerItemClickListener(BannerLayout.OnBannerItemClickListener onBannerItemClickListener) {
+    void setOnBannerItemClickListener(BannerLayout.OnBannerItemClickListener onBannerItemClickListener) {
         this.onBannerItemClickListener = onBannerItemClickListener;
     }
 
+    @NotNull
     @Override
     public WebBannerAdapter.MzViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return new MzViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(WebBannerAdapter.MzViewHolder holder, final int position) {
+    public void onBindViewHolder(@NotNull WebBannerAdapter.MzViewHolder holder, final int position) {
         if (urlList == null || urlList.isEmpty()) {
             return;
         }
         final int P = position % urlList.size();
         String url = urlList.get(P);
-        ImageView img = (ImageView) holder.imageView;
+        ImageView img = holder.imageView;
         Glide.with(context).load(url).into(img);
-        img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (onBannerItemClickListener != null) {
-                    onBannerItemClickListener.onItemClick(P);
-                }
+        img.setOnClickListener(v -> {
+            if (onBannerItemClickListener != null) {
+                onBannerItemClickListener.onItemClick(P);
             }
         });
     }
@@ -65,12 +65,12 @@ public class WebBannerAdapter extends RecyclerView.Adapter<WebBannerAdapter.MzVi
     }
 
 
-    public class MzViewHolder extends RecyclerView.ViewHolder {
+    class MzViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
 
         MzViewHolder(View itemView) {
             super(itemView);
-            imageView = (ImageView) itemView.findViewById(R.id.image);
+            imageView = itemView.findViewById(R.id.image);
         }
     }
 
