b27a9ffc28ef8ebae6ec52c5a5d5ce07a0be40e6
v.loitp <v.Loitp@cloudhms.net>
2019-10-24 11:51:13 +0700

    update ui

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/draggableview/DraggableViewActivity.kt
@@ -0,0 +1,53 @@
+package vn.loitp.app.activity.customviews.layout.draggableview
+
+import android.os.Bundle
+
+import com.core.base.BaseFontActivity
+import com.core.utilities.LLog
+import com.views.layout.draggablepanel.DraggableListener
+import com.views.layout.draggablepanel.DraggableView
+
+import loitp.basemaster.R
+
+class DraggableViewActivity : BaseFontActivity() {
+    private lateinit var draggableView: DraggableView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        draggableView = findViewById(R.id.draggable_view)
+
+        draggableView.isClickToMaximizeEnabled = true
+        draggableView.isClickToMinimizeEnabled = true
+        draggableView.setHorizontalAlphaEffectEnabled(true)
+
+        draggableView.setDraggableListener(object : DraggableListener {
+            override fun onMaximized() {
+                LLog.d(TAG, "onMaximized")
+            }
+
+            override fun onMinimized() {
+                LLog.d(TAG, "onMinimized")
+            }
+
+            override fun onClosedToLeft() {
+                LLog.d(TAG, "onClosedToLeft")
+            }
+
+            override fun onClosedToRight() {
+                LLog.d(TAG, "onClosedToRight")
+            }
+        })
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_draggable_view
+    }
+}
---
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/elasticdragdismisslayout/ElasticDragDismissLayoutActivity.kt
@@ -0,0 +1,45 @@
+package vn.loitp.app.activity.customviews.layout.elasticdragdismisslayout
+
+import android.os.Bundle
+import android.widget.TextView
+
+import com.core.base.BaseFontActivity
+import com.views.layout.elasticdragdismisslayout.ElasticDragDismissCallback
+import com.views.layout.elasticdragdismisslayout.ElasticDragDismissLinearLayout
+
+import loitp.basemaster.R
+
+//https://github.com/Commit451/ElasticDragDismissLayout?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=3098
+class ElasticDragDismissLayoutActivity : BaseFontActivity() {
+    private lateinit var tv: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tv = findViewById(R.id.tv)
+        val elasticDragDismissLinearLayout = findViewById<ElasticDragDismissLinearLayout>(R.id.draggable_frame)
+        elasticDragDismissLinearLayout.addListener(object : ElasticDragDismissCallback() {
+            override fun onDrag(elasticOffset: Float, elasticOffsetPixels: Float, rawOffset: Float, rawOffsetPixels: Float) {
+                tv.text = "onDrag $elasticOffset - $elasticOffsetPixels - $rawOffset - $rawOffsetPixels"
+            }
+
+            override fun onDragDismissed() {
+                tv.text = "onDragDismissed"
+
+                //if you are targeting 21+ you might want to finish after transition
+                finish()
+            }
+        })
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_elasticdragdismisslayout
+    }
+}
