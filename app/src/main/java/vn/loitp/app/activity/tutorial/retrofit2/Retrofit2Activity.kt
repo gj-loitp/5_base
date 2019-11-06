18c8a0e96b7f656cb70b9b19e716c6aa0f5408d0
v.loitp <v.Loitp@cloudhms.net>
2019-11-06 15:11:12 +0700

    update sqlite multi table advance

--- app/src/main/java/vn/loitp/app/activity/tutorial/retrofit2/Retrofit2Activity.kt
+++ app/src/main/java/vn/loitp/app/activity/tutorial/retrofit2/Retrofit2Activity.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.core.base.BaseFontActivity
+import com.core.utilities.LLog
 import com.restapi.restclient.RestClient2
 import com.views.LToast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_retrofit_2.*
 import loitp.basemaster.R
+import vn.loitp.app.app.LApplication
 import java.util.*
 
 //https://code.tutsplus.com/tutorials/connect-to-an-api-with-retrofit-rxjava-2-and-kotlin--cms-32133
 class Retrofit2Activity : BaseFontActivity(), Retrofit2Adapter.Listener {
     private var retrofit2Adapter: Retrofit2Adapter? = null
-    private var retroCryptoArrayList: ArrayList<RetroCrypto>? = null
+    private var retroCryptoArrayList: ArrayList<RetroCrypto> = ArrayList()
     private val BASE_URL = "https://api.nomics.com/v1/"
-    private var sampleService: SampleService? = null
+    private lateinit var sampleService: SampleService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,14 +35,18 @@ class Retrofit2Activity : BaseFontActivity(), Retrofit2Adapter.Listener {
     }
 
     private fun loadData() {
-        compositeDisposable.add(sampleService!!.getData()
+        LLog.d(TAG, "loadData")
+        compositeDisposable.add(sampleService.getData()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
-                    retroCryptoArrayList = ArrayList(it)
-                    retrofit2Adapter = Retrofit2Adapter(retroCryptoArrayList!!, this)
+                    LLog.d(TAG, "loadData success " + LApplication.gson.toJson(it))
+                    retroCryptoArrayList.clear()
+                    retroCryptoArrayList.addAll(it)
+                    retrofit2Adapter = Retrofit2Adapter(retroCryptoArrayList, this)
                     cryptocurrency_list.adapter = retrofit2Adapter
                 }, {
+                    LLog.e(TAG, "loadData error $it")
                     LToast.show(activity, it.toString(), R.drawable.l_bkg_horizontal)
                 }))
     }
