9cf78a1d4148adfbfa8994e19d3ae09ad50c867b
v.loitp <v.Loitp@cloudhms.net>
2020-02-05 10:13:03 +0700

    update lbutton

--- app/src/main/java/vn/loitp/app/activity/customviews/button/lbutton/LButtonActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/button/lbutton/LButtonActivity.kt
@@ -3,28 +3,11 @@ package vn.loitp.app.activity.customviews.button.lbutton
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
-
 import com.core.base.BaseFontActivity
-import com.views.LToast
-import com.views.button.lbutton.LButton
-
+import kotlinx.android.synthetic.main.activity_l_button.*
 import vn.loitp.app.R
 
 class LButtonActivity : BaseFontActivity(), OnClickListener {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val bt0 = findViewById<LButton>(R.id.bt_0)
-        bt0.setOnClickListener(this)
-
-        val bt1 = findViewById<LButton>(R.id.bt_1)
-        bt1.setPressedDrawable(R.drawable.l_circle_color_primary)
-        bt1.setOnClickListener(this)
-
-        val bt2 = findViewById<LButton>(R.id.bt_2)
-        bt2.setPressedDrawable(R.drawable.l_bt_color_primary)
-        bt2.setOnClickListener(this)
-    }
 
     override fun setFullScreen(): Boolean {
         return false
@@ -38,9 +21,21 @@ class LButtonActivity : BaseFontActivity(), OnClickListener {
         return R.layout.activity_l_button
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        bt0.setOnClickListener(this)
+
+        bt1.setPressedDrawable(R.drawable.l_circle_color_primary)
+        bt1.setOnClickListener(this)
+
+        bt2.setPressedDrawable(R.drawable.l_bt_color_primary)
+        bt2.setOnClickListener(this)
+    }
+
     override fun onClick(v: View) {
-        when (v.id) {
-            R.id.bt_0, R.id.bt_1, R.id.bt_2 -> LToast.show(activity, R.string.click)
+        when (v) {
+            bt0, bt1, bt2 -> showShort(getString(R.string.click))
         }
     }
 }
