7d491c121e62071b88c68924407de08b4e530fb3
v.loitp <v.Loitp@cloudhms.net>
2020-02-17 17:12:57 +0700

    update timepicker

--- app/src/main/java/vn/loitp/app/activity/picker/timepicker/TimePickerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/picker/timepicker/TimePickerActivity.kt
@@ -1,11 +1,17 @@
 package vn.loitp.app.activity.picker.timepicker
 
+import android.annotation.SuppressLint
+import android.app.TimePickerDialog
 import android.os.Bundle
 import com.core.base.BaseFontActivity
+import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_time_picker.*
 import vn.loitp.app.R
+import java.util.*
 
 class TimePickerActivity : BaseFontActivity() {
+
+    private var picker: TimePickerDialog? = null
 
     override fun setFullScreen(): Boolean {
         return false
@@ -19,6 +25,7 @@ class TimePickerActivity : BaseFontActivity() {
         return R.layout.activity_time_picker
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,6 +36,16 @@ class TimePickerActivity : BaseFontActivity() {
         }
         timePickerSpiner.setOnTimeChangedListener { _, hourOfDay, minute ->
             tvTimePickeSpinner.text = "$hourOfDay - $minute"
+        }
+        btDialog.setSafeOnClickListener {
+            val cal: Calendar = Calendar.getInstance()
+            val hour: Int = cal.get(Calendar.HOUR_OF_DAY)
+            val minutes: Int = cal.get(Calendar.MINUTE)
+            picker = TimePickerDialog(activity,
+                    TimePickerDialog.OnTimeSetListener { tp, sHour, sMinute ->
+                        btDialog.text = "$sHour - $sMinute"
+                    }, hour, minutes, true)
+            picker?.show()
         }
     }
 }
