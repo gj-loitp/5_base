52463899486d6e679012b417f6278d6dd3397b60
Loitp <v.loitp@cloudhms.net>
2020-04-25 15:39:21 +0700

    update shine button

--- app/src/main/java/vn/loitp/app/activity/customviews/button/shinebutton/ShineButtonActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/button/shinebutton/ShineButtonActivity.kt
@@ -1,25 +1,13 @@
 package vn.loitp.app.activity.customviews.button.shinebutton
 
 import android.os.Bundle
-import android.view.View
-
 import com.core.base.BaseFontActivity
-import com.views.button.shinebutton.LShineView
-import kotlinx.android.synthetic.main.activity_button_shine.*
-
 import vn.loitp.app.R
 
 class ShineButtonActivity : BaseFontActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bt0.setImage(R.mipmap.ic_launcher)
-        bt0.setSize(sizeImageViewInDP = 100, sizeShineButtonInDP = 80)
-        bt0.setOnClick(object : LShineView.Callback {
-            override fun onClick(view: View) {
-                showShort("onClick")
-            }
-        })
     }
 
     override fun setFullScreen(): Boolean {
