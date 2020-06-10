a53613ed8c77ac274add2d3b3dcb365ed7bec64f
v.loitp <v.Loitp@cloudhms.net>
2020-06-10 15:05:03 +0700

    update video

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/videoview/exoplayer/ExoPlayerActivity2.kt
@@ -0,0 +1,76 @@
+package vn.loitp.app.activity.customviews.videoview.exoplayer
+
+import android.content.res.Configuration
+import android.os.Bundle
+import com.core.base.BaseFontActivity
+import com.core.utilities.LScreenUtil.isLandscape
+import com.views.exo.PlayerManager
+import kotlinx.android.synthetic.main.activity_video_exo_player2.*
+import kotlinx.android.synthetic.main.exo_playback_control_view.*
+import vn.loitp.app.R
+
+//custom UI exo_playback_control_view.xml
+class ExoPlayerActivity2 : BaseFontActivity() {
+    private var playerManager: PlayerManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        playerView.useController = false
+        val linkPlay = "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd"
+        playerManager = PlayerManager(activity)
+
+        controls.showTimeoutMs = 0
+        playerManager?.init(context = this, playerView = playerView, linkPlay = linkPlay)
+        controls.player = playerManager?.player
+        playerManager?.updateSizePlayerView(activity = activity, playerView = playerView, exoFullscreen = exo_fullscreen)
+        exo_fullscreen.setOnClickListener {
+            playerManager?.toggleFullscreen(activity)
+        }
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_video_exo_player2
+    }
+
+    override fun onResume() {
+        super.onResume()
+        playerManager?.resumeVideo()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        playerManager?.pauseVideo()
+    }
+
+    public override fun onDestroy() {
+        playerManager?.release()
+        super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        if (isLandscape(activity)) {
+            playerManager?.toggleFullscreen(activity)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            playerManager?.updateSizePlayerView(activity = activity, playerView = playerView, exoFullscreen = exo_fullscreen)
+        } else {
+            playerManager?.updateSizePlayerView(activity = activity, playerView = playerView, exoFullscreen = exo_fullscreen)
+        }
+    }
+}
