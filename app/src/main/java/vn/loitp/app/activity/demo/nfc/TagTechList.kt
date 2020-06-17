cb04673388612049bb3cb98400b891a93f8b4126
v.loitp <v.Loitp@cloudhms.net>
2020-06-17 15:38:28 +0700

    update nfc

---
+++ app/src/main/java/vn/loitp/app/activity/demo/nfc/TagTechList.kt
@@ -0,0 +1,5 @@
+package vn.loitp.app.activity.demo.nfc
+
+import java.util.*
+
+internal class TagTechList : LinkedHashMap<String?, List<String?>?>()
---
+++ app/src/main/java/vn/loitp/app/activity/demo/nfc/TagWrapper.kt
@@ -0,0 +1,5 @@
+package vn.loitp.app.activity.demo.nfc
+
+internal class TagWrapper(val id: String) {
+    var techList = TagTechList()
+}
