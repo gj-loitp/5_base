0337a1afba2bd9139a4ca9bc1b4193da61189a4e
v.loitp <v.Loitp@cloudhms.net>
2020-06-17 10:45:04 +0700

    update demo

---
+++ app/src/main/java/vn/loitp/app/activity/demo/nfc/NFCActivity.kt
@@ -0,0 +1,19 @@
+package vn.loitp.app.activity.demo.nfc
+
+import com.core.base.BaseFontActivity
+import vn.loitp.app.R
+
+class NFCActivity : BaseFontActivity() {
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_menu_demo
+    }
+}
