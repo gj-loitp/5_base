19f3ded656fccd49980ef5047cfe72ee5524f59d
Loitp <v.loitp@cloudhms.net>
2020-06-19 22:40:21 +0700

    update view pager

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/viewpager/autoviewpager/ViewPagerAutoActivity.kt
@@ -0,0 +1,50 @@
+package vn.loitp.app.activity.customviews.viewpager.autoviewpager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.core.base.BaseFontActivity
+import com.core.common.Constants
+import com.core.utilities.LUIUtil
+import kotlinx.android.synthetic.main.activity_view_pager_auto.*
+import vn.loitp.app.R
+import vn.loitp.app.activity.customviews.viewpager.autoviewpager.FrmIv.Companion.newInstance
+
+class ViewPagerAutoActivity : BaseFontActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewPager.adapter = SamplePagerAdapter(supportFragmentManager)
+        //viewPager.setIndeterminate(true)
+        viewPager.setAutoScrollEnabled(true)
+        tabLayout.setupWithViewPager(viewPager)
+        LUIUtil.changeTabsFont(tabLayout, Constants.FONT_PATH)
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_view_pager_auto
+    }
+
+    private inner class SamplePagerAdapter internal constructor(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        override fun getItem(position: Int): Fragment {
+            return newInstance()
+        }
+
+        override fun getCount(): Int {
+            return 5
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return "Page Title $position"
+        }
+    }
+}
