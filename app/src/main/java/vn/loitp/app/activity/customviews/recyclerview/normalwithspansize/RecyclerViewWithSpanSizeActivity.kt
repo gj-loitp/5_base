33fe7b0106865d34b1a06a89fd9b6b4688c3ad4d
v.loitp <v.Loitp@cloudhms.net>
2020-06-23 17:02:10 +0700

    update recycler view

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/normalwithspansize/RecyclerViewWithSpanSizeActivity.kt
@@ -0,0 +1,79 @@
+package vn.loitp.app.activity.customviews.recyclerview.normalwithspansize
+
+import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
+import com.core.base.BaseFontActivity
+import com.core.utilities.LUIUtil
+import kotlinx.android.synthetic.main.activity_recycler_view.*
+import vn.loitp.app.R
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.MoviesAdapter
+import vn.loitp.app.common.Constants.URL_IMG
+import java.util.*
+
+class RecyclerViewWithSpanSizeActivity : BaseFontActivity() {
+    private val movieList: MutableList<Movie> = ArrayList()
+    private var mAdapter: MoviesAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mAdapter = MoviesAdapter(moviesList = movieList, callback = object : MoviesAdapter.Callback {
+            override fun onClick(movie: Movie, position: Int) {
+                showShort("Click " + movie.title)
+            }
+
+            override fun onLongClick(movie: Movie, position: Int) {}
+            override fun onLoadMore() {
+                loadMore()
+            }
+        })
+        val gridLayoutManager = GridLayoutManager(activity, 2)
+        rv.layoutManager = gridLayoutManager
+        rv.itemAnimator = DefaultItemAnimator()
+        rv.adapter = mAdapter
+        gridLayoutManager.spanSizeLookup = object : SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position % 5 == 0) {
+                    1
+                } else 2
+            }
+        }
+        //LUIUtil.setPullLikeIOSVertical(rv)
+        prepareMovieData()
+    }
+
+    private fun loadMore() {
+        LUIUtil.setDelay(mls = 2000, runnable = Runnable {
+            val newSize = 5
+            for (i in 0 until newSize) {
+                val movie = Movie(title = "Add new $i", genre = "Add new $i", year = "Add new: $i", cover = URL_IMG)
+                movieList.add(movie)
+            }
+            mAdapter?.notifyDataSetChanged()
+            showShort("Finish loadMore")
+        })
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_recycler_view
+    }
+
+    private fun prepareMovieData() {
+        for (i in 0..99) {
+            val movie = Movie(title = "Loitp $i", genre = "Action & Adventure $i", year = "Year: $i", cover = URL_IMG)
+            movieList.add(movie)
+        }
+        mAdapter?.notifyDataSetChanged()
+    }
+}
