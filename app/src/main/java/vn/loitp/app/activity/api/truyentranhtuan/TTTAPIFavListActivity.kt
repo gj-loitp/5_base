8d9d6e87062db00189218fd04c8200997a1d5db8
v.loitp <v.Loitp@cloudhms.net>
2020-07-01 15:24:08 +0700

    update api

---
+++ app/src/main/java/vn/loitp/app/activity/api/truyentranhtuan/TTTAPIFavListActivity.kt
@@ -0,0 +1,44 @@
+package vn.loitp.app.activity.api.truyentranhtuan
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import com.core.base.BaseFontActivity
+import com.core.utilities.LUIUtil
+import kotlinx.android.synthetic.main.activity_api_ttt_fav_list.*
+import vn.loitp.app.R
+import vn.loitp.app.activity.api.truyentranhtuan.helper.favlist.GetFavListTask
+import vn.loitp.app.activity.api.truyentranhtuan.model.comic.Comic
+
+class TTTAPIFavListActivity : BaseFontActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        indicatorView.hide()
+        favList
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_api_ttt_fav_list
+    }
+
+    private val favList: Unit
+        @SuppressLint("SetTextI18n")
+        get() {
+            GetFavListTask(mActivity = activity,
+                    callback = object : GetFavListTask.Callback {
+                        override fun onSuccess(comicList: List<Comic>) {
+                            LUIUtil.printBeautyJson(o = comicList, textView = textView)
+                            tvTitle.text = "Danh sách yêu thích: " + comicList.size
+                        }
+                    }).execute()
+        }
+}
