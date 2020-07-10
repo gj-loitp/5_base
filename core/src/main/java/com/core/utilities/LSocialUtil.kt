e0c6dff0cce14167fdb0cade8310c9dbaa02bfe5
v.loitp <v.Loitp@cloudhms.net>
2020-07-10 17:30:38 +0700

    update

--- core/src/main/java/com/core/utilities/LSocialUtil.kt
+++ core/src/main/java/com/core/utilities/LSocialUtil.kt
@@ -34,8 +34,7 @@ class LSocialUtil {
             }
         }
 
-        fun moreApp(activity: Activity) {
-            val nameOfDeveloper = "NgonTinh KangKang"
+        fun moreApp(activity: Activity, nameOfDeveloper: String = "NgonTinh KangKang") {
             val uri = "https://play.google.com/store/apps/developer?id=$nameOfDeveloper"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
             activity.startActivity(intent)
