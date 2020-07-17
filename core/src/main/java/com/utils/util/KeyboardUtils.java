81e41034b340331b8d3acc1328b53a03a0fdf209
v.loitp <v.Loitp@cloudhms.net>
2020-07-17 17:29:38 +0700

    update

--- core/src/main/java/com/utils/util/KeyboardUtils.java
+++ core/src/main/java/com/utils/util/KeyboardUtils.java
@@ -2,7 +2,6 @@ package com.utils.util;
 
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -50,44 +49,6 @@ public final class KeyboardUtils {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         if (imm == null) return;
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
-
-    /**
-     * 点击屏幕空白区域隐藏软键盘
-     * <p>根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘</p>
-     * <p>需重写dispatchTouchEvent</p>
-     * <p>参照以下注释代码</p>
-     */
-    public static void clickBlankArea2HideSoftInput() {
-        Log.d("tips", "U should copy the following code.");
-        /*
-        @Override
-        public boolean dispatchTouchEvent(MotionEvent ev) {
-            if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-                View v = getCurrentFocus();
-                if (isShouldHideKeyboard(v, ev)) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                }
-            }
-            return super.dispatchTouchEvent(ev);
-        }
-
-        // 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘
-        private boolean isShouldHideKeyboard(View v, MotionEvent event) {
-            if (v != null && (v instanceof EditText)) {
-                int[] l = {0, 0};
-                v.getLocationInWindow(l);
-                int left = l[0],
-                        top = l[1],
-                        bottom = top + v.getHeight(),
-                        right = left + v.getWidth();
-                return !(event.getX() > left && event.getX() < right
-                        && event.getY() > top && event.getY() < bottom);
-            }
-            return false;
-        }
-        */
     }
 
     /**
