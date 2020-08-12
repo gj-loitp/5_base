da6dbc5ee604650d585ac64b7bb8079eecaeef4f
v.loitp <v.Loitp@cloudhms.net>
2020-08-12 15:03:48 +0700

    update map

---
+++ app/src/main/java/vn/loitp/app/activity/demo/maptracker/MapTrackerActivity.kt
@@ -0,0 +1,181 @@
+package vn.loitp.app.activity.demo.maptracker
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.core.base.BaseFontActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import vn.loitp.app.R
+import java.io.IOException
+import java.util.*
+
+class MapTrackerActivity : BaseFontActivity(),
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private var googleApiClient: GoogleApiClient? = null
+    private var currentLocationMarker: Marker? = null
+    private var mGoogleMap: GoogleMap? = null
+
+    override fun setTag(): String? {
+        return javaClass.simpleName
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_map_tracker
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission()
+        }
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+        mGoogleMap?.apply {
+            mapType = GoogleMap.MAP_TYPE_NORMAL
+            uiSettings.isZoomControlsEnabled = true
+            uiSettings.isZoomGesturesEnabled = true
+            uiSettings.isCompassEnabled = true
+        }
+
+        //Initialize Google Play Services
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient()
+                mGoogleMap?.isMyLocationEnabled = true
+            }
+        } else {
+            buildGoogleApiClient()
+            mGoogleMap?.isMyLocationEnabled = true
+        }
+    }
+
+    @Synchronized
+    private fun buildGoogleApiClient() {
+        googleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+        googleApiClient?.connect()
+    }
+
+    override fun onConnected(bundle: Bundle?) {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 1000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this)
+        }
+    }
+
+    override fun onConnectionSuspended(i: Int) {}
+
+    override fun onLocationChanged(location: Location) {
+        currentLocationMarker?.remove()
+        //Showing Current Location Marker on Map
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val provider = locationManager.getBestProvider(Criteria(), true)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        var locations: Location? = null
+        provider?.let {
+            locations = locationManager.getLastKnownLocation(it)
+        }
+        val providerList = locationManager.allProviders
+
+        locations?.let { loc ->
+            if (providerList.size > 0) {
+                val longitude = loc.longitude
+                val latitude = loc.latitude
+                val geoCoder = Geocoder(applicationContext, Locale.getDefault())
+                try {
+                    val listAddresses = geoCoder.getFromLocation(latitude, longitude, 1)
+                    if (listAddresses.isNullOrEmpty()) {
+                        //do nothing
+                    } else {
+                        val state = listAddresses[0].adminArea
+                        val country = listAddresses[0].countryName
+                        val subLocality = listAddresses[0].subLocality
+                        markerOptions.title("$latLng,$subLocality,$state,$country")
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        currentLocationMarker = mGoogleMap?.addMarker(markerOptions)
+        mGoogleMap?.let {
+            it.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+            it.animateCamera(CameraUpdateFactory.zoomTo(11f))
+        }
+        if (googleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this)
+        }
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {}
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSIONS_REQUEST_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    if (googleApiClient == null) {
+                        buildGoogleApiClient()
+                    }
+                    mGoogleMap?.isMyLocationEnabled = true
+                }
+            } else {
+                showShort("Permission denied")
+            }
+        }
+    }
+
+    override fun setFullScreen(): Boolean {
+        return false
+    }
+
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_LOCATION = 99
+    }
+}
