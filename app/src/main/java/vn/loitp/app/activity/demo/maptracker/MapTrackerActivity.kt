a37e983cb36ab292a0020dc17dfc13c22a65ea87
v.loitp <v.Loitp@cloudhms.net>
2020-08-13 15:49:12 +0700

    update map

--- app/src/main/java/vn/loitp/app/activity/demo/maptracker/MapTrackerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/demo/maptracker/MapTrackerActivity.kt
@@ -2,28 +2,35 @@ package vn.loitp.app.activity.demo.maptracker
 
 import android.Manifest
 import android.content.Context
-import android.content.IntentSender
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Looper
+import android.provider.Settings
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.core.base.BaseFontActivity
+import com.core.utilities.LDialogUtil
 import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.interfaces.Callback2
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_map_tracker.*
 import vn.loitp.app.R
@@ -38,25 +45,22 @@ class MapTrackerActivity : BaseFontActivity(),
         GoogleApiClient.OnConnectionFailedListener {
 
     companion object {
-        private const val PERMISSIONS_REQUEST_LOCATION = 99
         private const val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 5000
         private const val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS: Long = 2000
-        private const val REQUEST_CHECK_SETTINGS = 100
     }
 
     private var googleApiClient: GoogleApiClient? = null
     private var currentLocationMarker: Marker? = null
     private var mGoogleMap: GoogleMap? = null
-
-    // location last updated time
     private var mLastUpdateTime: String? = null
-
     private var mFusedLocationClient: FusedLocationProviderClient? = null
     private var mSettingsClient: SettingsClient? = null
     private var mLocationRequest: LocationRequest? = null
     private var mLocationSettingsRequest: LocationSettingsRequest? = null
     private var mLocationCallback: LocationCallback? = null
     private var mCurrentLocation: Location? = null
+
+    private var isShowDialogCheck = false
 
     override fun setTag(): String? {
         return "loitpp" + javaClass.simpleName
@@ -72,13 +76,12 @@ class MapTrackerActivity : BaseFontActivity(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            checkLocationPermission()
-        }
+
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
 
         initLocation()
+
         btLocation.setSafeOnClickListener {
             startLocationUpdates()
         }
@@ -90,26 +93,23 @@ class MapTrackerActivity : BaseFontActivity(),
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (!isShowDialogCheck) {
+            checkPermission()
+        }
+    }
+
     private fun onChangeLocation() {
         logD("onChangeLocation " + mCurrentLocation?.latitude + " - " + mCurrentLocation?.longitude + ", mLastUpdateTime:" + mLastUpdateTime)
 
         currentLocationMarker?.remove()
         mCurrentLocation?.let { location ->
-            //Showing Current Location Marker on Map
             val latLng = LatLng(location.latitude, location.longitude)
             val markerOptions = MarkerOptions()
             markerOptions.position(latLng)
 
             val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//            val provider = locationManager.getBestProvider(Criteria(), true)
-//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-//                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                return
-//            }
-//            var locations: Location? = null
-//            provider?.let {
-//                locations = locationManager.getLastKnownLocation(it)
-//            }
             val providerList = locationManager.allProviders
 
             if (providerList.size > 0) {
@@ -122,11 +122,6 @@ class MapTrackerActivity : BaseFontActivity(),
                     if (listAddresses.isNullOrEmpty()) {
                         //do nothing
                     } else {
-//                        val state = listAddresses[0].adminArea
-//                        val country = listAddresses[0].countryName
-//                        val subLocality = listAddresses[0].subLocality
-//                        markerOptions.title("$latLng,$subLocality,$state,$country")
-
                         markerOptions.title(listAddresses[0].getAddressLine(0))
                     }
                 } catch (e: IOException) {
@@ -170,9 +165,14 @@ class MapTrackerActivity : BaseFontActivity(),
         mGoogleMap = googleMap
         mGoogleMap?.apply {
             mapType = GoogleMap.MAP_TYPE_NORMAL
-            uiSettings.isZoomControlsEnabled = true
+            uiSettings.isZoomControlsEnabled = false
             uiSettings.isZoomGesturesEnabled = true
             uiSettings.isCompassEnabled = true
+//            uiSettings.isMapToolbarEnabled = true
+            uiSettings.isMyLocationButtonEnabled = true
+            uiSettings.isRotateGesturesEnabled = true
+            uiSettings.isScrollGesturesEnabled = true
+            uiSettings.isTiltGesturesEnabled = true
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -201,7 +201,6 @@ class MapTrackerActivity : BaseFontActivity(),
     }
 
     private fun addMakerSydney() {
-        // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mGoogleMap?.let {
             it.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
@@ -231,25 +230,77 @@ class MapTrackerActivity : BaseFontActivity(),
 
     }
 
-    private fun checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_LOCATION)
-        }
+    private fun checkPermission() {
+        isShowDialogCheck = true
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                        if (report.areAllPermissionsGranted()) {
+                            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                                if (googleApiClient == null) {
+                                    buildGoogleApiClient()
+                                }
+                                mGoogleMap?.isMyLocationEnabled = true
+                            }
+                        } else {
+                            showShouldAcceptPermission()
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            showSettingsDialog()
+                        }
+                        isShowDialogCheck = true
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) {
+                        token.continuePermissionRequest()
+                    }
+                })
+                .onSameThread()
+                .check()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == PERMISSIONS_REQUEST_LOCATION) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    if (googleApiClient == null) {
-                        buildGoogleApiClient()
+    private fun showSettingsDialog() {
+        val alertDialog = LDialogUtil.showDialog2(
+                context = activity,
+                title = "Need Permissions",
+                msg = "This app needs permission to use this feature. You can grant them in app settings.",
+                button1 = "GOTO SETTINGS",
+                button2 = "Cancel",
+                callback2 = object : Callback2 {
+                    override fun onClick1() {
+                        isShowDialogCheck = false
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivityForResult(intent, 101)
                     }
-                    mGoogleMap?.isMyLocationEnabled = true
-                }
-            } else {
-                showShort("Permission denied")
-            }
-        }
+
+                    override fun onClick2() {
+                        onBackPressed()
+                    }
+                })
+        alertDialog.setCancelable(false)
+    }
+
+    private fun showShouldAcceptPermission() {
+        val alertDialog = LDialogUtil.showDialog2(
+                context = activity,
+                title = "Need Permissions",
+                msg = "This app needs permission to use this feature.",
+                button1 = "Okay",
+                button2 = "Cancel",
+                callback2 = object : Callback2 {
+                    override fun onClick1() {
+                        checkPermission()
+                    }
+
+                    override fun onClick2() {
+                        onBackPressed()
+                    }
+                })
+        alertDialog.setCancelable(false)
     }
 
     private fun drawPolyLineOnMap(list: List<LatLng>) {
@@ -265,43 +316,25 @@ class MapTrackerActivity : BaseFontActivity(),
         }
         val bounds = builder.build()
         //BOUND_PADDING is an int to specify padding of bound.. try 100.
-        val cu = CameraUpdateFactory.newLatLngBounds(bounds, 100)
-        mGoogleMap?.animateCamera(cu)
+        val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 100)
+        mGoogleMap?.animateCamera(cameraUpdate)
     }
 
     private fun startLocationUpdates() {
         mSettingsClient?.let { settingsClient ->
             settingsClient.checkLocationSettings(mLocationSettingsRequest)
                     .addOnSuccessListener(this) {
-                        logD("All location settings are satisfied.")
+                        showShort("All location settings are satisfied.")
                         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                             mFusedLocationClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
-
                             onChangeLocation()
                         } else {
                             showShort("Dont have permission ACCESS_FINE_LOCATION && ACCESS_COARSE_LOCATION")
                         }
                     }
                     .addOnFailureListener(this) { e ->
-                        when ((e as ApiException).statusCode) {
-                            LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
-                                logD("Location settings are not satisfied. Attempting to upgrade location settings ")
-                                try {
-                                    // Show the dialog by calling startResolutionForResult(), and check the
-                                    // result in onActivityResult().
-                                    val rae = e as ResolvableApiException
-                                    rae.startResolutionForResult(activity, REQUEST_CHECK_SETTINGS)
-                                } catch (sie: IntentSender.SendIntentException) {
-                                    sie.printStackTrace()
-                                }
-                            }
-                            LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
-                                val errorMessage = "Location settings are inadequate, and cannot be fixed here. Fix in Settings."
-                                logD(errorMessage)
-                                showShort(errorMessage)
-                            }
-                        }
+                        showShort(e.toString())
                         onChangeLocation()
                     }
         }
