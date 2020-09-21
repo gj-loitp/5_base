f1ba8c4b589d27cbdf4a08d9609f6adc74db345f
v.loitp <v.Loitp@cloudhms.net>
2020-09-21 17:27:12 +0700

    update

--- core/src/main/java/com/core/helper/gallery/member/MemberAdapter.kt
+++ core/src/main/java/com/core/helper/gallery/member/MemberAdapter.kt
@@ -4,56 +4,61 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.R
+import com.core.adapter.AnimationAdapter
 import com.core.helper.gallery.photos.PhotosDataCore
 import com.core.utilities.LImageUtil
 import com.core.utilities.LUIUtil
 import com.restapi.flickr.model.photosetgetphotos.Photo
+import kotlinx.android.synthetic.main.l_item_flickr_photos_member.view.*
 import java.util.*
 
 class MemberAdapter(private val context: Context, private val callback: Callback?)
-    : RecyclerView.Adapter<MemberAdapter.ViewHolder>() {
+    : AnimationAdapter() {
 
     private val logTag = javaClass.simpleName
-    private val inflater: LayoutInflater = LayoutInflater.from(context)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): ViewHolder {
-        return ViewHolder(inflater.inflate(R.layout.l_item_flickr_photos_member, viewGroup, false))
-    }
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val photo = PhotosDataCore.instance.getPhotoList()[position]
-
-        LImageUtil.load(context = context, url = photo.urlO, imageView = viewHolder.circleImageView)
-
-        if (photo.title.toLowerCase(Locale.getDefault()).startsWith("null")) {
-            viewHolder.tvTitle.visibility = View.INVISIBLE
-        } else {
-            viewHolder.tvTitle.visibility = View.VISIBLE
-            viewHolder.tvTitle.text = photo.title
-            LUIUtil.setTextShadow(textView = viewHolder.tvTitle)
-        }
-        viewHolder.circleImageView.setOnClickListener {
-            callback?.onClick(photo = photo, pos = position, imageView = viewHolder.circleImageView, textView = viewHolder.tvTitle)
-        }
-        viewHolder.circleImageView.setOnLongClickListener {
-            callback?.onLongClick(photo = photo, pos = position)
-            true
-        }
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.l_item_flickr_photos_member, viewGroup, false))
     }
 
     override fun getItemCount(): Int {
         return PhotosDataCore.instance.getPhotoList().size
     }
 
-    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val fl: FrameLayout = v.findViewById(R.id.fl)
-        val tvTitle: TextView = v.findViewById(R.id.tvTitle)
-        val circleImageView: ImageView = v.findViewById(R.id.circleImageView)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewHolder) {
+            val photo = PhotosDataCore.instance.getPhotoList()[position]
+            holder.bind(photo)
+        }
+    }
+
+    inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        fun bind(photo: Photo) {
+
+            LImageUtil.load(context = itemView.circleImageView.context, url = photo.urlO, imageView = itemView.circleImageView, resPlaceHolder = R.color.gray)
+
+            if (photo.title.toLowerCase(Locale.getDefault()).startsWith("null")) {
+                itemView.tvTitle.visibility = View.INVISIBLE
+            } else {
+                itemView.tvTitle.visibility = View.VISIBLE
+                itemView.tvTitle.text = photo.title
+                LUIUtil.setTextShadow(textView = itemView.tvTitle)
+            }
+            itemView.fl.setOnClickListener {
+                callback?.onClick(photo = photo, pos = bindingAdapterPosition, imageView = itemView.circleImageView, textView = itemView.tvTitle)
+            }
+            itemView.fl.setOnLongClickListener {
+                callback?.onLongClick(photo = photo, pos = bindingAdapterPosition)
+                true
+            }
+
+            setAnimation(viewToAnimate = itemView.fl, position = bindingAdapterPosition)
+        }
     }
 
     interface Callback {
