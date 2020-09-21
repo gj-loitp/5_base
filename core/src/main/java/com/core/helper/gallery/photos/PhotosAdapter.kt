6817405394c3df614d03ca72712bddd921ab42e3
v.loitp <v.Loitp@cloudhms.net>
2020-09-21 17:38:10 +0700

    update

--- core/src/main/java/com/core/helper/gallery/photos/PhotosAdapter.kt
+++ core/src/main/java/com/core/helper/gallery/photos/PhotosAdapter.kt
@@ -5,59 +5,55 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.R
+import com.core.adapter.AnimationAdapter
 import com.core.helper.gallery.photos.PhotosDataCore.Companion.instance
 import com.core.utilities.LImageUtil
 import com.core.utilities.LUIUtil
 import com.restapi.flickr.model.photosetgetphotos.Photo
-import com.views.layout.squarelayout.LSquareFrameLayout
+import kotlinx.android.synthetic.main.l_item_flickr_photos_core.view.*
 
 /**
  * Created by loitp on 14/04/15.
  */
 class PhotosAdapter internal constructor(private val context: Context, private val callback: Callback?)
-    : RecyclerView.Adapter<PhotosAdapter.ViewHolder>() {
+    : AnimationAdapter() {
 
     private val logTag = javaClass.simpleName
-    private val inflater: LayoutInflater = LayoutInflater.from(context)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): ViewHolder {
-        return ViewHolder(inflater.inflate(R.layout.l_item_flickr_photos_core, viewGroup, false))
-    }
-
-    override fun onViewRecycled(holder: ViewHolder) {
-        super.onViewRecycled(holder)
-        LImageUtil.clear(context = context, target = holder.imageView)
-    }
-
-    @SuppressLint("SetTextI18n")
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val photo = instance.getPhotoList()[position]
-
-        LImageUtil.load(context = context, url = photo.flickrLink1024, imageView = viewHolder.imageView)
-        viewHolder.tvSize.text = "${photo.widthO} x ${photo.heightO}"
-        LUIUtil.setTextShadow(textView = viewHolder.tvSize)
-
-        viewHolder.rootView.setOnClickListener {
-            callback?.onClick(photo = photo, pos = position)
-        }
-        viewHolder.rootView.setOnLongClickListener {
-            callback?.onLongClick(photo = photo, pos = position)
-            true
-        }
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.l_item_flickr_photos_core, viewGroup, false))
     }
 
     override fun getItemCount(): Int {
         return instance.getPhotoList().size
     }
 
-    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val imageView: ImageView = v.findViewById(R.id.imageView)
-        val tvSize: TextView = v.findViewById(R.id.tvSize)
-        val rootView: LSquareFrameLayout = v.findViewById(R.id.rootView)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewHolder) {
+            val photo = instance.getPhotoList()[position]
+            holder.bind(photo = photo)
+        }
+    }
+
+    inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        @SuppressLint("SetTextI18n")
+        fun bind(photo: Photo) {
+            LImageUtil.load(context = itemView.imageView.context, url = photo.flickrLink1024, imageView = itemView.imageView, resPlaceHolder = R.color.gray)
+            itemView.tvSize.text = "${photo.widthO} x ${photo.heightO}"
+            LUIUtil.setTextShadow(textView = itemView.tvSize)
+
+            itemView.layoutRootView.setOnClickListener {
+                callback?.onClick(photo = photo, pos = bindingAdapterPosition)
+            }
+            itemView.layoutRootView.setOnLongClickListener {
+                callback?.onLongClick(photo = photo, pos = bindingAdapterPosition)
+                true
+            }
+
+            setAnimation(viewToAnimate = itemView.layoutRootView, position = bindingAdapterPosition)
+        }
     }
 
     interface Callback {
