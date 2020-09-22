e475df30a27d622d8a5625980aa01ad5d41318b1
v.loitp <v.Loitp@cloudhms.net>
2020-09-22 11:54:07 +0700

    update

--- core/src/main/java/com/core/helper/gallery/albumonly/PhotosOnlyAdapter.kt
+++ core/src/main/java/com/core/helper/gallery/albumonly/PhotosOnlyAdapter.kt
@@ -1,11 +1,16 @@
 package com.core.helper.gallery.albumonly
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.R
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.core.adapter.AnimationAdapter
 import com.core.helper.gallery.photos.PhotosDataCore
 import com.core.utilities.LAnimationUtil
@@ -40,7 +45,22 @@ class PhotosOnlyAdapter(val context: Context, private val callback: Callback?) :
 
         internal fun bind(p: Photo, position: Int) {
 
-            LImageUtil.load(context = itemView.iv.context, url = p.urlO, imageView = itemView.iv)
+            LImageUtil.load(itemView.iv.context,
+                    p.urlO,
+                    itemView.iv,
+                    R.color.gray,
+                    R.color.gray,
+                    object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any, target: Target<Drawable?>, isFirstResource: Boolean): Boolean {
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any, target: Target<Drawable?>, dataSource: DataSource, isFirstResource: Boolean): Boolean {
+                            itemView.layoutControl?.visibility = View.VISIBLE
+                            itemView.viewLine?.visibility = View.VISIBLE
+                            return false
+                        }
+                    })
 
             if (p.title.toLowerCase(Locale.getDefault()).startsWith("null")) {
                 itemView.tvTitle.visibility = View.INVISIBLE
