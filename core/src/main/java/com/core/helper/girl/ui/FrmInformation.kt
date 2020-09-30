e10bc1749cc2049c14c41c10c725d34caf8b38bc
v.loitp <v.Loitp@cloudhms.net>
2020-09-30 17:32:50 +0700

    update girl

--- core/src/main/java/com/core/helper/girl/ui/FrmInformation.kt
+++ core/src/main/java/com/core/helper/girl/ui/FrmInformation.kt
@@ -12,6 +12,7 @@ import com.core.common.Constants
 import com.core.helper.adhelper.AdHelperActivity
 import com.core.utilities.LActivityUtil
 import com.core.utilities.LSocialUtil
+import com.views.textview.textdecorator.LTextDecorator
 import kotlinx.android.synthetic.main.l_frm_girl_information.*
 
 @LogTag("FrmInformation")
@@ -25,12 +26,40 @@ class FrmInformation : BaseFragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupViews()
+    }
+
+    fun setupViews() {
         btRateApp.setOnClickListener(this)
         btMoreApp.setOnClickListener(this)
         btShareApp.setOnClickListener(this)
         btLikeFbFanpage.setOnClickListener(this)
         btSupport.setOnClickListener(this)
         btAdHelper.setOnClickListener(this)
+
+        val text = "Cuộc sống hôm nay tuy vất vả nhưng cuộc đời ơi ta mến thương và mọi người có thể ủng hộ mình qua tài khoản: \n" +
+                "\n" +
+                "❤ Vietcombank\n" +
+                "Tên tài khoản: Trần Phú Lợi\n" +
+                "Số tài khoản: 0371000106443\n" +
+                "Ngân hàng Vietcombank, Chi nhánh Tân Định, Q1, TPHCM.\n\n" +
+                "❤ VPBank\n" +
+                "Tên tài khoản: Trần Phú Lợi\n" +
+                "Số tài khoản: 166210585\n" +
+                "Ngân hàng VPBank, chi nhánh Bà Chiểu, Bình Thạnh, TPHCM.\n\n" +
+                "❤ Timo\n" +
+                "Tên tài khoản: Trần Phú Lợi\n" +
+                "Số tài khoản: 164205468\n\n" +
+                "❤ Momo:\n" +
+                "Số điện thoại 0764088864\n" +
+                "\n\n" +
+                "Những đóng góp của mọi người là động lực và trách nhiệm để mình có thể cho ra nhiều ứng dụng hay hơn nữa. Chân thành cảm ơn!"
+
+        LTextDecorator
+                .decorate(textView, text)
+                .setTextColor(R.color.red, "❤", "Trần Phú Lợi", "0371000106443", "166210585", "164205468", "0764088864")
+                .underline("Chân thành cảm ơn!")
+                .build()
     }
 
     override fun onClick(v: View) {
