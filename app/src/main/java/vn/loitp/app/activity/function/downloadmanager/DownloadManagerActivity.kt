9426a7918c27b44a90e736fbb953b8ff47a89c83
v.loitp <v.Loitp@cloudhms.net>
2020-10-08 10:21:37 +0700

    update download

--- app/src/main/java/vn/loitp/app/activity/function/downloadmanager/DownloadManagerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/function/downloadmanager/DownloadManagerActivity.kt
@@ -28,7 +28,7 @@ class DownloadManagerActivity : BaseFontActivity() {
         const val FILE_NAME = "LoiDepTrai.txt"
     }
 
-    private var downloader: Downloader? = null
+    private var downloaderPhoto: Downloader? = null
     private var lStoreUtilModel: LStoreUtilModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,19 +40,18 @@ class DownloadManagerActivity : BaseFontActivity() {
 
     private fun setupViews() {
         btStartDownload.setOnClickListener {
-//            downloader?.cancelDownload()
             getDownloader()
-            downloader?.download()
+            downloaderPhoto?.download()
         }
         btCancelDownload.setSafeOnClickListener {
-            downloader?.cancelDownload()
+            downloaderPhoto?.cancelDownload()
         }
         btPauseDownload.setSafeOnClickListener {
-            downloader?.pauseDownload()
+            downloaderPhoto?.pauseDownload()
         }
         btResumeDownload.setSafeOnClickListener {
             getDownloader()
-            downloader?.resumeDownload()
+            downloaderPhoto?.resumeDownload()
         }
         btTestRandomColor.setOnClickListener {
             val randomColor = LStoreUtil.randomColor
@@ -73,6 +72,9 @@ class DownloadManagerActivity : BaseFontActivity() {
         }
         btReadTxtFromAsset.setSafeOnClickListener {
             lStoreUtilModel?.readTxtFromAsset("colors.json")
+        }
+        btStartDownloadZipGoogle.setSafeOnClickListener {
+
         }
     }
 
@@ -136,7 +138,7 @@ class DownloadManagerActivity : BaseFontActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun getDownloader() {
-        downloader = LStoreUtil.getDownloader(
+        downloaderPhoto = LStoreUtil.getDownloader(
                 folderName = Environment.DIRECTORY_PICTURES + "/" + "ZZZTestDownloader",
                 token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIwOGQ3MWQ2Zi0xZTc0LTYwYjQtOWJmMC1mM2E0YzVkMTkwZGUiLCJyb2xlIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAyIiwianRpIjoiMzUxMmZhM2YtYWMzNi00YmM2LWI5ZTEtOTEyMzc5Y2NlZjQ1IiwiRGF0YVR5cGVzIjoiMiIsIm5iZiI6MTU2OTQ3OTc1OSwiZXhwIjoxNTY5NTY5NzU5LCJpYXQiOjE1Njk0Nzk3NTksImlzcyI6Imh0dHBzOi8vZGV2LXBvcnRhbC52aW5ob21lcy52biIsImF1ZCI6Imh0dHBzOi8vZGV2LXBvcnRhbC52aW5ob21lcy52biJ9.6IfkQkMhI0g-XAbKdHNSH5HiP8fsRAJxnpojjyqwFBI,",
                 url = "https://images.unsplash.com/photo-1499909762747-883b3641e787?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1336&q=80",
