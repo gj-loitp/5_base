bb4d4c324c5e29397a8035a8ef26d803e7690ec8
v.loitp <v.Loitp@cloudhms.net>
2020-10-09 16:00:57 +0700

    update epub

--- core/src/main/java/com/core/helper/donate/FrmDonate.kt
+++ core/src/main/java/com/core/helper/donate/FrmDonate.kt
@@ -1,5 +1,6 @@
 package com.core.helper.donate
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import com.R
 import com.annotation.LogTag
 import com.core.base.BaseFragment
 import com.core.common.Constants
+import com.core.utilities.LAppResource
+import com.core.utilities.LSharedPrefsUtil
 import com.views.textview.textdecorator.LTextDecorator
 import kotlinx.android.synthetic.main.l_frm_donate.*
 
@@ -23,30 +26,32 @@ class FrmDonate : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupViews()
+        setupTheme()
     }
 
     private fun setupViews() {
         val text = Constants.DONATION_INFOR_LOITP
 
         LTextDecorator
-                .decorate(textView, text)
+                .decorate(textView = textView, content = text)
                 .setTextColor(R.color.red,
                         "❤ Vietcombank", "0371000106443",
                         "❤ Techcombank", "19034585806016",
                         "❤ Viet Capital Bank - Ngân hàng TMCP Bản Việt", "8007041105519",
                         "❤ VPBank", "166210585",
                         "❤ Momo", "0764088864")
-                //.setBackgroundColor(R.color.colorPrimary, "dolor", "elit")
-                //.strikethrough("Duis", "Praesent")
                 .underline("Chân thành cảm ơn!")
-                //.setSubscript("vitae")
-                //.makeTextClickable(new OnTextClickListener() {
-                //    @Override
-                //    public void onClick(View view, String text) {
-                //
-                //    }
-                //}, false, "porta", "commodo", "tempor venenatis nulla")
-                //.setTextColor(android.R.color.holo_green_light, "porta", "commodo", "tempor venenatis nulla")
                 .build()
+    }
+
+    private fun setupTheme() {
+        val isDarkTheme = LSharedPrefsUtil.instance.getBoolean(Constants.KEY_IS_DARK_THEME, true)
+        if (isDarkTheme) {
+            layoutRootViewDonate.setBackgroundColor(Color.BLACK)
+            textView.setTextColor(Color.WHITE)
+        } else {
+            layoutRootViewDonate.setBackgroundColor(LAppResource.getColor(R.color.whiteSmoke))
+            textView.setTextColor(Color.BLACK)
+        }
     }
 }
