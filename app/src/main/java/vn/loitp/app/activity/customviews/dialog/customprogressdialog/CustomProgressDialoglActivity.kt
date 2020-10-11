a89bafd24c5836f07608c0eceb279574feefd69e
Loitp <v.loitp@cloudhms.net>
2020-10-11 14:46:16 +0700

    update theme

--- app/src/main/java/vn/loitp/app/activity/customviews/dialog/customprogressdialog/CustomProgressDialoglActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/dialog/customprogressdialog/CustomProgressDialoglActivity.kt
@@ -7,6 +7,7 @@ import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LDialogUtil
 import com.core.utilities.LUIUtil
+import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_dialog_custom_progress.*
 import vn.loitp.app.R
 
@@ -18,7 +19,7 @@ class CustomProgressDialoglActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        bt0.setOnClickListener {
+        bt0.setSafeOnClickListener {
             val alertDialog = LDialogUtil.showCustomProgressDialog(context = this, amount = 0.1f)
             LUIUtil.setDelay(mls = 3000, runnable = Runnable {
                 alertDialog?.dismiss()
