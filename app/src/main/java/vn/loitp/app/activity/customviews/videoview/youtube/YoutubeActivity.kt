286b0f4a682be00a2c4cf1a1f3044a9bfa4a30bf
v.loitp <v.Loitp@cloudhms.net>
2020-10-13 11:15:52 +0700

    update style

--- app/src/main/java/vn/loitp/app/activity/customviews/videoview/youtube/YoutubeActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/videoview/youtube/YoutubeActivity.kt
@@ -6,6 +6,7 @@ import com.annotation.LayoutId
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LUIUtil
+import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_video_youtube.*
 import vn.loitp.app.R
 
@@ -17,10 +18,10 @@ class YoutubeActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        bt0.setOnClickListener {
+        bt0.setSafeOnClickListener {
             LUIUtil.playYoutube(activity = this, url = "https://www.youtube.com/watch?v=YE7VzlLtp-4&ab_channel=Blender")
         }
-        bt1.setOnClickListener {
+        bt1.setSafeOnClickListener {
             LUIUtil.playYoutubeWithId(activity = this, id = "YE7VzlLtp-4")
         }
     }
