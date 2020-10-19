81a55d5d8469786a02fef30b324c841538a3d299
v.loitp <v.Loitp@cloudhms.net>
2020-10-19 22:37:57 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/ads/MenuAdsActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/ads/MenuAdsActivity.kt
@@ -8,6 +8,7 @@ import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.helper.admobrewardedvideo.AdmobRewardedVideoActivity
 import com.core.utilities.LActivityUtil
+import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_menu_ads.*
 import vn.loitp.app.R
 import vn.loitp.app.activity.ads.admobbanner.AdmobBannerActivity
@@ -21,17 +22,17 @@ class MenuAdsActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        bt1.setOnClickListener {
+        bt1.setSafeOnClickListener {
             val intent = Intent(this, AdmobBannerActivity::class.java)
             startActivity(intent)
             LActivityUtil.tranIn(this)
         }
-        bt2.setOnClickListener {
+        bt2.setSafeOnClickListener {
             val intent = Intent(this, AdmobInterstitialActivity::class.java)
             startActivity(intent)
             LActivityUtil.tranIn(this)
         }
-        bt3.setOnClickListener {
+        bt3.setSafeOnClickListener {
             val intent = Intent(this, AdmobRewardedVideoActivity::class.java)
             intent.putExtra(AdmobRewardedVideoActivity.APP_ID, getString(R.string.str_app_id))
             intent.putExtra(AdmobRewardedVideoActivity.ID_REWARD, getString(R.string.str_reward))
