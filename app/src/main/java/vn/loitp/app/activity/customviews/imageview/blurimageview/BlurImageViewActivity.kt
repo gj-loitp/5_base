71412037d983256102df3529c68b75c12c54d3c5
v.loitp <v.Loitp@cloudhms.net>
2020-10-28 00:01:22 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/imageview/blurimageview/BlurImageViewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/imageview/blurimageview/BlurImageViewActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import com.annotation.IsFullScreen
 import com.annotation.LayoutId
 import com.annotation.LogTag
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.core.base.BaseFontActivity
 import com.core.common.Constants
-import com.core.utilities.LImageUtil
 import com.views.imageview.blur.LBlurImageView
+import jp.wasabeef.glide.transformations.BlurTransformation
 import kotlinx.android.synthetic.main.activity_imageview_blur.*
 import vn.loitp.app.R
 
@@ -22,7 +24,7 @@ class BlurImageViewActivity : BaseFontActivity() {
         setupViews()
     }
 
-    private fun setupViews(){
+    private fun setupViews() {
 //        method (load) :- load(int resource), load(Bitmap bitmap)
 //
 //        method(intesity):- intensity( int value) { Increase Blur and limit of value is in between 0 to 25 }
@@ -42,7 +44,11 @@ class BlurImageViewActivity : BaseFontActivity() {
                 .into(imageView)
 
         //imageView2
-        LImageUtil.load(context = this, url = Constants.URL_IMG_5, imageView = imageView2)
+//        LImageUtil.load(context = this, url = Constants.URL_IMG_5, imageView = imageView2)
 
+        Glide.with(this)
+                .load(Constants.URL_IMG)
+                .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
+                .into(imageView2)
     }
 }
