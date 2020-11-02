c3beea70098f8705abd9409d7c00389f169fe057
v.loitp <v.Loitp@cloudhms.net>
2020-11-02 14:08:09 +0700

    update

--- core/src/main/java/com/core/base/BaseModel.kt
+++ core/src/main/java/com/core/base/BaseModel.kt
@@ -4,4 +4,9 @@ import androidx.annotation.Keep
 import java.io.Serializable
 
 @Keep
-open class BaseModel : Serializable
+open class BaseModel : Serializable {
+
+    fun deepCopy(): BaseModel {
+        return BaseApplication.gson.fromJson(BaseApplication.gson.toJson(this), this.javaClass)
+    }
+}
