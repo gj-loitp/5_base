16097a79cabb7e513b3acb6d80032681542a83cc
v.loitp <v.Loitp@cloudhms.net>
2020-11-07 00:35:42 +0700

    update

---
+++ core/src/main/java/com/function/notification/config/VibrationSettings.kt
@@ -0,0 +1,25 @@
+package com.function.notification.config
+
+class VibrationSettings {
+
+    companion object {
+        var STD_VIBRATION = longArrayOf(300, 200, 300, 200)
+        var FAST_VIBRATION = longArrayOf(100, 100, 100, 100, 100, 100)
+    }
+
+    var pattern: LongArray? = null
+    var isVibrate = true
+
+    constructor(vararg arg: Long) {
+        pattern = arg
+    }
+
+    constructor(pattern: LongArray, vibrate: Boolean) {
+        this.pattern = pattern
+        isVibrate = vibrate
+    }
+
+    constructor(vibrate: Boolean) {
+        isVibrate = vibrate
+    }
+}
