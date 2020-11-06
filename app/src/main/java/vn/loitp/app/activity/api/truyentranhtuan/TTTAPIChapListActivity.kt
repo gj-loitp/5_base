c9427dec243b4a1030886a276db87298e1d8a239
v.loitp <v.Loitp@cloudhms.net>
2020-11-06 16:30:15 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/api/truyentranhtuan/TTTAPIChapListActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/api/truyentranhtuan/TTTAPIChapListActivity.kt
@@ -6,14 +6,17 @@ import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LUIUtil
-import kotlinx.android.synthetic.main.activity_api_ttt_chap_list.*
+import kotlinx.android.synthetic.main.activity_api_ttt_chap_list.indicatorView
+import kotlinx.android.synthetic.main.activity_api_ttt_chap_list.textView
+import kotlinx.android.synthetic.main.activity_api_ttt_chap_list.tvTitle
 import vn.loitp.app.R
-import vn.loitp.app.activity.api.truyentranhtuan.helper.chaplist.GetChapTask
-import vn.loitp.app.activity.api.truyentranhtuan.model.chap.TTTChap
+import vn.loitp.app.activity.api.truyentranhtuan.viewmodels.TTTViewModel
 
 @LogTag("TTTAPIChapListActivity")
 @IsFullScreen(false)
 class TTTAPIChapListActivity : BaseFontActivity() {
+
+    private var tttViewModel: TTTViewModel? = null
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_api_ttt_chap_list
@@ -22,23 +25,33 @@ class TTTAPIChapListActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val urlComic = "http://truyentranhtuan.com/one-piece/"
-        GetChapTask(
-                url = urlComic,
-                callback = object : GetChapTask.Callback {
-                    @SuppressLint("SetTextI18n")
-                    override fun onSuccess(tttChap: TTTChap?) {
-                        tttChap?.let {
-                            LUIUtil.printBeautyJson(tttChap, textView)
-                            indicatorView.smoothToHide()
-                            tvTitle.text = "Chap truyện One Piece - size: " + tttChap.chaps?.chap?.size
-                        }
-                    }
+        setupViewModels()
+        tttViewModel?.getListChap(link = "http://truyentranhtuan.com/one-piece/")
+    }
 
-                    override fun onError() {
-                        showShortError("onError")
+    @SuppressLint("SetTextI18n")
+    private fun setupViewModels() {
+        tttViewModel = getViewModel(TTTViewModel::class.java)
+        tttViewModel?.let { vm ->
+
+            vm.tttChapActionLiveData.observe(this, { actionData ->
+                val isDoing = actionData.isDoing
+                val isSuccess = actionData.isSuccess
+
+                if (isDoing == true) {
+                    indicatorView.smoothToShow()
+                } else {
+                    indicatorView.smoothToHide()
+                    if (isSuccess == true) {
+                        val tttChap = actionData.data
+                        tttChap?.let {
+                            LUIUtil.printBeautyJson(o = it, textView = textView)
+                        }
+                        tvTitle.text = "Chap truyện One Piece - size: " + tttChap?.chaps?.chap?.size
                     }
-                }).execute()
+                }
+            })
+        }
     }
 
 }
--- app/src/main/java/vn/loitp/app/activity/api/truyentranhtuan/viewmodels/TTTViewModel.kt
+++ app/src/main/java/vn/loitp/app/activity/api/truyentranhtuan/viewmodels/TTTViewModel.kt
@@ -10,6 +10,10 @@ import com.service.livedata.ActionLiveData
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import vn.loitp.app.activity.api.truyentranhtuan.model.chap.Chap
+import vn.loitp.app.activity.api.truyentranhtuan.model.chap.Chaps
+import vn.loitp.app.activity.api.truyentranhtuan.model.chap.Info
+import vn.loitp.app.activity.api.truyentranhtuan.model.chap.TTTChap
 import vn.loitp.app.activity.api.truyentranhtuan.model.comic.Comic
 
 /**
@@ -29,6 +33,7 @@ class TTTViewModel : BaseViewModel() {
     private val repository = ComicRepository(ComicApiClient.apiService)
 
     val listComicActionLiveData: ActionLiveData<ActionData<List<Comic>>> = ActionLiveData()
+    val tttChapActionLiveData: ActionLiveData<ActionData<TTTChap>> = ActionLiveData()
 
     fun getListComic(link: String) {
         listComicActionLiveData.set(
@@ -80,5 +85,74 @@ class TTTViewModel : BaseViewModel() {
             )
         }
 
+    }
+
+    fun getListChap(link: String) {
+        tttChapActionLiveData.set(
+                ActionData(isDoing = true)
+        )
+        logD(">>>getListChap link $link")
+
+        fun doTask(link: String): TTTChap {
+            val document: Document
+            val tttChap = TTTChap()
+            try {
+                val info = Info()
+                val listChap = ArrayList<Chap>()
+                document = Jsoup.connect(link).get()
+                val eInforBox = document.select("div#infor-box")
+
+                //img cover
+                val coverImage = eInforBox.select("img[src]")
+                // .com/wp-content/uploads/2014/08/anima-anh-bia.jpg
+                info.cover = coverImage.attr("src")
+
+                //infor comic
+                val infoComics = eInforBox.select("p")
+                val infoComicList = java.util.ArrayList<String>()
+                for (x in infoComics) {
+                    infoComicList.add(x.text())
+                }
+                try {
+                    info.otherName = infoComicList[0]
+                    info.author = infoComicList[1]
+                    info.type = infoComicList[2]
+                    info.newChap = infoComicList[3]
+                    info.summary = infoComicList[4]
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                val eChap = document.select("span[class=chapter-name]")
+                for (x in eChap) {
+                    val chap = Chap()
+                    chap.tit = x.text()
+                    val linkChap = x.select("a")
+                    // .com/anima-chuong-74/
+                    chap.url = linkChap.attr("href")
+                    listChap.add(chap)
+                }
+
+                val chaps = Chaps()
+                chaps.chap = listChap
+                tttChap.info = info
+                tttChap.chaps = chaps
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return tttChap
+        }
+
+        ioScope.launch {
+            val tttChap = doTask(link = link)
+            logD("tttChap " + BaseApplication.gson.toJson(tttChap))
+
+            tttChapActionLiveData.post(
+                    ActionData(
+                            isDoing = false,
+                            isSuccess = true,
+                            data = tttChap
+                    )
+            )
+        }
     }
 }
