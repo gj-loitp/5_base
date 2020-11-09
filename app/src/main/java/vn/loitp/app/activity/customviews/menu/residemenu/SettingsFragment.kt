423b491dbb9f5043b426f6dbe3ca5ae6a0ce0699
v.loitp <v.Loitp@cloudhms.net>
2020-11-09 17:55:46 +0700

    update

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/menu/residemenu/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package vn.loitp.app.activity.customviews.menu.residemenu
+
+import com.annotation.LogTag
+import com.core.base.BaseFragment
+import vn.loitp.app.R
+
+@LogTag("SettingsFragment")
+class SettingsFragment : BaseFragment() {
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.reside_menu_setting
+    }
+}
