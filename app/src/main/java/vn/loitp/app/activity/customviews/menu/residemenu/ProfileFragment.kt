c550fdc86a993f44563a312a4e0e00be5d5ac01a
v.loitp <v.Loitp@cloudhms.net>
2020-11-09 21:58:14 +0700

    update

---
+++ app/src/main/java/vn/loitp/app/activity/customviews/menu/residemenu/ProfileFragment.kt
@@ -0,0 +1,14 @@
+package vn.loitp.app.activity.customviews.menu.residemenu
+
+import com.annotation.LogTag
+import com.core.base.BaseFragment
+import vn.loitp.app.R
+
+@LogTag("ProfileFragment")
+class ProfileFragment : BaseFragment() {
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.reside_menu_profile
+    }
+
+}
