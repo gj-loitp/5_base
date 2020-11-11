b69b607fa842a7cebcc02cedfb207b0ab59d297f
v.loitp <v.Loitp@cloudhms.net>
2020-11-11 17:36:24 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/imageview/stfaiconimageviewer/ListActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/imageview/stfaiconimageviewer/ListActivity.kt
@@ -1,6 +1,7 @@
 package vn.loitp.app.activity.customviews.imageview.stfaiconimageviewer
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.recyclerview.widget.GridLayoutManager
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
@@ -8,6 +9,7 @@ import com.core.base.BaseFontActivity
 import com.core.utilities.LImageUtil
 import com.stfalcon.imageviewer.StfalconImageViewer
 import kotlinx.android.synthetic.main.activity_stfaiconimageviewer_list.*
+import kotlinx.android.synthetic.main.view_stf_overlay.view.*
 import vn.loitp.app.R
 import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie
 import vn.loitp.app.common.Constants
@@ -36,6 +38,11 @@ class ListActivity : BaseFontActivity() {
                 callback = object : StfAdapter.Callback {
                     override fun onClick(movie: Movie, mvList: MutableList<Movie>, position: Int) {
 
+                        val viewOverLay = LayoutInflater.from(this@ListActivity).inflate(R.layout.view_stf_overlay, null)
+                        viewOverLay.bt.setOnClickListener {
+                            showShortInformation("Click")
+                        }
+
                         StfalconImageViewer.Builder(
                                 this@ListActivity,
                                 mvList
@@ -47,8 +54,12 @@ class ListActivity : BaseFontActivity() {
                             )
                         }
                                 .withStartPosition(position)
+                                .withOverlayView(viewOverLay)
                                 .withImageChangeListener { pos ->
                                     logD("withImageChangeListener pos $pos")
+                                }
+                                .withDismissListener {
+                                    logD("withDismissListener")
                                 }
                                 .show(true)
                     }
