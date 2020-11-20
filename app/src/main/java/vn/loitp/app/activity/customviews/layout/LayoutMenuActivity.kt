f259635acf1b1a5526708ea72b6ffcd423621817
v.loitp <v.Loitp@cloudhms.net>
2020-11-20 23:23:05 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/LayoutMenuActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/LayoutMenuActivity.kt
@@ -35,6 +35,7 @@ import vn.loitp.app.activity.customviews.layout.squarelayout.SquareLayoutActivit
 import vn.loitp.app.activity.customviews.layout.swipebacklayout.SwipeBackLayoutActivity
 import vn.loitp.app.activity.customviews.layout.swiperefreshlayout.SwipeRefreshLayoutMenuActivity
 import vn.loitp.app.activity.customviews.layout.swipereveallayout.SwipeRevealLayoutActivity
+import vn.loitp.app.activity.customviews.layout.transformationlayout.transformationlayoutdemo.single.MainSingleActivity
 import vn.loitp.app.activity.customviews.layout.zoomlayout.ZoomLayoutActivity
 
 @LogTag("LayoutMenuActivity")
@@ -79,6 +80,7 @@ class LayoutMenuActivity : BaseFontActivity(), View.OnClickListener {
         btFlowLayout.setOnClickListener(this)
         btSplitPanelLayout.setOnClickListener(this)
         btTramsformationLayout.setOnClickListener(this)
+        btTramsformationLayoutSingle.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -111,6 +113,7 @@ class LayoutMenuActivity : BaseFontActivity(), View.OnClickListener {
             btFlowLayout -> intent = Intent(this, FlowLayoutActivity::class.java)
             btSplitPanelLayout -> intent = Intent(this, SplitPanelLayoutActivity::class.java)
             btTramsformationLayout -> intent = Intent(this, MainActivity::class.java)
+            btTramsformationLayoutSingle -> intent = Intent(this, MainSingleActivity::class.java)
         }
         intent?.let {
             startActivity(intent)
