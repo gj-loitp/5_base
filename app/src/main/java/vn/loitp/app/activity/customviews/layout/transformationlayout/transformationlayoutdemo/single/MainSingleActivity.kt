7f80c8e2e07a1b493ef6f6ac85e39b331d6d9006
v.loitp <v.Loitp@cloudhms.net>
2020-11-24 22:53:39 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/transformationlayout/transformationlayoutdemo/single/MainSingleActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/transformationlayout/transformationlayoutdemo/single/MainSingleActivity.kt
@@ -18,13 +18,20 @@ package vn.loitp.app.activity.customviews.layout.transformationlayout.transforma
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.annotation.IsFullScreen
+import com.annotation.LogTag
+import com.core.base.BaseFontActivity
 import vn.loitp.app.R
 
-class MainSingleActivity : AppCompatActivity() {
+@LogTag("MainSingleActivity")
+@IsFullScreen(false)
+class MainSingleActivity : BaseFontActivity() {
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_layout_transformation_single_main
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_layout_transformation_single_main)
 
         supportFragmentManager
                 .beginTransaction()
