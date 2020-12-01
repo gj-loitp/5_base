f508281aca84d09317bd9ee4a52333ece68379aa
v.loitp <v.Loitp@cloudhms.net>
2020-12-01 14:51:30 +0700

    update

--- core/src/main/java/com/utils/util/CloseUtils.kt
+++ core/src/main/java/com/utils/util/CloseUtils.kt
@@ -1,54 +1,31 @@
-package com.utils.util;
+package com.utils.util
 
-import java.io.Closeable;
-import java.io.IOException;
+import java.io.Closeable
+import java.io.IOException
 
-/**
- * <pre>
- *     author: Blankj
- *     blog  : http://blankj.com
- *     time  : 2016/10/09
- *     desc  : 关闭相关工具类
- * </pre>
- */
-public final class CloseUtils {
+class CloseUtils private constructor() {
 
-    private CloseUtils() {
-        throw new UnsupportedOperationException("u can't instantiate me...");
-    }
-
-    /**
-     * 关闭IO
-     *
-     * @param closeables closeables
-     */
-    public static void closeIO(Closeable... closeables) {
-        if (closeables == null) return;
-        for (Closeable closeable : closeables) {
-            if (closeable != null) {
+    companion object {
+        @JvmStatic
+        fun closeIO(vararg closeables: Closeable?) {
+            for (closeable in closeables) {
                 try {
-                    closeable.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    closeable?.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        fun closeIOQuietly(vararg closeables: Closeable?) {
+            for (closeable in closeables) {
+                try {
+                    closeable?.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
                 }
             }
         }
     }
 
-    /**
-     * 安静关闭IO
-     *
-     * @param closeables closeables
-     */
-    public static void closeIOQuietly(Closeable... closeables) {
-        if (closeables == null) return;
-        for (Closeable closeable : closeables) {
-            if (closeable != null) {
-                try {
-                    closeable.close();
-                } catch (IOException ignored) {
-                }
-            }
-        }
-    }
 }
