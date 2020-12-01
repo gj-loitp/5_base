2b7f1419a3b7cc5b7adbf3067690f0bd5e24126d
v.loitp <v.Loitp@cloudhms.net>
2020-12-01 16:06:07 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/netview/NetAdapter.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/netview/NetAdapter.kt
@@ -1,110 +1,55 @@
 package vn.loitp.app.activity.customviews.recyclerview.netview
 
-import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.annotation.LogTag
 import com.core.adapter.AnimationAdapter
-import com.core.utilities.LImageUtil
-import com.core.utilities.LScreenUtil
+import com.core.utilities.LStoreUtil
 import com.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.view_row_item_net.view.*
 import vn.loitp.app.R
-import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie
 
 @LogTag("NetAdapter")
-class NetAdapter(private val context: Context,
-                 private val column: Int,
-                 private val moviesList: MutableList<Movie>,
-                 private val callback: Callback?) :
-        AnimationAdapter() {
+class NetAdapter() : AnimationAdapter() {
 
-    private val sizeW: Int = LScreenUtil.screenWidth / column
-    private val sizeH: Int
-    private val sizeMarginTopBottom: Int
-    private val sizeMarginTopLeftRight: Int
+    private val listNet = ArrayList<Net>()
 
-    init {
-        sizeH = sizeW * 15 / 9
-        sizeMarginTopBottom = sizeW / 5
-        sizeMarginTopLeftRight = sizeW / 10
+    fun setListNet(listNet: ArrayList<Net>) {
+        this.listNet.clear()
+        this.listNet.addAll(listNet)
+        notifyDataSetChanged()
     }
 
-    interface Callback {
-        fun onClick(movie: Movie, position: Int)
-        fun onLongClick(movie: Movie, position: Int)
-        fun onLoadMore()
-    }
+    inner class NetViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    fun checkData() {
-        val indexEachColumn = itemCount % column
-        val missItemCount = column - indexEachColumn
-        if (missItemCount != column) {
-            for (i in 0 until missItemCount) {
-                val movie = Movie()
-                moviesList.add(movie)
-            }
-            notifyDataSetChanged()
+        fun bind(net: Net) {
+//            itemView.rootView.layoutParams.width = sizeW
+//            itemView.rootView.layoutParams.height = sizeH
+//            itemView.rootView.invalidate()
+
+            itemView.textView.text = net.name
+            LUIUtil.setTextShadow(textView = itemView.textView, color = Color.BLACK)
+            itemView.rootView.setBackgroundColor(LStoreUtil.randomColor)
+
+            //setAnimation(viewToAnimate = itemView.rootView, position = bindingAdapterPosition)
         }
     }
 
-    inner class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        fun bind(movie: Movie) {
-            itemView.rootView.layoutParams.width = sizeW
-            itemView.rootView.layoutParams.height = sizeH
-            itemView.rootView.invalidate()
-            val indexEachColumn = bindingAdapterPosition % column
-            itemView.textView.text = movie.title
-            itemView.textView.visibility = View.VISIBLE
-            when (indexEachColumn) {
-                0 -> {
-                    itemView.bkg.setImageResource(R.drawable.l_grid_item_background_left)
-                }
-                column - 1 -> {
-                    itemView.bkg.setImageResource(R.drawable.l_grid_item_background_right)
-                }
-                else -> {
-                    itemView.bkg.setImageResource(R.drawable.l_grid_item_background_center)
-                }
-            }
-            LUIUtil.setMargins(view = itemView.imageView, leftPx = sizeMarginTopLeftRight, topPx = sizeMarginTopBottom, rightPx = sizeMarginTopLeftRight, bottomPx = sizeMarginTopBottom)
-            val url = movie.cover
-            if (url != null) {
-                LImageUtil.load(context = context, any = url, imageView = itemView.imageView)
-            } else {
-                itemView.imageView.setImageResource(0)
-                itemView.textView.visibility = View.INVISIBLE
-            }
-            itemView.rootView.setOnClickListener {
-                callback?.onClick(movie, bindingAdapterPosition)
-            }
-            itemView.rootView.setOnLongClickListener {
-                callback?.onLongClick(movie, bindingAdapterPosition)
-                true
-            }
-            if (bindingAdapterPosition == moviesList.size - 1) {
-                callback?.onLoadMore()
-            }
-
-            setAnimation(viewToAnimate = itemView.imageView, position = bindingAdapterPosition)
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NetViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view_row_item_net, parent, false)
-        return MovieViewHolder(itemView)
+        return NetViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is MovieViewHolder) {
-            holder.bind(moviesList[position])
+        if (holder is NetViewHolder) {
+            holder.bind(listNet[position])
         }
     }
 
     override fun getItemCount(): Int {
-        return moviesList.size
+        return listNet.size
     }
 }
--- app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/netview/NetViewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/netview/NetViewActivity.kt
@@ -7,17 +7,17 @@ import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import kotlinx.android.synthetic.main.activity_net_view.*
 import vn.loitp.app.R
-import vn.loitp.app.activity.customviews.recyclerview.bookview.BookAdapter
 import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie
 import vn.loitp.app.common.Constants
 import java.util.*
+import kotlin.collections.ArrayList
 
 @LogTag("NetViewActivity")
 @IsFullScreen(false)
 class NetViewActivity : BaseFontActivity() {
 
-    private val movieList: MutableList<Movie> = ArrayList()
-    private var netAdapter: NetAdapter? = null
+    private val listNet = ArrayList<Net>()
+    private var netAdapter = NetAdapter()
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_net_view
@@ -26,45 +26,22 @@ class NetViewActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        netAdapter = NetAdapter(context = this, column = 3, moviesList = movieList,
-                callback = object : NetAdapter.Callback {
-                    override fun onClick(movie: Movie, position: Int) {
-                        showShortInformation("Click " + movie.title)
-                    }
-
-                    override fun onLongClick(movie: Movie, position: Int) {
-                        val isRemoved = movieList.remove(movie)
-                        if (isRemoved) {
-                            netAdapter?.apply {
-                                notifyItemRemoved(position)
-                                notifyItemRangeChanged(position, movieList.size)
-                                checkData()
-                            }
-                        }
-                    }
-
-                    override fun onLoadMore() {}
-                })
-        rvNetView.layoutManager = GridLayoutManager(this, 3)
-        rvNetView.adapter = netAdapter
-        prepareMovieData()
+        setupViews()
+        setupData()
     }
 
-    private fun prepareMovieData() {
-        var cover: String
-        for (i in 0..99) {
-            cover = if (i % 2 == 0) {
-                Constants.URL_IMG_1
-            } else {
-                Constants.URL_IMG_2
-            }
-            val movie = Movie("Loitp $i", "Action & Adventure $i", "Year: $i", cover)
-            movieList.add(movie)
+    private fun setupViews() {
+        rvNetView.layoutManager = GridLayoutManager(this, 3)
+        rvNetView.adapter = netAdapter
+    }
+
+    private fun setupData() {
+        for (i in 0..10) {
+            val net = Net()
+            net.name = "$i"
+            listNet.add(net)
         }
-        netAdapter?.apply {
-            checkData()
-            notifyDataSetChanged()
-        }
+        netAdapter.setListNet(listNet)
     }
 
 }
