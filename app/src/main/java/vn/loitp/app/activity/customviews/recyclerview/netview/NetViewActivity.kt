a75eef9b2ec398712e03a569eaf3f283cb531595
v.loitp <v.Loitp@cloudhms.net>
2020-12-01 15:42:28 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/netview/NetViewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/netview/NetViewActivity.kt
@@ -1,34 +1,70 @@
 package vn.loitp.app.activity.customviews.recyclerview.netview
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
-import com.core.utilities.LActivityUtil
-import kotlinx.android.synthetic.main.activity_recycler_view_menu.*
+import kotlinx.android.synthetic.main.activity_net_view.*
 import vn.loitp.app.R
-import vn.loitp.app.activity.customviews.recyclerview.bookview.BookViewActivity
-import vn.loitp.app.activity.customviews.recyclerview.concatadapter.ConcatAdapterActivity
-import vn.loitp.app.activity.customviews.recyclerview.diffutil.DiffUtilActivity
-import vn.loitp.app.activity.customviews.recyclerview.footer.RecyclerViewFooterActivity
-import vn.loitp.app.activity.customviews.recyclerview.footer2.RecyclerViewFooter2Activity
-import vn.loitp.app.activity.customviews.recyclerview.gallerylayoutmanager.GalleryLayoutManagerHorizontalActivity
-import vn.loitp.app.activity.customviews.recyclerview.gallerylayoutmanager.GalleryLayoutManagerVerticalActivity
-import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.RecyclerViewActivity
-import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerviewwithsingletondata.RecyclerViewWithSingletonDataActivity
-import vn.loitp.app.activity.customviews.recyclerview.normalwithspansize.RecyclerViewWithSpanSizeActivity
-import vn.loitp.app.activity.customviews.recyclerview.parallaxrecyclerview.ParallaxRecyclerViewActivity
-import vn.loitp.app.activity.customviews.recyclerview.parallaxrecyclerviewyayandroid.RecyclerViewParallaxYayaActivity
-import vn.loitp.app.activity.customviews.recyclerview.recyclertablayout.RecyclerTabLayoutMenuActivity
+import vn.loitp.app.activity.customviews.recyclerview.bookview.BookAdapter
+import vn.loitp.app.activity.customviews.recyclerview.normalrecyclerview.Movie
+import vn.loitp.app.common.Constants
+import java.util.*
 
 @LogTag("NetViewActivity")
 @IsFullScreen(false)
 class NetViewActivity : BaseFontActivity() {
 
+    private val movieList: MutableList<Movie> = ArrayList()
+    private var netAdapter: NetAdapter? = null
+
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_net_view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        netAdapter = NetAdapter(context = this, column = 3, moviesList = movieList,
+                callback = object : NetAdapter.Callback {
+                    override fun onClick(movie: Movie, position: Int) {
+                        showShortInformation("Click " + movie.title)
+                    }
+
+                    override fun onLongClick(movie: Movie, position: Int) {
+                        val isRemoved = movieList.remove(movie)
+                        if (isRemoved) {
+                            netAdapter?.apply {
+                                notifyItemRemoved(position)
+                                notifyItemRangeChanged(position, movieList.size)
+                                checkData()
+                            }
+                        }
+                    }
+
+                    override fun onLoadMore() {}
+                })
+        rvNetView.layoutManager = GridLayoutManager(this, 3)
+        rvNetView.adapter = netAdapter
+        prepareMovieData()
+    }
+
+    private fun prepareMovieData() {
+        var cover: String
+        for (i in 0..99) {
+            cover = if (i % 2 == 0) {
+                Constants.URL_IMG_1
+            } else {
+                Constants.URL_IMG_2
+            }
+            val movie = Movie("Loitp $i", "Action & Adventure $i", "Year: $i", cover)
+            movieList.add(movie)
+        }
+        netAdapter?.apply {
+            checkData()
+            notifyDataSetChanged()
+        }
     }
 
 }
