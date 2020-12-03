429b801b214dc6fae14f0f1aca9c8a1264c53e9d
v.loitp <v.Loitp@cloudhms.net>
2020-12-03 22:23:33 +0700

    update

--- core/src/main/java/com/game/findnumber/MenuActivity.kt
+++ core/src/main/java/com/game/findnumber/MenuActivity.kt
@@ -1,13 +1,16 @@
 package com.game.findnumber
 
 import android.os.Bundle
+import android.view.View
 import com.R
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LAnimationUtil
 import com.core.utilities.LScreenUtil
 import com.core.utilities.LSocialUtil
 import com.core.utilities.LUIUtil
+import com.daimajia.androidanimations.library.Techniques
 import kotlinx.android.synthetic.main.l_activity_find_number_menu.*
 
 @LogTag("MenuActivity")
@@ -29,6 +32,14 @@ class MenuActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        LUIUtil.setDelay(200) {
+            ivTitle?.visibility = View.VISIBLE
+            LAnimationUtil.play(
+                    view = ivTitle,
+                    duration = 1000,
+                    techniques = Techniques.ZoomInDown
+            )
+        }
         LUIUtil.setSafeOnClickListenerElastic(
                 view = ivBack,
                 runnable = {
