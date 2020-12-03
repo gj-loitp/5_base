0c14b3fff7a49cd17e07ac422d9f722e5d051a80
v.loitp <v.Loitp@cloudhms.net>
2020-12-03 22:32:08 +0700

    update

--- core/src/main/java/com/game/findnumber/SplashActivity.kt
+++ core/src/main/java/com/game/findnumber/SplashActivity.kt
@@ -2,14 +2,16 @@ package com.game.findnumber
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.R
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
-import com.core.utilities.LActivityUtil
-import com.core.utilities.LScreenUtil
-import com.core.utilities.LUIUtil
-import com.core.utilities.LValidateUtil
+import com.core.utilities.*
+import com.daimajia.androidanimations.library.Techniques
+import kotlinx.android.synthetic.main.l_activity_find_number_menu.*
+import kotlinx.android.synthetic.main.l_activity_find_number_menu.ivTitle
+import kotlinx.android.synthetic.main.l_activity_find_number_splash.*
 
 @LogTag("SplashActivity")
 @IsFullScreen(true)
@@ -32,12 +34,22 @@ class SplashActivity : BaseFontActivity() {
     }
 
     private fun goToHome() {
-        LUIUtil.setDelay(mls = 1000, runnable = {
-            val intent = Intent(this, MenuActivity::class.java)
-            startActivity(intent)
-            LActivityUtil.tranIn(this)
-            finish()
-        })
+        LUIUtil.setDelay(100) {
+            ivFindTheNumber?.visibility = View.VISIBLE
+            LAnimationUtil.play(
+                    view = ivFindTheNumber,
+                    duration = 1000,
+                    techniques = Techniques.ZoomIn,
+                    onEnd = {
+                        if (!this.isFinishing) {
+                            val intent = Intent(this, MenuActivity::class.java)
+                            startActivity(intent)
+                            LActivityUtil.tranIn(this)
+                            finish()
+                        }
+                    }
+            )
+        }
     }
 
 }
