22a61619e4783812a6f72c7f83d2e864d7785b0f
v.loitp <v.Loitp@cloudhms.net>
2020-12-04 10:36:39 +0700

    update

--- core/src/main/java/com/views/compass/CompassListener.kt
+++
@@ -1,9 +0,0 @@
-package com.views.compass
-
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-
-interface CompassListener {
-    fun onSensorChanged(event: SensorEvent)
-    fun onAccuracyChanged(sensor: Sensor, accuracy: Int)
-}
