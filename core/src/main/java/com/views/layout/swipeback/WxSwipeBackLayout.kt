38970a3212eae43fa5ca4c964c89b869e82de2af
v.loitp <v.Loitp@cloudhms.net>
2020-12-08 14:35:20 +0700

    update

--- core/src/main/java/com/views/layout/swipeback/WxSwipeBackLayout.kt
+++ core/src/main/java/com/views/layout/swipeback/WxSwipeBackLayout.kt
@@ -1,51 +1,43 @@
-package com.views.layout.swipeback;
+package com.views.layout.swipeback
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.View;
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.views.layout.swipeback.WxSwipeBackLayout
+import com.views.layout.swipeback.tools.Util.onPanelReset
+import com.views.layout.swipeback.tools.Util.onPanelSlide
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+class WxSwipeBackLayout @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : SwipeBackLayout(context, attrs, defStyleAttr) {
 
-import com.views.layout.swipeback.tools.Util;
-
-public class WxSwipeBackLayout extends SwipeBackLayout {
-    private static final String TAG = WxSwipeBackLayout.class.getSimpleName();
-
-    public WxSwipeBackLayout(@NonNull Context context) {
-        this(context, null);
+    companion object {
+        private val TAG = WxSwipeBackLayout::class.java.simpleName
     }
 
-    public WxSwipeBackLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public WxSwipeBackLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        OnSwipeBackListener defaultSwipeBackListener = new OnSwipeBackListener() {
-            @Override
-            public void onViewPositionChanged(View mView, float swipeBackFraction, float swipeBackFactor) {
-                invalidate();
-                Util.onPanelSlide(swipeBackFraction);
-            }
-
-            @Override
-            public void onViewSwipeFinished(View mView, boolean isEnd) {
-                if (isEnd) {
-                    finish();
-                }
-                Util.onPanelReset();
-            }
-        };
-        setSwipeBackListener(defaultSwipeBackListener);
-    }
-
-    @Override
-    public void setDirectionMode(int direction) {
-        super.setDirectionMode(direction);
-        if (direction != SwipeBackLayout.FROM_LEFT) {
-            throw new IllegalArgumentException("The direction of WxSwipeBackLayout must be FROM_LEFT");
+    override var directionMode: Int
+        get() = super.directionMode
+        set(direction) {
+            super.directionMode = direction
+            require(direction == FROM_LEFT) { "The direction of WxSwipeBackLayout must be FROM_LEFT" }
         }
-    }
 
+    init {
+        val defaultSwipeBackListener: OnSwipeBackListener = object : OnSwipeBackListener {
+            override fun onViewPositionChanged(mView: View?, swipeBackFraction: Float, swipeBackFactor: Float) {
+                invalidate()
+                onPanelSlide(swipeBackFraction)
+            }
+
+            override fun onViewSwipeFinished(mView: View?, isEnd: Boolean) {
+                if (isEnd) {
+                    finish()
+                }
+                onPanelReset()
+            }
+        }
+        setSwipeBackListener(defaultSwipeBackListener)
+    }
 }
