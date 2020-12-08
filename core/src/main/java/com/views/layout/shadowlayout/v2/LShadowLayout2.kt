94d13c26d89ce65d4701dbf0e749e88999d3ed9c
v.loitp <v.Loitp@cloudhms.net>
2020-12-08 15:08:18 +0700

    update

--- core/src/main/java/com/views/layout/shadowlayout/v2/LShadowLayout2.kt
+++ core/src/main/java/com/views/layout/shadowlayout/v2/LShadowLayout2.kt
@@ -1,171 +1,125 @@
-package com.views.layout.shadowlayout.v2;
+package com.views.layout.shadowlayout.v2
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.FrameLayout;
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import com.R
+import com.core.utilities.LAppResource
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.view.ViewCompat;
+class LShadowLayout2 @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
 
-import com.R;
-
-public class LShadowLayout2 extends FrameLayout {
-
-    public static final int ALL = 0x1111;
-
-    public static final int LEFT = 0x0001;
-
-    public static final int TOP = 0x0010;
-
-    public static final int RIGHT = 0x0100;
-
-    public static final int BOTTOM = 0x1000;
-
-    public static final int SHAPE_RECTANGLE = 0x0001;
-
-    public static final int SHAPE_OVAL = 0x0010;
-
-    /**
-     * 阴影的颜色
-     */
-    private int mShadowColor = Color.TRANSPARENT;
-
-    /**
-     * 阴影的大小范围
-     */
-    private float mShadowRadius = 0;
-
-    /**
-     * 阴影 x 轴的偏移量
-     */
-    private float mShadowDx = 0;
-
-    /**
-     * 阴影 y 轴的偏移量
-     */
-    private float mShadowDy = 0;
-
-    /**
-     * 阴影显示的边界
-     */
-    private int mShadowSide = ALL;
-
-    /**
-     * 阴影的形状，圆形/矩形
-     */
-    private int mShadowShape = SHAPE_RECTANGLE;
-
-    private ShadowDrawable mShadowDrawable;
-
-    public LShadowLayout2(@NonNull Context context) {
-        this(context, null, 0);
+    companion object {
+        const val ALL = 0x1111
+        const val LEFT = 0x0001
+        const val TOP = 0x0010
+        const val RIGHT = 0x0100
+        const val BOTTOM = 0x1000
+        const val SHAPE_RECTANGLE = 0x0001
+        const val SHAPE_OVAL = 0x0010
     }
 
-    public LShadowLayout2(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
+    private var mShadowColor = Color.TRANSPARENT
+    private var mShadowRadius = 0f
+    private var mShadowDx = 0f
+    private var mShadowDy = 0f
+    private var mShadowSide = ALL
+
+    private var mShadowShape = SHAPE_RECTANGLE
+    private var mShadowDrawable: ShadowDrawable? = null
+
+    @SuppressLint("CustomViewStyleable")
+    private fun initialize(attrs: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.LShadowLayout)
+        mShadowShape = typedArray.getInt(R.styleable.LShadowLayout_shadowShape, SHAPE_RECTANGLE)
+        mShadowRadius = typedArray.getDimension(R.styleable.LShadowLayout_shadowRadius, 0f)
+        mShadowColor = typedArray.getColor(R.styleable.LShadowLayout_shadowColor, LAppResource.getColor(R.color.black))
+        mShadowDx = typedArray.getDimension(R.styleable.LShadowLayout_shadowDx, 0f)
+        mShadowDy = typedArray.getDimension(R.styleable.LShadowLayout_shadowDy, 0f)
+        mShadowSide = typedArray.getInt(R.styleable.LShadowLayout_shadowSide, ALL)
+        typedArray.recycle()
+        mShadowDrawable = ShadowDrawable(
+                mShape = mShadowShape,
+                shadowColor = mShadowColor,
+                mShadowRadius = mShadowRadius,
+                mOffsetX = mShadowDx,
+                mOffsetY = mShadowDy
+        )
+        setLayerType(LAYER_TYPE_SOFTWARE, null)
     }
 
-    public LShadowLayout2(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        initialize(attrs);
-    }
+    @Suppress("NAME_SHADOWING")
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        var widthMeasureSpec = widthMeasureSpec
+        var heightMeasureSpec = heightMeasureSpec
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-    private void initialize(@Nullable AttributeSet attrs) {
-        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.LShadowLayout);
-        if (typedArray != null) {
-            mShadowShape = typedArray.getInt(R.styleable.LShadowLayout_shadowShape, SHAPE_RECTANGLE);
-            mShadowRadius = typedArray.getDimension(R.styleable.LShadowLayout_shadowRadius, 0);
-            mShadowColor = typedArray.getColor(R.styleable.LShadowLayout_shadowColor,
-                    getContext().getResources().getColor(android.R.color.black));
-            mShadowDx = typedArray.getDimension(R.styleable.LShadowLayout_shadowDx, 0);
-            mShadowDy = typedArray.getDimension(R.styleable.LShadowLayout_shadowDy, 0);
-            mShadowSide = typedArray.getInt(R.styleable.LShadowLayout_shadowSide, ALL);
-
-            typedArray.recycle();
-        }
-
-        mShadowDrawable = new ShadowDrawable(mShadowShape, mShadowColor,
-                mShadowRadius, mShadowDx, mShadowDy);
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null);       // 关闭硬件加速
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        float effect = mShadowRadius;
-        float rectLeft = 0;
-        float rectTop = 0;
-        float rectRight = this.getMeasuredWidth();
-        float rectBottom = this.getMeasuredHeight();
+        val effect = mShadowRadius
+        var rectLeft = 0f
+        var rectTop = 0f
+        var rectRight = this.measuredWidth.toFloat()
+        var rectBottom = this.measuredHeight.toFloat()
         //Log.i("ShadowLayout", "ShadowLayout onMeasure getMeasuredWidth " + this.getMeasuredWidth());
         //Log.i("ShadowLayout", "ShadowLayout onMeasure getMeasuredHeight " + this.getMeasuredHeight());
-        this.getWidth();
-        if ((mShadowSide & LEFT) == LEFT) {
-            rectLeft = -effect;
+        this.width
+        if (mShadowSide and LEFT == LEFT) {
+            rectLeft = -effect
         }
-        if ((mShadowSide & TOP) == TOP) {
-            rectTop = -effect;
+        if (mShadowSide and TOP == TOP) {
+            rectTop = -effect
         }
-        if ((mShadowSide & RIGHT) == RIGHT) {
-            rectRight = this.getMeasuredWidth() + effect;
+        if (mShadowSide and RIGHT == RIGHT) {
+            rectRight = this.measuredWidth + effect
         }
-        if ((mShadowSide & BOTTOM) == BOTTOM) {
-            rectBottom = this.getMeasuredHeight() + effect;
+        if (mShadowSide and BOTTOM == BOTTOM) {
+            rectBottom = this.measuredHeight + effect
         }
         if (mShadowDy != 0.0f) {
-            rectBottom = rectBottom + mShadowDy;
+            rectBottom += mShadowDy
         }
         if (mShadowDx != 0.0f) {
-            rectRight = rectRight + mShadowDx;
+            rectRight += mShadowDx
         }
-        widthMeasureSpec = MeasureSpec.makeMeasureSpec(MeasureSpec.getSize((int) (rectRight - rectLeft)), MeasureSpec.EXACTLY);
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec(MeasureSpec.getSize((int) (rectBottom - rectTop)), MeasureSpec.EXACTLY);
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        widthMeasureSpec = MeasureSpec.makeMeasureSpec(MeasureSpec.getSize((rectRight - rectLeft).toInt()), MeasureSpec.EXACTLY)
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(MeasureSpec.getSize((rectBottom - rectTop).toInt()), MeasureSpec.EXACTLY)
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         //Log.i("ShadowLayout", "ShadowLayout onMeasure getMeasuredWidth " + px2dip(this.getMeasuredWidth()));
         //Log.i("ShadowLayout", "ShadowLayout onMeasure getMeasuredHeight " + px2dip(this.getMeasuredHeight()));
     }
 
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
+    override fun dispatchDraw(canvas: Canvas) {
         //Log.i("ShadowLayout", "ShadowLayout dispatchDraw");
-        super.dispatchDraw(canvas);
-        ViewCompat.setBackground(LShadowLayout2.this, mShadowDrawable);
+        super.dispatchDraw(canvas)
+        ViewCompat.setBackground(this@LShadowLayout2, mShadowDrawable)
     }
 
-    @Override
-    public void setPadding(int left, int top, int right, int bottom) {
+    override fun setPadding(left: Int, top: Int, right: Int, bottom: Int) {
         // NO OP
     }
 
-    @Override
-    public void setPaddingRelative(int start, int top, int end, int bottom) {
+    override fun setPaddingRelative(start: Int, top: Int, end: Int, bottom: Int) {
         // NO OP
     }
 
-    /**
-     * dip2px dp 值转 px 值
-     *
-     * @param dpValue dp 值
-     * @return px 值
-     */
-    private float dp2Px(float dpValue) {
-        DisplayMetrics dm = getContext().getResources().getDisplayMetrics();
-        float scale = dm.density;
-        return (dpValue * scale + 0.5F);
+    private fun dp2Px(dpValue: Float): Float {
+        val dm = context.resources.displayMetrics
+        val scale = dm.density
+        return dpValue * scale + 0.5f
     }
 
-    /**
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
-     */
-    private int px2dip(float pxValue) {
-        final float scale = getContext().getResources().getDisplayMetrics().density;
-        return (int) (pxValue / scale + 0.5f);
+    private fun px2dip(pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+
+    init {
+        initialize(attrs)
     }
 }
-
