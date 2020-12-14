90a5bf7ff5809ce8174640d7a0ab58c4fef3f051
v.loitp <v.Loitp@cloudhms.net>
2020-12-14 14:31:59 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/wwlmusic/WWLActivityMusic.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/wwlmusic/WWLActivityMusic.kt
@@ -1,130 +1,117 @@
-package vn.loitp.app.activity.customviews.wwlmusic;
+package vn.loitp.app.activity.customviews.wwlmusic
 
-import android.os.Bundle;
-
-import com.annotation.IsFullScreen;
-import com.annotation.LogTag;
-import com.core.base.BaseFontActivity;
-import com.views.wwlmusic.layout.LWWLMusic;
-import com.views.wwlmusic.utils.LWWLMusicUiUtil;
-
-import vn.loitp.app.R;
-import vn.loitp.app.activity.customviews.wwlmusic.fragments.WWLPlaylistFragment;
-import vn.loitp.app.activity.customviews.wwlmusic.fragments.WWLWatchFragment;
-import vn.loitp.app.activity.customviews.wwlmusic.interfaces.FragmentHost;
-import vn.loitp.app.activity.customviews.wwlmusic.utils.WWLMusicDataset;
+import android.os.Bundle
+import com.annotation.IsFullScreen
+import com.annotation.LogTag
+import com.core.base.BaseFontActivity
+import com.views.wwlmusic.layout.LWWLMusic
+import com.views.wwlmusic.utils.LWWLMusicUiUtil.hideSystemUI
+import com.views.wwlmusic.utils.LWWLMusicUiUtil.showSystemUI
+import com.views.wwlmusic.utils.LWWLMusicUiUtil.updateStatusBarAlpha
+import kotlinx.android.synthetic.main.activity_wwl_music.*
+import vn.loitp.app.R
+import vn.loitp.app.activity.customviews.wwlmusic.fragments.WWLPlaylistFragment
+import vn.loitp.app.activity.customviews.wwlmusic.fragments.WWLWatchFragment
+import vn.loitp.app.activity.customviews.wwlmusic.interfaces.FragmentHost
+import vn.loitp.app.activity.customviews.wwlmusic.utils.WWLMusicDataset
 
 //https://github.com/vn-ttinc/Youtube-Watch-While-Layout
 @LogTag("WWLActivityMusic")
 @IsFullScreen(false)
-//TODO convert kotlin
-public class WWLActivityMusic extends BaseFontActivity implements LWWLMusic.Listener, FragmentHost {
-    private LWWLMusic LWWLMusic;
-    private float mLastAlpha;
-    private WWLWatchFragment watchFragment;
-    private WWLPlaylistFragment playlistFragment;
+class WWLActivityMusic : BaseFontActivity(), LWWLMusic.Listener, FragmentHost {
 
-    @Override
-    protected int setLayoutResourceId() {
-        return R.layout.activity_wwl_music;
+    private var mLastAlpha = 0f
+    private var frmWatch: WWLWatchFragment? = null
+    private var frmPlaylist: WWLPlaylistFragment? = null
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_wwl_music
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        this.LWWLMusic = findViewById(R.id.watchWhileLayout);
-        this.LWWLMusic.setListener(this);
-
-        this.watchFragment = (WWLWatchFragment) getSupportFragmentManager().findFragmentById(R.id.frmWatch);
-        this.playlistFragment = (WWLPlaylistFragment) getSupportFragmentManager().findFragmentById(R.id.frmPlaylist);
+        watchWhileLayout.setListener(this)
+        frmWatch = supportFragmentManager.findFragmentById(R.id.frmWatch) as WWLWatchFragment?
+        frmPlaylist = supportFragmentManager.findFragmentById(R.id.frmPlaylist) as WWLPlaylistFragment?
     }
 
-    @Override
-    public void WWL_onSliding(float offset) {
-        float alpha;
+    override fun WWL_onSliding(offset: Float) {
+        val alpha: Float
         if (offset > 2.0f) {
-            alpha = this.mLastAlpha * (3.0f - offset);
+            alpha = mLastAlpha * (3.0f - offset)
         } else {
-            if (offset > 1.0f) {
-                alpha = 0.25f + (0.75f * (2.0f - offset));
+            alpha = if (offset > 1.0f) {
+                0.25f + 0.75f * (2.0f - offset)
             } else {
-                alpha = 1.0f;
+                1.0f
             }
-            if (offset >= 0.0f && offset <= 1.0f) {
-                LWWLMusicUiUtil.updateStatusBarAlpha(this, 1.0f - offset);
+            if (offset in 0.0f..1.0f) {
+                updateStatusBarAlpha(activity = this, alpha = 1.0f - offset)
             }
         }
-        this.mLastAlpha = alpha;
+        mLastAlpha = alpha
     }
 
-    @Override
-    public void WWL_onClicked() {
-        if (this.LWWLMusic.mState == com.views.wwlmusic.layout.LWWLMusic.STATE_MINIMIZED) {
-            this.LWWLMusic.maximize(false);
+    override fun WWL_onClicked() {
+        if (watchWhileLayout.mState == LWWLMusic.STATE_MINIMIZED) {
+            watchWhileLayout.maximize(false)
         }
-        if (this.LWWLMusic.mState == com.views.wwlmusic.layout.LWWLMusic.STATE_MAXIMIZED) {
-            this.watchFragment.toggleControls();
+        if (watchWhileLayout.mState == LWWLMusic.STATE_MAXIMIZED) {
+            frmWatch?.toggleControls()
         }
     }
 
-    @Override
-    public void WWL_onHided() {
-        this.watchFragment.stopPlay();
+    override fun WWL_onHided() {
+        frmWatch?.stopPlay()
     }
 
-    @Override
-    protected void onDestroy() {
-        this.watchFragment.stopPlay();
-        super.onDestroy();
+    override fun onDestroy() {
+        frmWatch?.stopPlay()
+        super.onDestroy()
     }
 
-    @Override
-    public void WWL_minimized() {
-        this.mLastAlpha = 0.0f;
-        this.watchFragment.hideControls();
+    override fun WWL_minimized() {
+        mLastAlpha = 0.0f
+        frmWatch?.hideControls()
     }
 
-    @Override
-    public void WWL_maximized() {
-        this.mLastAlpha = 1.0f;
+    override fun WWL_maximized() {
+        mLastAlpha = 1.0f
     }
 
-    @Override
-    public void goToDetail(WWLMusicDataset.DatasetItem item) {
-        if (this.LWWLMusic.mState == com.views.wwlmusic.layout.LWWLMusic.STATE_HIDED) {
-            this.LWWLMusic.mState = com.views.wwlmusic.layout.LWWLMusic.STATE_MAXIMIZED;
-            this.LWWLMusic.mIsFullscreen = false;
-            if (this.LWWLMusic.canAnimate()) {
-                this.LWWLMusic.setAnimatePos(this.LWWLMusic.mMaxY);
+    override fun goToDetail(item: WWLMusicDataset.DatasetItem) {
+        if (watchWhileLayout.mState == LWWLMusic.STATE_HIDED) {
+            watchWhileLayout.mState = LWWLMusic.STATE_MAXIMIZED
+            watchWhileLayout.mIsFullscreen = false
+
+            watchWhileLayout.apply {
+                if (this.canAnimate()) {
+                    this.setAnimatePos(this.mMaxY)
+                }
+                this.reset()
             }
-            this.LWWLMusic.reset();
         }
-        this.LWWLMusic.maximize(false);
-
-        this.watchFragment.startPlay(item);
-        if (this.playlistFragment != null) {
-            this.playlistFragment.updateItem(item);
-        }
+        watchWhileLayout.maximize(false)
+        frmWatch?.startPlay(item)
+        frmPlaylist?.updateItem(item)
     }
 
-    @Override
-    public void onVideoCollapse() {
-        LWWLMusicUiUtil.showSystemUI(this);
-        this.LWWLMusic.exitFullscreenToMinimize();
-        this.watchFragment.switchFullscreen(false);
-        this.LWWLMusic.minimize(false);
+    override fun onVideoCollapse() {
+        showSystemUI(this)
+        watchWhileLayout.exitFullscreenToMinimize()
+        frmWatch?.switchFullscreen(false)
+        watchWhileLayout.minimize(false)
     }
 
-    @Override
-    public void onVideoFullscreen(boolean selected) {
+    override fun onVideoFullscreen(selected: Boolean) {
         if (selected) {
-            LWWLMusicUiUtil.hideSystemUI(this);
-            this.LWWLMusic.enterFullscreen();
+            hideSystemUI(this)
+            watchWhileLayout.enterFullscreen()
         } else {
-            LWWLMusicUiUtil.showSystemUI(this);
-            this.LWWLMusic.exitFullscreen();
+            showSystemUI(this)
+            watchWhileLayout.exitFullscreen()
         }
-        this.watchFragment.switchFullscreen(selected);
+        frmWatch?.switchFullscreen(selected)
     }
 }
