bfe80e64f5b597f26b541a6b7c1ae522f5cb5a78
v.loitp <v.Loitp@cloudhms.net>
2020-12-14 11:47:13 +0700

    update

--- core/src/main/java/com/views/wwlvideo/LWWLVideo.java
+++ core/src/main/java/com/views/wwlvideo/LWWLVideo.java
@@ -32,6 +32,7 @@ import static android.view.MotionEvent.ACTION_MOVE;
 import static android.view.MotionEvent.ACTION_POINTER_UP;
 import static android.view.MotionEvent.ACTION_UP;
 
+//14.12.2020 try to convert to kotlin but failed
 public class LWWLVideo extends ViewGroup {
     public static final int STATE_HIDED = 0;
     public static final int STATE_MAXIMIZED = 1;
