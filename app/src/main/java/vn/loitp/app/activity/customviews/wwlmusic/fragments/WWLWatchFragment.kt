24bb561531173c31d2196b611373796476f0a0e3
v.loitp <v.Loitp@cloudhms.net>
2020-12-14 15:08:08 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/wwlmusic/fragments/WWLWatchFragment.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/wwlmusic/fragments/WWLWatchFragment.kt
@@ -1,169 +1,141 @@
-package vn.loitp.app.activity.customviews.wwlmusic.fragments;
+package vn.loitp.app.activity.customviews.wwlmusic.fragments
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.SurfaceTexture;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.Surface;
-import android.view.TextureView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.app.Activity
+import android.content.Context
+import android.graphics.SurfaceTexture
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnPreparedListener
+import android.net.Uri
+import android.os.Bundle
+import android.view.Surface
+import android.view.TextureView.SurfaceTextureListener
+import android.view.View
+import android.view.ViewGroup
+import com.annotation.LogTag
+import com.core.base.BaseFragment
+import kotlinx.android.synthetic.main.wwl_music_watch_fragment.*
+import vn.loitp.app.R
+import vn.loitp.app.activity.customviews.wwlmusic.interfaces.FragmentHost
+import vn.loitp.app.activity.customviews.wwlmusic.layout.WWLMusicControlsOverlay
+import vn.loitp.app.activity.customviews.wwlmusic.utils.WWLMusicDataset
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.annotation.LogTag;
-import com.core.base.BaseFragment;
-
-import vn.loitp.app.R;
-import vn.loitp.app.activity.customviews.wwlmusic.interfaces.FragmentHost;
-import vn.loitp.app.activity.customviews.wwlmusic.layout.WWLMusicControlsOverlay;
-import vn.loitp.app.activity.customviews.wwlmusic.utils.WWLMusicDataset;
-
-//TODO convert kotlin
 @LogTag("WWLWatchFragment")
-public class WWLWatchFragment extends BaseFragment implements TextureView.SurfaceTextureListener, WWLMusicControlsOverlay.Listener {
-    private MediaPlayer.OnPreparedListener mOnPreparedListener = MediaPlayer::start;
-    private MediaPlayer mMediaPlayer;
-    private Surface mSurfaceView;
-    private String mUrl;
-    private WWLMusicControlsOverlay mPlayerWWLMusicControlsOverlay;
-    private FragmentHost mFragmentHost;
-    private TextView mTitleView;
-    private TextView mSubTitleView;
+class WWLWatchFragment : BaseFragment(), SurfaceTextureListener, WWLMusicControlsOverlay.Listener {
 
-    @Override
-    protected int setLayoutResourceId() {
-        return R.layout.wwl_music_watch_fragment;
+    private val mOnPreparedListener = OnPreparedListener { mediaPlayer: MediaPlayer ->
+        mediaPlayer.start()
+    }
+    private var mMediaPlayer: MediaPlayer? = null
+    private var mSurfaceView: Surface? = null
+    private var mUrl: String? = null
+    private var mPlayerWWLMusicControlsOverlay: WWLMusicControlsOverlay? = null
+    private var mFragmentHost: FragmentHost? = null
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.wwl_music_watch_fragment
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        TextureView mPlayerView = getFrmRootView().findViewById(R.id.playerView);
-        mPlayerView.requestFocus();
-        mPlayerView.setSurfaceTextureListener(this);
-        this.mPlayerWWLMusicControlsOverlay = new WWLMusicControlsOverlay(getContext());
-        this.mPlayerWWLMusicControlsOverlay.setListener(this);
-        ((ViewGroup) getFrmRootView()).addView(this.mPlayerWWLMusicControlsOverlay);
-        this.mTitleView = getFrmRootView().findViewById(R.id.liTitle);
-        this.mSubTitleView = getFrmRootView().findViewById(R.id.liSubtitle);
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        this.mFragmentHost = (FragmentHost) activity;
-    }
+        playerView.requestFocus()
+        playerView.surfaceTextureListener = this
+        mPlayerWWLMusicControlsOverlay = WWLMusicControlsOverlay(requireContext())
+        mPlayerWWLMusicControlsOverlay?.setListener(this)
 
-    public void startPlay(WWLMusicDataset.DatasetItem item) {
-        this.mUrl = item.url;
-        openVideo();
-        this.mTitleView.setText(item.title);
-        this.mSubTitleView.setText(item.subtitle);
-    }
-
-    public void stopPlay() {
-        if (this.mUrl != null) {
-            release();
+        if (frmRootView is ViewGroup) {
+            (frmRootView as ViewGroup).addView(mPlayerWWLMusicControlsOverlay)
         }
     }
 
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+        mFragmentHost = activity as FragmentHost
+    }
+
+    fun startPlay(item: WWLMusicDataset.DatasetItem) {
+        mUrl = item.url
+        openVideo()
+        liTitle.text = item.title
+        liSubtitle.text = item.subtitle
+    }
+
+    fun stopPlay() {
+        if (mUrl != null) {
+            release()
+        }
+    }
+
+    override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
         try {
-            this.mSurfaceView = new Surface(surface);
-            openVideo();
-        } catch (Exception e) {
-            e.printStackTrace();
+            mSurfaceView = Surface(surface)
+            openVideo()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+    override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {}
 
+    override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+        mSurfaceView = null
+        return true
     }
 
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-        this.mSurfaceView = null;
-        return true;
+    override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+
+    override fun doCollapse() {
+        mFragmentHost?.onVideoCollapse()
     }
 
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-
+    override fun doFullscreen(selected: Boolean) {
+        mFragmentHost?.onVideoFullscreen(selected)
     }
 
-    @Override
-    public void doCollapse() {
-        if (this.mFragmentHost != null) {
-            this.mFragmentHost.onVideoCollapse();
+    fun switchFullscreen(selected: Boolean) {
+        mPlayerWWLMusicControlsOverlay?.switchFullscreen(selected)
+    }
+
+    fun hideControls() {
+        mPlayerWWLMusicControlsOverlay?.hideControls()
+    }
+
+    fun toggleControls() {
+        mPlayerWWLMusicControlsOverlay?.toggleControls()
+    }
+
+    private fun openVideo() {
+        if (mUrl == null || mSurfaceView == null) {
+            return
         }
-    }
-
-    @Override
-    public void doFullscreen(boolean selected) {
-        if (this.mFragmentHost != null) {
-            this.mFragmentHost.onVideoFullscreen(selected);
-        }
-    }
-
-    public void switchFullscreen(boolean selected) {
-        if (this.mPlayerWWLMusicControlsOverlay != null) {
-            this.mPlayerWWLMusicControlsOverlay.switchFullscreen(selected);
-        }
-    }
-
-    public void hideControls() {
-        if (this.mPlayerWWLMusicControlsOverlay != null) {
-            this.mPlayerWWLMusicControlsOverlay.hideControls();
-        }
-    }
-
-    public void toggleControls() {
-        if (this.mPlayerWWLMusicControlsOverlay != null) {
-            this.mPlayerWWLMusicControlsOverlay.toggleControls();
-        }
-    }
-
-    private void openVideo() {
-        if (this.mUrl == null || this.mSurfaceView == null) {
-            return;
-        }
-
-        release();
-        logD("openVideo " + mUrl);
-
-        AudioManager am = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
-        am.requestAudioFocus(null, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-
+        release()
+        logD("openVideo $mUrl")
+        val am = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        am.requestAudioFocus(null, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN)
         try {
-            this.mMediaPlayer = new MediaPlayer();
-            this.mMediaPlayer.setOnPreparedListener(this.mOnPreparedListener);
-            this.mMediaPlayer.setDataSource(getContext(), Uri.parse(this.mUrl));
-            this.mMediaPlayer.setSurface(this.mSurfaceView);
-            this.mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            this.mMediaPlayer.setScreenOnWhilePlaying(true);
-            this.mMediaPlayer.prepareAsync();
-        } catch (Exception e) {
-            logE("openVideo " + e.toString());
+            mMediaPlayer = MediaPlayer()
+            mMediaPlayer?.apply {
+                this.setOnPreparedListener(mOnPreparedListener)
+                this.setDataSource(requireContext(), Uri.parse(mUrl))
+                this.setSurface(mSurfaceView)
+                this.setAudioStreamType(AudioManager.STREAM_MUSIC)
+                this.setScreenOnWhilePlaying(true)
+                this.prepareAsync()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
-    private void release() {
-        if (this.mMediaPlayer != null) {
-            this.mMediaPlayer.reset();
-            this.mMediaPlayer.release();
-            this.mMediaPlayer = null;
-
-            AudioManager am = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
-            am.abandonAudioFocus(null);
+    private fun release() {
+        mMediaPlayer?.apply {
+            this.reset()
+            this.release()
         }
+        mMediaPlayer = null
+        val am = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager?
+        am?.abandonAudioFocus(null)
     }
-
 }
+
