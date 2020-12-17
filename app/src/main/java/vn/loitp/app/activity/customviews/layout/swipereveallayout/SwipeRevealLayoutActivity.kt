420128027e9dbc8ac0921cb42ea8007b17c82c7a
v.loitp <v.Loitp@cloudhms.net>
2020-12-17 16:22:22 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/swipereveallayout/SwipeRevealLayoutActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/swipereveallayout/SwipeRevealLayoutActivity.kt
@@ -1,106 +1,97 @@
-package vn.loitp.app.activity.customviews.layout.swipereveallayout;
+package vn.loitp.app.activity.customviews.layout.swipereveallayout
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.annotation.IsFullScreen
+import com.annotation.LogTag
+import com.core.base.BaseFontActivity
+import com.core.utilities.LActivityUtil
+import kotlinx.android.synthetic.main.activity_swipe_reveal_layout.*
+import vn.loitp.app.R
+import vn.loitp.app.activity.customviews.layout.swipereveallayout.grid.SwipeRevealLayoutGridActivity
+import vn.loitp.app.activity.customviews.layout.swipereveallayout.list.SwipeRevealLayoutListActivity
+import vn.loitp.app.activity.customviews.layout.swipereveallayout.recycler.SwipeRevealLayoutRecyclerActivity
 
-import androidx.appcompat.widget.Toolbar;
-
-import com.annotation.IsFullScreen;
-import com.annotation.LogTag;
-import com.core.base.BaseFontActivity;
-import com.core.utilities.LActivityUtil;
-
-import vn.loitp.app.R;
-import vn.loitp.app.activity.customviews.layout.swipereveallayout.grid.SwipeRevealLayoutGridActivity;
-import vn.loitp.app.activity.customviews.layout.swipereveallayout.list.SwipeRevealLayoutListActivity;
-import vn.loitp.app.activity.customviews.layout.swipereveallayout.recycler.SwipeRevealLayoutRecyclerActivity;
-
-//TODO convert kotlin
 //https://github.com/chthai64/SwipeRevealLayout
 @LogTag("SwipeRevealLayoutActivity")
 @IsFullScreen(false)
-public class SwipeRevealLayoutActivity extends BaseFontActivity {
+class SwipeRevealLayoutActivity : BaseFontActivity() {
 
-    @Override
-    protected int setLayoutResourceId() {
-        return R.layout.activity_swipe_reveal_layout;
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_swipe_reveal_layout
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        final Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setSupportActionBar(toolbar)
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_swipe_reveal_layout, menu);
-        return true;
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_swipe_reveal_layout, menu)
+        return true
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_recycler_view:
-                startActivity(new Intent(this, SwipeRevealLayoutRecyclerActivity.class));
-                LActivityUtil.tranIn(this);
-                return true;
-
-            case R.id.action_list_view:
-                startActivity(new Intent(this, SwipeRevealLayoutListActivity.class));
-                LActivityUtil.tranIn(this);
-                return true;
-
-            case R.id.action_grid_view:
-                startActivity(new Intent(this, SwipeRevealLayoutGridActivity.class));
-                LActivityUtil.tranIn(this);
-                return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.actionRecyclerView -> {
+                startActivity(Intent(this, SwipeRevealLayoutRecyclerActivity::class.java))
+                LActivityUtil.tranIn(this)
+                return true
+            }
+            R.id.actionListView -> {
+                startActivity(Intent(this, SwipeRevealLayoutListActivity::class.java))
+                LActivityUtil.tranIn(this)
+                return true
+            }
+            R.id.actionGridView -> {
+                startActivity(Intent(this, SwipeRevealLayoutGridActivity::class.java))
+                LActivityUtil.tranIn(this)
+                return true
+            }
         }
-
-        return false;
+        return false
     }
 
-    public void layoutOneOnClick(View v) {
-        showShortInformation("Layout 1 clicked", true);
+    fun layoutOneOnClick() {
+        showShortInformation("Layout 1 clicked", true)
     }
 
-    public void layoutTwoOnClick(View v) {
-        showShortInformation("Layout 2 clicked", true);
+    fun layoutTwoOnClick() {
+        showShortInformation("Layout 2 clicked", true)
     }
 
-    public void layoutThreeOnClick(View v) {
-        showShortInformation("Layout 3 clicked", true);
+    fun layoutThreeOnClick() {
+        showShortInformation("Layout 3 clicked", true)
     }
 
-    public void layoutFourOnClick(View v) {
-        showShortInformation("Layout 4 clicked", true);
+    fun layoutFourOnClick() {
+        showShortInformation("Layout 4 clicked", true)
     }
 
-    public void moreOnClick(View v) {
-        showShortInformation("More clicked", true);
+    fun moreOnClick() {
+        showShortInformation("More clicked", true)
     }
 
-    public void deleteOnClick(View v) {
-        showShortInformation("Delete clicked", true);
+    fun deleteOnClick() {
+        showShortInformation("Delete clicked", true)
     }
 
-    public void archiveOnClick(View v) {
-        showShortInformation("Archive clicked", true);
+    fun archiveOnClick() {
+        showShortInformation("Archive clicked", true)
     }
 
-    public void helpOnClick(View v) {
-        showShortInformation("Help clicked", true);
+    fun helpOnClick() {
+        showShortInformation("Help clicked", true)
     }
 
-    public void searchOnClick(View v) {
-        showShortInformation("Search clicked", true);
+    fun searchOnClick() {
+        showShortInformation("Search clicked", true)
     }
 
-    public void starOnClick(View v) {
-        showShortInformation("Star clicked", true);
+    fun starOnClick() {
+        showShortInformation("Star clicked", true)
     }
 }
