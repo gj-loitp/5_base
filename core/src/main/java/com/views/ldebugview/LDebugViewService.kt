e1cd150fe541f0b2b4fdf759726ad8a6855390ed
v.loitp <v.Loitp@cloudhms.net>
2020-12-17 15:43:04 +0700

    update

--- core/src/main/java/com/views/ldebugview/LDebugViewService.kt
+++ core/src/main/java/com/views/ldebugview/LDebugViewService.kt
@@ -1,108 +1,102 @@
-package com.views.ldebugview;
+package com.views.ldebugview
 
-import android.annotation.SuppressLint;
-import android.app.Service;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PixelFormat;
-import android.os.Build;
-import android.os.IBinder;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.annotation.SuppressLint
+import android.app.Service
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.os.Build
+import android.os.IBinder
+import android.view.*
+import android.view.View.OnTouchListener
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import com.R
+import com.core.utilities.LAppResource
+import com.core.utilities.LDateUtil
+import com.core.utilities.LScreenUtil
+import com.core.utilities.LUIUtil
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
-import com.R;
-import com.core.utilities.LAppResource;
-import com.core.utilities.LDateUtil;
-import com.core.utilities.LScreenUtil;
-import com.core.utilities.LUIUtil;
+class LDebugViewService : Service(), OnTouchListener {
 
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+    private val logTag = javaClass.simpleName
+    private var mWindowManager: WindowManager? = null
+    private var mFloatingView: View? = null
+    private var params: WindowManager.LayoutParams? = null
+    private var collapsedView: View? = null
+    private var expandedView: View? = null
+    private var llRootTv: LinearLayout? = null
+    private var scrollView: ScrollView? = null
 
-public class LDebugViewService extends Service implements View.OnTouchListener {
-    private final String logTag = getClass().getSimpleName();
-    private WindowManager mWindowManager;
-    private View mFloatingView;
-    private WindowManager.LayoutParams params;
-    private View collapsedView;
-    private View expandedView;
-    private LinearLayout llRootTv;
-    private ScrollView scrollView;
-
-    public LDebugViewService() {
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+    @SuppressLint("InflateParams")
+    override fun onCreate() {
+        super.onCreate()
+        EventBus.getDefault().register(this)
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_debug_view_service, null)
 
-        EventBus.getDefault().register(this);
-
-        //Inflate the floating view layout we created
-        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_debug_view_service, null);
-
-        //Add the view to the window.
-        int LAYOUT_FLAG;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        val layoutFlag: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
         } else {
-            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+            @Suppress("DEPRECATION")
+            WindowManager.LayoutParams.TYPE_PHONE
         }
-        params = new WindowManager.LayoutParams(
+        params = WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                LAYOUT_FLAG,
+                layoutFlag,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
+                PixelFormat.TRANSLUCENT
+        )
 
         //Specify the view position
-        params.gravity = Gravity.TOP | Gravity.START;        //Initially view will be added to top-left corner
-        params.x = 0;
-        params.y = 100;
+        params?.let {
+            it.gravity = Gravity.TOP or Gravity.START //Initially view will be added to top-left corner
+            it.x = 0
+            it.y = 100
+        }
 
         //Add the view to the window
-        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        mWindowManager.addView(mFloatingView, params);
+        mWindowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+        mWindowManager?.addView(mFloatingView, params)
 
-        collapsedView = mFloatingView.findViewById(R.id.rlCollapse);
-        expandedView = mFloatingView.findViewById(R.id.llExpanded);
+        collapsedView = mFloatingView?.findViewById(R.id.rlCollapse)
+        expandedView = mFloatingView?.findViewById(R.id.llExpanded)
 
-        expandedView.getLayoutParams().width = LScreenUtil.Companion.getScreenWidth() / 2;
-        expandedView.getLayoutParams().height = LScreenUtil.Companion.getScreenHeight() * 2 / 3;
-        expandedView.requestLayout();
+        expandedView?.apply {
+            layoutParams.width = LScreenUtil.screenWidth / 2
+            layoutParams.height = LScreenUtil.screenHeight * 2 / 3
+            requestLayout()
+        }
 
-        llRootTv = mFloatingView.findViewById(R.id.ll_root_tv);
-        scrollView = mFloatingView.findViewById(R.id.scrollView);
+        llRootTv = mFloatingView?.findViewById(R.id.ll_root_tv)
+        scrollView = mFloatingView?.findViewById(R.id.scrollView)
 
         //Set the close button
-        ImageView closeButtonCollapsed = mFloatingView.findViewById(R.id.ivClose);
-        closeButtonCollapsed.setOnClickListener(view -> {
+        val closeButtonCollapsed = mFloatingView?.findViewById<ImageView>(R.id.ivClose)
+        closeButtonCollapsed?.setOnClickListener {
             //close the service and remove the from from the window
-            stopSelf();
-        });
+            stopSelf()
+        }
 
         //Set the close button
-        ImageView closeButton = mFloatingView.findViewById(R.id.ivCloseButton);
-        closeButton.setOnClickListener(view -> {
-            collapsedView.setVisibility(View.VISIBLE);
-            expandedView.setVisibility(View.GONE);
-        });
+        val closeButton = mFloatingView?.findViewById<ImageView>(R.id.ivCloseButton)
+        closeButton?.setOnClickListener {
+            collapsedView?.visibility = View.VISIBLE
+            expandedView?.visibility = View.GONE
+        }
 
         //Drag and move floating view using user's touch action.
-        mFloatingView.findViewById(R.id.rlRootContainer).setOnTouchListener(this);
+        mFloatingView?.findViewById<View>(R.id.rlRootContainer)?.setOnTouchListener(this)
     }
 
     /**
@@ -110,101 +104,106 @@ public class LDebugViewService extends Service implements View.OnTouchListener {
      *
      * @return true if the floating view is collapsed.
      */
-    private boolean isViewCollapsed() {
-        return mFloatingView == null || mFloatingView.findViewById(R.id.rlCollapse).getVisibility() == View.VISIBLE;
-    }
-
-    private int initialX;
-    private int initialY;
-    private float initialTouchX;
-    private float initialTouchY;
+    private val isViewCollapsed: Boolean
+        get() = mFloatingView == null || mFloatingView?.findViewById<View>(R.id.rlCollapse)?.visibility == View.VISIBLE
+    private var initialX = 0
+    private var initialY = 0
+    private var initialTouchX = 0f
+    private var initialTouchY = 0f
 
     @SuppressLint("ClickableViewAccessibility")
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (v.getId() == R.id.rlRootContainer) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        if (v.id == R.id.rlRootContainer) {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
 
                     //remember the initial position.
-                    initialX = params.x;
-                    initialY = params.y;
 
-                    //get the touch location
-                    initialTouchX = event.getRawX();
-                    initialTouchY = event.getRawY();
-                    return true;
-                case MotionEvent.ACTION_UP:
-                    int Xdiff = (int) (event.getRawX() - initialTouchX);
-                    int Ydiff = (int) (event.getRawY() - initialTouchY);
+                    params?.let {
+                        initialX = it.x
+                        initialY = it.y
+
+                        //get the touch location
+                        initialTouchX = event.rawX
+                        initialTouchY = event.rawY
+                    }
+
+                    return true
+                }
+                MotionEvent.ACTION_UP -> {
+                    val xDiff = (event.rawX - initialTouchX).toInt()
+                    val yDiff = (event.rawY - initialTouchY).toInt()
 
                     //The check for Xdiff <10 && YDiff< 10 because sometime elements moves a little while clicking.
                     //So that is click event.
-                    if (Xdiff < 10 && Ydiff < 10) {
-                        if (isViewCollapsed()) {
+                    if (xDiff < 10 && yDiff < 10) {
+                        if (isViewCollapsed) {
                             //When user clicks on the image view of the collapsed layout,
                             //visibility of the collapsed layout will be changed to "View.GONE"
                             //and expanded view will become visible.
-                            collapsedView.setVisibility(View.GONE);
-                            expandedView.setVisibility(View.VISIBLE);
+                            collapsedView?.visibility = View.GONE
+                            expandedView?.visibility = View.VISIBLE
                         }
                     }
-                    return true;
-                case MotionEvent.ACTION_MOVE:
+                    return true
+                }
+                MotionEvent.ACTION_MOVE -> {
                     //Calculate the X and Y coordinates of the view.
-                    params.x = initialX + (int) (event.getRawX() - initialTouchX);
-                    params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                    params?.x = initialX + (event.rawX - initialTouchX).toInt()
+                    params?.y = initialY + (event.rawY - initialTouchY).toInt()
 
                     //Update the layout with new X & Y coordinate
-                    mWindowManager.updateViewLayout(mFloatingView, params);
-                    return true;
+                    mWindowManager?.updateViewLayout(mFloatingView, params)
+                    return true
+                }
             }
-            return false;
+            return false
         }
-        return false;
+        return false
     }
 
     @SuppressLint("SetTextI18n")
-    private void print(LComunicateDebug.MsgFromActivity msgFromActivity) {
+    private fun print(msgFromActivity: LComunicateDebug.MsgFromActivity?) {
         if (msgFromActivity == null) {
-            return;
+            return
         }
-        String currentTime = LDateUtil.Companion.getDateCurrentTimeZoneMls(System.currentTimeMillis(), "HH:mm:ss");
-        TextView textView = new TextView(this);
-        LUIUtil.Companion.setTextSize(textView, LAppResource.INSTANCE.getDimenValue(R.dimen.txt_medium));
-
-        if (msgFromActivity.getAny() == null) {
-//            Log.d(logTag, "msgFromActivity.getObject() == null");
-            textView.setText(currentTime + " : " + msgFromActivity.getMsg());
+        val currentTime = LDateUtil.getDateCurrentTimeZoneMls(System.currentTimeMillis(), "HH:mm:ss")
+        val textView = TextView(this)
+        LUIUtil.setTextSize(textView, LAppResource.getDimenValue(R.dimen.txt_medium).toFloat())
+        if (msgFromActivity.any == null) {
+            textView.text = currentTime + " : " + msgFromActivity.msg
         } else {
-//            Log.d(logTag, "msgFromActivity.getObject() != null");
-            LUIUtil.Companion.printBeautyJson(msgFromActivity.getAny(), textView, currentTime);
+            LUIUtil.printBeautyJson(o = msgFromActivity.any, textView = textView, tag = currentTime)
         }
-
-        LUIUtil.Companion.setTextSize(textView, getBaseContext().getResources().getDimension(R.dimen.text_small));
-        if (msgFromActivity.getType() == LComunicateDebug.MsgFromActivity.TYPE_D) {
-            textView.setTextColor(Color.WHITE);
-        } else if (msgFromActivity.getType() == LComunicateDebug.MsgFromActivity.TYPE_E) {
-            textView.setTextColor(Color.RED);
-        } else if (msgFromActivity.getType() == LComunicateDebug.MsgFromActivity.TYPE_I) {
-            textView.setTextColor(Color.GREEN);
+        LUIUtil.setTextSize(textView = textView, size = baseContext.resources.getDimension(R.dimen.text_tiny))
+        when (msgFromActivity.type) {
+            LComunicateDebug.MsgFromActivity.TYPE_D -> {
+                textView.setTextColor(Color.WHITE)
+            }
+            LComunicateDebug.MsgFromActivity.TYPE_E -> {
+                textView.setTextColor(Color.RED)
+            }
+            LComunicateDebug.MsgFromActivity.TYPE_I -> {
+                textView.setTextColor(Color.GREEN)
+            }
         }
-        llRootTv.addView(textView);
-        scrollView.post(() -> scrollView.fullScroll(View.FOCUS_DOWN));
+        llRootTv?.addView(textView)
+        scrollView?.let { sv ->
+            sv.post {
+                sv.fullScroll(View.FOCUS_DOWN)
+            }
+        }
     }
 
-    @Override
-    public void onDestroy() {
-        EventBus.getDefault().unregister(this);
-        if (mFloatingView != null) {
-            mWindowManager.removeView(mFloatingView);
-        }
-        super.onDestroy();
+    override fun onDestroy() {
+        EventBus.getDefault().unregister(this)
+        mWindowManager?.removeView(mFloatingView)
+        super.onDestroy()
     }
 
     //listen msg from activity
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onEvent(LComunicateDebug.MsgFromActivity msg) {
-        print(msg);
+    fun onEvent(msg: LComunicateDebug.MsgFromActivity?) {
+        print(msgFromActivity = msg)
     }
 }
