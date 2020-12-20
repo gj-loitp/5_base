7491106aa7cde6d14b693238a50d9879e9e4ce65
v.loitp <v.Loitp@cloudhms.net>
2020-12-20 23:10:41 +0700

    update

--- core/src/main/java/com/views/calendar/cosmocalendar/view/SlowdownRecyclerView.kt
+++ core/src/main/java/com/views/calendar/cosmocalendar/view/SlowdownRecyclerView.kt
@@ -1,48 +1,43 @@
-package com.views.calendar.cosmocalendar.view;
+package com.views.calendar.cosmocalendar.view
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.animation.Interpolator;
+import android.content.Context
+import android.util.AttributeSet
+import android.view.animation.Interpolator
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
+import kotlin.math.pow
 
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.RecyclerView;
-
-//TODO convert kotlin
-public class SlowdownRecyclerView extends RecyclerView {
-
-    // Change pow to control speed.
-    // Bigger = faster. RecyclerView default is 5.
-    private static final int POW = 2;
-
-    private Interpolator interpolator;
-
-    public SlowdownRecyclerView(Context context) {
-        super(context);
-        createInterpolator();
+class SlowdownRecyclerView : RecyclerView {
+    companion object {
+        // Change pow to control speed.
+        // Bigger = faster. RecyclerView default is 5.
+        private const val POW = 2
     }
 
-    public SlowdownRecyclerView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        createInterpolator();
+    private var interpolator: Interpolator? = null
+
+    constructor(context: Context) : super(context) {
+        createInterpolator()
     }
 
-    public SlowdownRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        createInterpolator();
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        createInterpolator()
     }
 
-    private void createInterpolator() {
-        interpolator = new Interpolator() {
-            @Override
-            public float getInterpolation(float t) {
-                t = Math.abs(t - 1.0f);
-                return (float) (1.0f - Math.pow(t, POW));
-            }
-        };
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
+        createInterpolator()
     }
 
-    @Override
-    public void smoothScrollBy(int dx, int dy) {
-        super.smoothScrollBy(dx, dy, interpolator);
+    private fun createInterpolator() {
+        interpolator = Interpolator { t ->
+            @Suppress("NAME_SHADOWING")
+            var t = t
+            t = abs(t - 1.0f)
+            (1.0f - t.toDouble().pow(POW)).toFloat()
+        }
+    }
+
+    override fun smoothScrollBy(dx: Int, dy: Int) {
+        super.smoothScrollBy(dx, dy, interpolator)
     }
 }
