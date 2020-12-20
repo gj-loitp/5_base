8adfcd3365503f738bda1e3fb3034097d2ef29ac
v.loitp <v.Loitp@cloudhms.net>
2020-12-20 23:29:58 +0700

    update

--- core/src/main/java/com/views/calendar/cosmocalendar/view/MonthView.kt
+++ core/src/main/java/com/views/calendar/cosmocalendar/view/MonthView.kt
@@ -1,77 +1,76 @@
-package com.views.calendar.cosmocalendar.view;
+package com.views.calendar.cosmocalendar.view
 
-import android.annotation.TargetApi;
-import android.content.Context;
-import android.os.Build;
-import android.util.AttributeSet;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.views.calendar.cosmocalendar.adapter.DaysAdapter
+import com.views.calendar.cosmocalendar.model.Month
+import com.views.calendar.cosmocalendar.utils.Constants
 
-import androidx.annotation.AttrRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.StyleRes;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+class MonthView : FrameLayout {
+    private var rvDays: RecyclerView? = null
 
-import com.views.calendar.cosmocalendar.adapter.DaysAdapter;
-import com.views.calendar.cosmocalendar.model.Month;
-import com.views.calendar.cosmocalendar.utils.Constants;
-
-//TODO convert kotlin
-public class MonthView extends FrameLayout {
-
-    private RecyclerView rvDays;
-
-    public MonthView(@NonNull Context context) {
-        super(context);
-        init();
+    constructor(
+            context: Context
+    ) : super(context) {
+        init()
     }
 
-    public MonthView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
+    constructor(
+            context: Context,
+            attrs: AttributeSet?
+    ) : super(context, attrs) {
+        init()
     }
 
-    public MonthView(@NonNull Context context,
-                     @Nullable AttributeSet attrs,
-                     @AttrRes int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
+    constructor(
+            context: Context,
+            attrs: AttributeSet?,
+            @AttrRes defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr) {
+        init()
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public MonthView(@NonNull Context context,
-                     @Nullable AttributeSet attrs,
-                     @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init();
+    constructor(
+            context: Context,
+            attrs: AttributeSet?,
+            @AttrRes defStyleAttr: Int,
+            @StyleRes defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes) {
+        init()
     }
 
-    private void init() {
-        rvDays = new RecyclerView(getContext());
-        rvDays.setHasFixedSize(true);
-        rvDays.setNestedScrollingEnabled(false);
-        rvDays.setLayoutParams(generateLayoutParams());
-        final GridLayoutManager manager = new GridLayoutManager(getContext(), Constants.DAYS_IN_WEEK);
-        rvDays.setLayoutManager(manager);
-        addView(rvDays);
+    private fun init() {
+        rvDays = RecyclerView(context)
+        rvDays?.let {
+            it.setHasFixedSize(true)
+            it.isNestedScrollingEnabled = false
+            it.layoutParams = generateLayoutParams()
+            val layoutManager = GridLayoutManager(context, Constants.DAYS_IN_WEEK)
+            it.layoutManager = layoutManager
+            addView(it)
+        }
     }
 
-    public void setAdapter(DaysAdapter adapter) {
-        rvDays.setAdapter(adapter);
+    var adapter: DaysAdapter?
+        get() = rvDays?.adapter as DaysAdapter?
+        set(adapter) {
+            rvDays?.adapter = adapter
+        }
+
+    fun initAdapter(month: Month?) {
+        adapter?.setMonth(month)
     }
 
-    public DaysAdapter getAdapter() {
-        return (DaysAdapter) rvDays.getAdapter();
-    }
-
-    public void initAdapter(Month month) {
-        getAdapter().setMonth(month);
-    }
-
-    private FrameLayout.LayoutParams generateLayoutParams() {
-        return new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
+    private fun generateLayoutParams(): LayoutParams {
+        return LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
     }
 }
--- core/src/main/java/com/views/calendar/cosmocalendar/view/delegate/OtherDayDelegate.kt
+++ core/src/main/java/com/views/calendar/cosmocalendar/view/delegate/OtherDayDelegate.kt
@@ -1,29 +1,20 @@
-package com.views.calendar.cosmocalendar.view.delegate;
+package com.views.calendar.cosmocalendar.view.delegate
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.R
+import com.views.calendar.cosmocalendar.adapter.viewholder.OtherDayHolder
+import com.views.calendar.cosmocalendar.model.Day
+import com.views.calendar.cosmocalendar.view.CalendarView
 
-import com.R;
-import com.views.calendar.cosmocalendar.adapter.viewholder.OtherDayHolder;
-import com.views.calendar.cosmocalendar.model.Day;
-import com.views.calendar.cosmocalendar.view.CalendarView;
+class OtherDayDelegate(private val calendarView: CalendarView) {
+    fun onCreateDayHolder(parent: ViewGroup, viewType: Int): OtherDayHolder {
 
-//TODO convert kotlin
-public class OtherDayDelegate {
-
-    private CalendarView calendarView;
-
-    public OtherDayDelegate(CalendarView calendarView) {
-        this.calendarView = calendarView;
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_cosmo_calendar_other_day, parent, false)
+        return OtherDayHolder(view, calendarView)
     }
 
-    public OtherDayHolder onCreateDayHolder(ViewGroup parent, int viewType) {
-        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_cosmo_calendar_other_day, parent, false);
-        return new OtherDayHolder(view, calendarView);
-    }
-
-    public void onBindDayHolder(Day day, OtherDayHolder holder, int position) {
-        holder.bind(day);
+    fun onBindDayHolder(day: Day?, holder: OtherDayHolder, position: Int) {
+        holder.bind(day)
     }
 }
