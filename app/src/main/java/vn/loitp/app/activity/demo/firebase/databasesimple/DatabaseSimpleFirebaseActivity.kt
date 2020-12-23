be6a334d8671828a8b0122ccbcc1d43063c57673
v.loitp <v.Loitp@cloudhms.net>
2020-12-23 17:15:55 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/demo/firebase/databasesimple/DatabaseSimpleFirebaseActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/demo/firebase/databasesimple/DatabaseSimpleFirebaseActivity.kt
@@ -1,140 +1,115 @@
-package vn.loitp.app.activity.demo.firebase.databasesimple;
+package vn.loitp.app.activity.demo.firebase.databasesimple
 
-import android.os.Bundle;
-import android.view.View;
-import android.view.animation.OvershootInterpolator;
-
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.annotation.IsFullScreen;
-import com.annotation.LogTag;
-import com.core.base.BaseFontActivity;
-import com.core.utilities.LUIUtil;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
-import jp.wasabeef.recyclerview.animators.SlideInRightAnimator;
-import vn.loitp.app.R;
-import vn.loitp.app.common.Constants;
+import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.annotation.IsFullScreen
+import com.annotation.LogTag
+import com.core.base.BaseFontActivity
+import com.core.utilities.LUIUtil.Companion.setPullLikeIOSVertical
+import com.google.firebase.database.*
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter
+import jp.wasabeef.recyclerview.animators.SlideInRightAnimator
+import kotlinx.android.synthetic.main.activity_menu_firebase_simple.*
+import vn.loitp.app.R
+import vn.loitp.app.common.Constants.Companion.URL_IMG
+import vn.loitp.app.common.Constants.Companion.URL_IMG_1
+import java.util.*
 
 @LogTag("DatabaseSimpleFirebaseActivity")
 @IsFullScreen(false)
-public class DatabaseSimpleFirebaseActivity extends BaseFontActivity implements View.OnClickListener {
-    private final String ROOT_NODE = "loitp";
-    private DatabaseReference mFirebaseDatabase;
-    private final List<User> userList = new ArrayList<>();
-    private UserAdapter mAdapter;
-
-    @Override
-    protected int setLayoutResourceId() {
-        return R.layout.activity_menu_firebase_simple;
+class DatabaseSimpleFirebaseActivity : BaseFontActivity(), View.OnClickListener {
+    companion object {
+        private const val ROOT_NODE = "loitp"
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FirebaseDatabase mFirebaseInstance = FirebaseDatabase.getInstance();
-        mFirebaseDatabase = mFirebaseInstance.getReference();
+    private var mFirebaseDatabase: DatabaseReference? = null
+    private val userList = ArrayList<User>()
+    private var mAdapter: UserAdapter? = null
 
-        findViewById(R.id.btAdd).setOnClickListener(this);
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_menu_firebase_simple
+    }
 
-        setupUI();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        mFirebaseDatabase.child(ROOT_NODE).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.getValue() == null) {
-                    logD("onDataChange null => return");
-                    userList.clear();
-                    if (mAdapter != null) {
-                        mAdapter.notifyDataSetChanged();
+        val mFirebaseInstance = FirebaseDatabase.getInstance()
+        mFirebaseDatabase = mFirebaseInstance.reference
+        btAdd.setOnClickListener(this)
+        setupUI()
+
+        mFirebaseDatabase?.child(ROOT_NODE)?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.value == null) {
+                    logD("onDataChange null => return")
+                    userList.clear()
+                    mAdapter?.notifyDataSetChanged()
+                    return
+                }
+                userList.clear()
+                for (data in dataSnapshot.children) {
+                    val user = data.getValue(User::class.java)
+                    user?.let {
+                        userList.add(it)
                     }
-                    return;
                 }
-                userList.clear();
-                for (DataSnapshot data : dataSnapshot.getChildren()) {
-                    User user = data.getValue(User.class);
-                    /*if (!userList.contains(user)) {
-                        userList.add(user);
-                    }*/
-                    userList.add(user);
-                }
-                logD("userList.size: " + userList.size());
-                if (mAdapter != null) {
-                    mAdapter.notifyDataSetChanged();
-                }
+                logD("userList.size: " + userList.size)
+                mAdapter?.notifyDataSetChanged()
             }
 
-            @Override
-            public void onCancelled(@NotNull DatabaseError error) {
-                logE("Failed to read app title value " + error.toException());
+            override fun onCancelled(error: DatabaseError) {
+                logE("Failed to read app title value " + error.toException())
             }
-        });
+        })
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btAdd) {
-            addData();
+    override fun onClick(v: View) {
+        if (v == btAdd) {
+            addData()
         }
     }
 
-    private void addData() {
-        User user = new User();
-        user.setTimestamp(System.currentTimeMillis());
-        user.setAvt(Constants.Companion.getURL_IMG());
-        user.setName("loitp");
-        user.setMsg("dummy msg " + user.getTimestamp());
-        mFirebaseDatabase.child(ROOT_NODE).child(user.getTimestamp() + "").setValue(user)
-                .addOnCompleteListener(task -> {
-                    //do nothing
-                });
+    private fun addData() {
+        val user = User()
+        user.timestamp = System.currentTimeMillis()
+        user.avt = URL_IMG
+        user.name = "loitp"
+        user.msg = "dummy msg " + user.timestamp
+        mFirebaseDatabase?.child(ROOT_NODE)?.child(user.timestamp.toString() + "")?.setValue(user)?.addOnCompleteListener {
+            //do sth
+        }
     }
 
-    private void setupUI() {
-        RecyclerView recyclerView = findViewById(R.id.rv);
-
-        SlideInRightAnimator animator = new SlideInRightAnimator(new OvershootInterpolator(1f));
-        animator.setAddDuration(300);
-        recyclerView.setItemAnimator(animator);
-        //recyclerView.getItemAnimator().setAddDuration(1000);
-
-        mAdapter = new UserAdapter(this, userList, new UserAdapter.Callback() {
-            @Override
-            public void onClick(User user, int position) {
-                showShortInformation("onClick To Edit Data: " + user.getMsg(), true);
-
-                user.setMsg("Edited Msg " + System.currentTimeMillis());
-                user.setName("Edited Name");
-                user.setAvt(Constants.Companion.getURL_IMG_1());
-                mFirebaseDatabase.child(ROOT_NODE).child(user.getTimestamp() + "").setValue(user);
+    private fun setupUI() {
+        val animator = SlideInRightAnimator(OvershootInterpolator(1f))
+        animator.addDuration = 300
+        rv.itemAnimator = animator
+        mAdapter = UserAdapter(this, userList, object : UserAdapter.Callback {
+            override fun onClick(user: User, position: Int) {
+                showShortInformation("onClick To Edit Data: " + user.msg, true)
+                user.msg = "Edited Msg " + System.currentTimeMillis()
+                user.name = "Edited Name"
+                user.avt = URL_IMG_1
+                mFirebaseDatabase?.child(ROOT_NODE)?.child(user.timestamp.toString() + "")?.setValue(user)
             }
 
-            @Override
-            public void onLongClick(User user, int position) {
-                showShortInformation("onLongClick " + user.getMsg(), true);
-                mFirebaseDatabase.child(ROOT_NODE).child(user.getTimestamp() + "").removeValue();
+            override fun onLongClick(user: User, position: Int) {
+                showShortInformation("onLongClick " + user.msg, true)
+                mFirebaseDatabase?.child(ROOT_NODE)?.child(user.timestamp.toString() + "")?.removeValue()
             }
-        });
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-
-        ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(mAdapter);
-        scaleAdapter.setDuration(1000);
-        scaleAdapter.setInterpolator(new OvershootInterpolator());
-        scaleAdapter.setFirstOnly(true);
-        recyclerView.setAdapter(scaleAdapter);
-
-        LUIUtil.Companion.setPullLikeIOSVertical(recyclerView);
+        })
+        val mLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(applicationContext)
+        rv.layoutManager = mLayoutManager
+        mAdapter?.let {
+            val scaleAdapter = ScaleInAnimationAdapter(it)
+            scaleAdapter.setDuration(1000)
+            scaleAdapter.setInterpolator(OvershootInterpolator())
+            scaleAdapter.setFirstOnly(true)
+            rv.adapter = scaleAdapter
+            setPullLikeIOSVertical(rv)
+        }
     }
 }
