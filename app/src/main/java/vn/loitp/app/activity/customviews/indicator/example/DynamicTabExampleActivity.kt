15e5ae76218735f4f1407f4723d90b71476266a4
v.loitp <v.Loitp@cloudhms.net>
2020-12-24 14:56:37 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/indicator/example/DynamicTabExampleActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/indicator/example/DynamicTabExampleActivity.kt
@@ -1,88 +1,81 @@
-package vn.loitp.app.activity.customviews.indicator.example;
+package vn.loitp.app.activity.customviews.indicator.example
 
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import com.annotation.IsFullScreen
+import com.annotation.LogTag
+import com.core.base.BaseFontActivity
+import com.views.setSafeOnClickListener
+import kotlinx.android.synthetic.main.activity_dynamic_tab_example_layout.*
+import net.lucode.hackware.magicindicator.ViewPagerHelper
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ClipPagerTitleView
+import vn.loitp.app.R
+import java.util.*
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+@LogTag("DynamicTabExampleActivity")
+@IsFullScreen(false)
+class DynamicTabExampleActivity : BaseFontActivity() {
 
-import net.lucode.hackware.magicindicator.MagicIndicator;
-import net.lucode.hackware.magicindicator.ViewPagerHelper;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ClipPagerTitleView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
-import vn.loitp.app.R;
-
-public class DynamicTabExampleActivity extends AppCompatActivity {
-    private static final String[] CHANNELS = new String[]{"CUPCAKE", "DONUT", "ECLAIR", "GINGERBREAD", "HONEYCOMB", "ICE_CREAM_SANDWICH", "JELLY_BEAN", "KITKAT", "LOLLIPOP", "M", "NOUGAT"};
-    private final List<String> mDataList = new ArrayList<String>(Arrays.asList(CHANNELS));
-    private final ExamplePagerAdapter mExamplePagerAdapter = new ExamplePagerAdapter(mDataList);
-
-    private ViewPager mViewPager;
-    private CommonNavigator mCommonNavigator;
-
-    private Toast mToast;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dynamic_tab_example_layout);
-
-        mViewPager = (ViewPager) findViewById(R.id.view_pager);
-        mViewPager.setAdapter(mExamplePagerAdapter);
-
-        MagicIndicator mMagicIndicator = (MagicIndicator) findViewById(R.id.magicIndicator1);
-        mMagicIndicator.setBackgroundColor(Color.parseColor("#d43d3d"));
-        mCommonNavigator = new CommonNavigator(this);
-        mCommonNavigator.setSkimOver(true);
-        mCommonNavigator.setAdapter(new CommonNavigatorAdapter() {
-
-            @Override
-            public int getCount() {
-                return mDataList.size();
-            }
-
-            @Override
-            public IPagerTitleView getTitleView(Context context, final int index) {
-                ClipPagerTitleView clipPagerTitleView = new ClipPagerTitleView(context);
-                clipPagerTitleView.setText(mDataList.get(index));
-                clipPagerTitleView.setTextColor(Color.parseColor("#f2c4c4"));
-                clipPagerTitleView.setClipColor(Color.WHITE);
-                clipPagerTitleView.setOnClickListener(v -> mViewPager.setCurrentItem(index));
-                return clipPagerTitleView;
-            }
-
-            @Override
-            public IPagerIndicator getIndicator(Context context) {
-                return null;
-            }
-        });
-        mMagicIndicator.setNavigator(mCommonNavigator);
-        ViewPagerHelper.bind(mMagicIndicator, mViewPager);
-
-        mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+    companion object {
+        private val CHANNELS = arrayOf("CUPCAKE", "DONUT", "ECLAIR", "GINGERBREAD", "HONEYCOMB", "ICE_CREAM_SANDWICH", "JELLY_BEAN", "KITKAT", "LOLLIPOP", "M", "NOUGAT")
     }
 
-    public void randomPage(View view) {
-        mDataList.clear();
-        int total = new Random().nextInt(CHANNELS.length);
-        mDataList.addAll(Arrays.asList(CHANNELS).subList(0, total + 1));
+    private val mDataList = ArrayList(mutableListOf(*CHANNELS))
+    private val mExamplePagerAdapter = ExamplePagerAdapter(mDataList)
+    private var mCommonNavigator: CommonNavigator? = null
 
-        mCommonNavigator.notifyDataSetChanged();    // must call firstly
-        mExamplePagerAdapter.notifyDataSetChanged();
-
-        mToast.setText("" + mDataList.size() + " page");
-        mToast.show();
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_dynamic_tab_example_layout
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewPager.adapter = mExamplePagerAdapter
+        magicIndicator1.setBackgroundColor(Color.parseColor("#d43d3d"))
+        mCommonNavigator = CommonNavigator(this)
+        mCommonNavigator?.isSkimOver = true
+        mCommonNavigator?.adapter = object : CommonNavigatorAdapter() {
+            override fun getCount(): Int {
+                return mDataList.size
+            }
+
+            override fun getTitleView(context: Context, index: Int): IPagerTitleView {
+                val clipPagerTitleView = ClipPagerTitleView(context)
+                clipPagerTitleView.text = mDataList[index]
+                clipPagerTitleView.textColor = Color.parseColor("#f2c4c4")
+                clipPagerTitleView.clipColor = Color.WHITE
+                clipPagerTitleView.setOnClickListener {
+                    viewPager.currentItem = index
+                }
+                return clipPagerTitleView
+            }
+
+            override fun getIndicator(context: Context): IPagerIndicator? {
+                return null
+            }
+        }
+        magicIndicator1.navigator = mCommonNavigator
+        ViewPagerHelper.bind(magicIndicator1, viewPager)
+
+        btRandomPage.setSafeOnClickListener {
+            randomPage()
+        }
+    }
+
+    private fun randomPage() {
+        mDataList.clear()
+        val total = Random().nextInt(CHANNELS.size)
+        mDataList.addAll(mutableListOf(*CHANNELS).subList(0, total + 1))
+        mCommonNavigator?.notifyDataSetChanged() // must call firstly
+        mExamplePagerAdapter.notifyDataSetChanged()
+
+        showShortInformation("${mDataList.size} page")
+    }
+
 }
