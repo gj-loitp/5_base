a07cbf2bbae971678184b6099635b52b852c7cac
Loitp <v.loitp@cloudhms.net>
2020-12-27 18:58:02 +0700

    update

--- core/src/main/java/com/game/findnumber/ui/MenuActivity.kt
+++ core/src/main/java/com/game/findnumber/ui/MenuActivity.kt
@@ -1,19 +1,25 @@
 package com.game.findnumber.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.R
 import com.annotation.IsFullScreen
+import com.annotation.IsSwipeActivity
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.*
 import com.daimajia.androidanimations.library.Techniques
+import com.tombayley.activitycircularreveal.CircularReveal
 import kotlinx.android.synthetic.main.l_activity_find_number_menu.*
 
 @LogTag("MenuActivity")
 @IsFullScreen(true)
+@IsSwipeActivity(true)
 class MenuActivity : BaseFontActivity() {
+
+    private var activityCircularReveal: CircularReveal? = null
 
     override fun setLayoutResourceId(): Int {
         return R.layout.l_activity_find_number_menu
@@ -23,6 +29,9 @@ class MenuActivity : BaseFontActivity() {
         super.onCreate(savedInstanceState)
 
         LScreenUtil.toggleFullscreen(activity = this, isFullScreen = true)
+        activityCircularReveal = CircularReveal(rootView)
+        activityCircularReveal?.onActivityCreate(intent)
+
         setupViews()
 
         playAnim(delayInMls = 100)
@@ -32,6 +41,12 @@ class MenuActivity : BaseFontActivity() {
         super.onResume()
 
         playAnim(delayInMls = 0)
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        setResult(Activity.RESULT_OK, intent)
+        activityCircularReveal?.unRevealActivity(this)
     }
 
     private fun setupViews() {
--- core/src/main/java/com/game/findnumber/ui/SplashActivity.kt
+++ core/src/main/java/com/game/findnumber/ui/SplashActivity.kt
@@ -1,20 +1,27 @@
 package com.game.findnumber.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.R
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.*
 import com.daimajia.androidanimations.library.Techniques
+import com.tombayley.activitycircularreveal.CircularReveal
 import kotlinx.android.synthetic.main.l_activity_find_number_splash.*
 
 @LogTag("SplashActivity")
 @IsFullScreen(true)
 //TODO loitpp add sound
 class SplashActivity : BaseFontActivity() {
+
+    companion object {
+        const val REQUEST_CODE = 69
+    }
 
     override fun setLayoutResourceId(): Int {
         return R.layout.l_activity_find_number_splash
@@ -42,12 +49,30 @@ class SplashActivity : BaseFontActivity() {
                     onEnd = {
                         if (!this.isFinishing) {
                             val intent = Intent(this, MenuActivity::class.java)
-                            startActivity(intent)
-                            LActivityUtil.tranIn(this)
-                            finish()
+                            val builder = CircularReveal.Builder(
+                                    this,
+                                    indicatorView,
+                                    intent,
+                                    1000
+                            ).apply {
+                                revealColor = ContextCompat.getColor(
+                                        this@SplashActivity,
+                                        R.color.orange
+                                )
+                                requestCode = REQUEST_CODE
+                            }
+                            CircularReveal.presentActivity(builder)
                         }
                     }
             )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            onBackPressed()
         }
     }
 
