923e4cefbbfa6e5311ecccc4a9408b293b556471
Loitp <v.loitp@cloudhms.net>
2020-12-27 13:33:34 +0700

    update

---
+++ app/src/main/java/vn/loitp/app/activity/animation/activitytransitionreveal/OtherActivity.kt
@@ -0,0 +1,44 @@
+package vn.loitp.app.activity.animation.activitytransitionreveal
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import com.annotation.IsFullScreen
+import com.annotation.IsSwipeActivity
+import com.annotation.LogTag
+import com.core.base.BaseFontActivity
+import com.tombayley.activitycircularreveal.CircularReveal
+import vn.loitp.app.R
+
+@LogTag("OtherActivity")
+@IsFullScreen(false)
+@IsSwipeActivity(true)
+class OtherActivity : BaseFontActivity() {
+
+    companion object {
+        const val KEY_DATA_RETURN = "KEY_DATA_RETURN"
+    }
+
+    private var activityCircularReveal: CircularReveal? = null
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_other
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val rootView: View = findViewById(R.id.root_coord)
+
+        activityCircularReveal = CircularReveal(rootView)
+        activityCircularReveal?.onActivityCreate(intent)
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        intent.putExtra(KEY_DATA_RETURN, "data " + System.currentTimeMillis())
+        setResult(Activity.RESULT_OK, intent)
+        activityCircularReveal?.unRevealActivity(this)
+    }
+
+}
