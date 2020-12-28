743f683c389fb89116742c5eff0cd610451a6444
v.loitp <v.Loitp@cloudhms.net>
2020-12-28 15:22:52 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/wheelspiner/WheelSpinnerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/wheelspiner/WheelSpinnerActivity.kt
@@ -1,10 +1,14 @@
 package vn.loitp.app.activity.customviews.wheelspiner
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import kotlinx.android.synthetic.main.activity_wheel_spinner.*
 import vn.loitp.app.R
+import java.util.*
 
 @LogTag("WheelSpinnerActivity")
 @IsFullScreen(false)
@@ -21,7 +25,25 @@ class WheelSpinnerActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        val listImg: MutableList<Int> = ArrayList()
+        listImg.add(R.drawable.tatoo_1)
+        listImg.add(R.drawable.tatoo_2)
+        listImg.add(R.drawable.tatoo_3)
+        listImg.add(R.drawable.tatoo_4)
+        listImg.add(R.drawable.tatoo_5)
+        listImg.add(R.drawable.tatoo_6)
 
+        wheelSpinner.setArrowPointer(ivArrow)
+        wheelSpinner.setBitmapsId(listImg)
+        wheelSpinner.setOnItemSelectListener { bitmap: Bitmap? ->
+            ivSelectedTattoo.setImageBitmap(bitmap)
+            ivSelectedTattoo.visibility = View.VISIBLE
+        }
+
+        btnSpin.setOnClickListener {
+            ivSelectedTattoo.visibility = View.GONE
+            wheelSpinner.rotateWheel()
+        }
     }
 
 }
