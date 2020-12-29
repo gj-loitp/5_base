fd00f6a587d4d33fbdd6712434935e3a8f8c0899
v.loitp <v.Loitp@cloudhms.net>
2020-12-29 23:03:20 +0700

    update

--- core/src/main/java/com/game/findnumber/db/FindNumberDatabase.kt
+++ core/src/main/java/com/game/findnumber/db/FindNumberDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.game.findnumber.model.Level
 
-@Database(entities = [Level::class], version = 1)
+@Database(entities = [Level::class], version = 2)
 abstract class FindNumberDatabase : RoomDatabase() {
 
     abstract fun levelDao(): LevelDao
