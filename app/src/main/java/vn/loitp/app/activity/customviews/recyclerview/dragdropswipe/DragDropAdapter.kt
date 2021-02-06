4716ec38e6c9d678bb231c457403dec8690caf65
v.loitp <v.Loitp@cloudhms.net>
2021-02-06 22:33:45 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/dragdropswipe/DragDropAdapter.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/dragdropswipe/DragDropAdapter.kt
@@ -11,22 +11,22 @@ import vn.loitp.app.R
  * VinHMS
  * www.muathu@gmail.com
  */
-class MyAdapter(dataSet: List<String> = emptyList())
-    : DragDropSwipeAdapter<String, MyAdapter.ViewHolder>(dataSet) {
+class DragDropAdapter(dataSet: List<String> = emptyList())
+    : DragDropSwipeAdapter<String, DragDropAdapter.ViewHolder>(dataSet) {
 
     class ViewHolder(itemView: View) : DragDropSwipeAdapter.ViewHolder(itemView) {
-        val itemText: TextView = itemView.findViewById(R.id.item_text)
-        val dragIcon: ImageView = itemView.findViewById(R.id.drag_icon)
+        val itemText: TextView = itemView.findViewById(R.id.tv)
+        val dragIcon: ImageView = itemView.findViewById(R.id.ivDrag)
     }
 
-    override fun getViewHolder(itemLayout: View) = MyAdapter.ViewHolder(itemLayout)
+    override fun getViewHolder(itemLayout: View) = DragDropAdapter.ViewHolder(itemLayout)
 
-    override fun onBindViewHolder(item: String, viewHolder: MyAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(item: String, viewHolder: DragDropAdapter.ViewHolder, position: Int) {
         // Here we update the contents of the view holder's views to reflect the item's data
         viewHolder.itemText.text = item
     }
 
-    override fun getViewToTouchToStartDraggingItem(item: String, viewHolder: MyAdapter.ViewHolder, position: Int): View? {
+    override fun getViewToTouchToStartDraggingItem(item: String, viewHolder: DragDropAdapter.ViewHolder, position: Int): View? {
         // We return the view holder's view on which the user has to touch to drag the item
         return viewHolder.dragIcon
     }
--- app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/dragdropswipe/DragDropSwipeRecyclerviewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/recyclerview/dragdropswipe/DragDropSwipeRecyclerviewActivity.kt
@@ -6,6 +6,7 @@ import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeRecyclerView
+import kotlinx.android.synthetic.main.activity_recycler_drag_drop_swipe.*
 import vn.loitp.app.R
 
 //https://github.com/ernestoyaquello/DragDropSwipeRecyclerview
@@ -13,8 +14,7 @@ import vn.loitp.app.R
 @IsFullScreen(false)
 class DragDropSwipeRecyclerviewActivity : BaseFontActivity() {
 
-    private var mAdapter: MyAdapter? = null
-    private var mList: DragDropSwipeRecyclerView? = null
+    private var dragDropAdapter: DragDropAdapter? = null
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_recycler_drag_drop_swipe
@@ -23,20 +23,23 @@ class DragDropSwipeRecyclerviewActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
         val dataSet = ArrayList<String>()
         for (i in 0..20) {
             dataSet.add(element = "Item $i")
         }
-        mAdapter = MyAdapter(dataSet)
-        mList = findViewById(R.id.list)
-        mList?.layoutManager = LinearLayoutManager(this)
-        mList?.adapter = mAdapter
+        dragDropAdapter = DragDropAdapter(dataSet)
+        dragDropSwipeRecyclerView.layoutManager = LinearLayoutManager(this)
+        dragDropSwipeRecyclerView.adapter = dragDropAdapter
 
         //Set up the orientation
-        mList?.orientation = DragDropSwipeRecyclerView.ListOrientation.VERTICAL_LIST_WITH_VERTICAL_DRAGGING
+//        dragDropSwipeRecyclerView?.orientation = DragDropSwipeRecyclerView.ListOrientation.VERTICAL_LIST_WITH_VERTICAL_DRAGGING
 
         // This disallows swiping items to the right
-        mList?.disableSwipeDirection(DragDropSwipeRecyclerView.ListOrientation.DirectionFlag.RIGHT)
-
+//        dragDropSwipeRecyclerView?.disableSwipeDirection(DragDropSwipeRecyclerView.ListOrientation.DirectionFlag.RIGHT)
+//        dragDropSwipeRecyclerView?.disableSwipeDirection(DragDropSwipeRecyclerView.ListOrientation.DirectionFlag.LEFT)
     }
 }
