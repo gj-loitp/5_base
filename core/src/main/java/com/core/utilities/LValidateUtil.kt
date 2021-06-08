76037bbeaeed9b2b985b5162c26731edde0a8529
Loitp <v.loitp@cloudhms.net>
2021-06-08 11:50:35 +0700

    update

--- core/src/main/java/com/core/utilities/LValidateUtil.kt
+++ core/src/main/java/com/core/utilities/LValidateUtil.kt
@@ -41,7 +41,7 @@ class LValidateUtil {
             if (!containsUpper) {
                 return MSG_3
             }
-            //check specical character
+            //check special character
             val regexSpecial = ("[^!@#$%&*()_+=|<>?{}\\[\\]~-]")
             val patternSpecial = Pattern.compile(regexSpecial)
             val containsSpecial = patternSpecial.matcher(pw).find()
@@ -53,16 +53,17 @@ class LValidateUtil {
         }
 
         private val listPkg = listOf(
-                "loitp.basemaster",
-                "loitp93.basemaster.demo",
-                "loitp93.anhseyeuemtucainhindautien",
-                "com.mup.comic",
-                "loitp93.game.findnumber",
-                "com.loitp.igallery",
-                "com.loitp.haivl",
-                "com.loitp.biker",
-                "com.loitp.icomic",
-                "loitp93.truyenvn.cute.girl"
+            "loitp.basemaster",
+            "loitp93.basemaster.demo",
+            "loitp93.anhseyeuemtucainhindautien",
+            "com.mup.comic",
+            "loitp93.game.findnumber",
+            "com.loitp.igallery",
+            "com.loitp.haivl",
+            "com.loitp.biker",
+            "com.loitp.icomic",
+            "loitp93.truyenvn.cute.girl",
+            "loitp93.rss.vnexpress",
         )
 
         fun isValidPackageName(): Boolean {
