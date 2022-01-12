7d7019820dffcddaee2114901d998415014d881c
Loitp <v.loitp@cloudhms.net>
2022-01-12 14:17:33 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/picker/unicornfilepicker/UnicornFilePickerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/picker/unicornfilepicker/UnicornFilePickerActivity.kt
@@ -1,6 +1,9 @@
 package vn.loitp.app.activity.picker.unicornfilepicker
 
+import abhishekti7.unicorn.filepicker.UnicornFilePicker
+import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
@@ -11,6 +14,10 @@ import vn.loitp.app.R
 @LogTag("UnicornFilePickerActivity")
 @IsFullScreen(false)
 class UnicornFilePickerActivity : BaseFontActivity() {
+
+    companion object {
+        const val REQ_UNICORN_FILE = 999
+    }
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_unicorn_file_picker
@@ -23,7 +30,31 @@ class UnicornFilePickerActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
-        btVideo.setSafeOnClickListener {
+        btPhoto.setSafeOnClickListener {
+            UnicornFilePicker.from(this)
+                .addConfigBuilder()
+                .selectMultipleFiles(false)
+                .showOnlyDirectory(true)
+                .setRootDirectory(Environment.getExternalStorageDirectory().absolutePath)
+                .showHiddenFiles(false)
+                .setFilters(arrayOf("png", "jpg", "jpeg"))
+                .addItemDivider(true)
+                .theme(R.style.UnicornFilePicker_Dracula)
+                .build()
+                .forResult(REQ_UNICORN_FILE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_UNICORN_FILE && resultCode == RESULT_OK) {
+            val files = data?.getStringArrayListExtra("filePaths")
+            var s = ""
+            files?.forEach {
+                s = s + "\n" + it
+            }
+            tv.text = s
         }
     }
 }
