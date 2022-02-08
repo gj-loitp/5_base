28870b29df034d1301c560afe5ffdeabf16072ac
Loitp <v.loitp@cloudhms.net>
2022-02-08 10:48:14 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/picker/scopedmediapicker/ScopedMediaPickerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/picker/scopedmediapicker/ScopedMediaPickerActivity.kt
@@ -1,9 +1,13 @@
 package vn.loitp.app.activity.picker.scopedmediapicker
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LSocialUtil
+import com.core.utilities.LUIUtil
+import kotlinx.android.synthetic.main.activity_0.*
 import vn.loitp.app.R
 
 @LogTag("ScopedMediaPickerActivity")
@@ -21,5 +25,27 @@ class ScopedMediaPickerActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.let {
+                LUIUtil.setSafeOnClickListenerElastic(
+                    view = it,
+                    runnable = {
+                        LSocialUtil.openUrlInBrowser(
+                            context = context,
+                            url = "https://github.com/bhoomit11/ScopedMediaPicker"
+                        )
+                    }
+                )
+                it.isVisible = true
+            }
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = javaClass.simpleName
+        }
     }
 }
