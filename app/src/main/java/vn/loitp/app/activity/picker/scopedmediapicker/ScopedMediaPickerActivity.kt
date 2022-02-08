fe8b43376669113e7c47e76dc8f8aca99b14d7a5
Loitp <v.loitp@cloudhms.net>
2022-02-08 14:49:48 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/picker/scopedmediapicker/ScopedMediaPickerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/picker/scopedmediapicker/ScopedMediaPickerActivity.kt
@@ -1,6 +1,6 @@
 package vn.loitp.app.activity.picker.scopedmediapicker
 
-import android.content.Intent
+// import com.media.scopemediapicker.ScopedMediaPicker
 import android.os.Bundle
 import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
@@ -8,7 +8,6 @@ import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LSocialUtil
 import com.core.utilities.LUIUtil
-import com.media.scopemediapicker.ScopedMediaPicker
 import kotlinx.android.synthetic.main.activity_scoped_media_picker.*
 import vn.loitp.app.R
 
@@ -16,13 +15,13 @@ import vn.loitp.app.R
 @IsFullScreen(false)
 class ScopedMediaPickerActivity : BaseFontActivity() {
 
-    private val scopedMediaPicker by lazy {
-        ScopedMediaPicker(
-            activity = this@ScopedMediaPickerActivity,
-            requiresCrop = true,
-            allowMultipleFiles = true
-        )
-    }
+//    private val scopedMediaPicker by lazy {
+//        ScopedMediaPicker(
+//            activity = this@ScopedMediaPickerActivity,
+//            requiresCrop = true,
+//            allowMultipleFiles = true
+//        )
+//    }
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_scoped_media_picker
@@ -58,29 +57,36 @@ class ScopedMediaPickerActivity : BaseFontActivity() {
             this.tvTitle?.text = javaClass.simpleName
         }
         btnCapture.setOnClickListener {
+            pickCapture()
+        }
+        btnFile.setOnClickListener {
+            pickFile()
+        }
+    }
 
-            // CHON HINH VA VIDEO CUNG LUC
-            scopedMediaPicker.startMediaPicker(
-                mediaType = ScopedMediaPicker.MEDIA_TYPE_IMAGE
-                    or ScopedMediaPicker.MEDIA_TYPE_VIDEO,
-                actionType = ScopedMediaPicker.ACTION_TYPE_GALLERY
-            ) { pathList, type ->
-                when (type) {
-                    ScopedMediaPicker.MEDIA_TYPE_IMAGE -> {
-                        logD(">>>MEDIA_TYPE_IMAGE")
-                    }
-                    ScopedMediaPicker.MEDIA_TYPE_VIDEO -> {
-                        logD(">>>MEDIA_TYPE_VIDEO")
-                    }
-                }
-                var s = ""
-                pathList.forEach {
-                    s += it + "\n"
-                }
-                tvPath.text = s
-            }
+    private fun pickCapture() {
+        // CHON HINH VA VIDEO CUNG LUC
+//        scopedMediaPicker.startMediaPicker(
+//            mediaType = ScopedMediaPicker.MEDIA_TYPE_IMAGE
+//                    or ScopedMediaPicker.MEDIA_TYPE_VIDEO,
+//            actionType = ScopedMediaPicker.ACTION_TYPE_GALLERY
+//        ) { pathList, type ->
+//            when (type) {
+//                ScopedMediaPicker.MEDIA_TYPE_IMAGE -> {
+//                    logD(">>>MEDIA_TYPE_IMAGE")
+//                }
+//                ScopedMediaPicker.MEDIA_TYPE_VIDEO -> {
+//                    logD(">>>MEDIA_TYPE_VIDEO")
+//                }
+//            }
+//            var s = ""
+//            pathList.forEach {
+//                s += it + "\n"
+//            }
+//            tvPath.text = s
+//        }
 
-            // CHON HINH TU GALLERY
+        // CHON HINH TU GALLERY
 //            scopedMediaPicker.startMediaPicker(
 //                mediaType = ScopedMediaPicker.MEDIA_TYPE_IMAGE,
 //                actionType = ScopedMediaPicker.ACTION_TYPE_GALLERY
@@ -92,7 +98,7 @@ class ScopedMediaPickerActivity : BaseFontActivity() {
 //                tvPath.text = s
 //            }
 
-            // CHON HINH TU GALLERY VA CAMERA
+        // CHON HINH TU GALLERY VA CAMERA
 //            scopedMediaPicker.startMediaPicker(
 //                mediaType = ScopedMediaPicker.MEDIA_TYPE_IMAGE,
 //                actionType = ScopedMediaPicker.ACTION_TYPE_GALLERY or ScopedMediaPicker.ACTION_TYPE_CAMERA
@@ -104,7 +110,7 @@ class ScopedMediaPickerActivity : BaseFontActivity() {
 //                tvPath.text = s
 //            }
 
-            // CHON VIDEO TU GALLERY
+        // CHON VIDEO TU GALLERY
 //            scopedMediaPicker.startMediaPicker(
 //                mediaType = ScopedMediaPicker.MEDIA_TYPE_VIDEO,
 //                actionType = ScopedMediaPicker.ACTION_TYPE_GALLERY
@@ -115,29 +121,29 @@ class ScopedMediaPickerActivity : BaseFontActivity() {
 //                }
 //                tvPath.text = s
 //            }
-        }
-        btnFile.setOnClickListener {
-            scopedMediaPicker.startFilePicker() { pathList ->
-                var s = ""
-                pathList.forEach {
-                    s += it.fileName + "\n"
-                }
-                tvPath.text = s
-            }
-        }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        scopedMediaPicker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    private fun pickFile() {
+//        scopedMediaPicker.startFilePicker() { pathList ->
+//            var s = ""
+//            pathList.forEach {
+//                s += it.fileName + "\n"
+//            }
+//            tvPath.text = s
+//        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        scopedMediaPicker.onActivityResult(requestCode, resultCode, data)
-    }
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        scopedMediaPicker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        scopedMediaPicker.onActivityResult(requestCode, resultCode, data)
+//    }
 }
