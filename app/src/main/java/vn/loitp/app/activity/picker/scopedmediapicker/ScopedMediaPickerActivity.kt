3a1bacf80998ab435f8da4e2c52758918e996c64
Loitp <v.loitp@cloudhms.net>
2022-02-08 11:22:22 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/picker/scopedmediapicker/ScopedMediaPickerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/picker/scopedmediapicker/ScopedMediaPickerActivity.kt
@@ -1,5 +1,6 @@
 package vn.loitp.app.activity.picker.scopedmediapicker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
@@ -7,12 +8,23 @@ import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LSocialUtil
 import com.core.utilities.LUIUtil
+import com.media.scopemediapicker.ScopedMediaPicker
 import kotlinx.android.synthetic.main.activity_0.*
+import kotlinx.android.synthetic.main.activity_0.lActionBar
+import kotlinx.android.synthetic.main.activity_scoped_media_picker.*
 import vn.loitp.app.R
 
 @LogTag("ScopedMediaPickerActivity")
 @IsFullScreen(false)
 class ScopedMediaPickerActivity : BaseFontActivity() {
+
+    private val scopedMediaPicker by lazy {
+        ScopedMediaPicker(
+            activity = this@ScopedMediaPickerActivity,
+            requiresCrop = true,
+            allowMultipleFiles = true
+        )
+    }
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_scoped_media_picker
@@ -47,5 +59,29 @@ class ScopedMediaPickerActivity : BaseFontActivity() {
             this.viewShadow?.isVisible = true
             this.tvTitle?.text = javaClass.simpleName
         }
+
+        btnFile.setOnClickListener {
+            scopedMediaPicker.startFilePicker() { pathList ->
+                var s = ""
+                pathList.forEach {
+                    s += it.fileName + "\n"
+                }
+                tvPath.text = s
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        scopedMediaPicker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        scopedMediaPicker.onActivityResult(requestCode, resultCode, data)
     }
 }
