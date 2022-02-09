be9c9e006c898b01a9c02955ba64505814e939d8
Loitp <v.loitp@cloudhms.net>
2022-02-09 10:02:51 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/picker/attachmentmanager/AttachmentManagerActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/picker/attachmentmanager/AttachmentManagerActivity.kt
@@ -2,18 +2,67 @@ package vn.loitp.app.activity.picker.attachmentmanager
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LSocialUtil
 import com.core.utilities.LUIUtil
-import kotlinx.android.synthetic.main.activity_0.*
+import com.mirza.attachmentmanager.managers.AttachmentManager
+import com.mirza.attachmentmanager.models.AttachmentDetail
+import com.views.setSafeOnClickListener
+import kotlinx.android.synthetic.main.activity_0.lActionBar
+import kotlinx.android.synthetic.main.activity_attachment_manager.*
 import vn.loitp.app.R
 
 @LogTag("AttachmentManagerActivity")
 @IsFullScreen(false)
 class AttachmentManagerActivity : BaseFontActivity() {
+    private var attachmentManager: AttachmentManager? = null
+    private var allAttachments: ArrayList<AttachmentDetail>? = arrayListOf()
+    private var mLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            attachmentManager?.manipulateAttachments(this, result.resultCode, result.data)?.let {
+
+                if (it.size > 0 && it[0].mimeType?.contains("pdf", ignoreCase = true) == true) {
+                    if (it.size > 0 && it[0].size!! <= 2000000) {
+                        allAttachments?.addAll(it)
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "File size can't be more than 2MB",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                } else if (it.size > 0) {
+                    allAttachments?.addAll(it)
+                } else {
+                    // do sth
+                }
+            }
+            var s = ""
+            allAttachments?.forEach {
+                logD(">>> ${it.path}")
+                s += it.path + "\n"
+            }
+            tvPath.text = s
+        }
+    var gallery = arrayOf(
+//        "image/png",
+//        "image/jpg",
+//        "image/jpeg",
+        "image/*",
+        "video/*"
+    )
+    var files = arrayOf(
+        "application/msword",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .ppt & .pptx
+        "application/pdf",
+        "video/*",
+        "image/*",
+    )
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_attachment_manager
@@ -21,6 +70,19 @@ class AttachmentManagerActivity : BaseFontActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        attachmentManager = AttachmentManager.AttachmentBuilder(this) // must pass Context
+            .fragment(null) // pass fragment reference if you are in fragment
+            .setUiTitle(getString(R.string.m_choose)) // title of dialog or bottom sheet
+            .allowMultiple(true) // set true if you want make multiple selection, default is false
+            .asBottomSheet(false) // set true if you need to show selection as bottom sheet, default is as Dialog
+            .setOptionsTextColor(android.R.color.holo_green_light)
+            .setImagesColor(R.color.colorAccent)
+//            .hide(HideOption.DOCUMENT)// You can hide any option do you want
+            .setMaxPhotoSize(200000) // Set max camera photo size in bytes
+            .galleryMimeTypes(gallery) // mime types for gallery
+            .filesMimeTypes(files) // mime types for files
+            .build()
 
         setupViews()
     }
@@ -50,5 +112,22 @@ class AttachmentManagerActivity : BaseFontActivity() {
             this.viewShadow?.isVisible = true
             this.tvTitle?.text = AttachmentManagerActivity::class.java.simpleName
         }
+        btPick.setSafeOnClickListener {
+            attachmentManager?.openSelection(mLauncher)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        attachmentManager?.handlePermissionResponse(
+            requestCode,
+            permissions,
+            grantResults,
+            mLauncher
+        )
     }
 }
