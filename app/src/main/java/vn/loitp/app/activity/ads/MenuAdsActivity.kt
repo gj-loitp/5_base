04f3a5c1ffb7d8433e47b0e6befc8384e92a4c9c
Loitp <v.loitp@cloudhms.net>
2022-02-10 15:38:15 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/ads/MenuAdsActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/ads/MenuAdsActivity.kt
@@ -2,10 +2,12 @@ package vn.loitp.app.activity.ads
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LActivityUtil
+import com.core.utilities.LUIUtil
 import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_menu_ads.*
 import vn.loitp.app.R
@@ -27,6 +29,17 @@ class MenuAdsActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = MenuAdsActivity::class.java.simpleName
+        }
         bt1.setSafeOnClickListener {
             val intent = Intent(this, AdMobBannerActivity::class.java)
             startActivity(intent)
