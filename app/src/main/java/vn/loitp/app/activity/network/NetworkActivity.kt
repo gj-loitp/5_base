bfe16f0f1e57b10ac515e1a2e36dd82ba1411461
loitp <freuss47@gmail.com>
2022-03-04 17:40:52 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/network/NetworkActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/network/NetworkActivity.kt
@@ -7,8 +7,14 @@ import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LConnectivityUtil
 import com.data.EventBusData
-import kotlinx.android.synthetic.main.frm_text.*
+import com.function.network.TrafficUtils
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_network.*
 import vn.loitp.app.R
+import java.util.concurrent.TimeUnit
 
 @LogTag("NetworkActivity")
 @IsFullScreen(false)
@@ -22,6 +28,9 @@ class NetworkActivity : BaseFontActivity() {
         super.onCreate(savedInstanceState)
 
         showStatus(LConnectivityUtil.isConnected())
+        btn.setOnClickListener {
+            doSomeWork()
+        }
     }
 
     override fun onNetworkChange(event: EventBusData.ConnectEvent) {
@@ -34,4 +43,44 @@ class NetworkActivity : BaseFontActivity() {
     private fun showStatus(isConnected: Boolean) {
         textView.text = "isConnected: $isConnected"
     }
+
+    override fun onDestroy() {
+        compositeDisposable.clear() // clearing it : do not emit after destroy
+        super.onDestroy()
+    }
+
+    private fun doSomeWork() {
+        compositeDisposable.add(
+            observable
+                .subscribeOn(Schedulers.io()) // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(observer)
+        )
+    }
+
+    private val observable: Observable<out Long>
+        get() = Observable.interval(0, 2, TimeUnit.SECONDS)
+
+    private val observer: DisposableObserver<Long?>
+        get() = object : DisposableObserver<Long?>() {
+            @SuppressLint("SetTextI18n")
+            override fun onNext(value: Long) {
+                logD("=============================onNext : $value")
+                logD("isWifiConnected ${TrafficUtils.isWifiConnected(this@NetworkActivity)}")
+                logD("getNetworkSpeed ${TrafficUtils.getNetworkSpeed()}")
+
+                tv.post {
+                    tv.text =
+                        "isWifiConnected ${TrafficUtils.isWifiConnected(this@NetworkActivity)}\ngetNetworkSpeed ${TrafficUtils.getNetworkSpeed()}"
+                }
+            }
+
+            override fun onError(e: Throwable) {
+                logD("onError : ${e.message}")
+            }
+
+            override fun onComplete() {
+                logD("onComplete")
+            }
+        }
 }
