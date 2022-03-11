3ceec84d9ed4b37140e7def67b7085d1e2cc21c5
loitp <freuss47@gmail.com>
2022-03-11 15:07:14 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/dialog/DialogMenuActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/dialog/DialogMenuActivity.kt
@@ -3,10 +3,12 @@ package vn.loitp.app.activity.customviews.dialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LActivityUtil
+import com.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_dialog_menu.*
 import vn.loitp.app.R
 import vn.loitp.app.activity.customviews.dialog.customdialog.CustomDialogActivity
@@ -30,6 +32,17 @@ class DialogMenuActivity : BaseFontActivity(), View.OnClickListener {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.setImageResource(R.color.transparent)
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = DialogMenuActivity::class.java.simpleName
+        }
         btOriginalDialog.setOnClickListener(this)
         btPrettyDialog.setOnClickListener(this)
         btCustomProgressDialog.setOnClickListener(this)
