0f49fed306ab8589bfead057475a9d47c7cbeb26
loitp <freuss47@gmail.com>
2022-03-25 15:00:59 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/custombehavior/CustomBehaviorActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/custombehavior/CustomBehaviorActivity.kt
@@ -1,9 +1,11 @@
 package vn.loitp.app.activity.customviews.layout.constraintlayout.custombehavior
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LUIUtil
 import com.google.android.material.snackbar.Snackbar
 import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_custom_behavior.*
@@ -24,7 +26,17 @@ class CustomBehaviorActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
-
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = CustomBehaviorActivity::class.java.simpleName
+        }
         fab.setSafeOnClickListener {
             Snackbar.make(
                 coordinatorLayout,
--- app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/demo/ConstraintLayoutDemoActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/constraintlayout/demo/ConstraintLayoutDemoActivity.kt
@@ -2,9 +2,11 @@ package vn.loitp.app.activity.customviews.layout.constraintlayout.demo
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LUIUtil
 import com.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_constraintlayout_demo.*
 import vn.loitp.app.R
@@ -24,6 +26,17 @@ class ConstraintLayoutDemoActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = ConstraintLayoutDemoActivity::class.java.simpleName
+        }
         button.setSafeOnClickListener {
             it.visibility = View.GONE
         }
