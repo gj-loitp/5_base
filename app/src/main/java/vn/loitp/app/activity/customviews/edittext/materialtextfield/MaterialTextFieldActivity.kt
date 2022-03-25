c6536e2bafeee99ed601241495407292dfdf2707
loitp <freuss47@gmail.com>
2022-03-25 14:09:20 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/edittext/materialtextfield/MaterialTextFieldActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/edittext/materialtextfield/MaterialTextFieldActivity.kt
@@ -1,11 +1,14 @@
 package vn.loitp.app.activity.customviews.edittext.materialtextfield
 
+import android.os.Bundle
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LSocialUtil
+import com.core.utilities.LUIUtil
+import kotlinx.android.synthetic.main.activity_0.*
 import vn.loitp.app.R
-
-// guide https://github.com/florent37/MaterialTextField
 
 @LogTag("MaterialTextFieldActivity")
 @IsFullScreen(false)
@@ -13,5 +16,37 @@ class MaterialTextFieldActivity : BaseFontActivity() {
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_editext_material_textfield
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.let {
+                LUIUtil.setSafeOnClickListenerElastic(
+                    view = it,
+                    runnable = {
+                        LSocialUtil.openUrlInBrowser(
+                            context = context,
+                            url = "https://github.com/florent37/MaterialTextField"
+                        )
+                    }
+                )
+                it.isVisible = true
+                it.setImageResource(R.drawable.ic_baseline_code_48)
+            }
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = MaterialTextFieldActivity::class.java.simpleName
+        }
     }
 }
