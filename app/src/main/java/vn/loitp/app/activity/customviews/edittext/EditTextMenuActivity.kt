ae81ba210e7f0e1deb665b35f75074f546292408
loitp <freuss47@gmail.com>
2022-03-25 13:53:45 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/edittext/EditTextMenuActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/edittext/EditTextMenuActivity.kt
@@ -3,10 +3,12 @@ package vn.loitp.app.activity.customviews.edittext
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LActivityUtil
+import com.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_edittext_menu.*
 import vn.loitp.app.R
 import vn.loitp.app.activity.customviews.edittext.animatedexpandableedittext.AnimatedExpandableEditTextActivity
@@ -28,6 +30,21 @@ class EditTextMenuActivity : BaseFontActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = EditTextMenuActivity::class.java.simpleName
+        }
         btAnimatedExpandableEditText.setOnClickListener(this)
         btAutoResizeEditText.setOnClickListener(this)
         btMaterialTextField.setOnClickListener(this)
