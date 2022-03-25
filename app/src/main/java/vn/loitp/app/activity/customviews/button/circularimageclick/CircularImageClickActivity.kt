a0d6c2a825155b9f7eef12a8254b9c4c1ed8f588
loitp <freuss47@gmail.com>
2022-03-25 13:26:26 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/button/circularimageclick/CircularImageClickActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/button/circularimageclick/CircularImageClickActivity.kt
@@ -2,9 +2,11 @@ package vn.loitp.app.activity.customviews.button.circularimageclick
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LUIUtil
 import com.views.button.circularimageclick.LCircularClickImageButton
 import kotlinx.android.synthetic.main.activity_button_circular_image_click.*
 import vn.loitp.app.R
@@ -24,11 +26,22 @@ class CircularImageClickActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
-        circleButton.setOnCircleClickListener(object :
-                LCircularClickImageButton.OnCircleClickListener {
-                override fun onClick(v: View?) {
-                    showShortInformation("onClick")
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
                 }
-            })
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = CircularImageClickActivity::class.java.simpleName
+        }
+        circleButton.setOnCircleClickListener(object :
+            LCircularClickImageButton.OnCircleClickListener {
+            override fun onClick(v: View?) {
+                showShortInformation("onClick")
+            }
+        })
     }
 }
--- app/src/main/java/vn/loitp/app/activity/customviews/button/goodview/GoodViewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/button/goodview/GoodViewActivity.kt
@@ -2,14 +2,15 @@ package vn.loitp.app.activity.customviews.button.goodview
 
 import android.graphics.Color
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LSocialUtil
+import com.core.utilities.LUIUtil
 import com.views.button.goodview.LGoodView
 import kotlinx.android.synthetic.main.activity_button_goodview.*
 import vn.loitp.app.R
-
-// https://github.com/venshine/GoodView
 
 @LogTag("GoodViewActivity")
 @IsFullScreen(false)
@@ -28,6 +29,30 @@ class GoodViewActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.let {
+                LUIUtil.setSafeOnClickListenerElastic(
+                    view = it,
+                    runnable = {
+                        LSocialUtil.openUrlInBrowser(
+                            context = context,
+                            url = "https://github.com/venshine/GoodView"
+                        )
+                    }
+                )
+                it.isVisible = true
+                it.setImageResource(R.drawable.ic_baseline_code_48)
+            }
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = GoodViewActivity::class.java.simpleName
+        }
+
         lGoodView = LGoodView(this)
         bt.setOnClickListener {
             lGoodView?.let {
