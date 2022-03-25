bfe16e921a38650afd517e7fc535fa8fe2f2b3e6
loitp <freuss47@gmail.com>
2022-03-25 15:26:09 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/shadowlayout/ShadowLayoutActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/shadowlayout/ShadowLayoutActivity.kt
@@ -3,15 +3,17 @@ package vn.loitp.app.activity.customviews.layout.shadowlayout
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LAppResource
+import com.core.utilities.LSocialUtil
+import com.core.utilities.LUIUtil
 import com.utils.util.ConvertUtils
 import kotlinx.android.synthetic.main.activity_layout_shadow.*
 import vn.loitp.app.R
 
-// https://github.com/lijiankun24/ShadowLayout
 @LogTag("ShadowLayoutActivity")
 @IsFullScreen(false)
 class ShadowLayoutActivity : BaseFontActivity(), View.OnClickListener {
@@ -23,6 +25,33 @@ class ShadowLayoutActivity : BaseFontActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.let {
+                LUIUtil.setSafeOnClickListenerElastic(
+                    view = it,
+                    runnable = {
+                        LSocialUtil.openUrlInBrowser(
+                            context = context,
+                            url = "https://github.com/lijiankun24/ShadowLayout"
+                        )
+                    }
+                )
+                it.isVisible = true
+                it.setImageResource(R.drawable.ic_baseline_code_48)
+            }
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = ShadowLayoutActivity::class.java.simpleName
+        }
         tvChangeOval.setOnClickListener(this)
         tvChangeRadius.setOnClickListener(this)
         tvChangeRectangle.setOnClickListener(this)
