162439bc7f366813ee923ad72e054507330dd885
loitp <freuss47@gmail.com>
2022-03-31 09:49:02 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/splitpanellayout/SplitPanelLayoutActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/splitpanellayout/SplitPanelLayoutActivity.kt
@@ -1,9 +1,11 @@
 package vn.loitp.app.activity.customviews.layout.splitpanellayout
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
+import com.core.utilities.LUIUtil
 import com.views.layout.splitpanellayout.SplitPaneLayout
 import kotlinx.android.synthetic.main.activity_layout_split_panel.*
 import vn.loitp.app.R
@@ -21,6 +23,21 @@ class SplitPanelLayoutActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = SplitPanelLayoutActivity::class.java.simpleName
+        }
         splitPaneLayout.onSplitterPositionChangedListener =
             object : SplitPaneLayout.OnSplitterPositionChangedListener {
                 override fun onSplitterPositionChanged(
