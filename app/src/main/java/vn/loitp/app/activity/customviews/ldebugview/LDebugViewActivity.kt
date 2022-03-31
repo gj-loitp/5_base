dc42a529d97be0c86a4b8bbd75d453b71c7da453
loitp <freuss47@gmail.com>
2022-03-31 10:14:24 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/ldebugview/LDebugViewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/ldebugview/LDebugViewActivity.kt
@@ -3,10 +3,12 @@ package vn.loitp.app.activity.customviews.ldebugview
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.base.BaseModel
+import com.core.utilities.LUIUtil
 import com.views.ldebugview.LComunicateDebug
 import com.views.ldebugview.LDebug
 import kotlinx.android.synthetic.main.activity_l_debugview.*
@@ -36,6 +38,17 @@ class LDebugViewActivity : BaseFontActivity(), View.OnClickListener {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = LDebugViewActivity::class.java.simpleName
+        }
         btStart.setOnClickListener(this)
         btStop.setOnClickListener(this)
         btSendD.setOnClickListener(this)
--- app/src/main/java/vn/loitp/app/activity/customviews/menu/MenuMenuActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/menu/MenuMenuActivity.kt
@@ -3,10 +3,12 @@ package vn.loitp.app.activity.customviews.menu
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LActivityUtil
+import com.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_menu_menu.*
 import vn.loitp.app.R
 import vn.loitp.app.activity.customviews.menu.drawerbehavior.DrawerBehaviorMainActivity
@@ -23,6 +25,21 @@ class MenuMenuActivity : BaseFontActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = MenuMenuActivity::class.java.simpleName
+        }
         btResideMenu.setOnClickListener(this)
         btDrawerBehavior.setOnClickListener(this)
     }
