65d57dd92fbdf894322fce2e94fabf26f952ccfc
loitp <freuss47@gmail.com>
2022-03-31 10:47:14 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/progressloadingview/MenuProgressLoadingViewsActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/progressloadingview/MenuProgressLoadingViewsActivity.kt
@@ -3,10 +3,12 @@ package vn.loitp.app.activity.customviews.progressloadingview
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.utilities.LActivityUtil
+import com.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_progress_loading_menu.*
 import vn.loitp.app.R
 import vn.loitp.app.activity.customviews.progressloadingview.circularprogressbar.CircularProgressBarActivity
@@ -24,6 +26,21 @@ class MenuProgressLoadingViewsActivity : BaseFontActivity(), View.OnClickListene
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.isVisible = false
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = MenuProgressLoadingViewsActivity::class.java.simpleName
+        }
         btCircularProgressBar.setOnClickListener(this)
         btCircularProgressIndicator.setOnClickListener(this)
         btWindow.setOnClickListener(this)
