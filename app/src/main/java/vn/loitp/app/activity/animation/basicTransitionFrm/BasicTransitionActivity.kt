f496455252a84bdf1a29e26c96f846ab9669c7ac
loitp <loitp@galaxy.one>
2022-07-18 22:17:30 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/animation/basicTransitionFrm/BasicTransitionActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/animation/basicTransitionFrm/BasicTransitionActivity.kt
@@ -1,9 +1,12 @@
 package vn.loitp.app.activity.animation.basicTransitionFrm
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
+import com.loitpcore.core.utilities.LUIUtil
+import kotlinx.android.synthetic.main.activity_basic_transition.*
 import vn.loitp.app.R
 
 // https://github.com/googlesamples/android-BasicTransition/#readme
@@ -23,6 +26,22 @@ class BasicTransitionActivity : BaseFontActivity() {
             val fragment = BasicTransitionFragment()
             transaction.replace(R.id.sample_content_fragment, fragment)
             transaction.commit()
+        }
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.setImageResource(R.color.transparent)
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = BasicTransitionActivity::class.java.simpleName
         }
     }
 }
