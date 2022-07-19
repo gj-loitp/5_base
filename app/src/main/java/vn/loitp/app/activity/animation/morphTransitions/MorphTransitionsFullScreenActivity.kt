360c185d3b763868c90d12957e39efbf635e92a4
loitp <loitp@galaxy.one>
2022-07-19 09:06:47 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/animation/morphTransitions/MorphTransitionsFullScreenActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/animation/morphTransitions/MorphTransitionsFullScreenActivity.kt
@@ -1,10 +1,12 @@
 package vn.loitp.app.activity.animation.morphTransitions
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.loitpcore.animation.morphTransitions.FabTransform
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
+import com.loitpcore.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_morph_transitions_full_screen.*
 import vn.loitp.app.R
 
@@ -18,6 +20,21 @@ class MorphTransitionsFullScreenActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
         FabTransform.setup(this, rootView)
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.setImageResource(R.color.transparent)
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = MorphTransitionsFullScreenActivity::class.java.simpleName
+        }
     }
 }
