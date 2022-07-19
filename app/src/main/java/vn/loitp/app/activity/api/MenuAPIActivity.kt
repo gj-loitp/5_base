e5f6c660e85952b4467d7464903b327808ead35c
loitp <loitp@galaxy.one>
2022-07-19 10:08:05 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/api/MenuAPIActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/api/MenuAPIActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.core.view.isVisible
 import com.loitpcore.annotation.IsAutoAnimation
 import com.loitpcore.annotation.IsFullScreen
+import com.loitpcore.annotation.IsShowAdWhenExit
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
 import com.loitpcore.core.utilities.LActivityUtil
@@ -21,6 +22,7 @@ import vn.loitp.app.activity.api.truyentranhtuan.MenuTTTAPIActivity
 @LogTag("MenuAPIActivity")
 @IsFullScreen(false)
 @IsAutoAnimation(true)
+@IsShowAdWhenExit(true)
 class MenuAPIActivity : BaseFontActivity(), View.OnClickListener {
 
     override fun setLayoutResourceId(): Int {
