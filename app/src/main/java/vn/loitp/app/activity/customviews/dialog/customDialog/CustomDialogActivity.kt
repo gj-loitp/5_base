11213dc6238f60ca8c1cdf4672979fc29ad5de77
loitp <loitp@galaxy.one>
2022-07-19 11:21:27 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/dialog/customDialog/CustomDialogActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/dialog/customDialog/CustomDialogActivity.kt
@@ -1,10 +1,12 @@
 package vn.loitp.app.activity.customviews.dialog.customDialog
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
 import com.loitpcore.core.utilities.LScreenUtil
+import com.loitpcore.core.utilities.LUIUtil
 import com.loitpcore.views.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_custom_dialog.*
 import vn.loitp.app.R
@@ -26,6 +28,17 @@ class CustomDialogActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.setImageResource(R.color.transparent)
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = CustomDialogActivity::class.java.simpleName
+        }
         btToggleFullScreen.setSafeOnClickListener {
             isFullScreen = !isFullScreen
             LScreenUtil.toggleFullscreen(activity = this, isFullScreen = isFullScreen)
