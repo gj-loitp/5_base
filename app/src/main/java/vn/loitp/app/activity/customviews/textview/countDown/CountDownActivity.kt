de4b61a0bbbd42649733778ef018c0d8c7f7a987
loitp <loitp@galaxy.one>
2022-07-26 14:47:01 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/textview/countDown/CountDownActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/textview/countDown/CountDownActivity.kt
@@ -1,9 +1,11 @@
 package vn.loitp.app.activity.customviews.textview.countDown
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
+import com.loitpcore.core.utilities.LUIUtil
 import com.loitpcore.views.setSafeOnClickListener
 import com.loitpcore.views.textView.countDown.LCountDownView
 import kotlinx.android.synthetic.main.activity_text_view_count_down.*
@@ -24,6 +26,17 @@ class CountDownActivity : BaseFontActivity() {
     }
 
     private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBackPressed()
+                }
+            )
+            this.ivIconRight?.setImageResource(R.color.transparent)
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = CountDownActivity::class.java.simpleName
+        }
         countDownView.setShowOrHide(false)
         countDownView.setCallback(object : LCountDownView.Callback {
             override fun onTick() {
