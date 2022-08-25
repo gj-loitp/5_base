766ab9888284df11a902a10650ef9853deb9c48b
loitp <loitp@galaxy.one>
2022-08-25 09:16:08 +0700

    update efvp

--- app/src/main/java/vn/loitp/app/activity/customviews/viewPager/easyFlipViewPager/BookOnboardingActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/viewPager/easyFlipViewPager/BookOnboardingActivity.kt
@@ -1,36 +1,42 @@
 package vn.loitp.app.activity.customviews.viewPager.easyFlipViewPager
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
+import com.loitpcore.annotation.IsAutoAnimation
+import com.loitpcore.annotation.IsFullScreen
+import com.loitpcore.annotation.LogTag
+import com.loitpcore.core.base.BaseFontActivity
 import com.wajahatkarim3.easyflipviewpager.BookFlipPageTransformer
+import kotlinx.android.synthetic.main.activity_book_onboarding.*
 import vn.loitp.app.R
 
-class BookOnboardingActivity : AppCompatActivity() {
+@LogTag("BookOnboardingActivity")
+@IsFullScreen(false)
+@IsAutoAnimation(false)
+class BookOnboardingActivity : BaseFontActivity() {
 
-    private lateinit var mPager: ViewPager
     private var mPagerAdapter: PagerAdapter? = null
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_book_onboarding
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_onboarding)
 
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = findViewById(R.id.pager)
         mPagerAdapter = BookOnboardingPagerAdapter(supportFragmentManager)
-        mPager.adapter = mPagerAdapter
-        mPager.clipToPadding = true
-        mPager.setPageTransformer(true, BookFlipPageTransformer())
+        pager.adapter = mPagerAdapter
+        pager.clipToPadding = true
+        pager.setPageTransformer(true, BookFlipPageTransformer())
     }
 
-    class BookOnboardingPagerAdapter : FragmentPagerAdapter {
+    class BookOnboardingPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         var fragments = arrayListOf<BookPageIntroFragment>()
 
-        constructor(fm: FragmentManager) : super(fm) {
+        init {
             val titles =
                 arrayOf("All About Reading", "Find Your Love", "Pick Your Books", "Enjoy Your Time")
             val subtitles = arrayOf(
@@ -45,9 +51,8 @@ class BookOnboardingActivity : AppCompatActivity() {
                 R.drawable.pick_your_books,
                 R.drawable.enjoy_your_time
             )
-
             for (i in 0..3) {
-                var frag = BookPageIntroFragment.newInstance(titles[i], subtitles[i], imageIds[i])
+                val frag = BookPageIntroFragment.newInstance(titles[i], subtitles[i], imageIds[i])
                 fragments.add(frag)
             }
         }
