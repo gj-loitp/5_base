ea70451c6a2f169113afdf27e152f55b2c14817b
loitp <loitp@galaxy.one>
2022-09-23 14:16:37 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/viewPager/vertical/ViewPagerVerticalActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/viewPager/vertical/ViewPagerVerticalActivity.kt
@@ -37,7 +37,7 @@ class ViewPagerVerticalActivity : BaseFontActivity() {
             LUIUtil.setSafeOnClickListenerElastic(
                 view = this.ivIconLeft,
                 runnable = {
-                    onBackPressed()
+                    onBaseBackPressed()
                 }
             )
             this.ivIconRight?.setImageResource(R.color.transparent)
