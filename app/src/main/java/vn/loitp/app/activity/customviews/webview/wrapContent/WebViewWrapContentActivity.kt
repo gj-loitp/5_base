b8914be32cfe8228e1b305f38f872bac24b0a74c
loitp <loitp@galaxy.one>
2022-09-26 23:04:54 +0700

    update webview

--- app/src/main/java/vn/loitp/app/activity/customviews/webview/wrapContent/WebViewWrapContentActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/webview/wrapContent/WebViewWrapContentActivity.kt
@@ -1,12 +1,18 @@
 package vn.loitp.app.activity.customviews.webview.wrapContent
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.loitpcore.annotation.IsAutoAnimation
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
+import com.loitpcore.core.utilities.LScreenUtil
 import kotlinx.android.synthetic.main.activity_web_view_wrap_content.*
 import vn.loitp.app.R
 
@@ -26,7 +32,7 @@ class WebViewWrapContentActivity : BaseFontActivity() {
         setupViews()
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "SetJavaScriptEnabled")
     private fun setupViews() {
 //        lActionBar.apply {
 //            LUIUtil.setSafeOnClickListenerElastic(
@@ -54,8 +60,20 @@ class WebViewWrapContentActivity : BaseFontActivity() {
         wv.loadDataWithBaseURL(null, HtmlContent.body, "text/html", "UTF-8", null)
 //        wv.webViewClient = object : WebViewClient() {
 //            override fun onPageFinished(view: WebView, url: String) {
+//                logE("onPageFinished ${LScreenUtil.screenHeight}")
+//                logE("onPageFinished ${wv.height}")
+//                logE("onPageFinished ${wv.contentHeight}")
 //            }
 //        }
+
+        wv.settings.javaScriptEnabled = true
+        wv.addJavascriptInterface(JsObject(this), "HTMLOUT")
+        wv.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                view.loadUrl("javascript:( function () { var h = document.body.scrollHeight; window.HTMLOUT.toString(h); } ) ()")
+            }
+        }
+
 //        val viewTreeObserver: ViewTreeObserver = wv.viewTreeObserver
 //        viewTreeObserver.addOnGlobalLayoutListener(object :
 //            ViewTreeObserver.OnGlobalLayoutListener {
@@ -78,5 +96,20 @@ class WebViewWrapContentActivity : BaseFontActivity() {
 //            }
 //        })
 
+    }
+
+    internal class JsObject(val context: Context) {
+        @JavascriptInterface
+        fun toString(jsResult: String) {
+            Log.e("loitpp", "scrolH: $jsResult")
+            //use this if you need actual onscreen measurements
+//            float webViewHeight = (Integer.parseInt(jsResult) * getResources().getDisplayMetrics().density);
+
+            val webViewHeight =
+                (Integer.parseInt(jsResult) * context.resources.displayMetrics.density)
+            Log.e("loitpp", "webViewHeight: $webViewHeight")
+
+            Log.e("loitpp", "screenHeight: ${LScreenUtil.screenHeight}")
+        }
     }
 }
