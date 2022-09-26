314d44f775f84dbe2f38587f2b5dc5ea14963904
loitp <loitp@galaxy.one>
2022-09-26 22:36:52 +0700

    update webview

--- app/src/main/java/vn/loitp/app/activity/customviews/webview/wrapContent/WebViewWrapContentActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/webview/wrapContent/WebViewWrapContentActivity.kt
@@ -1,13 +1,12 @@
 package vn.loitp.app.activity.customviews.webview.wrapContent
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import androidx.core.view.isVisible
 import com.loitpcore.annotation.IsAutoAnimation
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
-import com.loitpcore.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_web_view_wrap_content.*
 import vn.loitp.app.R
 
@@ -28,25 +27,28 @@ class WebViewWrapContentActivity : BaseFontActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun setupViews() {
-        lActionBar.apply {
-            LUIUtil.setSafeOnClickListenerElastic(
-                view = this.ivIconLeft,
-                runnable = {
-                    onBaseBackPressed()
-                }
-            )
-            this.ivIconRight?.let {
-                LUIUtil.setSafeOnClickListenerElastic(
-                    view = it,
-                    runnable = {
-                        showShortInformation(msg = "onClickMenu", isTopAnchor = false)
-                    }
-                )
-                it.isVisible = true
-                it.setImageResource(R.drawable.ic_baseline_code_48)
-            }
-            this.viewShadow?.isVisible = true
-            this.tvTitle?.text = WebViewWrapContentActivity::class.java.simpleName
-        }
+//        lActionBar.apply {
+//            LUIUtil.setSafeOnClickListenerElastic(
+//                view = this.ivIconLeft,
+//                runnable = {
+//                    onBaseBackPressed()
+//                }
+//            )
+//            this.ivIconRight?.let {
+//                LUIUtil.setSafeOnClickListenerElastic(
+//                    view = it,
+//                    runnable = {
+//                        showShortInformation(msg = "onClickMenu", isTopAnchor = false)
+//                    }
+//                )
+//                it.isVisible = true
+//                it.setImageResource(R.drawable.ic_baseline_code_48)
+//            }
+//            this.viewShadow?.isVisible = true
+//            this.tvTitle?.text = WebViewWrapContentActivity::class.java.simpleName
+//        }
+
+        wv.setBackgroundColor(Color.RED)
+        wv.loadDataWithBaseURL(null, HtmlContent.body, "text/html", "UTF-8", null)
     }
 }
