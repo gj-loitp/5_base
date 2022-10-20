bfbf34740f19436915f8b6003270fbae6814230f
loitp <loitp@galaxy.one>
2022-10-20 23:19:36 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/game/osero/TopOseroActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/game/osero/TopOseroActivity.kt
@@ -1,12 +1,16 @@
 package vn.loitp.app.activity.game.osero
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.loitpcore.annotation.IsAutoAnimation
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
+import com.loitpcore.core.utilities.LUIUtil
 import com.loitpcore.views.setSafeOnClickListener
+import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.activity_top.*
+import kotlinx.android.synthetic.main.activity_top.lActionBar
 import vn.loitp.app.R
 import vn.loitp.app.activity.game.osero.game.GameOseroActivity
 import vn.loitp.app.activity.game.osero.model.ai.AINone
@@ -25,6 +29,21 @@ class TopOseroActivity : BaseFontActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lActionBar.apply {
+            LUIUtil.setSafeOnClickListenerElastic(
+                view = this.ivIconLeft,
+                runnable = {
+                    onBaseBackPressed()
+                }
+            )
+            this.ivIconRight?.setImageResource(R.color.transparent)
+            this.viewShadow?.isVisible = true
+            this.tvTitle?.text = TopOseroActivity::class.java.simpleName
+        }
         topModeManualButton.setSafeOnClickListener { startGameActivity(AINone()) }
         topModeAIWeakButton.setSafeOnClickListener { startGameActivity(AIWeak()) }
         topModeAIStrongButton.setSafeOnClickListener { startGameActivity(AIStrong()) }
