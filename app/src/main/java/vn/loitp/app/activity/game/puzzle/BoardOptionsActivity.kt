be4bed1315e7f5dbbe26c2a49248ed8f12471d74
loitp <loitp@galaxy.one>
2022-11-24 16:04:50 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/game/puzzle/BoardOptionsActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/game/puzzle/BoardOptionsActivity.kt
@@ -1,15 +1,24 @@
 package vn.loitp.app.activity.game.puzzle
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.loitpcore.annotation.IsAutoAnimation
+import com.loitpcore.annotation.IsFullScreen
+import com.loitpcore.annotation.LogTag
+import com.loitpcore.core.base.BaseFontActivity
 import vn.loitp.app.R
 import vn.loitp.app.activity.game.puzzle.ui.boardoptions.BoardOptionsFragment
 
-class BoardOptionsActivity : AppCompatActivity() {
+@LogTag("BoardOptionsActivity")
+@IsFullScreen(false)
+@IsAutoAnimation(true)
+class BoardOptionsActivity : BaseFontActivity() {
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_puzzle_board_options
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_puzzle_board_options)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, BoardOptionsFragment.newInstance()).commitNow()
--- app/src/main/java/vn/loitp/app/activity/game/puzzle/GameActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/game/puzzle/GameActivity.kt
@@ -5,18 +5,30 @@ import android.os.Bundle
 import android.util.Size
 import android.view.MenuItem
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import com.loitpcore.annotation.IsAutoAnimation
+import com.loitpcore.annotation.IsFullScreen
+import com.loitpcore.annotation.LogTag
+import com.loitpcore.core.base.BaseFontActivity
 import vn.loitp.app.R
 import vn.loitp.app.activity.game.puzzle.ui.boardoptions.BoardOptionsViewModel
 import vn.loitp.app.activity.game.puzzle.ui.boardoptions.BoardTitledSize
 import vn.loitp.app.activity.game.puzzle.ui.game.GameBoard
 
-data class BoardActivityParams(val bitmap: Bitmap, val size: BoardTitledSize)
+data class BoardActivityParams(
+    val bitmap: Bitmap, val size: BoardTitledSize
+)
 
-class GameActivity : AppCompatActivity() {
+@LogTag("BoardOptionsActivity")
+@IsFullScreen(false)
+@IsAutoAnimation(true)
+class GameActivity : BaseFontActivity() {
     companion object {
         lateinit var initialConfig: BoardActivityParams
+    }
+
+    override fun setLayoutResourceId(): Int {
+        return R.layout.activity_game_puzzle
     }
 
     private val viewModel: BoardOptionsViewModel by lazy {
@@ -29,8 +41,7 @@ class GameActivity : AppCompatActivity() {
         viewModel.boardSize.observe(this) {
             it?.let {
                 board.resize(
-                    Size(it.width, it.height),
-                    viewModel.boardImage.value
+                    Size(it.width, it.height), viewModel.boardImage.value
                 )
             }
         }
@@ -52,7 +63,6 @@ class GameActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_game_puzzle)
         setSupportActionBar(findViewById(R.id.tbBoardOptions))
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
