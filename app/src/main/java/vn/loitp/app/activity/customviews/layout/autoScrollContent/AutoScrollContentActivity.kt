9776b66d0324fc20403c3c9b34d160a596f3f817
loitp <loitp@galaxy.one>
2022-12-14 15:49:40 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/layout/autoScrollContent/AutoScrollContentActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/autoScrollContent/AutoScrollContentActivity.kt
@@ -1,7 +1,11 @@
 package vn.loitp.app.activity.customviews.layout.autoScrollContent
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.loitpcore.annotation.IsAutoAnimation
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
@@ -10,11 +14,20 @@ import com.loitpcore.core.utilities.LSocialUtil
 import com.loitpcore.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_auto_scroll_content.*
 import vn.loitp.app.R
+import vn.loitp.app.databinding.ActivityAutoScrollContentBinding
 
 @LogTag("AutoScrollContentActivity")
 @IsFullScreen(false)
 @IsAutoAnimation(false)
 class AutoScrollContentActivity : BaseFontActivity() {
+
+    private lateinit var binding: ActivityAutoScrollContentBinding
+    private val lm = LinearLayoutManager(this)
+    private var reverse = false
+    private var loop = false
+    private var canTouch = false
+    private var orientationLm = LinearLayoutManager.HORIZONTAL
+    private var currentSpeed = 40
 
     override fun setLayoutResourceId(): Int {
         return R.layout.activity_auto_scroll_content
@@ -22,6 +35,9 @@ class AutoScrollContentActivity : BaseFontActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAutoScrollContentBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         setupViews()
     }
@@ -49,5 +65,141 @@ class AutoScrollContentActivity : BaseFontActivity() {
             }
             this.tvTitle?.text = AutoScrollContentActivity::class.java.simpleName
         }
+
+        lm.orientation = LinearLayoutManager.HORIZONTAL
+        binding.rvAutoScrollContent.layoutManager = lm
+
+        setUpAutoScrollContent(
+            listOf(
+                ExampleModel("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.")
+            )
+        ) {
+            Toast.makeText(this, "Item clicked", Toast.LENGTH_SHORT).show()
+        }
+        setUpSwLoopChecked()
+        setUpSwReverseChecked()
+        setUpSwHorizontalVerticalChecked()
+        setUpSwCanTouchChecked()
+        setUpSeekBarCurrentSpeed()
+    }
+
+    private fun setUpAutoScrollContent(
+        messagesList: List<ExampleModel>,
+        onItemClicked: (String) -> Unit
+    ) {
+
+        val adapter = ExampleAdapter().apply {
+            submitList(messagesList)
+        }
+
+        binding.rvAutoScrollContent.adapter = adapter
+
+        binding.rvAutoScrollContent.setItemClickListener { viewHolder, position ->
+            viewHolder?.let {
+                adapter.onLinkItem(viewHolder, position, onItemClicked)
+            }
+        }
+    }
+
+    private fun setUpSwLoopChecked() {
+        binding.swLoppContent.setOnCheckedChangeListener { _, isChecked ->
+            loop = isChecked
+            setUpLoop()
+            setUpSpeed()
+            setUpReverse()
+            setUpCanTouch()
+            setUpOrientationLm()
+        }
+    }
+
+    private fun setUpSwReverseChecked() {
+        binding.swReverseContent.setOnCheckedChangeListener { _, isChecked ->
+            reverse = isChecked
+            setUpLoop()
+            setUpSpeed()
+            setUpReverse()
+            setUpCanTouch()
+            setUpOrientationLm()
+        }
+    }
+
+    private fun setUpSwCanTouchChecked() {
+        binding.swCanTouchContent.setOnCheckedChangeListener { _, isChecked ->
+            canTouch = isChecked
+            setUpLoop()
+            setUpSpeed()
+            setUpReverse()
+            setUpCanTouch()
+            setUpOrientationLm()
+        }
+    }
+
+    private fun setUpSwHorizontalVerticalChecked() {
+        binding.swHorizontalVerticalContent.setOnCheckedChangeListener { _, isChecked ->
+            orientationLm = if (isChecked) {
+                LinearLayoutManager.VERTICAL
+            } else {
+                LinearLayoutManager.HORIZONTAL
+            }
+
+            setUpLoop()
+            setUpSpeed()
+            setUpReverse()
+            setUpCanTouch()
+            setUpOrientationLm()
+        }
+    }
+
+    private fun setUpSeekBarCurrentSpeed() {
+        binding.seekbar.progress = currentSpeed
+        binding.seekbar.max = 100
+
+        binding.seekbar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+                currentSpeed = seek.progress
+                setUpSpeed()
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                currentSpeed = seek.progress
+                setUpSpeed()
+                showShortInformation("Progress is: " + seek.progress + "%")
+            }
+        })
+    }
+
+    private fun setUpSpeed() {
+        openAutoScroll()
+    }
+
+    private fun setUpLoop() {
+        binding.rvAutoScrollContent.setLoopEnabled(loop)
+        openAutoScroll()
+    }
+
+    private fun setUpReverse() {
+        openAutoScroll()
+    }
+
+    private fun setUpCanTouch() {
+        binding.rvAutoScrollContent.setCanTouch(canTouch)
+    }
+
+    private fun openAutoScroll() {
+        binding.rvAutoScrollContent.openAutoScroll(speed = currentSpeed, reverse = reverse)
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun setUpOrientationLm() {
+        lm.orientation = orientationLm
+        binding.rvAutoScrollContent.layoutManager = lm
     }
 }
---
+++ app/src/main/java/vn/loitp/app/activity/customviews/layout/autoScrollContent/ExampleViewHolder.kt
@@ -0,0 +1,20 @@
+package vn.loitp.app.activity.customviews.layout.autoScrollContent
+
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.viewbinding.ViewBinding
+import vn.loitp.app.databinding.ItemAutoscrollContentBinding
+
+class ExampleViewHolder(
+    private val binding: ViewBinding
+) : ViewHolder(binding.root) {
+
+    fun bind(item: ExampleModel) {
+        val binding = (binding as ItemAutoscrollContentBinding)
+
+        binding.tvMessage.text = item.message
+    }
+
+    fun onLinkItem(item: ExampleModel, onLinkItem: (String) -> Unit) {
+        onLinkItem.invoke(item.message)
+    }
+}
