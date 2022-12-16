399592cea09f7eb5b1e691a35d97191f48f90f67
loitp <loitp@galaxy.one>
2022-12-16 14:08:00 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/customviews/cardView/CardViewActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/customviews/cardView/CardViewActivity.kt
@@ -2,13 +2,17 @@ package vn.loitp.app.activity.customviews.cardView
 
 import android.os.Bundle
 import androidx.core.view.isVisible
+import com.google.android.material.shape.CornerFamily
 import com.loitpcore.annotation.IsAutoAnimation
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
+import com.loitpcore.core.ext.setSafeOnClickListener
 import com.loitpcore.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.activity_card_view.*
+import kotlinx.android.synthetic.main.layout_bottom_sheet_sample.*
 import vn.loitp.app.R
+
 
 @LogTag("CardViewActivity")
 @IsFullScreen(false)
@@ -16,7 +20,7 @@ import vn.loitp.app.R
 class CardViewActivity : BaseFontActivity() {
 
     override fun setLayoutResourceId(): Int {
-        return R.layout.activity_card_view
+        return vn.loitp.app.R.layout.activity_card_view
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +39,21 @@ class CardViewActivity : BaseFontActivity() {
             )
             this.ivIconRight?.isVisible = false
             this.tvTitle?.text = CardViewActivity::class.java.simpleName
+        }
+
+        btChange.setSafeOnClickListener {
+            val radiusTL = resources.getDimension(R.dimen.round_small)
+            val radiusTR = resources.getDimension(R.dimen.round_medium)
+            val radiusBL = resources.getDimension(R.dimen.round_large)
+            val radiusBR = resources.getDimension(R.dimen.round_largest)
+
+            mcvTest.shapeAppearanceModel = mcvTest.shapeAppearanceModel
+                .toBuilder()
+                .setTopLeftCorner(CornerFamily.ROUNDED, radiusTL)
+                .setTopRightCorner(CornerFamily.ROUNDED, radiusTR)
+                .setBottomRightCorner(CornerFamily.ROUNDED, radiusBL)
+                .setBottomLeftCorner(CornerFamily.ROUNDED, radiusBR)
+                .build()
         }
     }
 }
