0b63e44ff7c292c6e5f817591319aaa15357a872
loitp <loitp@galaxy.one>
2022-12-20 10:52:53 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/api/MenuAPIActivity.kt
+++ app/src/main/java/vn/loitp/app/activity/api/MenuAPIActivity.kt
@@ -1,13 +1,11 @@
 package vn.loitp.app.activity.api
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.loitpcore.annotation.IsAutoAnimation
 import com.loitpcore.annotation.IsFullScreen
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseFontActivity
-import com.loitpcore.core.utilities.LActivityUtil
 import com.loitpcore.core.utilities.LUIUtil
 import com.loitpcore.restApi.restClient.RestClient
 import kotlinx.android.synthetic.main.activity_menu_api.*
@@ -34,12 +32,9 @@ class MenuAPIActivity : BaseFontActivity(), View.OnClickListener {
 
     private fun setupViews() {
         lActionBar.apply {
-            LUIUtil.setSafeOnClickListenerElastic(
-                view = this.ivIconLeft,
-                runnable = {
-                    onBaseBackPressed()
-                }
-            )
+            LUIUtil.setSafeOnClickListenerElastic(view = this.ivIconLeft, runnable = {
+                onBaseBackPressed()
+            })
             this.ivIconRight?.setImageResource(R.color.transparent)
             this.tvTitle?.text = MenuAPIActivity::class.java.simpleName
         }
@@ -50,25 +45,14 @@ class MenuAPIActivity : BaseFontActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        var intent: Intent? = null
         when (v) {
-            btCoroutineAPI -> {
-                intent = Intent(this, CoroutineAPIActivity::class.java)
-            }
+            btCoroutineAPI -> launchActivity(CoroutineAPIActivity::class.java)
             btGalleryAPI -> {
-                RestClient.init(getString(R.string.flickr_URL))
-                intent = Intent(this, GalleryAPIActivity::class.java)
+                RestClient.init(baseApiUrl = getString(R.string.flickr_URL))
+                launchActivity(GalleryAPIActivity::class.java)
             }
-            btComicAPI -> {
-                intent = Intent(this, MenuTTTAPIActivity::class.java)
-            }
-            btTestRetrofit2 -> {
-                intent = Intent(this, TestAPIRetrofit2Activity::class.java)
-            }
-        }
-        intent?.let { i ->
-            startActivity(i)
-            LActivityUtil.tranIn(this)
+            btComicAPI -> launchActivity(MenuTTTAPIActivity::class.java)
+            btTestRetrofit2 -> launchActivity(TestAPIRetrofit2Activity::class.java)
         }
     }
 }
