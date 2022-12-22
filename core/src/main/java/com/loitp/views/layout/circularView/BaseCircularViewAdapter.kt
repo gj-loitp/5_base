005352957f266780970ea42409815a676b3c7b15
loitp <loitp@galaxy.one>
2022-12-22 13:16:41 +0700

    update

--- core/src/main/java/com/loitp/views/layout/circularView/BaseCircularViewAdapter.kt
+++ core/src/main/java/com/loitp/views/layout/circularView/BaseCircularViewAdapter.kt
@@ -1,6 +1,6 @@
-package com.loitp.views.layout.circularView;
+package com.loitp.views.layout.circularView
 
-import android.database.DataSetObserver;
+import android.database.DataSetObserver
 
 /**
  * Created by Loitp on 04,August,2022
@@ -9,46 +9,46 @@ import android.database.DataSetObserver;
  * +840766040293
  * freuss47@gmail.com
  */
-public interface BaseCircularViewAdapter {
+interface BaseCircularViewAdapter {
     /**
      * Get the count of how many markers will show around the circular view. This number should be zero or positive.
      *
      * @return Number of markers to show.
      */
-    int getCount();
+    val count: Int
 
     /**
-     * Setup the marker that should show at a given position. The position will be between 0 and the value returned by {@link #getCount()}.
+     * Setup the marker that should show at a given position. The position will be between 0 and the value returned by [.getCount].
      *
      * @param position Position of the marker to show.
      * @param marker   The marker that will be used to display.
      */
-    void setupMarker(int position, Marker marker);
+    fun setupMarker(position: Int, marker: Marker?)
 
     /**
      * Register an observer on this adapter.
      *
      * @param observer observer to register.
      */
-    void registerDataSetObserver(DataSetObserver observer);
+    fun registerDataSetObserver(observer: DataSetObserver?)
 
     /**
      * Unregister an observer on this adapter.
      *
      * @param observer observer to unregister.
      */
-    void unregisterDataSetObserver(DataSetObserver observer);
+    fun unregisterDataSetObserver(observer: DataSetObserver?)
 
     /**
      * Notifies the attached observers that the underlying data has been changed
      * and any View reflecting the data set should refresh itself.
      */
-    void notifyDataSetChanged();
+    fun notifyDataSetChanged()
 
     /**
      * Notifies the attached observers that the underlying data is no longer valid
      * or available. Once invoked this adapter is no longer valid and should
      * not report further data set changes.
      */
-    void notifyDataSetInvalidated();
+    fun notifyDataSetInvalidated()
 }
