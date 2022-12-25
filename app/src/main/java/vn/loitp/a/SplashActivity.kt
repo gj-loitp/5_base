14455e0b08e3a9ace905be80e4b08d5b9b54215f
loitp <loitp@galaxy.one>
2022-12-25 13:16:47 +0700

    update

--- app/src/main/java/vn/loitp/a/SplashActivity.kt
+++ app/src/main/java/vn/loitp/a/SplashActivity.kt
@@ -91,6 +91,7 @@ class SplashActivity : BaseFontActivity() {
             PermissionX.init(this)
                 .permissions(
                     Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.CAMERA,
 //                    Manifest.permission.READ_EXTERNAL_STORAGE,
 //                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 )
