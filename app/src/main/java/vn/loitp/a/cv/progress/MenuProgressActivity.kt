a9e9896ac5adeea4064828bb25df0c588b245551
loitp <loitp@galaxy.one>
2023-01-03 10:44:37 +0700

    update

--- app/src/main/java/vn/loitp/a/cv/progress/MenuProgressActivity.kt
+++ app/src/main/java/vn/loitp/a/cv/progress/MenuProgressActivity.kt
@@ -1,13 +1,11 @@
 package vn.loitp.a.cv.progress
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.core.view.isVisible
 import com.loitp.annotation.IsFullScreen
 import com.loitp.annotation.LogTag
 import com.loitp.core.base.BaseFontActivity
-import com.loitp.core.utilities.LActivityUtil
+import com.loitp.core.ext.setSafeOnClickListener
 import com.loitp.core.utilities.LUIUtil
 import kotlinx.android.synthetic.main.a_pb_menu.*
 import vn.loitp.R
@@ -15,11 +13,12 @@ import vn.loitp.a.cv.progress.circularPb.CircularProgressBarActivity
 import vn.loitp.a.cv.progress.circularProgressIndicator.CircularProgressIndicatorActivity
 import vn.loitp.a.cv.progress.comparingPerformanceBar.ComparingPerformanceBarActivity
 import vn.loitp.a.cv.progress.segmentedPb.StandardExampleActivity
+import vn.loitp.a.cv.progress.tileProgressView.TileProgressViewActivity
 import vn.loitp.a.cv.progress.window.WindowProgressActivity
 
 @LogTag("MenuProgressActivity")
 @IsFullScreen(false)
-class MenuProgressActivity : BaseFontActivity(), View.OnClickListener {
+class MenuProgressActivity : BaseFontActivity() {
 
     override fun setLayoutResourceId(): Int {
         return R.layout.a_pb_menu
@@ -42,28 +41,23 @@ class MenuProgressActivity : BaseFontActivity(), View.OnClickListener {
             this.ivIconRight?.isVisible = false
             this.tvTitle?.text = MenuProgressActivity::class.java.simpleName
         }
-        btCircularProgressBar.setOnClickListener(this)
-        btCircularProgressIndicator.setOnClickListener(this)
-        btWindow.setOnClickListener(this)
-        btSegmentedProgressBar.setOnClickListener(this)
-        btComparingPerformanceBar.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        var intent: Intent? = null
-        when (v) {
-            btCircularProgressBar -> intent = Intent(this, CircularProgressBarActivity::class.java)
-            btCircularProgressIndicator ->
-                intent =
-                    Intent(this, CircularProgressIndicatorActivity::class.java)
-            btWindow -> intent = Intent(this, WindowProgressActivity::class.java)
-            btSegmentedProgressBar -> intent = Intent(this, StandardExampleActivity::class.java)
-            btComparingPerformanceBar -> intent =
-                Intent(this, ComparingPerformanceBarActivity::class.java)
+        btCircularProgressBar.setSafeOnClickListener {
+            launchActivity(CircularProgressBarActivity::class.java)
         }
-        intent?.let {
-            startActivity(it)
-            LActivityUtil.tranIn(this)
+        btCircularProgressIndicator.setSafeOnClickListener {
+            launchActivity(CircularProgressIndicatorActivity::class.java)
+        }
+        btWindow.setSafeOnClickListener {
+            launchActivity(WindowProgressActivity::class.java)
+        }
+        btSegmentedProgressBar.setSafeOnClickListener {
+            launchActivity(StandardExampleActivity::class.java)
+        }
+        btComparingPerformanceBar.setSafeOnClickListener {
+            launchActivity(ComparingPerformanceBarActivity::class.java)
+        }
+        btTileProgressView.setSafeOnClickListener {
+            launchActivity(TileProgressViewActivity::class.java)
         }
     }
 }
