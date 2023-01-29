4956b7dd7df43073236d114afdbf213a87bbc65b
loitp <loitp@galaxy.one>
2023-01-29 14:45:05 +0700

    up

--- app/src/main/java/vn/loitp/app/EmptyActivity.kt
+++ app/src/main/java/vn/loitp/app/EmptyActivity.kt
@@ -7,44 +7,46 @@ import com.loitp.annotation.IsAutoAnimation
 import com.loitp.annotation.IsFullScreen
 import com.loitp.annotation.LogTag
 import com.loitp.core.base.BaseActivityFont
+import com.loitp.core.common.NOT_FOUND
 import com.loitp.core.ext.openUrlInBrowser
 import com.loitp.core.ext.setSafeOnClickListener
 import com.loitp.core.ext.setSafeOnClickListenerElastic
-import kotlinx.android.synthetic.main.a_0.*
 import vn.loitp.R
 import vn.loitp.a.func.activityAndService.ActivityServiceCommunicateActivityFont
+import vn.loitp.databinding.A0Binding
 
 @LogTag("EmptyActivity")
 @IsFullScreen(false)
 @IsAutoAnimation(false)
 class EmptyActivity : BaseActivityFont() {
 
+    private lateinit var binding: A0Binding
+
     override fun setLayoutResourceId(): Int {
-        return R.layout.a_0
+        return NOT_FOUND
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = A0Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setupViews()
         testInputData()
     }
 
     private fun setupViews() {
-        lActionBar.apply {
-            this.ivIconLeft.setSafeOnClickListenerElastic(
-                runnable = {
-                    onBaseBackPressed()
-                }
-            )
+        binding.lActionBar.apply {
+            this.ivIconLeft.setSafeOnClickListenerElastic(runnable = {
+                onBaseBackPressed()
+            })
             this.ivIconRight?.apply {
-                this.setSafeOnClickListenerElastic(
-                    runnable = {
-                        context.openUrlInBrowser(
-                            url = "https://github.com/tplloi/base"
-                        )
-                    }
-                )
+                this.setSafeOnClickListenerElastic(runnable = {
+                    context.openUrlInBrowser(
+                        url = "https://github.com/tplloi/base"
+                    )
+                })
                 isVisible = true
                 setImageResource(R.drawable.ic_baseline_code_48)
             }
@@ -58,8 +60,8 @@ class EmptyActivity : BaseActivityFont() {
         if (input.isNullOrEmpty()) {
             //do nothing
         } else {
-            tv.text = "Click me to back screen $input"
-            tv.setSafeOnClickListener {
+            binding.tv.text = "Click me to back screen $input"
+            binding.tv.setSafeOnClickListener {
                 setResultActivity { data ->
                     data.putExtra(
                         /* name = */ ActivityServiceCommunicateActivityFont.KEY_OUTPUT,
