3949e43e160ddd9bc2da74a7026b2dc6834bed71
loitp <loitp@galaxy.one>
2023-02-06 10:39:36 +0700

    up

--- app/src/main/java/vn/loitp/up/a/cv/MenuCustomViewsActivity.kt
+++ app/src/main/java/vn/loitp/up/a/cv/MenuCustomViewsActivity.kt
@@ -49,8 +49,8 @@ import vn.loitp.a.cv.sw.MenuSwitchToggleActivity
 import vn.loitp.a.cv.treeView.TreeViewActivity
 import vn.loitp.a.cv.tv.MenuTextViewActivity
 import vn.loitp.a.cv.video.MenuVideoViewActivity
-import vn.loitp.a.cv.vp.MenuViewPagerActivity
 import vn.loitp.databinding.AMenuCustomViewBinding
+import vn.loitp.up.a.cv.vp.MenuViewPagerActivity
 import vn.loitp.up.a.cv.wheelSpiner.WheelSpinnerActivity
 import vn.loitp.up.a.cv.wheelView.WheelViewActivity
 import vn.loitp.up.a.cv.wv.MenuWebViewActivity
---
+++ app/src/main/java/vn/loitp/up/a/cv/vp/MenuViewPagerActivity.kt
@@ -0,0 +1,80 @@
+package vn.loitp.up.a.cv.vp
+
+import android.os.Bundle
+import android.view.View
+import com.loitp.annotation.IsAutoAnimation
+import com.loitp.annotation.IsFullScreen
+import com.loitp.annotation.LogTag
+import com.loitp.core.base.BaseActivityFont
+import com.loitp.core.common.NOT_FOUND
+import com.loitp.core.ext.setSafeOnClickListenerElastic
+import vn.loitp.R
+import vn.loitp.a.cv.vp.auto.ViewPagerAutoActivity
+import vn.loitp.a.cv.vp.detectSwipeOut.DetectViewPagerSwipeOutActivity
+import vn.loitp.a.cv.vp.detectSwipeOut2.ViewPagerSwipeOut2Activity
+import vn.loitp.a.cv.vp.easyFlip.EFVPActivity
+import vn.loitp.a.cv.vp.lockable.LockableViewPagerActivity
+import vn.loitp.a.cv.vp.parrallax.ParallaxViewPagerActivity
+import vn.loitp.a.cv.vp.refresh.RefreshViewPagerActivity
+import vn.loitp.a.cv.vp.vertical.ViewPagerVerticalActivity
+import vn.loitp.a.cv.vp.vp2.ViewPager2Activity
+import vn.loitp.a.cv.vp.vpWithTabLayout.ViewPagerWithTabLayoutActivity
+import vn.loitp.databinding.AMenuVpBinding
+
+@LogTag("MenuViewPagerActivity")
+@IsFullScreen(false)
+@IsAutoAnimation(true)
+class MenuViewPagerActivity : BaseActivityFont(), View.OnClickListener {
+
+    private lateinit var binding: AMenuVpBinding
+
+    override fun setLayoutResourceId(): Int {
+        return NOT_FOUND
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = AMenuVpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.lActionBar.apply {
+            this.ivIconLeft.setSafeOnClickListenerElastic(
+                runnable = {
+                    onBaseBackPressed()
+                }
+            )
+            this.ivIconRight?.setImageResource(R.color.transparent)
+            this.tvTitle?.text = MenuViewPagerActivity::class.java.simpleName
+        }
+        binding.btAutoViewPager.setOnClickListener(this)
+        binding.btParallaxViewPager.setOnClickListener(this)
+        binding.btDetectViewPagerSwipeOut.setOnClickListener(this)
+        binding.btViewPagerTabLayout.setOnClickListener(this)
+        binding.btDetectViewPagerSwipeOut2.setOnClickListener(this)
+        binding.btLockableViewPager.setOnClickListener(this)
+        binding.btVerticalViewPager.setOnClickListener(this)
+        binding.btViewPager2.setOnClickListener(this)
+        binding.btRefreshViewPager.setOnClickListener(this)
+        binding.btEasyFlipViewPager.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            binding.btAutoViewPager -> launchActivity(ViewPagerAutoActivity::class.java)
+            binding.btParallaxViewPager -> launchActivity(ParallaxViewPagerActivity::class.java)
+            binding.btDetectViewPagerSwipeOut -> launchActivity(DetectViewPagerSwipeOutActivity::class.java)
+            binding.btViewPagerTabLayout -> launchActivity(ViewPagerWithTabLayoutActivity::class.java)
+            binding.btDetectViewPagerSwipeOut2 -> launchActivity(ViewPagerSwipeOut2Activity::class.java)
+            binding.btLockableViewPager -> launchActivity(LockableViewPagerActivity::class.java)
+            binding.btVerticalViewPager -> launchActivity(ViewPagerVerticalActivity::class.java)
+            binding.btViewPager2 -> launchActivity(ViewPager2Activity::class.java)
+            binding.btRefreshViewPager -> launchActivity(RefreshViewPagerActivity::class.java)
+            binding.btEasyFlipViewPager -> launchActivity(EFVPActivity::class.java)
+        }
+    }
+}
