bd4e863583f7313d1e67155d440a13eaf843688f
loitp <loitp@galaxy.one>
2023-02-06 10:55:44 +0700

    up

--- app/src/main/java/vn/loitp/up/a/cv/vp/lockable/LockableViewPagerActivity.kt
+++ app/src/main/java/vn/loitp/up/a/cv/vp/lockable/LockableViewPagerActivity.kt
@@ -1,4 +1,4 @@
-package vn.loitp.a.cv.vp.lockable
+package vn.loitp.up.a.cv.vp.lockable
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,29 +8,34 @@ import com.loitp.annotation.IsFullScreen
 import com.loitp.annotation.LogTag
 import com.loitp.core.base.BaseActivityFont
 import com.loitp.core.common.FONT_PATH
+import com.loitp.core.common.NOT_FOUND
 import com.loitp.core.ext.changeTabsFont
 import com.loitp.core.ext.setSafeOnClickListener
 import com.loitp.core.ext.setSafeOnClickListenerElastic
-import kotlinx.android.synthetic.main.a_vp_lockable.*
 import vn.loitp.R
 import vn.loitp.a.cv.vp.auto.FrmIv
+import vn.loitp.databinding.AVpLockableBinding
 
 @LogTag("LockableViewPagerActivity")
 @IsFullScreen(false)
 class LockableViewPagerActivity : BaseActivityFont() {
+    private lateinit var binding: AVpLockableBinding
 
     override fun setLayoutResourceId(): Int {
-        return R.layout.a_vp_lockable
+        return NOT_FOUND
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = AVpLockableBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setupViews()
     }
 
     private fun setupViews() {
-        lActionBar.apply {
+        binding.lActionBar.apply {
             this.ivIconLeft.setSafeOnClickListenerElastic(
                 runnable = {
                     onBaseBackPressed()
@@ -39,14 +44,14 @@ class LockableViewPagerActivity : BaseActivityFont() {
             this.ivIconRight?.setImageResource(R.color.transparent)
             this.tvTitle?.text = LockableViewPagerActivity::class.java.simpleName
         }
-        vp.adapter = SamplePagerAdapter(supportFragmentManager)
-        tabLayout.setupWithViewPager(vp)
-        tabLayout.changeTabsFont(fontName = FONT_PATH)
-        btEnable.setSafeOnClickListener {
-            vp.swipeLocked = false
+        binding.vp.adapter = SamplePagerAdapter(supportFragmentManager)
+        binding.tabLayout.setupWithViewPager(binding.vp)
+        binding.tabLayout.changeTabsFont(fontName = FONT_PATH)
+        binding.btEnable.setSafeOnClickListener {
+            binding.vp.swipeLocked = false
         }
-        btDisable.setSafeOnClickListener {
-            vp.swipeLocked = true
+        binding.btDisable.setSafeOnClickListener {
+            binding.vp.swipeLocked = true
         }
     }
 
