f8b52c18da5a6732b3bd98cc31edb31f4a7d0531
loitp <www.muathu@gmail.com>
2023-03-16 23:34:38 +0700

    up

---
+++ core/src/main/java/com/loitp/core/ext/Admob.kt
@@ -0,0 +1,8 @@
+package com.loitp.core.ext
+
+//https://developers.google.com/admob/android/test-ads
+//search setTestDeviceIds in logcat to get test device id
+
+object Admob {
+    const val TEST_DEVICE_LG_V60 = "969A00745257B14B6AF8C9BD1D6E02EC"
+}
