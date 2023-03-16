c85f00d51440f3f92ce5f176de904ceea3a97f91
loitp <www.muathu@gmail.com>
2023-03-16 23:41:45 +0700

    up

--- core/src/main/java/com/loitp/core/ext/Admob.kt
+++ core/src/main/java/com/loitp/core/ext/Admob.kt
@@ -5,4 +5,5 @@ package com.loitp.core.ext
 
 object Admob {
     const val TEST_DEVICE_LG_V60 = "969A00745257B14B6AF8C9BD1D6E02EC"
+    const val TEST_DEVICE_SAMSUNG_A50 = "D2CA64E6296C2FA32565EF6A4DC09533"
 }
