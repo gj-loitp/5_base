c20aa0cabaddefc3486a76c52f5ebb8001fcf81d
loitp <www.muathu@gmail.com>
2023-03-17 12:52:07 +0700

    up

--- app/src/main/java/vn/loitp/up/a/demo/ad/banner/BannerActivity.kt
+++ app/src/main/java/vn/loitp/up/a/demo/ad/banner/BannerActivity.kt
@@ -1,6 +1,10 @@
 package vn.loitp.up.a.demo.ad.banner
 
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.loitp.annotation.IsAutoAnimation
 import com.loitp.annotation.IsFullScreen
 import com.loitp.annotation.LogTag
@@ -41,6 +45,44 @@ class BannerActivity : BaseActivityFont() {
             this.tvTitle?.text = BannerActivity::class.java.simpleName
         }
 
+        setupAdmob()
     }
 
+    private fun setupAdmob() {
+        // Log the Mobile Ads SDK version.
+        logE("Google Mobile Ads SDK Version: " + MobileAds.getVersion())
+        // Initialize the Mobile Ads SDK with an AdMob App ID.
+        MobileAds.initialize(this) {}
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder().setTestDeviceIds(
+                listOf(
+                    getString(R.string.admob_test_device_id_lg_v60),
+                    getString(R.string.admob_test_device_id_samsung_a50),
+                )
+            ).build()
+        )
+        // Create an ad request.
+        val adRequest = AdRequest.Builder().build()
+        // Start loading the ad in the background.
+        binding.adView.loadAd(adRequest)
+    }
+
+    public override fun onPause() {
+        binding.adView.pause()
+        super.onPause()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        binding.adView.resume()
+    }
+
+    public override fun onDestroy() {
+        binding.adView.destroy()
+        super.onDestroy()
+    }
 }
--- core/src/main/java/com/loitp/core/ext/Admob.kt
+++
@@ -1,9 +0,0 @@
-package com.loitp.core.ext
-
-//https://developers.google.com/admob/android/test-ads
-//search setTestDeviceIds in logcat to get test device id
-
-object Admob {
-    const val TEST_DEVICE_LG_V60 = "969A00745257B14B6AF8C9BD1D6E02EC"
-    const val TEST_DEVICE_SAMSUNG_A50 = "D2CA64E6296C2FA32565EF6A4DC09533"
-}
