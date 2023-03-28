33de0292ac3cce4891687a8a462d7228a3787bb2
loitp <www.muathu@gmail.com>
2023-03-28 10:40:39 +0700

    up

--- app/src/main/java/vn/loitp/up/a/demo/ad/interstitial/InterstitialActivity.kt
+++ app/src/main/java/vn/loitp/up/a/demo/ad/interstitial/InterstitialActivity.kt
@@ -59,6 +59,7 @@ class InterstitialActivity : BaseActivityFont() {
         interstitialAd = MaxInterstitialAd(getString(R.string.INTER), this)
         interstitialAd?.setListener(object : MaxAdListener {
             override fun onAdLoaded(p0: MaxAd?) {
+                logI("onAdLoaded")
                 retryAttempt = 0
             }
 
@@ -67,6 +68,7 @@ class InterstitialActivity : BaseActivityFont() {
             }
 
             override fun onAdHidden(p0: MaxAd?) {
+                logI("onAdHidden")
                 // Interstitial Ad is hidden. Pre-load the next ad
                 interstitialAd?.loadAd()
             }
@@ -76,6 +78,7 @@ class InterstitialActivity : BaseActivityFont() {
             }
 
             override fun onAdLoadFailed(p0: String?, p1: MaxError?) {
+                logI("onAdLoadFailed")
                 retryAttempt++
                 val delayMillis = TimeUnit.SECONDS.toMillis(2.0.pow(min(6, retryAttempt)).toLong())
 
@@ -88,6 +91,7 @@ class InterstitialActivity : BaseActivityFont() {
             }
 
             override fun onAdDisplayFailed(p0: MaxAd?, p1: MaxError?) {
+                logI("onAdDisplayFailed")
                 // Interstitial ad failed to display. We recommend loading the next ad.
                 interstitialAd?.loadAd()
             }
@@ -101,7 +105,7 @@ class InterstitialActivity : BaseActivityFont() {
         interstitialAd?.loadAd()
     }
 
-    fun showAd() {
+    private fun showAd() {
         if (interstitialAd?.isReady == true) {
             interstitialAd?.showAd()
         }
