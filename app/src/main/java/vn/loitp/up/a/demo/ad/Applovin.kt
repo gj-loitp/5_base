2630e241691e2ef4e359a61535456483dc52642d
phuloi.tran <phuloi.tran@samsung.com>
2023-07-05 10:26:02 +0700

    update

--- app/src/main/java/vn/loitp/up/a/demo/ad/Applovin.kt
+++ app/src/main/java/vn/loitp/up/a/demo/ad/Applovin.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.core.view.isVisible
 import com.applovin.mediation.MaxAd
 import com.applovin.mediation.MaxAdFormat
 import com.applovin.mediation.MaxAdViewAdListener
@@ -66,6 +67,7 @@ object Applovin {
             ad.setListener(object : MaxAdViewAdListener {
                 override fun onAdLoaded(p0: MaxAd?) {
                     i(log, "onAdLoaded")
+                    viewGroup.isVisible = true
                 }
 
                 override fun onAdDisplayed(p0: MaxAd?) {
@@ -82,6 +84,7 @@ object Applovin {
 
                 override fun onAdLoadFailed(p0: String?, p1: MaxError?) {
                     i(log, "onAdLoadFailed")
+                    viewGroup.isVisible = false
                 }
 
                 override fun onAdDisplayFailed(p0: MaxAd?, p1: MaxError?) {
